c7c000a42d3218313af668677fbedaffbb51e785
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-02-16 18:59:27 +0000

    corrected

--- www/offices/index.tcl
+++ www/offices/index.tcl
@@ -1,78 +1,403 @@
-# /www/intranet/offices/index.tcl
+# /packages/intranet-core/www/intranet/offices/index.tcl
+#
+# Copyright (C) 1998-2004 various parties
+# The code is based on ArsDigita ACS 3.4
+#
+# This program is free software. You can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option)
+# any later version. This program is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# ---------------------------------------------------------------
+# 1. Page Contract
+# ---------------------------------------------------------------
 
 ad_page_contract {
+    Shows all offices. Lots of dimensional sliders
 
-    Lists all offices
-    Last Change:
-    Added number of employees in parentheses after the office names
-    May 3, 2000
-    @param none
-    @author Mark Dettinger <dettinger@arsdigita.com>
-    @creation-date 
+    @param status_id if specified, limits view to those of this status
+    @param type_id   if specified, limits view to those of this type
+    @param order_by  Specifies order for the table
 
-    @cvs-id index.tcl,v 3.17.2.7 2000/09/22 01:38:39 kevin Exp
-} {}
+    @author mbryzek@arsdigita.com
+    @author Frank Bergmann (frank.bergmann@project-open.com)
+} {
+    { status_id:integer 0 }
+    { type_id:integer 0 }
+    { start_idx:integer 1 }
+    { order_by "Office" }
+    { how_many "" }
+    { letter:trim "all" }
+    { view_name "office_list" }
+}
+
+# ---------------------------------------------------------------
+# Office List Page
+#
+# This is a "classical" List-Page. It consists of the sections:
+#    1. Page Contract: 
+#	Receive the filter values defined as parameters to this page.
+#    2. Defaults & Security:
+#	Initialize variables, set default values for filters 
+#	(categories) and limit filter values for unprivileged users
+#    3. Define Table Columns:
+#	Define the table columns that the user can see.
+#	Again, restrictions may apply for unprivileged users,
+#	for example hiding office names to freelancers.
+#    4. Define Filter Categories:
+#	Extract from the database the filter categories that
+#	are available for a specific user.
+#	For example "potential", "invoiced" and "partially paid" 
+#	projects are not available for unprivileged users.
+#    5. Generate SQL Query
+#	Compose the SQL query based on filter criteria.
+#	All possible columns are selected from the DB, leaving
+#	the selection of the visible columns to the table columns,
+#	defined in section 3.
+#    6. Format Filter
+#    7. Format the List Table Header
+#    8. Format Result Data
+#    9. Format Table Continuation
+#   10. Join Everything Together
+
+# ---------------------------------------------------------------
+# 2. Defaults & Security
+# ---------------------------------------------------------------
 
 set user_id [ad_maybe_redirect_for_registration]
-
-set sql_query \
-	"select ug.group_id, ug.group_name, nvl(o.public_p,'f') as public_p, 
-                nvl(number_users,0) as office_size
-           from user_groups ug, im_offices o, 
-             --- create a view that has at least one row per office   
-             (select count(*) as number_users, o.group_id
-                from im_offices o, im_employees_active emp
-               where ad_group_member_p(emp.user_id, o.group_id) = 't'
-               group by o.group_id) m
-          where ug.parent_group_id=[im_office_group_id]
-            and ug.group_id=o.group_id(+)
-            and ug.group_id=m.group_id(+)
-               and o.public_p='t'
-          group by ug.group_id, ug.group_name, public_p, number_users
-          order by public_p desc, lower(group_name), group_id"
-
-# jruiz 20020604: Added public_p filter, show only public offices
-
-
-set results ""
-set last_public_p ""
-set number_public_p 0
-
-db_foreach intranet_offices_get_offices $sql_query {
-    if { [string compare $last_public_p $public_p] != 0 } {
-	if { ![empty_string_p $last_public_p] } {
-	    append results "</ul>\n"
-	}
-	append results "<p><b>Offices whose information is [util_decode $public_p "t" "public" "not public"]</b>\n<ul>\n"
-	set last_public_p $public_p
-    }
-    if { [string compare $public_p "t"] == 0 } {
-	incr number_public_p
-    }
-    append results "  <li> <a href=view?[export_url_vars group_id]>$group_name</a> ($office_size)\n"
-} if_no_rows {
-    set results "  <p><ul><b> There are no offices </b>\n" 
-}
-
-append results "</ul>\n"
-
-db_release_unused_handles
-
+set current_user_id $user_id
 set page_title "Offices"
 set context_bar [ad_context_bar $page_title]
+set page_focus "im_header_form.keywords"
+set return_url "/intranet/offices/index"
 
-set page_body "
-$results
-<ul>
-<li><a href=ae>Add an office</a>
-"
+set customer_view_page "/intranet/customers/view"
+set user_view_page "/intranet/users/view"
+set office_view_page "/intranet/offices/view"
+set letter [string toupper $letter]
 
-if { $number_public_p > 0 } {
-    append page_body "<li><a href=public>View public office information</a>\n"
+if {![im_permission $user_id view_offices]} {
+    ad_return_complaint 1 "You don't have permissions to view offices"
+    return
 }
 
-append page_body "</ul>\n"
-
-doc_return  200 text/html [im_return_template]
+set end_idx [expr $start_idx + $how_many - 1]
 
 
+# ---------------------------------------------------------------
+# 3. Define Table Columns
+# ---------------------------------------------------------------
+
+# Define the column headers and column contents that 
+# we want to show:
+#
+set view_id [db_string get_view_id "select view_id from im_views where view_name=:view_name"]
+set column_headers [list]
+set column_vars [list]
+
+set column_sql "
+select
+	column_name,
+	column_render_tcl,
+	visible_for
+from
+	im_view_columns
+where
+	view_id=:view_id
+	and group_id is null
+order by
+	sort_order"
+
+db_foreach column_list_sql $column_sql {
+    if {[eval $visible_for]} {
+	lappend column_headers "$column_name"
+	lappend column_vars "$column_render_tcl"
+    }
+}
+
+# ---------------------------------------------------------------
+# 4. Define Filters
+# ---------------------------------------------------------------
+
+# status_types will be a list of pairs of (project_status_id, project_status)
+set status_types [im_memoize_list select_office_status_types \
+	"select office_status_id, office_status
+           from im_office_status
+          order by lower(office_status)"]
+set status_types [linsert $status_types 0 0 All]
+
+
+# office_types will be a list of pairs of (office_type_id, office_type)
+set office_types [im_memoize_list select_offices_types \
+	"select office_type_id, office_type
+           from im_office_types
+          order by lower(office_type)"]
+set office_types [linsert $office_types 0 0 All]
+
+# ---------------------------------------------------------------
+# 5. Generate SQL Query
+# ---------------------------------------------------------------
+
+# Now let's generate the sql query
+set criteria [list]
+
+set bind_vars [ns_set create]
+if { ![empty_string_p $status_id] && $status_id != 0 } {
+    ns_set put $bind_vars status_id $status_id
+    lappend criteria "c.office_status_id=:status_id"
+}
+
+if { $type_id > 0 } {
+    ns_set put $bind_vars type_id $type_id
+    lappend criteria "c.office_type_id=:type_id"
+}
+
+if { ![empty_string_p $letter] && [string compare $letter "ALL"] != 0 && [string compare $letter "SCROLL"] != 0 } {
+    lappend criteria "im_first_letter_default_to_a(c.office_name)=:letter"
+}
+
+set order_by_clause ""
+switch $order_by {
+    "Phone" { set order_by_clause "order by upper(phone_work), upper(office_name)" }
+    "Email" { set order_by_clause "order by upper(email), upper(office_name)" }
+    "Type" { set order_by_clause "order by upper(office_type), upper(office_name)" }
+    "Status" { set order_by_clause "order by upper(office_status), upper(office_name)" }
+    "Contact Person" { set order_by_clause "order by upper(last_name), upper(first_names), upper(office_name)" }
+    "Office" { set order_by_clause "order by upper(office_name)" }
+}
+
+set where_clause [join $criteria " and\n            "]
+if { ![empty_string_p $where_clause] } {
+    set where_clause " and $where_clause"
+}
+
+set sql "
+select
+	o.*,
+	im_name_from_user_id(o.contact_person_id) as contact_person_name,
+	im_email_from_user_id(o.contact_person_id) as contact_person_email,
+        im_category_from_id(o.office_type_id) as office_type,
+        im_category_from_id(o.office_status_id) as office_status,
+	c.customer_id,
+	c.customer_name
+from 
+	im_offices o,
+	im_customers c
+where
+	o.office_id=c.main_office_id(+)
+	$where_clause"
+
+# ---------------------------------------------------------------
+# 5a. Limit the SQL query to MAX rows and provide << and >>
+# ---------------------------------------------------------------
+
+# Limit the search results to N data sets only
+# to be able to manage large sites
+#
+if {[string compare $letter "ALL"]} {
+
+    # Set these limits to negative values to deactivate them
+    set total_in_limited -1
+    set how_many -1
+    set selection "$sql $order_by_clause"
+
+} else {
+
+    set limited_query [im_select_row_range $sql $start_idx $end_idx]
+    # We can't get around counting in advance if we want to be able to 
+    # sort inside the table on the page for only those users in the 
+    # query results
+    set total_in_limited [db_string projects_total_in_limited "
+	select count(*) 
+        from
+		im_offices c
+        where 
+		1=1
+		$where_clause
+	"]
+    
+    set selection "$sql $order_by_clause"
+
+#    set selection "select z.* from ($limited_query) z $order_by_clause"
+}	
+
+ns_log Notice $selection
+
+# ---------------------------------------------------------------
+# 6. Format the Filter
+# ---------------------------------------------------------------
+
+set filter_html "
+<form method=get action='/intranet/offices/index' name=filter_form>
+[export_form_vars start_idx order_by how_many letter view_name]
+<table border=0 cellpadding=0 cellspacing=0>
+<tr> 
+  <td colspan='2' class=rowtitle align=center>
+    Filter Offices
+  </td>
+</tr>
+<tr>
+  <td valign=top>Office Status: </td>
+  <td valign=top>[im_select status_id $status_types ""]</td>
+</tr>
+<tr>
+  <td valign=top>Office Type: </td>
+  <td valign=top>
+    [im_select type_id $office_types ""]
+    <input type=submit value=Go name=submit>
+  </td>
+</tr>
+</table>
+</form>"
+
+
+# ----------------------------------------------------------
+# Do we have to show administration links?
+
+set admin_html ""
+if {[im_permission $current_user_id "add_offices"]} {
+    append admin_html "<li><a href=/intranet/offices/new>Add a new Office</a>\n"
+}
+
+set office_filter_html $filter_html
+
+if {"" != $admin_html} {
+    set office_filter_html "
+
+<table border=0 cellpadding=0 cellspacing=0>
+<tr>
+  <td> <!-- TD for the left hand filter HTML -->
+    $filter_html
+  </td> <!-- end of left hand filter TD -->
+  <td>&nbsp;</td>
+  <td valign=top>
+    <table border=0 cellpadding=0 cellspacing=0>
+    <tr>
+      <td class=rowtitle align=center>
+        Admin Offices
+      </td>
+    </tr>
+    <tr>
+      <td>
+        $admin_html
+      </td>
+    </tr>
+    </table>
+  </td>
+</tr>
+</table>
+"
+}
+
+
+# ---------------------------------------------------------------
+# 7. Format the List Table Header
+# ---------------------------------------------------------------
+
+# Set up colspan to be the number of headers + 1 for the # column
+set colspan [expr [llength $column_headers] + 1]
+
+# Format the header names with links that modify the
+# sort order of the SQL query.
+#
+set table_header_html ""
+set url "index?"
+set query_string [export_ns_set_vars url [list order_by]]
+if { ![empty_string_p $query_string] } {
+    append url "$query_string&"
+}
+
+append table_header_html "<tr>\n"
+foreach col $column_headers {
+    if { [string compare $order_by $col] == 0 } {
+	append table_header_html "  <td class=rowtitle>$col</td>\n"
+    } else {
+	append table_header_html "  <td class=rowtitle><a href=\"${url}order_by=[ns_urlencode $col]\">$col</a></td>\n"
+    }
+}
+append table_header_html "</tr>\n"
+
+# ---------------------------------------------------------------
+# 8. Format the Result Data
+# ---------------------------------------------------------------
+
+set table_body_html ""
+set bgcolor(0) " class=roweven "
+set bgcolor(1) " class=rowodd "
+set ctr 0
+set idx $start_idx
+db_foreach projects_info_query $selection {
+
+    # Append together a line of data based on the "column_vars" parameter list
+    append table_body_html "<tr$bgcolor([expr $ctr % 2])>\n"
+    foreach column_var $column_vars {
+	append table_body_html "\t<td valign=top>"
+	set cmd "append table_body_html $column_var"
+	eval $cmd
+	append table_body_html "</td>\n"
+    }
+    append table_body_html "</tr>\n"
+
+    incr ctr
+    if { $how_many > 0 && $ctr >= $how_many } {
+	break
+    }
+    incr idx
+}
+
+# Show a reasonable message when there are no result rows:
+if { [empty_string_p $table_body_html] } {
+    set table_body_html "
+        <tr><td colspan=$colspan><ul><li><b> 
+        There are currently no entries matching the selected criteria
+        </b></ul></td></tr>"
+}
+
+if { $ctr == $how_many && $end_idx < $total_in_limited } {
+    # This means that there are rows that we decided not to return
+    # Include a link to go to the next page
+    set next_start_idx [expr $end_idx + 1]
+    set next_page_url "index?start_idx=$next_start_idx&[export_ns_set_vars url [list start_idx]]"
+} else {
+    set next_page_url ""
+}
+
+if { $start_idx > 1 } {
+    # This means we didn't start with the first row - there is
+    # at least 1 previous row. add a previous page link
+    set previous_start_idx [expr $start_idx - $how_many]
+    if { $previous_start_idx < 1 } {
+	set previous_start_idx 1
+    }
+    set previous_page_url "index?start_idx=$previous_start_idx&[export_ns_set_vars url [list start_idx]]"
+} else {
+    set previous_page_url ""
+}
+
+
+# ---------------------------------------------------------------
+# 9. Format Table Continuation
+# ---------------------------------------------------------------
+
+# nothing to do here ... (?)
+set table_continuation_html ""
+
+# ---------------------------------------------------------------
+# Office-Navbar
+# ---------------------------------------------------------------
+
+set letter "none"
+set next_page_url ""
+set previous_page_url ""
+
+set office_navbar_html "
+<br>
+[im_office_navbar $letter "/intranet/offices/view" $next_page_url $previous_page_url [list start_idx order_by how_many view_name letter]]
+"
+
+db_release_unused_handles
