a5c9995185380c4864aecc4f4f81735184865d41
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-02-20 13:11:33 +0000

    added TCL files

---
+++ tcl/intranet-admin-group-procs.tcl
@@ -0,0 +1,254 @@
+# /packages/intranet-core/tcl/intranet-permissions-procs.tcl
+#
+# Copyright (C) 2004 Project/Open
+# The code is based on work from ArsDigita ACS 3.4
+#
+# This program is free software. You can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option)
+# any later version. This program is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+ad_library {
+    Project/Open specific permissions routines.
+    The P/O permission model is based on the OpenACS model,
+    extending it by several concepts:
+}
+
+# --------------------------------------------------------------
+# Show the members of the Admin Group of the current Business Object.
+# --------------------------------------------------------------
+#
+# set company_members [im_group_member_component $customer_id $user_id $user_admin_p $return_url [im_employee_group_id]]
+
+
+ad_proc -public im_group_member_component { group_id current_user_id { add_admin_links 0 } { return_url "" } { limit_to_users_in_group_id "" } { dont_allow_users_in_group_id "" } {also_add_to_group_id "" } } {
+
+    Returns an html formatted list of all the users in the specified
+    group. 
+
+    Required Arguments:
+    -------------------
+    - group_id: Group we're interested in.
+    - current_user_id: The user_id of the person viewing the page that
+      called this function. 
+
+    Optional Arguments:
+    -------------------
+    - description: A description of the group. We use pass this to the
+      spam function for UI
+    - add_admin_links: Boolean. If 1, we add links to add/email
+      people. Current user must be member of the specified group_id to add
+      him/herself
+    - return_url: Where to go after we do something like add a user
+    - limit_to_users_in_group_id: Only shows users who belong to
+      group_id and who are also members of the group specified in
+      limit_to_users_in_group_id. For example, if group_id is an intranet
+      project, and limit_to_users_group_id is the group_id of the employees
+      group, we only display users who are members of both the employees and
+      this project groups
+    - dont_allow_users_in_group_id: Similar to
+      limit_to_users_in_group_id, but says that if a user belongs to the
+      group_id specified by dont_allow_users_in_group_id, then don't display
+      that user.  
+    - also_add_to_group_id: If we're adding users to a group, we might
+      also want to add them to another group at the same time. If you set
+      also _add_to_group_id to a group_id, the user will be added first to
+      group_id, then to also_add_to_group_id. Note that adding the person to
+      both groups is NOT atomic.
+
+    Notes:
+    -------------------
+    This function has quickly become complicated. Any proposals to simplify
+    are welcome...
+
+} {
+
+# ------------------------------------------------------------------
+# Create the feature box for adding and removing employees
+# ------------------------------------------------------------------
+
+    # ------------------ limit_to_users_in_group_id ---------------------
+    if { [empty_string_p $limit_to_users_in_group_id] } {
+	set limit_to_group_id_sql ""
+    } else {
+	set limit_to_group_id_sql "
+and exists (select 1 
+	from 
+		group_member_map map2,
+		groups ug
+	where 
+		map2.group_id = ug.group_id
+		and map2.member_id = users.user_id 
+		and map2.group_id = :limit_to_users_in_group_id
+	)
+"
+    } 
+
+
+    # ------------------ dont_allow_users_in_group_id ---------------------
+    if { [empty_string_p $dont_allow_users_in_group_id] } {
+	set dont_allow_sql ""
+    } else {
+	set dont_allow_sql "
+and not exists (
+	select 1 
+	from 
+		group_member_map map2, 
+		groups ug
+	where 
+		map2.group_id = ug.group_id
+		and map2.member_id = users.user_id 
+		and map2.group_id = :dont_allow_users_in_group_id
+	)
+"
+    } 
+
+    # ------------------ Main SQL ----------------------------------------
+    # Old Comment: We need a "distinct" because there can be more than one
+    # Old Comment: mapping between a user and a group, one for each role.
+    # 
+    # fraber: Abolished the "distinct" because the role assignment page 
+    # now takes care that a user is assigned only once to a group.
+    # We neeed this if we want to show the role of the user.
+    #
+    set sql_query "
+select
+	users.user_id, 
+	im_email_from_user_id(users.user_id) as email,
+	im_name_from_user_id(users.user_id) as name,
+	map.rel_type as member_role
+from
+	users,
+	group_member_map map
+where
+	map.member_id = users.user_id
+	and map.group_id = :group_id
+	$limit_to_group_id_sql 
+	$dont_allow_sql
+order by lower(name)"
+
+
+    # ------------------ Format the table header ------------------------
+    set colspan 1
+    set header_html "
+      <tr> 
+	<td class=rowtitle align=middle>Name</td>"
+if {$add_admin_links} {
+    incr colspan
+    append header_html "
+	<td class=rowtitle align=middle>[im_gif delete]</td>"
+}
+    append header_html "
+      </tr>"
+
+    # ------------------ Format the table body ----------------
+    set td_class(0) "class=roweven"
+    set td_class(1) "class=rowodd"
+    set found 0
+    set count 0
+    set body_html ""
+    db_foreach users_in_group $sql_query {
+
+	# Return the first letter of the role (admin, member, ...)
+	# defaulting to a "-" if there was not role...
+	append $member_role "-"
+	set profile_letter [string toupper [string range $member_role 0 0]]
+
+	incr count
+	if { $current_user_id == $user_id } { set found 1 }
+
+	# determine how to show the user: 
+	# -1: Show name only, 0: don't show, 1:Show link
+	set show_user [im_show_user_style $user_id $current_user_id $group_id]
+	ns_log Notice "im_group_member_component: user_id=$user_id, show_user=$show_user"
+
+	if {$show_user == 0} { continue }
+
+	append body_html "
+<tr $td_class([expr $count % 2])>
+  <input type=hidden name=member_id value=$user_id>
+  <td>"
+	if {$show_user > 0} {
+append body_html "<A HREF=/intranet/users/view?user_id=$user_id>$name</A>"
+	} else {
+append body_html $name
+	}
+
+	append body_html "($profile_letter)
+  </td>"
+	if {$add_admin_links} {
+	    append body_html "
+  <td align=middle>
+    <input type=checkbox name=delete_user value=$user_id>
+  </td>"
+	}
+	append body_html "</tr>"
+    }
+
+    if { [empty_string_p $body_html] } {
+	set body_html "<tr><td colspan=$colspan><i>none</i></td></tr>\n"
+    }
+
+
+    # ------------------ Format the table footer with buttons ------------
+    set footer_html ""
+    if {$add_admin_links} {
+	append footer_html "
+    <tr>
+      <td align=right>
+	<A HREF=/intranet/member-add?[export_url_vars group_id also_add_to_group_id return_url]>
+	  Add member</A>&nbsp;
+      </td>"
+	append footer_html "
+      <td><input type=submit value=Del name=submit></td>
+      </td>
+    </tr>"
+}
+
+    # ------------------ Join table header, body and footer ----------------
+    set html "
+<form method=POST action=/intranet/member-update>
+[export_form_vars group_id return_url]
+    <table bgcolor=white cellpadding=1 cellspacing=1 border=0>
+      $header_html
+      $body_html
+      $footer_html
+    </table>
+</form>
+"
+    return $html
+}
+
+
+ad_proc -public im_project_add_member { group_id user_id role} {
+    Make a specified user a member of a (project) group
+} {
+    
+    db_transaction {
+	db_exec_plsql insert_user_group_map "
+begin
+  user_group_member_add(:group_id, :user_id, :role);
+end;
+"
+    }
+    
+    # Second, add an empty "estimations" field that is necessary
+    # for every project group member.
+#    set sql "
+#	insert into user_group_member_field_map values
+#	(:group_id, :user_id, 'estimation_days', '')"	
+#    db_transaction {
+#	db_dml insert_user_member_field_map $sql
+#    }
+
+    db_release_unused_handles
+}
+
+
+
+
---
+++ tcl/intranet-profile-procs.tcl
@@ -0,0 +1,172 @@
+# /packages/intranet-core/tcl/intranet-profile-procs.tcl
+#
+# Copyright (C) 2004 Project/Open
+# The code is based on work from ArsDigita ACS 3.4
+#
+# This program is free software. You can redistribute it
+# and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation;
+# either version 2 of the License, or (at your option)
+# any later version. This program is distributed in the
+# hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+
+
+# ------------------------------------------------------------------
+# User Profile Box
+# ------------------------------------------------------------------
+
+ad_proc -public im_user_profile_component { user_id { disabled "" }} {
+    Returns a piece of HTML representing a multi-
+    select box with the profiles of the user.
+
+    @param user_id User to show
+    @param disabled Set to "disabled" to show the widget in a 
+    disabled state.
+} {
+
+    # get the current profile of this user
+    set current_profiles [im_profiles_of_user $user_id]
+    set cp [list]
+    foreach p $current_profiles { lappend cp [lindex $p 0] } 
+    ns_log Notice "/users/view: current_profiles=$current_profiles"
+    ns_log Notice "/users/view: cp=$cp"
+
+    # A list of lists containing group_id/group_name tuples
+    set all_profiles [im_profiles_all]
+    ns_log Notice "/users/view: all_profiles=$all_profiles"
+    
+    set profile_html "
+<select name=profile size=7 multiple $disabled>
+"
+
+    foreach profile $all_profiles {
+        set group_id [lindex $profile 0]
+        set group_name [lindex $profile 1]
+        set selected [lsearch -exact $cp $group_id]
+        if {$selected > -1} {
+	    append profile_html "<option value=$group_id selected>$group_name</option>\n"
+        } else {
+	    append profile_html "<option value=$group_id>$group_name</option>\n"
+        }
+    }
+    append profile_html "</select>\n"
+}
+
+
+# ------------------------------------------------------------------
+# Get various sets of profiles
+# ------------------------------------------------------------------
+
+ad_proc -public im_profiles_all {} {
+    Returns the list of all available profiles in the system.
+    The returned list consists of (group_id - group_name) tuples.
+} {
+    # Get the list of all profiles
+    set profile_sql {
+select
+	g.group_id,
+	g.group_name
+from
+	acs_objects o,
+	groups g
+where
+	g.group_id = o.object_id
+	and o.object_type = 'im_profile'
+order by lower(g.group_name)
+}
+    # Make a list
+    set options [list]
+    db_foreach profiles $profile_sql {
+	lappend options [list $group_id "$group_name"]
+    }
+    return $options
+}
+
+
+ad_proc -public im_profiles_of_user { user_id } {
+    Returns a list of the profiles of the current user.
+    The returned list consists of (group_id - group_name) tuples.
+} {
+    # Get the list of profiles for the current user
+    set profile_sql {
+select DISTINCT
+        g.group_id,
+	g.group_name
+from
+        acs_objects o,
+        groups g,
+        group_member_map m
+where
+        m.member_id = :user_id
+        and m.group_id = g.group_id
+        and g.group_id = o.object_id
+        and o.object_type = 'im_profile'
+order by lower(g.group_name)
+    }
+
+    # Make a list
+    set options [list]
+    db_foreach profiles $profile_sql {
+	lappend options [list $group_id "$group_name"]
+    }
+    return $options
+}
+
+
+ad_proc -public im_profiles_managable_for_user { user_id } {
+    Returns the list of profiles that a user has available to manage 
+    other users.<br>
+    This function allows for a kind of "sub-administrators"
+    where for example Employees are able to manage Freelancers.<BR>
+    This list may be empty in the case of unprivileged users
+    such as customers or freelancers.
+} {
+    set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $user_id]
+
+    # Get the list of all profiles administratable
+    # by the current user.
+    set profile_sql {
+select DISTINCT
+	g.group_name,
+	g.group_id
+from
+	acs_objects o,
+	groups g,
+	all_object_party_privilege_map perm
+where
+	perm.object_id = g.group_id
+	and perm.party_id = :user_id
+	and perm.privilege = 'read'
+	and g.group_id = o.object_id
+	and o.object_type = 'im_profile'
+order by lower(g.group_name)
+}
+    # We need a special treatment for Admin in order to
+    # bootstrap the system...
+    if {$user_is_admin_p} {
+	set profile_sql {
+select
+        g.group_name,
+	g.group_id
+from
+        acs_objects o,
+        groups g
+where
+        g.group_id = o.object_id
+        and o.object_type = 'im_profile'
+order by lower(g.group_name)
+	}
+    }
+
+    # Make a list
+    set options [list]
+    db_foreach profiles $profile_sql {
+	lappend options [list $group_id "$group_name"]
+    }
+    return $options
+}
+
