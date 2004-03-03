de48dbab2a9db8483f5181cfad3d50142180ceee
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-03-03 17:22:00 +0000

    changes from proknow

--- www/trans-tasks/index.tcl
+++ www/trans-tasks/index.tcl
@@ -7,8 +7,10 @@ ad_page_contract {
     @author fraber@project-open.com
     @creation-date Nov 2003
 } {
-    group_id:integer
+    group_id:optional
 }
+set group_id 1087
+
 
 set user_id [ad_maybe_redirect_for_registration]
 set return_url [im_url_with_query]
@@ -19,17 +21,24 @@ set task_return_url "/intranet/trans-tasks/task-list?[export_url_vars group_id r
 set missing_task_list [im_task_missing_file_list $group_id]
 
 set page_body "
+
 [im_task_component $user_id $group_id $return_url]
-<A HREF=/intranet/projects/view?group_id=$group_id>Return to Project Page</A>
-<p>
+
+--------------
+
 [im_new_task_component $user_id $group_id $return_url]
-<br>
+
+--------------
 
 [im_task_status_component $user_id $group_id $task_return_url]
 
-[im_task_error_component $user_id $group_id $user_admin_p $user_is_employee_p $return_url $missing_task_list]
+--------------
 
-<a href=task-trados?[export_url_vars group_id return_url]>trados</a>
+[im_task_error_component $user_id $group_id $return_url]
+
+--------------
+
+<br><a href=task-trados?[export_url_vars group_id return_url]>trados</a>
 "
 
 db_release_unused_handles
