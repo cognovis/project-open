04607c13e4871c5d43dde29e7ac449f2e5962073
Frank Bergmann <frank.bergmann@project-open.com>
2004-04-28 20:19:26 +0000

    restored...

--- www/index.tcl
+++ www/index.tcl
@@ -15,41 +15,58 @@ ad_page_contract {
     { bread_crum_path "" }
 }
 
-set html "<pre>\n"
+set user_id [ad_maybe_redirect_for_registration]
+set base_path [im_filestorage_home_path]
+set object_name "Home"
+set folder_type "home"
+set object_id 0
+set return_url "/intranet-filestorage/"
+set current_url_without_vars [ns_conn url]
 
-set header_vars [ns_conn headers]
-foreach var [ad_ns_set_keys $header_vars] {
-    set value [ns_set get $header_vars $var]
+if {1} {
 
-    append html "header:	$var	= $value\n"
-}
+    set html [im_filestorage_pol_component $user_id $object_id $object_name $base_path $folder_type $current_url_without_vars]
 
+    doc_return  200 text/html $html
+    db_release_unused_handles
 
-set form_vars [ns_conn form]
-if {"" != $form_vars} {
-foreach var [ad_ns_set_keys $form_vars] {
-    set value [ns_set get $form_vars $var]
+    return
 
-    append html "form:	$var	= $value\n"
-}
-}
+} else {
 
+    set html "<pre>\n"
 
-append html "\nquery: [ns_conn query]"
-
-append html "</pre>\n"
-
-
-append html "
+    set header_vars [ns_conn headers]
+    foreach var [ad_ns_set_keys $header_vars] {
+	set value [ns_set get $header_vars $var]
+	
+	append html "header:	$var	= $value\n"
+    }
+    
+    
+    set form_vars [ns_conn form]
+    if {"" != $form_vars} {
+	foreach var [ad_ns_set_keys $form_vars] {
+	    set value [ns_set get $form_vars $var]
+	    
+	    append html "form:	$var	= $value\n"
+	}
+    }
+    
+    
+    append html "\nquery: [ns_conn query]"
+    
+    append html "</pre>\n"
+    
+    
+    append html "
 <form action=index method=POST>
 <input type=text name=erter value=ertz>
 <input type=submit name=adsf value=sdfg>
 </form>
 "
-
-
-
-
-doc_return  200 text/html $html
-db_release_unused_handles
-
+    
+    doc_return  200 text/html $html
+    db_release_unused_handles
+    
+}
