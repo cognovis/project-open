043a588a52f160026ce7fa31fc810f6d897452b6
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-05-06 21:24:52 +0000

    - Filestorage: Files are now downloadable - CRM-Tracking: New component on Home page   with failed logins - CRM-Tracking: intranet-crm-tracking/index   shows a detailed list of tracking events - Unregistered users: New component on Home   page - Unregistered users: New view for /users/index   including a new menu entry "Unregistered".

---
+++ www/delete.tcl
@@ -0,0 +1,53 @@
+# /packages/intranet-filestorage/www/erase-folder.tcl
+#
+# Copyright (C) 2003-2004 Project/Open
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+ad_page_contract {
+    Show the content a specific subdirectory
+
+    @param id_path
+    @param file_id 
+    @param dir_id
+    @param object_id
+    @param bread_crum_path
+    @param folder_type
+    @param return_url
+
+    @author pvilarmau@hotmail.com
+    @author santitrenchs@santitrenchs.com
+    @frank.bergmann@project-open.com
+} {
+   
+    id_path:array,optional
+    file_id:array,optional
+    dir_id:array,optional
+    object_id:notnull
+    bread_crum_path
+    folder_type
+    return_url:notnull
+}
+
+# User id already verified by filters
+set user_id [ad_maybe_redirect_for_registration]
+
+set base_path [im_filestorage_base_path $folder_type $object_id]
+
+foreach id [array names file_id] {
+    set erase [im_filestorage_erase_files $object_id $base_path/$id_path($id)]
+}
+foreach id [array names dir_id] {
+    set erase [im_filestorage_delete_folder $object_id $base_path/$id_path($id)]
+}
+ad_returnredirect $return_url
+
+
+
+
+
+
+
+
+
