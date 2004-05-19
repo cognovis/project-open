26104cb8c6e2520a2ef9bfa8866cc307e75892e5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-05-19 16:28:16 +0000

    added perms

--- www/add-perms-2.tcl
+++ www/add-perms-2.tcl
@@ -23,7 +23,6 @@ ad_page_contract {
    
     id_path:array,optional
     dir_id:array,optional
-
     view_profile:array,optional
     view_role:array,optional
     read_profile:array,optional
@@ -32,17 +31,49 @@ ad_page_contract {
     write_role:array,optional
     admin_profile:array,optional
     admin_role:array,optional
-
     object_id:notnull
     bread_crum_path
     folder_type
     return_url:notnull
 }
 
+ns_logg Notice "add-perms-2: object_id=$object_id"
+ns_log Notice "add-perms-2: folder_type=$folder_typee"
+ns_log Notice "add-perms-2: dir_id=$dir_id"
+
+
+# -------------------------------------------------------
+# Page Code
+# -------------------------------------------------------
+
+# User id already verified by filters
+set user_id [ad_maybe_redirect_for_registration]
+
+foreach id [array names dir_id] {
+    set path $id_path($id)
+    ns_log Notice "add-perms-2: path=$path"
+
+    foreach profile_id [array names view_profile] {
+	ns_log Notice "add-perms-2: perm=view, path=$path, profile_id=$profile_id"
+	im_filestorage_add_profile_perm "view" $path $object_id $profile_id 1
+    }
+}
+ad_returnredirect $return_url
+
+
+
+
+
+
+# -------------------------------------------------------
+# Common permission-adding procedure
+# -------------------------------------------------------
+
 ad_proc im_filestorage_add_profile_perm { perm path object_id profile_id p} {
 
     # Make sure the folder exists...
     set folder_exists_p [db_string folder_exists "select count(*) from im_fs_folders where object_id=:object_id and path=:path"]
+    ns_log Notice "add-perms-2: exists=$folder_exists_p for $path"
     if {!$folder_exists_p} {
 	db_dml insert_folder_sql "
 insert into im_fs_folders
@@ -62,25 +93,6 @@ values ($folder_id, $profile_id, $p)"
 	# nothing... 
      }
 }
-
-# User id already verified by filters
-set user_id [ad_maybe_redirect_for_registration]
-
-foreach id [array names dir_id] {
-    set path $id_path($id)
-
-    foreach profile_id [array names view_profile] {
-	ns_log Notice "add-perms-2: perm=view, path=$path, profile_id=$profile_id"
-	im_filestorage_add_profile_perm "view" $path $object_id $profile_id 1
-    }
-
-
-#    set erase [im_filestorage_delete_folder $object_id $base_path/$id_path($id)]
-}
-ad_returnredirect $return_url
-
-
-
 
 
 
