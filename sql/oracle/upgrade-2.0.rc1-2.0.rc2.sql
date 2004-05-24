537f40cd3b819498a1ea83967a26e36d11ee9eb6
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-05-24 19:25:31 +0000

    - added upgrade script for intranet-translation

---
+++ sql/oracle/upgrade-2.0.rc1-2.0.rc2.sql
@@ -0,0 +1,8 @@
+
+alter table im_trans_tasks add match_x number(12,0);
+alter table im_trans_tasks add match_rep number(12,0);
+alter table im_trans_tasks add match75 number(12,0);
+alter table im_trans_tasks add match50 number(12,0);
+
+alter table im_trans_trados_matrix add match_x number(12,4);
+alter table im_trans_trados_matrix add match_rep number(12,4);
