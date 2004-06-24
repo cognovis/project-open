c0fed4d8903e3341c4cfe8bee5572c3193c1d7f3
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-06-24 14:08:22 +0000

    - added checkout-add.sh to intranet-core

---
+++ checkout-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cvs checkout intranet-big-brother
+cvs checkout intranet-core
+cvs checkout intranet-cost
+cvs checkout intranet-crm-tracking
+cvs checkout intranet-dynvals
+cvs checkout intranet-filestorage
+cvs checkout intranet-forum
+cvs checkout intranet-freelance
+cvs checkout intranet-hr
+cvs checkout intranet-invoices
+cvs checkout intranet-payments
+cvs checkout intranet-planning
+cvs checkout intranet-procedures
+cvs checkout intranet-riskmanagement
+cvs checkout intranet-timesheet
+cvs checkout intranet-trans-invoices
+cvs checkout intranet-translation
+cvs checkout intranet-translation-quality
+cvs checkout intranet-travel
