faa2c34eaabc9e056f2aea1f209b8c0d918b72ba
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-08-01 21:05:43 +0000

    - fixed the project_select box, so that it only shows "Open" projects where the user   is a member

--- www/new.tcl
+++ www/new.tcl
@@ -303,7 +303,7 @@ if {$object_id == 0} {
     append table_body "
 	<tr $bgcolor([expr $ctr % 2])>
 	  <td>In Project</td><td>
-[im_project_select object_id $project_status_open "" "" "" $user_id]
+[im_project_select object_id $object_id "Open" "" "" $user_id]
 	  </td>
 	</tr>\n"
     incr ctr
