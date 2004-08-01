25589af8f69e9a22b496064708bc6535a5bc417b
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-08-01 20:43:25 +0000

    - added upgrade directory and 2.0.0-2.0.1.sql file

---
+++ sql/oracle/upgrade/upgrade-2.0.0-2.0.1.sql
@@ -0,0 +1,11 @@
+------------------------------------------------------------------------------
+-- packages/intranet-core/sql/oracle/upgrade-2.0.0-2.0.1.sql
+--
+-- @author frank.bergmann@project-open.com
+-- @creation-date 2004-08-01
+--
+
+update im_view_columns set column_name='Company' where column_id=1;
+
+@../intranet-customers.sql
+@../intranet-categories.sql
