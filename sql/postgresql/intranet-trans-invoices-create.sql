712497cff45714c54368596f5add66dc700b4704
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2004-09-21 19:38:49 +0000

    - Porting to postgresql

---
+++ sql/postgresql/intranet-trans-invoices-create.sql
@@ -0,0 +1,524 @@
+-- /packages/intranet-trans-invoices/sql/oracle/intranet-trans-invoices-create.sql
+--
+-- Copyright (c) 2003-2004 Project/Open
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author frank.bergmann@project-open.com
+-- @author juanjoruizx@yahoo.es
+
+-- Translation Invoicing for Project/Open
+--
+-- Defines:
+--	im_trans_prices			List of prices with defaults
+--
+
+
+---------------------------------------------------------
+-- Translation Invoices
+--
+-- We have made a "Translation Invoice" a separate object
+-- mainly because it requires a different treatment when
+-- it gets deleted, because of its interaction with
+-- im_trans_tasks and im_projects, that are affected
+-- (set back to the status "delivered") when a trans-invoice
+-- is deleted.
+
+
+create table im_trans_invoices (
+	invoice_id		integer
+				constraint im_trans_invoices_pk
+				primary key
+				constraint im_trans_invoices_fk
+				references im_invoices
+);
+
+
+select acs_object_type__create_type (
+        'im_trans_invoice',      -- object_type
+        'Trans Invoice',         -- pretty_name
+        'Trans Invoices',        -- pretty_plural
+        'im_invoice',            -- supertype
+        'im_trans_invoices',     -- table_name
+        'invoice_id',            -- id_column
+        'im_trans_invoice',      -- package_name
+        'f',                     -- abstract_p
+        null,                    -- type_extension_table
+        'im_trans_invoice__name' -- name_method
+    );
+
+
+create or replace function im_trans_invoice__new (
+	integer, --  default null
+	varchar, -- default 'im_trans_invoice'
+	timestamptz, -- default now()
+	integer,
+	varchar, -- default null
+	integer, -- default null
+	varchar, 
+	integer,
+	integer,
+	integer, -- default null
+	timestamptz, -- default now()
+	char, -- default 'EUR'
+	integer, -- default null
+	integer, -- default 602
+	integer, -- default 700
+	integer, -- default null
+	integer, -- default 30
+	numeric,
+	numeric, -- default 0
+	numeric, -- default 0
+	varchar -- default null
+) returns integer as '
+DECLARE
+        p_invoice_id            alias for $1;
+        p_object_type             alias for $2;
+        p_creation_date           alias for $3;
+        p_creation_user           alias for $4;
+        p_creation_ip             alias for $5;
+        p_context_id              alias for $6;
+        p_invoice_nr              alias for $7;
+        p_company_id              alias for $8;
+        p_provider_id             alias for $9;
+        p_company_contact_id      alias for $10;
+        p_invoice_date            alias for $11;
+        p_invoice_currency        alias for $12;
+        p_invoice_template_id     alias for $13;
+        p_invoice_status_id       alias for $14;
+        p_invoice_type_id         alias for $15;
+        p_payment_method_id       alias for $16;
+        p_payment_days            alias for $17;
+        p_amount                  alias for $18;
+        p_vat                     alias for $19;
+        p_tax                     alias for $20;
+        p_note                    alias for $21;
+
+	v_invoice_id	integer;
+BEGIN
+	v_invoice_id := im_invoice__new(
+	p_invoice_id   
+	p_object_type	
+	p_creation_date
+	p_creation_user
+	p_creation_ip	
+	p_context_id	
+	p_invoice_nr	
+	p_company_id	
+	p_provider_id	
+	p_company_contact_id
+	p_invoice_date
+	p_invoice_currency
+	p_invoice_template_id
+	p_invoice_status_id	
+	p_invoice_type_id
+	p_payment_method_id
+	p_payment_days
+	p_amount	
+	p_vat
+	p_tax
+	p_note
+	);
+
+	-- insert to create a referential integrity constraint
+	-- to avoid that im_invoices is used to delete such an
+	-- invoice without resetting the im_trans_tasks and
+	-- im_projects dependencies.
+	insert into im_trans_invoices (
+		invoice_id
+	) values (
+		v_invoice_id
+	);
+
+	return v_invoice_id;
+end;' language 'plpgsql';
+
+
+    -- Delete a single invoice (if we know its ID...)
+create or replace function  im_trans_invoice__delete (integer)
+returns integer as '
+DECLAREdeclare
+	p_invoice_id	alias for $1;
+BEGIN
+     	-- Reset the status of all project to "delivered" that
+	-- were included in the invoice
+	update im_projects
+	set project_status_id = 78
+	where project_id in (
+		select distinct
+			r.object_id_one
+		from
+			acs_rels r
+		where
+			r.object_id_two = p_invoice_id
+	);
+
+	-- Set all projects back to "delivered" that have tasks
+	-- that were included in the invoices to delete.
+	update im_projects
+	set project_status_id = 78
+	where project_id in (
+		select distinct
+			t.project_id
+		from
+			im_trans_tasks t
+		where
+			t.invoice_id = p_invoice_id
+	);
+
+	-- Reset the status of all invoiced tasks to delivered.
+	update	im_trans_tasks t
+	set	invoice_id = null
+	where	t.invoice_id = p_invoice_id;
+
+	-- Erase the invoice itself
+	delete from 	im_trans_invoices
+	where		invoice_id = p_invoice_id;
+
+	-- Erase the CostItem
+	PERFORM im_invoice__del(p_invoice_id);
+
+	return 0;
+end;' language 'plpgsql';
+
+
+create or replace function im_trans_invoice__name (integer) 
+returns varchar as '
+DECLARE
+	p_invoice_id alias for $1;
+	v_name	varchar(40);
+BEGIN
+	select	invoice_nr
+	into	v_name
+	from	im_invoices
+	where	invoice_id = p_invoice_id;
+
+	return v_name;
+end;' language 'plpgsql';
+
+
+-- Add links to edit im_trans_invoices objects...
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_trans_invoice','view','/intranet-invoices/view?invoice_id=');
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_trans_invoice','edit','/intranet-invoices/new?invoice_id=');
+
+
+---------------------------------------------------------
+-- Translation Prices
+--
+-- The price model is very specific to every translation business,
+-- so we need to allow maximum customization.
+-- On the TCL API-Level we asume that we are able to determine
+-- a price for every im_task, given the im_company and the
+-- im_project.
+-- What is missing here are promotions and other types of 
+-- exceptions. However, discounts are handled on the level
+-- of invoice, together with VAT and other taxes.
+--
+-- The price model for the Translation Industry is based on
+-- the variables:
+--	- UOM: Unit of Measure: Hours, source words, lines,...
+--	- Company: There may be different rates for each company
+--	- Task Type
+--	- Target language
+--	- Source language
+--	- Subject Area
+
+create sequence im_trans_prices_seq start 10000;
+create table im_trans_prices (
+	price_id		integer 
+				constraint im_trans_prices_pk
+				primary key,
+	--
+	-- "Input variables"
+	uom_id			integer not null 
+				constraint im_trans_prices_uom_id
+				references im_categories,
+	company_id		integer not null 
+				constraint im_trans_prices_company_id
+				references im_companies,
+	task_type_id		integer
+				constraint im_trans_prices_task_type_id
+				references im_categories,
+	target_language_id	integer
+				constraint im_trans_prices_target_lang
+				references im_categories,
+	source_language_id	integer
+				constraint im_trans_prices_source_lang
+				references im_categories,
+	subject_area_id		integer
+				constraint im_trans_prices_subject_are
+				references im_categories,
+	valid_from		timestamptz,
+	valid_through		timestamptz,
+				-- make sure the end date is after start date
+				constraint im_trans_prices_date_const
+				check(valid_through - valid_from >= 0),
+	--
+	-- "Output variables"
+	currency		char(3) references currency_codes(ISO),
+	price			numeric(12,4)
+);
+
+-- make sure the same price doesn't get defined twice 
+create unique index im_price_idx on im_trans_prices (
+	uom_id, company_id, task_type_id, target_language_id, 
+	source_language_id, subject_area_id, currency
+);
+
+
+------------------------------------------------------
+-- Views to Business Objects
+--
+
+-- Calculate a match value between a price list item and an invoice_item
+-- The higher the match value the better the fit.
+
+create or replace function im_trans_prices_calc_relevancy ( 
+       integer, integer, integer, integer, integer, integer, integer, integer, integer, integer
+) returns numeric as '
+DECLARE
+	v_price_company_id		alias for $1;		
+	v_item_company_id		alias for $2;
+	v_price_task_type_id		alias for $3;	
+	v_item_task_type_id		alias for $4;
+	v_price_subject_area_id		alias for $5;	
+	v_item_subject_area_id		alias for $6;
+	v_price_target_language_id	alias for $7;	
+	v_item_target_language_id	alias for $8;
+	v_price_source_language_id	alias for $9;	
+	v_item_source_language_id	alias for $10
+
+	match_value			numeric;
+	v_internal_company_id		integer;
+	v_price_target_language		varchar(100);
+	v_item_target_language		varchar(100);
+	v_price_source_language		varchar(100);
+	v_item_source_language		varchar(100);
+BEGIN
+	match_value := 0;
+
+	select company_id
+	into v_internal_company_id
+	from im_companies
+	where company_path=''internal'';
+
+	-- Hard matches for task type
+	if v_price_task_type_id = v_item_task_type_id then
+		match_value := match_value + 4;
+	end if;
+	if not(v_price_task_type_id is null) and v_price_task_type_id != v_item_task_type_id then
+		match_value := match_value - 4;
+	end if;
+
+	-- Default matching for source language:
+	-- "de" <-> "de_DE" = + 1
+	-- "de_DE" <-> "de_DE" = +3
+	-- "es" <-> "de_DE" = -10
+	if (v_price_source_language_id is not null) and  (v_item_source_language_id is not null) then
+		-- only add or subtract match_values if both are defined...
+		select	category
+		into	v_price_source_language
+		from	im_categories
+		where	category_id = v_price_source_language_id;
+	
+		select	category
+		into	v_item_source_language
+		from	im_categories
+		where	category_id = v_item_source_language_id;
+
+		if substr(v_price_source_language,1,2) = substr(v_item_source_language,1,2) then
+			-- the main part of the language have matched
+			match_value := match_value + 2;
+			if v_price_source_language_id = v_item_source_language_id then
+				-- the main part have matched and the country variants are the same
+				match_value := match_value + 1;
+			end if;
+		else
+			match_value := match_value - 10;
+		end if;
+	end if;
+
+
+	-- Default matching for target language:
+	if (v_price_target_language_id is not null) and  (v_item_target_language_id is not null) then
+		-- only add or subtract match_values if both are defined...
+		select	category
+		into	v_price_target_language
+		from	im_categories
+		where	category_id = v_price_target_language_id;
+	
+		select	category
+		into	v_item_target_language
+		from	im_categories
+		where	category_id = v_item_target_language_id;
+
+		if substr(v_price_target_language,1,2) = substr(v_item_target_language,1,2) then
+			-- the main part of the language have matched
+			match_value := match_value + 1;		
+			if v_price_target_language_id = v_item_target_language_id then
+				-- the main part have matched and the country variants are the same
+				match_value := match_value + 1;
+			end if;
+		else
+			match_value := match_value - 10;
+		end if;
+	end if;
+
+
+	if v_price_subject_area_id = v_item_subject_area_id then
+		match_value := match_value + 1;
+	end if;
+	if not(v_price_subject_area_id is null) and v_price_subject_area_id != v_item_subject_area_id then
+		match_value := match_value - 10;
+	end if;
+
+	-- Company logic - "Internal" doesnt give a penalty 
+	-- but doesnt count as high as an exact match
+	--
+	if v_price_company_id = v_item_company_id then
+		match_value := (match_value + 6)*2;
+	end if;
+	if v_price_company_id = v_internal_company_id then
+		match_value := match_value + 1;
+	end if;
+	if v_price_company_id != v_internal_company_id and v_price_company_id != v_item_company_id then
+		match_value := match_value -10;
+	end if;
+
+	return match_value;
+end;' language 'plpgsql';
+
+
+---------------------------------------------------------
+-- Register the component in the core TCL pages
+--
+-- These DB-entries allow the pages of Project/Open Core
+-- to render the forum components in the Home, Users, Projects
+-- and Company pages.
+--
+-- The TCL code in the "component_tcl" field is executed
+-- via "im_component_bay" in an "uplevel" statemente, exactly
+-- as if it would be written inside the .adp <%= ... %> tag.
+-- I know that's relatively dirty, but TCL doesn't provide
+-- another way of "late binding of component" ...
+
+-- delete potentially existing menus and plugins if this
+-- file is sourced multiple times during development...
+
+select im_component_plugin__del_module('intranet-trans-invoices');
+select im_menu__del_module('intranet-trans-invoices');
+
+
+
+
+-- Show the translation specific fields in the ProjectViewPage
+--
+select im_component_plugin__new (
+        null,                           -- plugin_id
+        'acs_object',                   -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        'Company Translation Prices',   -- plugin_name
+        'intranet-trans-invoices',      -- package_name
+        'left',                         -- location
+        '/intranet/companies/view',     -- page_url
+        null,                           -- view_name
+        100,                            -- sort_order
+	'im_trans_price_component $user_id $company_id $return_url'
+    );
+
+
+
+
+-- Add a "Translation Invoice" into the Invoice Menu
+--
+create or replace function inline_01 ()
+returns integer as '
+declare
+	-- Menu IDs
+	v_menu			integer;
+	v_invoices_menu		integer;
+	v_project_menu		integer;
+
+	-- Groups
+	v_accounting		 integer;
+	v_senman		integer;
+	v_companies		integer;
+	v_freelancers		integer;
+	v_admins		integer;
+begin
+
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+    select group_id into v_companies from groups where group_name = ''Customers'';
+    select group_id into v_freelancers from groups where group_name = ''Freelancers'';
+
+    select menu_id
+    into v_invoices_menu
+    from im_menus
+    where label=''finance'';
+
+    v_menu := im_menu__new (
+        null,                           -- menu_id
+        ''acs_object'',                 -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        ''intranet-trans-invoices'',    -- package_name
+        ''new_trans_invoice'',          -- label
+        ''New Trans Invoice'',          -- name
+        ''/intranet-trans-invoices/invoices/new'',  -- url
+        70,                             -- sort_order
+        v_invoices_menu,                -- parent_menu_id
+        null                            -- visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_companies, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_freelancers, ''read'');
+
+    select menu_id
+    into v_project_menu
+    from im_menus
+    where label=''project'';
+
+    v_menu := im_menu__new (
+        null,                           -- menu_id
+        ''acs_object'',                 -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        ''intranet-trans-invoices'',    -- package_name
+        ''project_pos'',                -- label
+        ''POs'',                        -- name
+        ''/intranet-trans-invoices/purchase-orders/index?cost_type_id=3706'',  -- url
+        70,                             -- sort_order
+        v_project_menu,                 -- parent_menu_id
+        null                            -- visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_companies, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_freelancers, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+
+select inline_01 ();
+
+drop function inline_01 ();
+
+	
+
