baba316526f897a536fe8af54069eb9e6e67f333
barna <>
2004-09-27 17:04:06 +0000

    Companies profile to Customers

--- sql/oracle/intranet-hr-create.sql
+++ sql/oracle/intranet-hr-create.sql
@@ -373,7 +373,7 @@ begin
     select group_id into v_proman from groups where group_name = 'Project Managers';
     select group_id into v_accounting from groups where group_name = 'Accounting';
     select group_id into v_employees from groups where group_name = 'Employees';
-    select group_id into v_companies from groups where group_name = 'Companies';
+    select group_id into v_companies from groups where group_name = 'Customers';
     select group_id into v_freelancers from groups where group_name = 'Freelancers';
 
     select menu_id
