008f7e9170cfb239890ff45c4b47e22b82475349
Frank Bergmann <frank.bergmann@project-open.com>
2005-02-28 18:22:02 +0000

    - disable project cost component

--- sql/postgresql/intranet-cost-create.sql
+++ sql/postgresql/intranet-cost-create.sql
@@ -1148,25 +1148,28 @@ select  im_component_plugin__new (
 	'im_costs_project_finance_component $user_id $project_id'  -- component_tcl
     );
 
+
 -- Show the cost component in project page
---
-select  im_component_plugin__new (
-	null,				 -- plugin_id
-	'acs_object',			 -- object_type
-	now(),				 -- creation_date
-	null,				 -- creation_user
-	null,				 -- creation_ip
-	null,				 -- context_id
+-- 
+-- replace by project finance component
+-- select  im_component_plugin__new (
+-- 	null,				 -- plugin_id
+-- 	'acs_object',			 -- object_type
+-- 	now(),				 -- creation_date
+-- 	null,				 -- creation_user
+-- 	null,				 -- creation_ip
+-- 	null,				 -- context_id
+-- 
+-- 	'Project Cost Component',	 -- plugin_name
+-- 	'intranet-cost',		 -- package_name
+-- 	'left',				 -- location
+-- 	'/intranet/projects/view',	 -- page_url
+-- 	null,				 -- view_name
+-- 	90,				 -- sort_order
+-- 
+-- 	'im_costs_project_component $user_id $project_id'  -- component_tcl
+--     );
 
-	'Project Cost Component',	 -- plugin_name
-	'intranet-cost',		 -- package_name
-	'left',				 -- location
-	'/intranet/projects/view',	 -- page_url
-	null,				 -- view_name
-	90,				 -- sort_order
-
-	'im_costs_project_component $user_id $project_id'  -- component_tcl
-    );
 
 -- Show the cost component in companies page
 --
