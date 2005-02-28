3f517ced8b1e5d281b9a0dc2ee758d463354f923
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-02-28 18:37:00 +0000

    - added German

--- all-catalog-edit.sh
+++ all-catalog-edit.sh
@@ -21,3 +21,25 @@ cvs edit intranet-trans-invoices/catalog/intranet-trans-invoices.en_US.ISO-8859-
 cvs edit intranet-translation/catalog/intranet-translation.en_US.ISO-8859-1.xml
 cvs edit intranet-trans-quality/catalog/intranet-trans-quality.en_US.ISO-8859-1.xml
 cvs edit intranet-travel/catalog/intranet-travel.en_US.ISO-8859-1.xml
+
+
+cvs edit intranet-core/catalog/intranet-core.de_DE.ISO-8859-1.xml
+cvs edit intranet-big-brother/catalog/intranet-big-brother.de_DE.ISO-8859-1.xml
+cvs edit intranet-core/catalog/intranet-core.de_DE.ISO-8859-1.xml
+cvs edit intranet-cost/catalog/intranet-cost.de_DE.ISO-8859-1.xml
+cvs edit intranet-crm-tracking/catalog/intranet-crm-tracking.de_DE.ISO-8859-1.xml
+cvs edit intranet-dynvals/catalog/intranet-dynvals.de_DE.ISO-8859-1.xml
+cvs edit intranet-filestorage/catalog/intranet-filestorage.de_DE.ISO-8859-1.xml
+cvs edit intranet-forum/catalog/intranet-forum.de_DE.ISO-8859-1.xml
+cvs edit intranet-freelance/catalog/intranet-freelance.de_DE.ISO-8859-1.xml
+cvs edit intranet-hr/catalog/intranet-hr.de_DE.ISO-8859-1.xml
+cvs edit intranet-invoices/catalog/intranet-invoices.de_DE.ISO-8859-1.xml
+cvs edit intranet-payments/catalog/intranet-payments.de_DE.ISO-8859-1.xml
+cvs edit intranet-planning/catalog/intranet-planning.de_DE.ISO-8859-1.xml
+cvs edit intranet-procedures/catalog/intranet-procedures.de_DE.ISO-8859-1.xml
+cvs edit intranet-riskmanagement/catalog/intranet-riskmanagement.de_DE.ISO-8859-1.xml
+cvs edit intranet-timesheet/catalog/intranet-timesheet.de_DE.ISO-8859-1.xml
+cvs edit intranet-trans-invoices/catalog/intranet-trans-invoices.de_DE.ISO-8859-1.xml
+cvs edit intranet-translation/catalog/intranet-translation.de_DE.ISO-8859-1.xml
+cvs edit intranet-trans-quality/catalog/intranet-trans-quality.de_DE.ISO-8859-1.xml
+cvs edit intranet-travel/catalog/intranet-travel.de_DE.ISO-8859-1.xml
