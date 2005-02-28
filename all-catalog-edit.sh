e1b9405c16f55a6c41d9e98c5bec2f6b16b039f9
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-02-28 18:34:48 +0000

    - added

---
+++ all-catalog-edit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+cvs edit intranet-core/catalog/intranet-core.en_US.ISO-8859-1.xml
+cvs edit intranet-big-brother/catalog/intranet-big-brother.en_US.ISO-8859-1.xml
+cvs edit intranet-core/catalog/intranet-core.en_US.ISO-8859-1.xml
+cvs edit intranet-cost/catalog/intranet-cost.en_US.ISO-8859-1.xml
+cvs edit intranet-crm-tracking/catalog/intranet-crm-tracking.en_US.ISO-8859-1.xml
+cvs edit intranet-dynvals/catalog/intranet-dynvals.en_US.ISO-8859-1.xml
+cvs edit intranet-filestorage/catalog/intranet-filestorage.en_US.ISO-8859-1.xml
+cvs edit intranet-forum/catalog/intranet-forum.en_US.ISO-8859-1.xml
+cvs edit intranet-freelance/catalog/intranet-freelance.en_US.ISO-8859-1.xml
+cvs edit intranet-hr/catalog/intranet-hr.en_US.ISO-8859-1.xml
+cvs edit intranet-invoices/catalog/intranet-invoices.en_US.ISO-8859-1.xml
+cvs edit intranet-payments/catalog/intranet-payments.en_US.ISO-8859-1.xml
+cvs edit intranet-planning/catalog/intranet-planning.en_US.ISO-8859-1.xml
+cvs edit intranet-procedures/catalog/intranet-procedures.en_US.ISO-8859-1.xml
+cvs edit intranet-riskmanagement/catalog/intranet-riskmanagement.en_US.ISO-8859-1.xml
+cvs edit intranet-timesheet/catalog/intranet-timesheet.en_US.ISO-8859-1.xml
+cvs edit intranet-trans-invoices/catalog/intranet-trans-invoices.en_US.ISO-8859-1.xml
+cvs edit intranet-translation/catalog/intranet-translation.en_US.ISO-8859-1.xml
+cvs edit intranet-trans-quality/catalog/intranet-trans-quality.en_US.ISO-8859-1.xml
+cvs edit intranet-travel/catalog/intranet-travel.en_US.ISO-8859-1.xml
