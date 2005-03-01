6555166110ffc61414d087cc539a28a97c797682
Frank Bergmann <frank.bergmann@project-open.com>
2005-03-01 17:32:12 +0000

    - small fixes before V3.0.beta1

--- sql/postgresql/intranet-cost-create.sql
+++ sql/postgresql/intranet-cost-create.sql
@@ -1122,6 +1122,65 @@ begin
     return 0;
 end;' language 'plpgsql';
 
+select inline_0 ();
+
+drop function inline_0 ();
+
+
+
+create or replace function inline_0 ()
+returns integer as'
+declare
+        -- Menu IDs
+        v_menu                  integer;
+        v_project_menu          integer;
+
+        -- Groups
+        v_employees             integer;
+        v_accounting            integer;
+        v_senman                integer;
+        v_customers             integer;
+        v_freelancers           integer;
+        v_proman                integer;
+        v_admins                integer;
+begin
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+
+    select menu_id
+    into v_project_menu
+    from im_menus
+    where label=''project'';
+
+    v_menu := im_menu__new (
+        null,                   -- p_menu_id
+        ''acs_object'',           -- object_type
+        now(),                  -- creation_date
+        null,                   -- creation_user
+        null,                   -- creation_ip
+        null,                   -- context_id
+        ''intranet-core'',      -- package_name
+        ''project_finance'',      -- label
+        ''Finance'',            -- name
+        ''/intranet/projects/view?view_name=finance'',  -- url
+        20,                     -- sort_order
+        v_project_menu,         -- parent_menu_id
+        null                    -- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+
+select inline_0 ();
+
+drop function inline_0 ();
+
+
 
 -------------------------------------------------------------
 -- Cost Components
