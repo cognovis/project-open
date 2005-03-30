20827a6d5fef6734c6d98981951b322c985316bf
anonymous <>
2005-03-30 17:14:28 +0000

    - closed the "images" folder of HomeFsComponent - Set home path to C:/ProjectOpen/doc for documentation

--- sql/stage-win32.sql
+++ sql/stage-win32.sql
@@ -7,7 +7,7 @@
 -- @author      frank.bergmann@project-open.com
 
 -----------------------------------------------------------
--- Replace Parameters
+-- Replace Filestorage Directories
 
 -- Replace all parameters with "/web/ptdemo" by "C:/ProjectOpen"
 update apm_parameter_values
@@ -18,6 +18,35 @@ where attr_value like '/web/ptdemo%';
 update apm_parameter_values
 set attr_value = '/bin/find'
 where attr_value = '/usr/bin/find';
+
+-- Set the "Home Path" to the P/O documentation dir.
+update apm_parameter_values
+set attr_value = 'C:/ProjectOpen/doc'
+where parameter_id in (
+	select parameter_id
+	from apm_parameters
+	where parameter_name = 'HomeBasePathUnix'
+      );
+
+-- Close the "images" folder of object 0 (Home FS)
+-- so that the images are hidden
+update im_fs_folder_status
+set open_p = 'c'
+where folder_id in (
+	select folder_id
+	from im_fs_folders
+	where object_id = 0 and path = 'images'
+);
+
+
+-----------------------------------------------------------
+-- Enable Components
+
+-- Enable the help text on the home page
+update im_component_plugins
+set location = 'left'
+where plugin_name = 'Home Page Help Blurb';
+
 
 
 -- We still need to uninstall the packages that are not public...
