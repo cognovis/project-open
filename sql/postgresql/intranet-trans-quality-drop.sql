0a96eed286a1658530e78708215d35c40a665b16
anonymous <>
2005-03-30 16:13:16 +0000

    - added removing menus and plugins

--- sql/postgresql/intranet-trans-quality-drop.sql
+++ sql/postgresql/intranet-trans-quality-drop.sql
@@ -8,7 +8,18 @@
 -- @author frank.bergmann@project-open.com
 -- @author guillermo.belcic@project-open.com
 
+-----------------------------------------------------------
+-- Drop Tables
+
 drop sequence quality_report_id;
 drop table im_trans_quality_entries;
 drop table im_trans_quality_reports;
+
+-----------------------------------------------------------
+-- Remove menus and plugin components
+
+select im_menu__del_module('intranet-trans-quality');
+select im_component_plugin__del_module('intranet-trans-quality');
+
+
 
