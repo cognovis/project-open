d7017d5a2f7e286be927ed0178d2c414527b2fc7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-14 16:19:30 +0000

    - copied in non-CVS versions

--- sql/postgresql/intranet-wiki-create.sql
+++ sql/postgresql/intranet-wiki-create.sql
@@ -50,3 +50,35 @@ select inline_0 ();
 
 drop function inline_0 ();
 
+
+
+create or replace function inline_0 ()
+returns integer as ' 
+declare
+    v_plugin            integer;
+begin
+    -- Home Page
+    -- Set the wiki to the very end.
+    --
+    v_plugin := im_component_plugin__new (
+	null,					-- plugin_id
+	''acs_object'',				-- object_type
+	now(),					-- creation_date
+	null,					-- creation_user
+	null,					-- creation_ip
+	null,					-- context_id
+	''Project Wiki Component'',		-- plugin_name
+	''intranet-wiki'',			-- package_name
+        ''right'',				-- location
+	''/intranet/projects/view'',		-- page_url
+        null,					-- view_name
+        80,					-- sort_order
+        ''im_wiki_project_component $project_id'' -- component_tcl
+    );
+    return 0;
+end;' language 'plpgsql';
+
+select inline_0 ();
+
+drop function inline_0 ();
+
