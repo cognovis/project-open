60a4a1f58c7dba3b6d65b2daad3a646cb094bad1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-15 18:05:09 +0000

    - removed hard-coded folder id and "wiki_mount"

--- sql/postgresql/intranet-wiki-create.sql
+++ sql/postgresql/intranet-wiki-create.sql
@@ -10,15 +10,16 @@
 -- Sets up an interface to the OpenACS Wiki System
 
 ---------------------------------------------------------
--- delete potentially existing menus and plugins if this 
+-- Delete potentially existing menus and plugins if this 
 -- file is sourced multiple times during development...
 
 select im_component_plugin__del_module('intranet-wiki');
 select im_menu__del_module('intranet-wiki');
 
 
+
 ---------------------------------------------------------
--- Register the component:
+-- Register components
 
 create or replace function inline_0 ()
 returns integer as ' 
@@ -82,3 +83,70 @@ select inline_0 ();
 
 drop function inline_0 ();
 
+
+
+
+
+---------------------------------------------------------
+-- Setup the "Wiki" main menu entry
+--
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        -- Menu IDs
+        v_menu                  integer;
+	v_main_menu		integer;
+
+        -- Groups
+        v_employees             integer;
+        v_accounting            integer;
+        v_senman                integer;
+        v_companies             integer;
+        v_freelancers           integer;
+        v_proman                integer;
+        v_admins                integer;
+BEGIN
+
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_proman from groups where group_name = ''Project Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+    select group_id into v_employees from groups where group_name = ''Employees'';
+    select group_id into v_companies from groups where group_name = ''Customers'';
+    select group_id into v_freelancers from groups where group_name = ''Freelancers'';
+
+    select menu_id
+    into v_main_menu
+    from im_menus
+    where label=''main'';
+
+    v_menu := im_menu__new (
+        null,                   -- p_menu_id
+        ''acs_object'',         -- object_type
+        now(),                  -- creation_date
+        null,                   -- creation_user
+        null,                   -- creation_ip
+        null,                   -- context_id
+        ''intranet-wiki'',      -- package_name
+        ''wiki'',               -- label
+        ''Wiki'',               -- name
+        ''/intranet-wiki/'',    -- url
+        75,                     -- sort_order
+        v_main_menu,            -- parent_menu_id
+        null                    -- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_companies, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_freelancers, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+
+select inline_0 ();
+drop function inline_0 ();
