702390e0eaa09e95ff7366221266f77ddd69a38c
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-18 19:23:12 +0000

    Initial revision

---
+++ tcl/aa-test-init.tcl
@@ -0,0 +1,9 @@
+ad_library {
+  Server startup initialization code for the acs-automated-testing package
+
+  @author Peter Marklund
+  @creation-date 4:th of April 2003
+  @cvs-id $Id$
+}
+
+# LARS: Moved to aa-test-procs.tcl file. See comment there.
