c1b8c36e178b9a42e592f616d9f5d7eb24714f42
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-19 14:51:24 +0000

    Initial Import

---
+++ tcl/intranet-update-client-procs.tcl
@@ -0,0 +1,14 @@
+# /packages/intranet-update-client/tcl/intranet-update-client-procs.tcl
+
+ad_library {
+    Project/Open Automatic Software Updates
+    @author frank.bergmann@project-open.com
+    @creation-date 27 April 2005
+}
+
+
+ad_proc im_update_component { } {
+    Stub for some future components...
+} {
+    return ""
+}
---
+++ www/index.adp
@@ -0,0 +1,11 @@
+<master src="../../intranet-core/www/master">
+<property name="title">Wiki</property>
+<property name="main_navbar_label">wiki</property>
+
+<table width="100%">
+<tr valign=top>
+  <td>@page_list;noquote@</td>
+  <td>@wiki_component;noquote@</td>
+</tr>
+</table>
+
