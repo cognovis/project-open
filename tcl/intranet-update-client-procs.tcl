8abb8eb8b68803f435fd1cdf71a53ba8eb7e85f1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-27 15:07:07 +0000

    - added "provides" info to info file - deleted the update-server functionality

--- tcl/intranet-update-client-procs.tcl
+++ tcl/intranet-update-client-procs.tcl
@@ -1,17 +1,16 @@
 # /packages/intranet-update-client/tcl/intranet-update-client-procs.tcl
+#
+# Copyright (C) 2003-2005 Project/Open
+#
+# All rights reserved. Please check
+# http://www.project-open.com/ for licensing details.
+
 
 ad_library {
     Project/Open Automatic Software Updates
     @author frank.bergmann@project-open.com
     @creation-date 27 April 2005
 }
-
-
-#ad_proc -public im_update_client_package_id {} {
-#    Returns the package id of the intranet-update-client package
-#} {
-#    return [util_memoize "im_update_client_package_id_helper"]
-#}
 
 ad_proc -private im_update_client_package_id {} {
     return [db_string im_package_update_client_id {
