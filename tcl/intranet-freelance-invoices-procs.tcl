00883c958211ed163a45adb064299443c1c94b63
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-29 18:39:17 +0000

    Initial Import

---
+++ tcl/intranet-freelance-invoices-procs.tcl
@@ -0,0 +1,10 @@
+# /packages/intranet-freelance-invoices/tcl/intranet-freelance-invoices-procs.tcl
+
+ad_library {
+    Bring together all "components" (=HTML + SQL code)
+    related to creating Provider Purchase Orders
+
+    @author fraber@fraber.de
+    @creation-date  27 June 2003
+}
+
