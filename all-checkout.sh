40e779b6aa3b90abc0429c859055f863a431e3a8
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-04-30 16:42:26 +0000

    - updated the number of modules to include OpenACS modules

--- all-checkout.sh
+++ all-checkout.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
 
+cvs checkout acs-admin
+cvs checkout acs-api-browser
+cvs checkout acs-authentication
+cvs checkout acs-automated-testing
+cvs checkout acs-bootstrap-installer
+cvs checkout acs-content-repository
+cvs checkout acs-core-docs
+cvs checkout acs-datetime
+cvs checkout acs-kernel
+cvs checkout acs-lang
+cvs checkout acs-mail
+cvs checkout acs-messaging
+cvs checkout acs-reference
+cvs checkout acs-service-contract
+cvs checkout acs-subsite
+cvs checkout acs-tcl
+cvs checkout acs-templating
+cvs checkout acs-workflow
+cvs checkout acs_admin_tools
+
+cvs checkout cms
+
 cvs checkout intranet-big-brother
 cvs checkout intranet-core
 cvs checkout intranet-cost
 cvs checkout intranet-crm-tracking
 cvs checkout intranet-dynvals
+cvs checkout intranet-expfind-ora
+cvs checkout intranet-expfind-pg
+cvs checkout intranet-expfind2-ora
+cvs checkout intranet-expfind2-pg
 cvs checkout intranet-filestorage
 cvs checkout intranet-forum
 cvs checkout intranet-freelance
+cvs checkout intranet-freelance-invoices
+cvs checkout intranet-freelance-recruiting
 cvs checkout intranet-hr
 cvs checkout intranet-invoices
 cvs checkout intranet-payments
 cvs checkout intranet-planning
 cvs checkout intranet-procedures
 cvs checkout intranet-riskmanagement
+cvs checkout intranet-search
+cvs checkout intranet-search-ora
+cvs checkout intranet-search-oracle
+cvs checkout intranet-search-pg
 cvs checkout intranet-spam
 cvs checkout intranet-timesheet
 cvs checkout intranet-trans-invoices
-cvs checkout intranet-translation
 cvs checkout intranet-trans-quality
+cvs checkout intranet-translation
 cvs checkout intranet-travel
+cvs checkout intranet-update-client
+cvs checkout intranet-update-server
+cvs checkout intranet-wiki
+
+cvs checkout ref-timezones
+cvs checkout search
+cvs checkout wiki
