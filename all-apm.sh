a6cdbc0e27ecdd4f244a47ce176f32027691d8ad
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-05-02 15:07:33 +0000

    - added the new packages

--- all-apm.sh
+++ all-apm.sh
@@ -1,5 +1,5 @@
 #!/bin/csh
-POVER="3.0.beta5"
+POVER="3.0.0.2"
 export POVER
 rm -f *.apm
 /bin/tar czf intranet-big-brother-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-big-brother
@@ -10,6 +10,8 @@ rm -f *.apm
 /bin/tar czf intranet-filestorage-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-filestorage
 /bin/tar czf intranet-forum-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-forum
 /bin/tar czf intranet-freelance-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-freelance
+/bin/tar czf intranet-freelance-invoices-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-freelance-invoices
+/bin/tar czf intranet-freelance-recruiting-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-freelance-recruiting
 /bin/tar czf intranet-hr-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-hr
 /bin/tar czf intranet-invoices-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-invoices
 /bin/tar czf intranet-payments-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-payments
@@ -20,4 +22,6 @@ rm -f *.apm
 /bin/tar czf intranet-translation-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-translation
 /bin/tar czf intranet-trans-quality-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-trans-quality
 /bin/tar czf intranet-travel-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-travel
-
+/bin/tar czf intranet-update-client-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-update-client
+/bin/tar czf intranet-update-server-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-update-server
+/bin/tar czf intranet-wiki-${POVER}.apm --exclude='*CVS*' --exclude='*~' intranet-wiki
