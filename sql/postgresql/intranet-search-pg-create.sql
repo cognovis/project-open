f8964db84db4b4deff6de48975d1a70a03c5f592
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-05-12 20:08:00 +0000

    - building P/O search...

--- sql/postgresql/intranet-search-pg-create.sql
+++ sql/postgresql/intranet-search-pg-create.sql
@@ -1,71 +1,196 @@
--- /packages/intranet-forum/sql/oracle/intranet-forum-sc-create.sql
+-- /packages/intranet-forum/sql/oracle/intranet-forum-pg-create.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003-2005 Project/Open
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
 --
--- @author pepels@gmail.com
+-- @author frank.bergmann@project-open.com
 
------------------------------------------------------------
--- Tasks, Incidents, News and Discussions (TIND)
+-- FIXME need to load tsearch2.sql from postgresql/share/contrib
+-- (on debian /usr/share/postgresql/contrib)
+
+
+-- "Abbreviation" of object_type for search purposes -
+-- we don't want to add a varchar(100) to the main search
+-- table...
 --
--- ftscontenprovider support for content in intranet-core
------------------------------------------------------------
--- Topics
+create table im_search_object_types (
+	object_type_id	integer
+			constraint im_search_object_types_pk
+			primary key,
+	object_type	varchar(100)
+			constraint im_search_objects_object_type_fk
+			references acs_object_types
+			on delete cascade
+);
+
+insert into im_search_object_types values (0,'im_project');
+
+
+
+-- The main search table with Full Text Index.
 --
-
-select acs_sc_impl__new(
-	   'FtsContentProvider',		-- impl_contract_name 
-           'im_project',			-- impl_name (object in which we are going to search)
-	   'projects'				-- impl_owner_name(just a silly parameter?)
+create table im_search_objects (
+	object_id		integer
+				constraint im_search_objects_object_id_fk
+				references acs_objects
+				on delete cascade,
+				-- may include "object types" outside of OpenACS
+				-- that are not in the "acs_object_types" table.
+	object_type_id		integer
+				constraint im_search_objects_object_type_id_fk
+				references im_search_object_types
+				on delete cascade,
+				-- What is the topmost container for this object?
+				-- Allows to speed up the elimination of objects
+				-- that the current user can't access
+	biz_object_id		integer
+				constraint im_search_objects_biz_obj_id_fk
+				references acs_objects
+				on delete cascade,
+				-- counter for number of accesses to this object
+				-- either from the permission() proc or from
+				-- reading in the server log file.
+	hit_count		integer,
+				-- Full Text Index
+	fti			tsvector,
+				-- For tables that don't respect the OpenACS object 
+				-- scheme we may get "object_id"s that start with 0.
+	primary key (object_id, object_type_id)
 );
 
-select acs_sc_impl_alias__new(
-           'FtsContentProvider',		-- impl_contract_name
-           'im_project',        		-- impl_name
-	   'datasource',			-- impl_operation_name
-	   'projects__datasource', 	    	-- impl_alias
-	   'TCL'				-- impl_pl
-);
-
-select acs_sc_impl_alias__new(
-           'FtsContentProvider',		-- impl_contract_name
-           'im_project',        		-- impl_name
-	   'url',				-- impl_operation_name
-	   'projects__url',			-- impl_alias
-	   'TCL'				-- impl_pl
-);
+create index im_search_objects_fti_idx on im_search_objects using gist(fti);
+create index im_search_objects_object_id_idx on im_search_objects (object_id);
 
 
-create function projects__itrg ()
-returns trigger as '
+
+
+create or replace function im_search_insert (integer, varchar, integer, varchar)
+returns integer as '
+declare
+	p_object_id	alias for $1;
+	p_object_type	alias for $2;
+	p_biz_object_id	alias for $3;
+	p_text		alias for $4;
+
+	v_object_type_id	integer;
+        ts2_result		varchar;
 begin
-    perform search_observer__enqueue(new.project_id,''INSERT'');
-    return new;
+	select	object_type_id
+	into	v_object_type_id
+	from	im_search_object_types
+	where	object_type = p_object_type;
+
+	insert into im_search_objects (
+		object_id,
+		object_type_id,
+		biz_object_id,
+		fti
+	) values (
+		p_object_id,
+		v_object_type_id,
+		p_biz_object_id,
+		to_tsvector(''default'', p_text)
+	);
+
+	return 0;
 end;' language 'plpgsql';
 
-create function projects__dtrg ()
-returns trigger as '
+
+create or replace function im_search_update (integer, varchar, integer, varchar)
+returns integer as '
+declare
+	p_object_id	alias for $1;
+	p_object_type	alias for $2;
+	p_biz_object_id	alias for $3;
+	p_text		alias for $4;
+
+	v_object_type_id	integer;
+        ts2_result		varchar;
 begin
-    perform search_observer__enqueue(old.project_id,''DELETE'');
-    return old;
+	select	object_type_id
+	into	v_object_type_id
+	from	im_search_object_types
+	where	object_type = p_object_type;
+
+	update im_search_objects set
+		object_type_id	= v_object_type_id,
+		biz_object_id	= p_biz_object_id,
+		fti		= to_tsvector(''default'', p_text)
+	where
+		object_id	= p_object_id;
+
+	return 0;
 end;' language 'plpgsql';
 
-create function projects__utrg ()
+
+
+create or replace function im_projects_tsearch_insert () 
 returns trigger as '
 begin
-    perform search_observer__enqueue(old.project_id,''UPDATE'');
-    return old;
+	perform im_search_insert(new.project_id, ''im_project'', 0, new.project_name);
+	return new;
+end;' language 'plpgsql';
+
+CREATE TRIGGER im_projects_tsearch_insert_tr 
+BEFORE INSERT ON im_projects
+FOR EACH ROW 
+EXECUTE PROCEDURE im_projects_tsearch_insert();
+
+
+
+create or replace function im_projects_tsearch_update () 
+returns trigger as '
+begin
+	perform im_search_update(new.project_id, ''im_project'', 0, new.project_name);
+	return new;
+end;' language 'plpgsql';
+
+CREATE TRIGGER im_projects_tsearch_update_tr 
+BEFORE UPDATE ON im_projects
+FOR EACH ROW 
+EXECUTE PROCEDURE im_projects_tsearch_update();
+
+
+
+
+
+
+-- select im_search_insert(9689, 'im_project', 0, 
+-- 'Installation of Project/Open at Tigerpond');
+
+
+
+
+CREATE TRIGGER im_search_objects_tr BEFORE UPDATE OR INSERT ON im_search_objects
+FOR EACH ROW EXECUTE PROCEDURE tsearch2(fti, 'projec test');
+
+create or replace function ts2_to_tsvector ( varchar, varchar ) 
+returns varchar as '
+declare
+	ts2_cfg alias for $1;
+	ts2_txt alias for $2;
+	ts2_result varchar;
+begin
+	perform set_curcfg(ts2_cfg);
+	select to_tsvector(ts2_cfg,ts2_txt) into ts2_result;
+	return ts2_result;
 end;' language 'plpgsql';
 
 
-create trigger projects__itrg after insert on im_projects
-for each row execute procedure projects__itrg (); 
 
-create trigger projects__dtrg after delete on im_projects
-for each row execute procedure projects__dtrg (); 
+create or replace function ts2_to_tsquery ( varchar, varchar ) 
+returns tsquery as '
+declare
+	ts2_cfg alias for $1;
+	ts2_txt alias for $2;
+	ts2_result tsquery;
+begin
+	perform set_curcfg(ts2_cfg);
+	select 1 into ts2_result;
+	select to_tsquery(ts2_cfg,ts2_txt) into ts2_result;
+	return ts2_result;
+end;' language 'plpgsql';
 
-create trigger projects__utrg after update on im_projects
-for each row execute procedure projects__utrg (); 
 
--- tcl/intranet-search-pg-procs.tcl
+++ tcl/intranet-search-pg-procs.tcl
@@ -6,50 +6,12 @@
 # http://www.project-open.com/license/ for details.
 
 ad_library {
-    This procs implements support for ftscontentprovider to intranet-core/projects
-    TODO:
-    	Add support to filestorage
-	Much more!
+    Support for searching P/O Objects
 	
-    @author pepels@gmail.com
+    @author frank.bergmann@project-open.com
 }
 
 
-
-ad_proc projects__datasource {
-    object_id
-} {
-    This proc returns the searchable data to be indexed
-    @author pepels@gmail.com
-} {
-    db_0or1row projects_datasource {
-	select p.project_id as object_id, 
-	       p.project_name as title, 
-	       p.description as content,
-	       'text/plain' as mime,
-	       '' as keywords,
-	       'text' as storage_type
-	from im_projects p
-	where project_id = :object_id
-    } -column_array datasource
-
-    return [array get datasource]
-}
-
-
-ad_proc projects__url {
-    object_id
-} {
-    This proc returns the url to the project which has project_id=object_id
-    @author pepels@gmail.com
-} {
-
-    set package_id [apm_package_id_from_key intranet-core]
-    db_1row get_url_stub "
-        select site_node__url(node_id) as url_stub
-        from site_nodes
-        where object_id=:package_id
-    "
 
     set url "${url_stub}projects/view?project_id=$object_id"
     return $url
