b84679f5d2c257b3762a2ee5b859522741c3a755
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-05-26 18:56:59 +0000

    - added script for company export view

---
+++ sql/postgresql/upgrade/upgrade-3.0.0.0.1-3.0.0.0.7.sql
@@ -0,0 +1,87 @@
+
+
+insert into im_views (view_id, view_name, visible_for, view_type_id)
+values (3, 'company_cvs', 'view_companies', 1400);
+
+
+--
+delete from im_view_columns where column_id >= 300 and column_id <= 399;
+--
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(301,3,NULL,'Company Name','$company_name','','',1,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(303,3,NULL,'Company Path','$company_path','','',3,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(305,3,NULL,'Company Type','$company_type','','',5,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(307,3,NULL,'Company Status','$company_status','','',7,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(309,3,NULL,'Contact Email','$company_contact_email','','',9,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(311,3,NULL,'Accounting Email','$accounting_contact_email','','',9,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(315,3,NULL,'Referral Source','$referral_source','','',15,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(317,3,NULL,'Note','$note','','',17,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(319,3,NULL,'Annual Revenue','$annual_revenue','','',19,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(321,3,NULL,'Billable','$billable_p','','',21,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(323,3,NULL,'VAT Nr','$vat_number','','',23,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(325,3,NULL,'Phone','$phone','','',25,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(327,3,NULL,'Fax','$fax','','',27,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(329,3,NULL,'Address Line1','$address_line1','','',29,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(331,3,NULL,'Address Line2','$address_line2','','',31,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(333,3,NULL,'City','$address_city','','',33,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(335,3,NULL,'Postal Code','$address_postal_code','','',35,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(337,3,NULL,'Country Code','$address_country_code','','',37,'');
+
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values 
+(339,3,NULL,'Note','"$company_note"','','',39,'');
+
+
