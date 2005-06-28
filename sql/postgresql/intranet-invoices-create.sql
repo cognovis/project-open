f80d4971131b889dfad31fa6c2a2464172f14820
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-06-28 20:37:19 +0000

    - fixed error with bad double "single quotes" in im_cost__new

--- sql/postgresql/intranet-invoices-create.sql
+++ sql/postgresql/intranet-invoices-create.sql
@@ -189,7 +189,7 @@ create or replace function im_invoice__new (
 returns integer as '
 declare
 	p_invoice_id		alias for $1;		-- invoice_id default null
-	p_object_type		alias for $2;		-- object_type default ''im_invoice''
+	p_object_type		alias for $2;		-- object_type default im_invoice
 	p_creation_date		alias for $3;		-- creation_date default now()
 	p_creation_user		alias for $4;		-- creation_user
 	p_creation_ip		alias for $5;		-- creation_ip default null
@@ -199,7 +199,7 @@ declare
 	p_provider_id		alias for $9;		-- provider_id
 	p_company_contact_id	alias for $10;		-- company_contact_id default null
 	p_invoice_date		alias for $11;		-- invoice_date now()
-	p_invoice_currency	alias for $12;		-- invoice_currency default ''EUR''
+	p_invoice_currency	alias for $12;		-- invoice_currency default EUR
 	p_invoice_template_id	alias for $13;		-- invoice_template_id default null
 	p_invoice_status_id	alias for $14;		-- invoice_status_id default 602
 	p_invoice_type_id	alias for $15;		-- invoice_type_id default 700
