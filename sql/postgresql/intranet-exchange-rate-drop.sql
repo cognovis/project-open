07865ecd9d9a6a727767429e8ef4b8ee9c90c92a
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-07-06 10:29:57 +0000

    Initial Import

---
+++ sql/postgresql/intranet-exchange-rate-drop.sql
@@ -0,0 +1,12 @@
+-- /packages/intranet-hr/sql/oracle/intranet-hr-drop.sql
+--
+-- ]project[ Exchange Rate Module
+-- Copyright (c) 2003-2004 Project/Open
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+-- @author frank.bergmann@project-open.com
+
+
+drop table im_exchange_rates;
+
