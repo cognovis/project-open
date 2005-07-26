4695b0ebfa6541d4ef4f2d15b511f933454ac564
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-07-26 13:03:29 +0000

    - now working...

---
+++ tcl/intranet-reporting-procs.tcl
@@ -0,0 +1,374 @@
+# /packages/intranet-reporting/tcl/intranet-reporting-procs.tcl
+#
+# Copyright (C) 2003-2004 Project/Open
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+ad_library {
+    Reporting Component Library
+    @author frank.bergmann@project-open.com
+}
+
+# -------------------------------------------------------
+# Package Procs
+# -------------------------------------------------------
+
+ad_proc -public im_package_reporting_id {} {
+    Returns the package id of the intranet-reporting module
+} {
+    return [util_memoize "im_package_reporting_id_helper"]
+}
+
+ad_proc -private im_package_reporting_id_helper {} {
+    return [db_string im_package_core_id {
+        select package_id from apm_packages
+        where package_key = 'intranet-reporting'
+    } -default 0]
+}
+
+
+# -------------------------------------------------------
+# Reporting Procs
+# -------------------------------------------------------
+
+ad_proc im_report_render_header {
+    -group_def
+    -last_value_array_list
+} {
+    Renders a single row in a project-open report. 
+    The procedure takes a report definition, an array of the
+    "last_values" (from the last row) and the current variables
+    via upvar and writes a report line to the page via ns_write.
+    Returns an array of the new values for the current row.
+} {
+    set group_level 1
+    ns_log Notice "render_header:"
+    ns_log Notice "render_header: last_value_array_list=$last_value_array_list"
+    array set last_value_array $last_value_array_list
+
+    while {[llength $group_def] > 0} {
+	
+	array set group_array $group_def
+	set group_var $group_array(group_by)
+	set header $group_array(header)
+	set content $group_array(content)
+	ns_log Notice "render_header: level=$group_level, group_var=$group_var"
+
+	# -------------------------------------------------------
+	# Determine last and new value for the current group group_level
+	set last_value ""
+	set new_value ""
+	if {$group_var != ""} {
+	    if {[info exists last_value_array($group_level)]} {
+		set last_value $last_value_array($group_level)
+	    }
+	    upvar $group_var $group_var
+	    
+	    if {![info exists $group_var]} {
+		ad_return_complaint 1 "Header: Level $group_level: Group var '$group_var' doesn't exist" 
+	    }
+	    set cmd "set new_value \"\$$group_var\""
+	    eval $cmd
+	    ns_log Notice "render_header: level=$group_level, last_value='$last_value', new_value='$new_value'"
+	}
+
+	# -------------------------------------------------------
+	# Write out the header if last_value != new_value
+	if {$content == "" || $new_value != $last_value} {
+	    ns_write "<tr class=roweven>\n"
+	    foreach field $header {
+		set value ""
+		if {"" != $field} {
+		    set cmd "set value \"$field\""
+		    set value [uplevel 1 $cmd]
+		}
+		ns_write "<td>$value</td>\n"
+	    }
+	    ns_write "</tr>\n"
+	}
+
+	# -------------------------------------------------------
+	# Set the "old_var" to the currently new var
+	set last_value_array($group_level) $new_value
+
+	# -------------------------------------------------------
+	# Prepare the next iteration of the while loop:
+	# continue with the "row" part of the current level
+	set group_def {}
+	if {[info exists group_array(content)]} {
+	    set group_def $group_array(content)
+	}
+	incr group_level
+
+    }
+    ns_log Notice "render_header: after group_by headers"
+
+    return [array get last_value_array]
+}
+
+
+
+
+ad_proc im_report_render_footer {
+    -group_def
+    -last_value_array_list
+} {
+    Renders the footer stack of a single row in a project-open report. 
+    The procedure acts similar to im_report_render_header,
+    but returns a list of results instead of writing the results
+    to the web page immediately.
+    This is done, because the decision what footer lines to display
+    can only be taken when the next row is displayed.
+    Returns a list of report lines, each together with the group_var.
+    A group_var with a value different from the current one is the
+    trigger to display each line.
+} {
+    ns_log Notice "render_footer:"
+    array set last_value_array $last_value_array_list
+
+    # Split group_def and assign to an array for reverse access
+    set group_level 1
+    while {[llength $group_def] > 0} {
+	set group_def_array($group_level) $group_def
+	ns_log Notice "render_footer: group_def_array($group_level) = ..."
+	array set group_array $group_def
+        set group_def {}
+        if {[info exists group_array(content)]} {
+            set group_def $group_array(content)
+        }
+        incr group_level
+    }
+    set group_level [expr $group_level - 1]
+
+    while {$group_level > 0} {
+	ns_log Notice "render_footer: level=$group_level"
+	array set group_array $group_def_array($group_level)
+	set group_var $group_array(group_by)
+	set footer $group_array(footer)
+	set content $group_array(content)
+
+        # -------------------------------------------------------
+        # Determine the new value for the current group_level
+        set new_value ""
+        if {$group_var != ""} {
+            upvar $group_var $group_var
+            if {![info exists $group_var]} {
+                ad_return_complaint 1 "Header: Level $group_level: Group var '$group_var' doesn't exist"
+            }
+            set cmd "set new_value \"\$$group_var\""
+            eval $cmd
+            ns_log Notice "render_header: level=$group_level, new_value='$new_value'"
+        }
+
+	# -------------------------------------------------------
+	# Write out the footer to an array
+	set footer_line [list]
+	foreach field $footer {
+	    set value ""
+	    if {"" != $field} {
+		set cmd "set value \"$field\""
+		set value [uplevel 1 $cmd]
+	    }
+	    lappend footer_line $value
+	}
+	set footer_record [list \
+	    line $footer_line \
+	    new_value $new_value
+	]
+	# Store the result for display later
+	set footer_array($group_level) $footer_record
+
+	set group_level [expr $group_level - 1]
+    }
+    ns_log Notice "render_footer: after group_by footers"
+
+    return [array get footer_array]
+}
+
+
+
+
+ad_proc im_report_display_footer {
+    -group_def
+    -footer_array_list
+    -last_value_array_list
+    {-display_all_footers_p 0}
+} {
+    Display the footer stack of a single row in a project-open report. 
+} {
+    ns_log Notice "display_footer:"
+    array set last_value_array $last_value_array_list
+    array set footer_array $footer_array_list
+
+    # -------------------------------------------------------
+    # Abort if there are no footer values, because this
+    # is probably the first time that this routine is executed
+    if {[llength $footer_array_list] == 0} {
+	return
+    }
+
+    set group_def_org $group_def
+
+    # -------------------------------------------------------
+    # Determine the "return_group_level" to which we have to go _back_.
+    # This level determines the number of footers that we need to write out.
+    #
+    set return_group_level 1
+    while {[llength $group_def] > 0} {
+
+	array set group_array $group_def
+	set group_var $group_array(group_by)
+	set header $group_array(header)
+	set content $group_array(content)
+	ns_log Notice "display_footer: level=$return_group_level, group_var=$group_var"
+
+	# -------------------------------------------------------
+	# 
+	set footer_record_list $footer_array($return_group_level)
+	array set footer_record $footer_record_list
+	set new_record_value $footer_record(new_value)
+
+	# -------------------------------------------------------
+	# Determine new value for the current group return_group_level
+	set new_value ""
+	if {$group_var != ""} {
+	    upvar $group_var $group_var
+	    set cmd "set new_value \"\$$group_var\""
+	    eval $cmd
+	}
+
+	# -------------------------------------------------------
+	# Check if new_value != new_record_value.
+	# In this case we have found the first level in which the
+	# results differ. This is the level where we have to return.
+	ns_log Notice "display_footer: level=$return_group_level, group_var=$group_var, new_record_value=$new_record_value, new_value=$new_value"
+	if {![string equal $new_value $new_record_value]} {
+	    # leave the while loop
+	    break
+	}
+
+	# -------------------------------------------------------
+	# Prepare the next iteration of the while loop:
+	# continue with the "row" part of the current level
+	set group_def {}
+	if {[info exists group_array(content)]} {
+	    set group_def $group_array(content)
+	}
+	incr return_group_level
+
+    }
+
+    # Restore the group_def destroyed by the previous while loop
+    set group_def $group_def_org
+
+
+    # -------------------------------------------------------
+    # Calculate the maximum level in the report definition
+    set max_group_level 1
+    while {[llength $group_def] > 0} {
+	set group_def_array($max_group_level) $group_def
+	ns_log Notice "display_footer: group_def_array($max_group_level) = ..."
+	array set group_array $group_def
+        set group_def {}
+        if {[info exists group_array(content)]} {
+            set group_def $group_array(content)
+        }
+        incr max_group_level
+    }
+    set max_group_level [expr $max_group_level - 2]
+
+
+    if {$display_all_footers_p} { set return_group_level 1 }
+
+    # -------------------------------------------------------
+    # Now let's display all footers between max_group_level and
+    # return_group_level.
+    #
+    ns_log Notice "display_footer: max_group_level=$max_group_level, return_group_level=$return_group_level"
+    for {set group_level $max_group_level} { $group_level >= $return_group_level} { set group_level [expr $group_level-1]} {
+
+	# -------------------------------------------------------
+	# Extract the footer_line
+	#
+	set footer_record_list $footer_array($group_level)
+	array set footer_record $footer_record_list
+	set new_record_value $footer_record(new_value)
+	set footer_line $footer_record(line)
+
+	# -------------------------------------------------------
+	# Write out the header if last_value != new_value
+
+	ns_log Notice "display_footer: writing footer for group_level=$group_level"
+	ns_write "<tr class=rowodd>\n"
+	foreach field $footer_line {
+	    ns_write "<td>$field</td>\n"
+	}
+	ns_write "</tr>\n"
+
+    }
+}
+
+
+
+
+
+ad_proc im_report_update_counters {
+    -counters
+} {
+    Takes a definition of the report counters
+    and update the counter values according to
+    the variables in the parent frame
+} {
+    upvar counter_sum counter_sum
+    upvar counter_count counter_count
+    upvar counter_reset counter_reset
+
+    foreach counter_list $counters {
+	array set counter $counter_list
+	set pretty_name $counter(pretty_name)
+	set reset $counter(reset)
+	set var $counter(var)
+	set expr $counter(expr)
+
+	# Reset the counter if necessary
+	set last_reset ""
+	set reset_performed 0
+	if {[info exists counter_reset($var)]} { 
+	    set last_reset $counter_reset($var) 
+	}
+
+	set cmd "set reset_val \[expr $reset\]"
+	set reset_val [uplevel 1 $cmd]
+
+	set cmd "set expr_val \[expr $expr\]"
+	set expr_val [uplevel 1 $cmd]
+
+	if {$last_reset != $reset_val} {
+	    set counter_sum($var) 0
+	    set counter_count($var) 0
+	    set counter_reset($var) $reset_val
+	    set reset_performed 1
+	}
+
+	# Update the counter
+	set last_sum 0
+	if {[info exists counter_sum($var)]} { 
+	    set last_sum $counter_sum($var) 
+	}
+
+	set last_count $counter_count($var)
+	set last_sum [expr $last_sum + $expr_val]
+
+
+	incr last_count
+	set counter_sum($var) $last_sum
+	set counter_count($var) $last_count
+
+	# Store the counter result in a local variable,
+	# so that the row expressions can access it
+	upvar $var $var
+	set $var $last_sum
+    }
+}
+
--- www/report.tcl
+++ www/report.tcl
@@ -5,175 +5,172 @@ ad_page_contract {
 }
 
 set user_id [ad_maybe_redirect_for_registration]
-set page_title "Backup"
+set page_title "Invoices Report"
 set context_bar [im_context_bar $page_title]
 set context ""
 set today [db_string today "select to_char(sysdate, 'YYYYMMDD.HHmm') from dual"]
-set user_admin_p [im_is_user_site_wide_or_intranet_admin $user_id]
-if {!$user_admin_p} {
-    ad_return_complaint 1 "<li>You have insufficient privileges to see this page"
-    return
-}
+
+# ------------------------------------------------------------
+# Define the report - SQL, counters, headers and footers 
+#
+
+set sql "
+    select
+	ic.*,
+	ii.*,
+	coalesce(ic.paid_amount, 0) as paid_amount,
+	to_char(ic.effective_date, 'YYYY-MM') as effective_month,
+	to_char(ic.effective_date, 'YYYY-MM-DD') as effective_date_formatted,
+	cust.company_name as customer_name,
+	prov.company_name as provider_name
+    from
+	im_costs ic,
+	im_invoices ii,
+	im_companies cust,
+	im_companies prov
+    where
+	ic.cost_id = ii.invoice_id
+	and ic.customer_id = cust.company_id
+	and ic.provider_id = prov.company_id
+    order by
+	to_char(ic.effective_date, 'YYYY-MM'),
+	lower(cust.company_name)
+
+"
+
+set amount_counter [list \
+	pretty_name Amount \
+	var amount_subtotal \
+	reset \$customer_id \
+	expr \$amount
+]
+
+set paid_counter [list \
+	pretty_name Paid \
+	var paid_subtotal \
+	reset \$customer_id \
+	expr \$paid_amount
+]
+
+set counters [list \
+	$amount_counter \
+	$paid_counter \
+]
+
+
+
+set row {$effective_date_formatted $customer_name $provider_name "$amount $currency" "$paid_amount $paid_currency" "row"}
+
+set content3 [list  \
+    header $row  \
+    group_by "" \
+    content {} \
+]
+
+set header2 {"$effective_month" "$customer_name" "" "" "" "header2" }
+set footer2 {"$effective_month" "" "" "\$amount_subtotal" "" "footer2" }
+set content2 [list  \
+    header $header2 \
+    footer $footer2 \
+    group_by customer_name \
+    content $content3
+]
+
+set header1 {"" "" "" "" "" "header1" }
+set footer1 {"" "" "" "" "" "footer1" }
+set report_def [list \
+    header $header1 \
+    footer $footer1 \
+    group_by effective_month \
+    content $content2 \
+]
+
+# Global header/footer
+set header0 {"Date" "Customer" "Provider" "Amount" "Paid" "header0"}
+set footer0 {"" "" "" "" "" "footer0"}
+
 
 
 # ------------------------------------------------------------
-# Return the page header.
+# Start formatting the page
 #
 
-ad_return_top_of_page "[im_header]\n[im_navbar]"
-ns_write "<H1>$page_title</H1>\n"
-
-set joined_ids [join [array names view] ","]
-
-set sql "
-select
-	v.*
-from 
-	im_views v
-where 
-	v.view_id in ($joined_ids)
+ad_return_top_of_page "
+[im_header]
+[im_navbar]
 "
+ns_write "<H1>$page_title</H1>\n"
+ns_write "<table border=0 cellspacing=1 cellpadding=1>\n"
 
-    # Get the Backup SQL
-    #
-    set rows [db_0or1row get_backup_info "
-select
-	view_sql as backup_sql,
-	view_name
-from
-	im_views
-where
-	view_id = 1004
-"]
-
-
-	#init counters
-	set total_month_0409 ""
-	set total_month_0410 ""
-	set total_month_0411 ""
-	set total_month_0504 ""
-	set total_month_0505 ""
-	set total_month_0506 ""
-	
-	# Get counters
-	set counters(month_0409) {set total_month_0409 [expr $total_month_0409 + $month_0409]}  
-	set counters(month_0410) {set total_month_0410 [expr $total_month_0410 + $month_0410]}
-	set counters(month_0411) {set total_month_0411 [expr $total_month_0411 + $month_0411]}  
-	set counters(month_0504) {set total_month_0504 [expr $total_month_0504 + $month_0504]}  
-	set counters(month_0505) {set total_month_0505 [expr $total_month_0505 + $month_0505]}  
-	set counters(month_0506) {set total_month_0506 [expr $total_month_0506 + $month_0506]} 
-	set counter_list [array names counters]
-	set counter_group due_month
-
-    # Define the column headers and column contents that
-    # we want to show:
-    #
-    set column_sql "
-select
-	column_name,
-	column_render_tcl,
-	visible_for
-from
-	im_view_columns
-where
-	view_id=1004
-	and group_id is null
-order by
-	sort_order"
-
-    set column_headers [list]
-    set column_vars [list]
-    set header ""
-    set row_ctr 0
-    db_foreach column_list_sql $column_sql {
-		lappend column_headers "$column_name"
-		lappend column_vars "$column_name"
-
-		append header "<th>$column_name</th>"
-		incr row_ctr
+ns_write "<tr class=rowtitle>\n"
+foreach field $header0 {
+    set value ""
+    if {"" != $field} {
+	set cmd "set value \"$field\""
+	eval "$cmd"
     }
+    ns_write "<td>$value</td>\n"
+}
+ns_write "</tr>\n"
 
-# Execute the query
+
+set amount_subtotal 1
+
+# Initialize the footer array:
+# We have to display this array with the _next_ row,
+# because the _display_ decision depends on the values
+# of the next row.
+# In contrast, the render information is related to
+# the previous row, so that the footer needs to be
+# rendered in the same db_forach iteration as its row.
 #
-set bgcolor(0) " class=roweven "
-set bgcolor(1) " class=rowodd "
-set ctr 0
-set results ""
-db_foreach projects_info_query $backup_sql {
+set footer_array_list [list]
 
-		if { [info exists old_group_value] } {
-			set group_value [set $counter_group]
-			ns_log WARNING "$old_group_value != $group_value"
-					
-			if { $old_group_value != $group_value } {
-				append results "<tr>$sub_total_row</tr>\n"
-			}		
-		}
-		
+db_foreach sql $sql {
 
-		# Append a line of data based on the "column_vars" parameter list
-		set row_ctr 0
-		append results "<tr$bgcolor([expr $ctr % 2])>\n"
-		set sub_total_row ""
-		foreach column_var $column_vars {
-			append results "\t<td valign=top>"
-			set cmd "append results [set $column_var]"
-			eval $cmd
-			
-			incr row_ctr
-			set next_row ""
-			
-			# check counters
-			if { [lsearch -exact $counter_list $column_var] != "-1" } {
-				set cmd $counters($column_var)
-				eval $cmd
-				set total_value [set total_$column_var]			
-				if { [info exists old_group_value] } {
-					set group_value [set $counter_group]
-					if { $old_group_value != $group_value } {
-						#reset total
-						set total_$column_var ""
-					}		
-				}
+    im_report_display_footer \
+	-group_def $report_def \
+        -footer_array_list $footer_array_list \
+	-last_value_array_list [array get group_by_last_value]
 
-				append sub_total_row "<td>subtotal $column_var: $total_value</td>"
-			} else {
-				append sub_total_row "<td>&nbsp;</td>"
-			}
-			append results "</td>\n"
-		
-		}
+    im_report_update_counters -counters $counters
 
+    set value_list_header [im_report_render_header \
+	-group_def $report_def \
+	-last_value_array_list [array get group_by_last_value] \
+    ]
+    ns_log Notice "report: value_list_header=$value_list_header"
+    array set group_by_last_value $value_list_header
 
-		#store row for counter reset and print
-	    set old_group_value [set $counter_group]
-	    
-	    append results "</tr>\n"
-		incr ctr
+    set footer_array_list [im_report_render_footer \
+	-group_def $report_def \
+	-last_value_array_list [array get group_by_last_value] \
+    ]
+    array set footer_array $footer_array_list
+
 }
 
-#add last subtotal
-set group_value [set $counter_group]
-ns_log WARNING "$old_group_value != $group_value"
-append results "<tr>$sub_total_row</tr>\n"
+im_report_display_footer \
+    -group_def $report_def \
+    -footer_array_list $footer_array_list \
+    -last_value_array_list [array get group_by_last_value] \
+    -display_all_footers_p 1
 
 
-ns_write "<table><tr>$header</tr> $results </table>"
+
+ns_write "<tr class=rowtitle>\n"
+foreach field $footer0 {
+    set value ""
+    if {"" != $field} {
+	set cmd "set value \"$field\""
+	eval "$cmd"
+    }
+    ns_write "<td>$value</td>\n"
+}
+ns_write "</tr>\n"
 
 
-ns_write "
-Successfully finished
-<pre>$backup_sql
-
-
-$total_month_0409
-$total_month_0410
-$total_month_0411
-$total_month_0504
-$total_month_0505
-$total_month_0506
-</pre>
-"
+ns_write "<table>\n"
 
 ns_write [im_footer]
 
