2501c3d99a9b72d25b422be153a11405fa638034
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-09-06 14:08:11 +0000

    - disabled spambayes spam processing for intranet-mail-import - added version V3.0.0.6 to ASUS

--- tcl/intranet-mail-import-procs.tcl
+++ tcl/intranet-mail-import-procs.tcl
@@ -215,7 +215,11 @@ namespace eval im_mail_import {
 
 	    set spam_header ""
 	    if {[info exists email_headers(x-spambayes-classification)]} {
-		set spam_header $email_headers(x-spambayes-classification)
+
+# Temporarily disabled spam - until
+# Spambayes is trained correctly.
+#
+#		set spam_header $email_headers(x-spambayes-classification)
 		ns_log Notice "im_mail_import.process_mails: spam_header=$spam_header"
 	    } else {
 		ns_log Notice "im_mail_import.process_mails: No spam header found"
