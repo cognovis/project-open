9105beede40d68c89211aaf6363afe950e0eff1a
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-11-23 12:30:05 +0000

    - removed RegistrationProvidesRandomPasswordP and generated_pwd_p conditions   from the OR clause responsible for sending out registration mails to   users.

--- tcl/local-procs.tcl
+++ tcl/local-procs.tcl
@@ -450,9 +450,12 @@ ad_proc -private auth::local::registration::Register {
 
     # LARS TODO: Move this out of the local driver and into the auth framework
     # Send password confirmation email to user
-    if { $generated_pwd_p || \
-             [parameter::get -parameter RegistrationProvidesRandomPasswordP -package_id [ad_conn subsite_id] -default 0] || \
-             [parameter::get -parameter EmailRegistrationConfirmationToUserP -package_id [ad_conn subsite_id] -default 0] } {
+
+    # Fraber: 051123: remove the "$generated_pwd_p || \" and
+    #  [parameter::get -parameter RegistrationProvidesRandomPasswordP -package_id [ad_conn subsite_id] -default 0] || 
+    # Because we don't want to send people a mail if we register them
+    # for CRM
+    if { [parameter::get -parameter EmailRegistrationConfirmationToUserP -package_id [ad_conn subsite_id] -default 0] } {
 
 	with_catch errmsg {
             auth::password::email_password \
