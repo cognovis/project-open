37935d7cb1e4ea6c8753c6a894cc7332aa12b563
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-04-09 20:33:18 +0000

    - First steps towards making im_timesheet_tasks a subtype of Project

--- sql/common/intranet-categories.sql
+++ sql/common/intranet-categories.sql
@@ -338,11 +338,18 @@ insert into im_category_hierarchy values (81,83);
 
 
 -- Project Type
+insert into im_categories (CATEGORY_ID, CATEGORY, CATEGORY_TYPE) values 
+('84', 'Project Task', 'Intranet Project Type');
 insert into im_categories ( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE) values 
 ('', 'f', '85', 'Unknown', 'Intranet Project Type');
 insert into im_categories ( CATEGORY_DESCRIPTION, ENABLED_P, CATEGORY_ID, CATEGORY, CATEGORY_TYPE) values 
 ('', 'f', '86', 'Other', 'Intranet Project Type');
+
+
 -- 87 - 97 reserved for Translation
+-- 97 - Strategic Consulting  	Consulting Project 	
+-- 98 - Software Maintenance 		
+-- 99 - Software Development 
 
 
 -- Hiring Source
