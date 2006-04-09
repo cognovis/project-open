7233ce66d08b4f342172b2a29a590904420660ed
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-04-09 20:33:44 +0000

    - Unifying Projects and TimesheetTask

---
+++ sql/postgresql/upgrade/upgrade-3.1.3.0.0-3.2.0.0.0.sql
@@ -0,0 +1,29 @@
+
+-------------------------------------------------------------
+-- Updates to upgrade to the "unified" V3.2 model where
+-- Task is a subclass of Project.
+-------------------------------------------------------------
+
+-- Drop the generic uniquentess constraint on project_nr.
+alter table im_projects drop constraint im_projects_nr_un;
+
+-- Create a new constraing that makes sure that the project_nr
+-- are unique per parent-project.
+-- Project with parent_id != null don't have a filestorage...
+--
+
+
+alter table im_projects drop constraint im_projects_nr_unn;
+alter table im_projects add
+        constraint im_projects_nr_unn
+        unique(parent_id, project_nr);
+
+
+-- Add a new category for the project_type.
+-- Puff, difficult to find one while maintaining compatible
+-- the the fixed IDs from ACS 3.4 Intranet...
+--
+insert into im_categories (CATEGORY_ID, CATEGORY, CATEGORY_TYPE) 
+values ('84', 'Project Task', 'Intranet Project Type');
+
+
