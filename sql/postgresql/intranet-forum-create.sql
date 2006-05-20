209f66819e40711e51944f27ad63596c6aaf67c4
dotproj <>
2006-05-20 10:55:14 +0000

    - fixed a HTML tag closing issue

--- sql/postgresql/intranet-forum-create.sql
+++ sql/postgresql/intranet-forum-create.sql
@@ -498,7 +498,7 @@ SELECT im_component_plugin__new (
         null,                           -- view_name
         10,                             -- sort_order
 	'im_forum_component -user_id $user_id -forum_object_id $project_id -current_page_url $current_url -return_url $return_url -forum_type "project" -export_var_list [list project_id forum_start_idx forum_order_by forum_how_many forum_view_name] -view_name [im_opt_val forum_view_name] -forum_order_by [im_opt_val forum_order_by] -start_idx [im_opt_val forum_start_idx] -restrict_to_mine_p "f" -restrict_to_new_topics 0',
-	'im_forum_create_bar "<B>[_ intranet-forum.Forum_Items]<B>" $project_id $return_url'
+	'im_forum_create_bar "<B>[_ intranet-forum.Forum_Items]</B>" $project_id $return_url'
 );
 
 
