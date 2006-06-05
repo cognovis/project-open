f4fdd55f3f724b3590f67a4f32e73e167341c9e2
dotproj <>
2006-06-05 20:46:08 +0000

    - added upgrade file

---
+++ sql/postgresql/upgrade/upgrade-3.2.0.0.0-3.2.1.0.0.sql
@@ -0,0 +1,17 @@
+-- upgrade-3.2.0.0.0-3.2.1.0.0.sql
+
+
+-----------------------------------------------------------
+-- Copy 'Intranet Project Type' Category into the range
+-- of 4000-4099
+-----------------------------------------------------------
+
+-- 2006-05-28: Not possible: The static WF uses the hard
+-- coded Intranet Project Type values...
+
+
+
+-- Set the counter for the next categories to above the fixed
+-- category range.
+
+
