c3cd9d3d3408aa886b919247f37d71aa980d5cec
arvato <>
2006-06-28 08:22:40 +0000

    - fixed error showing layers in IE6

--- www/js/showhide.js
+++ www/js/showhide.js
@@ -8,6 +8,13 @@ function initPortlet(){
 };
 
 function ShowLayer(id, action){
+
+  if (navigator.userAgent.indexOf("Opera")!=-1
+	&& document.getElementById) type="OP";
+  if (document.all) type="IE";
+  if (document.layers) type="NN";
+  if (!document.all && document.getElementById) type="MO";
+
   if (type=="IE") eval("document.all." + id + ".style.visibility='" + action + "'");
   if (type=="NN") eval("document." + id + ".visibility='" + action + "'");
   if (type=="MO" || type=="OP")
