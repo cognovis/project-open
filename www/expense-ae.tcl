166c1d7ff649805a2f245374f14363dd78d28a11
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-07-22 16:44:52 +0000

    - first version of ad_form'ed expense form

--- www/expense-ae.tcl
+++ www/expense-ae.tcl
@@ -22,6 +22,7 @@ ad_page_contract {
     { return_url "/intranet-expenses/"}
     expense_id:integer,optional
     expense_amount:float,optional
+    expense_date:optional
     {form_mode "edit"}
 }
 
@@ -40,6 +41,9 @@ set action_url "new"
 set page_title [lang::message::lookup "" intranet-expenses.New_Expense "New Expense Item"]
 set context_bar [im_context_bar $page_title]
 set date_format "YYYY-MM-DD"
+
+set action_url "/intranet-expenses/expenses-ae"
+
 
 if {"" != $project_id} {
     append return_url "index?[export_vars -url project_id]"
@@ -82,12 +86,6 @@ set expense_type_options [db_list_of_lists expense_types "
 	from im_expense_type
 "]
 
-# ------------------------------------------------------------------
-# Build the form
-# ------------------------------------------------------------------
-
-set form_id "expense_ae"
-set focus "$form_id\.var_name"
 
 set include_empty 0
 set currency_options [im_currency_options $include_empty]
@@ -107,115 +105,88 @@ set expense_payment_type_options [db_list_of_lists expense_payment_type "
 set expense_payment_type_options [linsert $expense_payment_type_options 0 [list [lang::message::lookup "" "intranet-expenses.--Select--" "-- Please Select --"] 0]]
 
 
-template::form::create $form_id \
-    -cancel_url "$return_url" \
-    -mode "$form_mode" \
-    -view_buttons [list [list "[_ intranet-core.Back]" back]]
-
-element::create $form_id expense_id \
-    -widget hidden \
-    -optional
-
-element::create $form_id project_id \
-    -optional \
-    -datatype integer \
-    -widget select \
-    -options $project_options \
-    -label [lang::message::lookup "" intranet-expenses.Project "Project"]
-set project_id_value [template::element::get_value $form_id project_id]
-if {"" == $project_id_value} {
-    template::element::set_value $form_id project_id $project_id
-}
 
 
-element::create $form_id expense_amount \
-    -datatype text \
-    -widget text \
-    -html {size 10} \
-    -label "[_ intranet-expenses.Amount]"
-
-element::create $form_id expense_currency \
-    -datatype text \
-    -widget select \
-    -options $currency_options \
-    -label "[_ intranet-expenses.Currency]"
-
-element::create $form_id vat_included \
-    -datatype text \
-    -widget text \
-    -html {size 6} \
-    -label "[_ intranet-expenses.Vat_Included]"
-template::element::set_value $form_id vat_included 0
-
-element::create $form_id expense_date \
-    -datatype date \
-    -widget text \
-    -html {size 10} \
-    -label "[_ intranet-expenses.Expense_Date]"
-template::element::set_value $form_id expense_date $today
-
-element::create $form_id external_company_name \
-    -datatype text \
-    -widget text \
-    -html {size 40} \
-    -label "[_ intranet-expenses.External_company_name]"
-
-element::create $form_id external_company_vatnr \
-    -optional \
-    -datatype text \
-    -widget text \
-    -html {size 20} \
-    -label "[_ intranet-expenses.External_Company_VatNr]"
-
-element::create $form_id receipt_reference \
-    -optional \
-    -datatype text \
-    -widget text \
-    -html {size 40} \
-    -label "[_ intranet-expenses.Receipt_reference]"
-
-element::create $form_id expense_type_id \
-    -datatype integer \
-    -widget select \
-    -options $expense_type_options \
-    -label "[_ intranet-expenses.Expense_Type]"
+# ------------------------------------------------------------------
+# Form defaults
+# ------------------------------------------------------------------
 
 
-element::create $form_id billable_p \
-    -datatype text \
-    -widget radio \
-    -options {{yes t} {no f}}\
-    -label "[_ intranet-expenses.Billable_p]"
-template::element::set_value $form_id billable_p "f"
+# if {"" == $reimbursable_value} {
+#     template::element::set_value $form_id reimbursable "100"
+# }
 
 
-template::element::set_value $form_id billable_p "f"
+# !!! set expense_date_value [template::element::get_value $form_id expense_date]
+# if {"" == $expense_date_value} {
+#     template::element::set_value $form_id expense_date $today
+# }
 
-element::create $form_id reimbursable \
-    -datatype text \
-    -widget text \
-    -html {size 10} \
-    -label "[_ intranet-expenses.reimbursable]"
-set reimbursable_value [template::element::get_value $form_id reimbursable]
-if {"" == $reimbursable_value} {
-    template::element::set_value $form_id reimbursable "100"
-}
+# set expense_payment_type_value [template::element::get_value $form_id expense_payment_type_id]
+# if {"" == $expense_payment_type_value} {
+#     template::element::set_value $form_id expense_payment_type_id [im_expense_payment_type_cash]
+# }
 
 
-template::element::set_value $form_id reimbursable 100
 
-element::create $form_id expense_payment_type_id \
-    -datatype integer \
-    -widget select \
-    -options $expense_payment_type_options \
-    -label "[_ intranet-expenses.Expense_Payment_Type]"
+# ------------------------------------------------------------------
+# Build the form
+# ------------------------------------------------------------------
 
-element::create $form_id note \
-    -optional \
-    -datatype text \
-    -widget textarea \
-    -html {cols 40} \
-    -label "[_ intranet-expenses.Note]"
+set form_id "expense_ae"
+set focus "$form_id\.var_name"
+
+ad_form \
+    -name $form_id \
+    -cancel_url $return_url \
+    -action $action_url \
+    -mode $form_mode \
+    -export {return_url} \
+    -form {
+        expense_id:key
+        {project_id:text(select) 
+	    {label "[lang::message::lookup {} intranet-expenses.Project Project]" } 
+	    {options $project_options}
+	}
+	{expense_amount:text(text) {label "[_ intranet-expenses.Amount]"} {html {size 10}}}
+	{expense_currency:text(select) 
+	    {label "[_ intranet-expenses.Currency]"}
+	    {options $currency_options} 
+	}
+	{vat_included:text(text) {label "[_ intranet-expenses.Vat_Included]"} {html {size 60}}}
+	{expense_date:text(text) {label "[_ intranet-expenses.Expense_Date]"} {html {size 10}}}
+	{external_company_name:text(text) {label "[_ intranet-expenses.External_company_name]"} {html {size 40}}}
+	{external_company_vatnr:text(text) {label "[_ intranet-expenses.External_Company_VatNr]"} {html {size 20}}}
+	{receipt_reference:text(text) {label "[_ intranet-expenses.Receipt_reference]"} {html {size 40}}}
+	{expense_type_id:text(select) 
+	    {label "[_ intranet-expenses.Expense_Type]"}
+	    {options $expense_type_options} 
+	}
+        {billable_p:text(radio) {label "[_ intranet-expenses.Billable_p]"} {options {{[_ intranet-core.Yes] t} {[_ intranet-core.No] f}}} }
+	{reimbursable:text(text) {label "[_ intranet-expenses.reimbursable]"} {html {size 10}}}
+	{expense_payment_type_id:text(select) 
+	    {label "[_ intranet-expenses.Expense_Payment_Type]"}
+	    {options $expense_payment_type_options} 
+	}
+        {note:text(textarea),optional {label "[_ intranet-expenses.Note]"} {html {cols 40}}}
+    }
+
+
+
+#    check conditions
+#    if {![empty_string_p $vat_included]} {
+#        if {0>$vat_included || 100<$vat_included} {
+#            template::element::set_error $form_id vat_included "[_ intranet-expenses.vat_included_not_valid]"
+#            incr n_errors
+#        }
+#    }
+
+#    if {![empty_string_p $reimbursable]} {
+#        if {0>$reimbursable || 100<$reimbursable} {
+#            template::element::set_error $form_id reimbursable "[_ intranet-expenses.reimbursable_not_valid]"
+#            incr n_errors
+#        }
+#    }
 
 
 
@@ -232,11 +203,15 @@ set expense_id_exists [exists_and_not_null expense_id]
 
 
 # ------------------------------------------------------------------
-# Set editing variables
+# Form Actions
 # ------------------------------------------------------------------
 
-if {!$is_submission && [exists_and_not_null expense_id]} {
-    db_0or1row expense_info "
+ad_form -extend -name $form_id -on_request {
+    # Populate elements from local variables
+} 
+
+ad_form -extend -name $form_id -select_query {
+    
 	select	*,
 		to_char(c.effective_date, :date_format) as expense_date
 	from	im_costs c,
@@ -244,88 +219,38 @@ if {!$is_submission && [exists_and_not_null expense_id]} {
 	where
 		c.cost_id = :expense_id
 		and c.cost_id = e.expense_id
-    "
 
-    # Reconstruct the amount from the fractioned amount
-    # including VAT
-    set expense_amount [format %.2f [expr $amount * [expr 1 + [expr $vat_included / 100]]]]
-
-    # Returns the list of all element variables
-    set elements [template::form::get_elements $form_id]
-
-    foreach elem $elements {
-	if {[info exists $elem]} {
-	    set elem_val [expr "\$$elem"]
-	    template::element::set_value $form_id $elem $elem_val
-	}
-    }
 }
 
+# Reconstruct the amount from the fractioned amount
+# including VAT
+# !!! set expense_amount [format %.2f [expr $amount * [expr 1 + [expr $vat_included / 100]]]]
 
 
-# ------------------------------------------------------------------
-# Set default variables if New
-# ------------------------------------------------------------------
-
-set expense_date_value [template::element::get_value $form_id expense_date]
-if {"" == $expense_date_value} {
-    template::element::set_value $form_id expense_date $today
-}
-
-set expense_payment_type_value [template::element::get_value $form_id expense_payment_type_id]
-if {"" == $expense_payment_type_value} {
-    template::element::set_value $form_id expense_payment_type_id [im_expense_payment_type_cash]
-}
+ad_form -extend -name $form_id -new_data {
+    
+    db_exec_plsql cost_center_insert {}
+    
+} 
 
 
-# ------------------------------------------------------------------
-# is_request
-# ------------------------------------------------------------------
+ad_form -extend -name $form_id -edit_data {
+    
+    db_dml cost_center_update "
+        update im_cost_centers set
+                cost_center_name        = :cost_center_name,
+                cost_center_label       = :cost_center_label,
+                cost_center_code        = :cost_center_code,
+                cost_center_type_id     = :cost_center_type_id,
+                cost_center_status_id   = :cost_center_status_id,
+                department_p            = :department_p,
+                parent_id               = :parent_id,
+                manager_id              = :manager_id,
+                description             = :description
+        where
+                cost_center_id = :cost_center_id
+"
 
-if {[form is_request $form_id]} {
-    ns_log notice "is request"
-
-    form get_values $form_id
-    if {[exists_and_not_null expense_id]} {
-	# get db values for current expense
-
-    }
-    template::element::set_value $form_id expense_currency $currency
-}
-
-# ------------------------------------------------------------------
-# is_submission
-# ------------------------------------------------------------------
-
-if {[form is_submission $form_id]} {
-    form get_values $form_id
-    #check conditions
-    set n_errors 0
-    if {![empty_string_p $vat_included]} {
-        if {0>$vat_included || 100<$vat_included} {
-            template::element::set_error $form_id vat_included "[_ intranet-expenses.vat_included_not_valid]"
-            incr n_errors
-        }
-    }
-
-    if {![empty_string_p $reimbursable]} {
-        if {0>$reimbursable || 100<$reimbursable} {
-            template::element::set_error $form_id reimbursable "[_ intranet-expenses.reimbursable_not_valid]"
-            incr n_errors
-        }
-    }
-    if {0 < $n_errors} {
-	return
-    }
-}
-
-# ------------------------------------------------------------------
-# is_valid
-# Go and save the values
-# ------------------------------------------------------------------
-
-if {[form is_valid $form_id]} {
-    form get_values $form_id
 
     # temp vars
     set expense_name "$expense_id"
@@ -381,6 +306,18 @@ where
 "
 
 
-    template::forward $return_url
+
+
 }
 
+ad_form -extend -name $form_id -on_submit {
+    
+    ns_log Notice "new1: on_submit"
+    
+}
+
+ad_form -extend -name $form_id -after_submit {
+
+    ad_returnredirect $return_url
+    ad_script_abort
+}
