46880c0e72e0e6f4e6c3de644f62638fabcaf651
Frank Bergmann <frank.bergmann@project-open.com>
2006-09-08 12:30:22 +0000

    - added a new multiple currency warning

--- sql/postgresql/intranet-exchange-rate-create.sql
+++ sql/postgresql/intranet-exchange-rate-create.sql
@@ -108,3 +108,104 @@ BEGIN
 end;' language 'plpgsql';
 
 -- select im_exchange_rate(to_date('2005-07-01','YYYY-MM-DD'), 'EUR', 'USD');
+
+
+select im_component_plugin__del_module('intranet-exchange-rate');
+select im_menu__del_module('intranet-exchange-rate');
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+	-- Menu IDs
+	v_menu		integer;
+	v_admin_menu	integer;
+
+	-- Groups
+	v_accounting	integer;
+	v_senman	integer;
+	v_admins	integer;
+BEGIN
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+
+    select menu_id
+    into v_admin_menu
+    from im_menus
+    where label=''admin'';
+
+    v_menu := im_menu__new (
+	null,			-- p_menu_id
+	''acs_object'',		-- object_type
+	now(),			-- creation_date
+	null,			-- creation_user
+	null,			-- creation_ip
+	null,			-- context_id
+	''intranet-exchange-rate'',  -- package_name
+	''admin_exchange_rates'',    -- label
+	''Exchange Rates'',	-- name
+	''/intranet-exchange-rate/'',   -- url
+	80,			-- sort_order
+	v_admin_menu,		-- parent_menu_id
+	null			-- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+	-- Menu IDs
+	v_menu		integer;
+	v_finance_menu	integer;
+
+	-- Groups
+	v_accounting	integer;
+	v_senman	integer;
+	v_admins	integer;
+BEGIN
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+
+    select menu_id
+    into v_finance_menu
+    from im_menus
+    where label=''finance'';
+
+    v_menu := im_menu__new (
+	null,			-- p_menu_id
+	''acs_object'',		-- object_type
+	now(),			-- creation_date
+	null,			-- creation_user
+	null,			-- creation_ip
+	null,			-- context_id
+	''intranet-exchange-rate'',  -- package_name
+	''finance_exchange_rates'',    -- label
+	''Exchange Rates'',	-- name
+	''/intranet-exchange-rate/'',   -- url
+	80,			-- sort_order
+	v_finance_menu,		-- parent_menu_id
+	null			-- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
