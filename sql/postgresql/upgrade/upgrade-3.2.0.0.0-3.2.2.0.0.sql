d3853c7adbd265318ededfa0036e8a5ac7402d06
Frank Bergmann <frank.bergmann@project-open.com>
2006-09-09 18:16:09 +0000

    - Removed Exchange Rates menu in "Finance" - Added "Cost Centers" menu in Admin

--- sql/postgresql/upgrade/upgrade-3.2.0.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.0.0.0-3.2.2.0.0.sql
@@ -28,3 +28,55 @@ VALUES (3724,'Delivery Note','Intranet Cost Type');
 insert into im_category_hierarchy values (3708,3724);
 
 
+
+-------------------------------------------------------------
+-- 
+---------------------------------------------------------
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+	-- Menu IDs
+	v_menu		integer;
+	v_admin_menu	integer;
+
+	-- Groups
+	v_accounting	integer;
+	v_senman	integer;
+	v_admins	integer;
+BEGIN
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+
+    select menu_id
+    into v_admin_menu
+    from im_menus
+    where label=''admin'';
+
+    v_menu := im_menu__new (
+	null,			-- p_menu_id
+	''acs_object'',		-- object_type
+	now(),			-- creation_date
+	null,			-- creation_user
+	null,			-- creation_ip
+	null,			-- context_id
+	''intranet-cost'',  -- package_name
+	''admin_cost_centers'',    -- label
+	''Cost Centers'',	-- name
+	''/intranet-cost/cost-centers/index'',   -- url
+	85,			-- sort_order
+	v_admin_menu,		-- parent_menu_id
+	null			-- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
