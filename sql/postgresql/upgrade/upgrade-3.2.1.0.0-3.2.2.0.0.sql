8b8eff67af103e3616e923e58075f08e92e5c257
arvato <>
2006-09-19 20:19:07 +0000

    - restricted to trans menus

--- sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
@@ -6,34 +6,6 @@ update im_menus
 set visible_tcl = '[im_cost_type_write_p $user_id 3702]'
 where label = 'invoices_trans_new_quote';
 
-
-update im_menus 
-set visible_tcl = '[im_cost_type_write_p $user_id 3700]'
-where label = 'invoices_customers_new_invoice';
-
-
-update im_menus 
-set visible_tcl = '[im_cost_type_write_p $user_id 3700]'
-where label = 'invoices_customers_new_invoice_from_quote';
-
-
-update im_menus 
-set visible_tcl = '[im_cost_type_write_p $user_id 3702]'
-where label = 'invoices_customers_new_quote';
-
-
 update im_menus 
 set visible_tcl = '[im_cost_type_write_p $user_id 3700]'
 where label = 'invoices_trans_new_cust_invoice';
-
-
-update im_menus 
-set visible_tcl = '[im_cost_type_write_p $user_id 3704]'
-where label = 'invoices_providers_new_bill_from_po';
-
-
-update im_menus 
-set visible_tcl = '[im_cost_type_write_p $user_id 3704]'
-where label = 'invoices_providers_new_bill';
-
-
