af5d8ffca3a4b5db60609599bf0f600ada2af008
arvato <>
2006-09-26 13:15:34 +0000

    - added sql docu files

--- sql/postgresql/costs.sql
+++ sql/postgresql/costs.sql
@@ -1,56 +1,4 @@
 
--- Get everything from a Cost Center
-        select  cc.*
-        from    im_cost_centers cc
-        where   cc.cost_center_id = :cost_center_id
-;
-
-
--- Create a new Cost Center
-        PERFORM im_cost_center__new (
-                null,                   -- cost_center_id
-                'im_cost_center',       -- object_type
-                now(),                  -- creation_date
-                null,                   -- creation_user
-                null,                   -- creation_ip
-                null,                   -- context_id
-
-                :cost_center_name,
-                :cost_center_label,
-                :cost_center_code,
-                :cost_center_type_id,
-                :cost_center_status_id,
-                :parent_id,
-                :manager_id,
-                :department_p,
-                :description,
-                :note
-        )
-;
-
-
--- Update a Cost Center
-        update im_cost_centers set
-                cost_center_name        = :cost_center_name,
-                cost_center_label       = :cost_center_label,
-                cost_center_code        = :cost_center_code,
-                cost_center_type_id     = :cost_center_type_id,
-                cost_center_status_id   = :cost_center_status_id,
-                department_p            = :department_p,
-                parent_id               = :parent_id,
-                manager_id              = :manager_id,
-                description             = :description
-        where
-                cost_center_id = :cost_center_id
-;
-
-
--- Delete a Cost Center
-
-PERFORM im_cost_center__delete(:cost_center_id);
-
-
-
 -- Get a list of all Cost Items visible for the current user,
 -- together with customer, provider and project (if associated
 -- 1:1).
@@ -221,4 +169,276 @@ where
         where object_id_one = p.project_id
               and p.parent_id = :project_id
     )
+
+
+
+
+-------------------------------------------------------------
+-- Costs
+--
+-- Costs is the superclass for all financial items such as 
+-- Invoices, Quotes, Purchase Orders, Bills (from providers), 
+-- Travel Costs, Payroll Costs, Fixed Costs, Amortization Costs,
+-- etc. in order to allow for simple SQL queries revealing the
+-- financial status of a company.
+--
+-- Costs are also used for controlling, namely by assigning costs
+-- to projects, companies and cost centers in order to allow for 
+-- (more or less) accurate profit & loss calculation.
+-- This assignment sometimes requires to split a large cost item
+-- into several smaller items in order to assign them more 
+-- accurately to project, companies or cost centers ("redistribution").
+--
+-- Costs reference acs_objects for customer and provider in order to
+-- allow costs to be created for example between an employee and the
+-- company in the case of travel costs.
+--
+
+-- prompt *** intranet-costs: Creating im_costs
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_object_type	integer;
+begin
+    v_object_type := acs_object_type__create_type (
+	''im_cost'',		-- object_type
+	''Cost'',		-- pretty_name
+	''Costs'',		-- pretty_plural
+	''acs_object'',		-- supertype
+	''im_costs'',		-- table_name
+	''cost_id'',		-- id_column
+	''im_costs'',		-- package_name
+	''f'',			-- abstract_p
+	null,			-- type_extension_table
+	''im_costs__name''	-- name_method
+    );
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+-- prompt *** intranet-costs: Creating im_costs
+create table im_costs (
+	cost_id			integer
+				constraint im_costs_pk
+				primary key
+				constraint im_costs_cost_fk
+                                references acs_objects,
+	-- force a name because we may want to use object.name()
+	-- later to list cost
+	cost_name		varchar(400)
+				constraint im_costs_name_nn
+				not null,
+        -- Nr is a current number to provide a unique 
+	-- identifier of a cost item for backup/restore.
+	cost_nr			varchar(400)
+				constraint im_costs_nr_nn
+				not null,
+	project_id		integer
+				constraint im_costs_project_fk
+				references im_projects,
+				-- who pays?
+	customer_id		integer
+				constraint im_costs_customer_nn
+				not null
+				constraint im_costs_customer_fk
+				references acs_objects,
+				-- who gets paid?
+	cost_center_id		integer
+				constraint im_costs_cost_center_fk
+				references im_cost_centers,
+	provider_id		integer
+				constraint im_costs_provider_nn
+				not null
+				constraint im_costs_provider_fk
+				references acs_objects,
+	investment_id		integer
+				constraint im_costs_inv_fk
+				references acs_objects,
+	cost_status_id		integer
+				constraint im_costs_status_nn
+				not null
+				constraint im_costs_status_fk
+				references im_categories,
+	cost_type_id		integer
+				constraint im_costs_type_nn
+				not null
+				constraint im_costs_type_fk
+				references im_categories,
+	-- reference to an object that has caused this cost,
+	-- in particular to im_repeating_costs
+	cause_object_id		integer
+				constraint im_costs_cause_fk
+				references acs_objects,
+	template_id		integer
+				constraint im_cost_template_fk
+				references im_categories,
+	-- when does the invoice start to be valid?
+	-- due_date is effective_date + payment_days.
+	effective_date		timestamptz,
+	-- start_blocks are the first days every month. This allows
+	-- for fast monthly grouping
+	start_block		timestamptz
+				constraint im_costs_startblck_fk
+				references im_start_months,
+	payment_days		integer,
+	-- amount=null means calculated amount, for example
+	-- with an invoice
+	amount			numeric(12,3),
+	currency		char(3) 
+				constraint im_costs_currency_fk
+				references currency_codes(iso),
+	paid_amount		numeric(12,3),
+	paid_currency		char(3) 
+				constraint im_costs_paid_currency_fk
+				references currency_codes(iso),
+	-- % of total price is VAT
+	vat			numeric(12,5) default 0,
+	-- % of total price is TAX
+	tax			numeric(12,5) default 0,
+	-- Classification of variable against fixed costs
+	variable_cost_p		char(1)
+				constraint im_costs_var_ck
+				check (variable_cost_p in ('t','f')),
+	needs_redistribution_p	char(1)
+				constraint im_costs_needs_redist_ck
+				check (needs_redistribution_p in ('t','f')),
+	-- Points to its parent if the parent was distributed
+	parent_id		integer
+				constraint im_costs_parent_fk
+				references im_costs,
+	-- Indicates that this cost has been redistributed to
+	-- potentially several other costs, so we don't want to
+	-- include this item in sums.
+	redistributed_p		char(1)
+				constraint im_costs_redist_ck
+				check (redistributed_p in ('t','f')),
+	planning_p		char(1)
+				constraint im_costs_planning_ck
+				check (planning_p in ('t','f')),
+	planning_type_id	integer
+				constraint im_costs_planning_type_fk
+				references im_categories,
+	description		varchar(4000),
+	note			varchar(4000)
+);
+create index im_costs_cause_object_idx on im_costs(cause_object_id);
+create index im_costs_start_block_idx on im_costs(start_block);
+
+
+
+-------------------------------------------------------------
+-- Cost Object Packages
+--
+
+-- create or replace package body im_cost
+-- is
+create or replace function im_cost__new (
+       integer,
+       varchar,
+       timestamptz,
+       integer,
+       varchar,
+       integer,
+       varchar,
+       integer,
+       integer,
+       integer,
+       integer,
+       integer,
+       integer,
+       integer,
+       integer,
+       timestamptz,
+       integer,
+       numeric,
+       varchar,
+       numeric,
+       numeric,
+       varchar,
+       varchar,
+       varchar,
+       varchar,
+       integer,
+       varchar,
+       varchar
+)
+returns integer as '
+declare
+	p_cost_id		alias for $1;		-- cost_id default null
+	p_object_type		alias for $2;		-- object_type default ''im_cost''
+	p_creation_date		alias for $3;		-- creation_date default now()
+	p_creation_user		alias for $4;		-- creation_user default null
+	p_creation_ip		alias for $5;		-- creation_ip default null
+	p_context_id		alias for $6;		-- context_id default null
+
+	p_cost_name		alias for $7;		-- cost_name default null
+	p_parent_id		alias for $8;		-- parent_id default null
+	p_project_id		alias for $9;		-- project_id default null
+	p_customer_id		alias for $10;		-- customer_id
+	p_provider_id		alias for $11;		-- provider_id
+	p_investment_id		alias for $12;		-- investment_id default null
+
+	p_cost_status_id	alias for $13;		-- cost_status_id
+	p_cost_type_id		alias for $14;		-- cost_type_id
+	p_template_id		alias for $15;		-- template_id default null
+
+	p_effective_date	alias for $16;		-- effective_date default now()
+	p_payment_days		alias for $17;		-- payment_days default 30
+	p_amount		alias for $18;		-- amount default null
+	p_currency		alias for $19;		-- currency default ''EUR''
+	p_vat			alias for $20;		-- vat default 0
+	p_tax			alias for $21;		-- tax default 0
+
+	p_variable_cost_p	alias for $22;		-- variable_cost_p default ''f''
+	p_needs_redistribution_p alias for $23;		-- needs_redistribution_p default ''f''
+	p_redistributed_p	alias for $24;		-- redistributed_p default ''f''
+	p_planning_p		alias for $25;		-- planning_p default ''f''
+	p_planning_type_id	alias for $26;		-- planning_type_id default null
+
+	p_note			alias for $27;		-- note default null
+	p_description		alias for $28;		-- description default null
+	v_cost_cost_id		integer;
+ begin
+end' language 'plpgsql';
+
+
+-- Delete a single cost (if we know its ID...)
+create or replace function im_cost__delete (integer)
+returns integer as '
+DECLARE
+	p_cost_id alias for $1;
+begin
+end' language 'plpgsql';
+
+
+create or replace function im_cost__name (integer)
+returns varchar as '
+DECLARE
+	p_cost_id  alias for $1;	-- cost_id
+	v_name  varchar(40);
+begin
+end;' language 'plpgsql';
+
+
+
+
+
+
+
+create or replace function im_cost_nr_from_id (integer)
+returns varchar as '
+DECLARE
+        p_id    alias for $1;
+        v_name  varchar(50);
+BEGIN
+        select cost_nr
+        into v_name
+        from im_costs
+        where cost_id = p_id;
+
+        return v_name;
+end;' language 'plpgsql';
+
 
--- sql/postgresql/menus.sql
+++ sql/postgresql/menus.sql
@@ -1,5 +1,3 @@
-
-
 -- Select a specific menu. Label is used as a fixed reference
 -- See the Menu maintenance screens for the name of the parent 
 -- menu.
@@ -20,5 +18,181 @@ where label='finance'
                 and im_object_permission_p(m.menu_id, :user_id, 'read') = 't'
         order by sort_order
 ;
+
+-- How to create new menus
+-- This function creates a new menu in the "Admin" section
+-- only visible for Administrators.
+--
+create or replace function inline_1 ()
+returns integer as '
+declare
+      -- Menu IDs
+      v_menu                  integer;
+      v_admin_menu	      integer;
+
+      -- Groups
+      v_admins                integer;
+begin
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+
+    select menu_id
+    into v_admin_menu
+    from im_menus
+    where label=''admin'';
+
+    v_menu := im_menu__new (
+        null,                   -- p_menu_id
+        ''acs_object'',           -- object_type
+        now(),                  -- creation_date
+        null,                   -- creation_user
+        null,                   -- creation_ip
+        null,                   -- context_id
+        ''intranet-core'',      -- package_name
+        ''admin_user_exits'',   -- label
+        ''User Exits'',            -- name
+        ''/intranet/admin/user_exits'', -- url
+        110,                     -- sort_order
+        v_admin_menu,         -- parent_menu_id
+        null                    -- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    return 0;
+end;' language 'plpgsql';
+select inline_1 ();
+drop function inline_1();
+
+
+
+
+
+
+
+
+---------------------------------------------------------
+-- Menus
+--
+-- Dynamic Menus are necessary to allow Project/Open modules
+-- to extend the core at some point in the future without
+-- that core would need know about these extensions in
+-- advance.
+--
+-- Menus entries are basicly mappings from a Name into a URL.
+--
+-- In addition, menu entries contain a parent_menu_id,
+-- allowing for a tree view of all menus (to build a left-
+-- hand-side navigation bar).
+--
+-- The same parent_menu_id field allows a particular page 
+-- to find out about its submenus items to display by checking 
+-- the super-menu that points to the page and by selecting
+-- all of its sub-menu-items. However, the develpers needs to
+-- avoid multiple menu pointers to the same page because
+-- this leads to an ambiguity about the supermenu.
+-- These ambiguities are resolved by taking the menu from
+-- the highest possible hierarchy level and then using the
+-- lowest sort_key.
+
+
+SELECT acs_object_type__create_type (
+        'im_menu',		    -- object_type
+        'Menu',			    -- pretty_name
+        'Menus',		    -- pretty_plural
+        'acs_object',               -- supertype
+        'im_menus',		    -- table_name
+        'menu_id',		    -- id_column
+        'im_menu',		    -- package_name
+        'f',                        -- abstract_p
+        null,                       -- type_extension_table
+        'im_menu.name'  -- name_method
+    );
+
+
+-- The idea is to use OpenACS permissions in the future to
+-- control who should see what menu.
+
+CREATE TABLE im_menus (
+	menu_id 		integer
+				constraint im_menu_id_pk
+				primary key
+				constraint im_menu_id_fk
+				references acs_objects,
+				-- used to remove all menus from one package
+				-- when uninstalling a package
+	package_name		varchar(200) not null,
+				-- symbolic name of the menu that cannot be
+				-- changed using the menu editor.
+				-- It cat be used as a constant by TCL pages to
+				-- locate their menus.
+	label			varchar(200) not null,
+				-- the name that should appear on the tab
+	name			varchar(200) not null,
+				-- On which pages should the menu appear?
+	url			varchar(200) not null,
+				-- sort order WITHIN the same level
+	sort_order		integer,
+				-- parent_id allows for tree view for navbars
+	parent_menu_id		integer
+				constraint im_parent_menu_id_fk
+				references im_menus,	
+				-- hierarchical codification of menu levels
+	tree_sortkey		varchar(100),
+				-- TCL expression that needs to be either null
+				-- or evaluate (expr *) to 1 in order to display 
+				-- the menu.
+	visible_tcl		varchar(1000) default null,
+				-- Managmenent of different configurations
+	enabled_p		char(1) default('t')
+                                constraint im_menus_enabled_ck
+                                check (enabled_p in ('t','f')),
+				-- Make sure there are no two identical
+				-- menus on the same _level_.
+	constraint im_menus_label_un
+	unique(label)
+);
+
+create or replace function im_menu__new (integer, varchar, timestamptz, integer, varchar, integer,
+varchar, varchar, varchar, varchar, integer, integer, varchar) returns integer as '
+declare
+	p_menu_id	  alias for $1;   -- default null
+        p_object_type	  alias for $2;   -- default ''acs_object''
+        p_creation_date	  alias for $3;   -- default now()
+        p_creation_user	  alias for $4;   -- default null
+        p_creation_ip	  alias for $5;   -- default null
+        p_context_id	  alias for $6;   -- default null
+	p_package_name	  alias for $7;
+	p_label		  alias for $8;
+	p_name		  alias for $9;
+	p_url		  alias for $10;
+	p_sort_order	  alias for $11;
+	p_parent_menu_id  alias for $12;
+	p_visible_tcl	  alias for $13;  -- default null
+begin
+end;' language 'plpgsql';
+
+
+
+-- Delete a single menu (if we know its ID...)
+create or replace function im_menu__delete (integer) returns integer as '
+DECLARE
+	p_menu_id	alias for $1;
+BEGIN
+end;' language 'plpgsql';
+
+
+-- Delete all menus of a module.
+create or replace function im_menu__del_module (varchar) returns integer as '
+DECLARE
+	p_module_name   alias for $1;
+BEGIN
+end;' language 'plpgsql';
+
+
+-- Returns the name of the menu
+create or replace function im_menu__name (integer) returns varchar as '
+DECLARE
+        p_menu_id   alias for $1;
+BEGIN
+end;' language 'plpgsql';
 
 
