2ad30070303e076cdfe7a2b27945ddf34d5e541e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-04 11:09:14 +0000

    - added dynamic upgrade

--- sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
@@ -3,6 +3,7 @@
 -- -------------------------------------------------------------------
 -- Cost Centers
 
+delete from im_view_columns where column_id = 3402;
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
 extra_select, extra_where, sort_order, visible_for) values (
 3402,34,NULL,'CostCenter','$cost_center_code','','',2,'');
