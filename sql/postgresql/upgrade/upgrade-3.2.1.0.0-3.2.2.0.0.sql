10e8c2bbed1265c4d2f26dc050086cc690d66c24
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-04 10:15:46 +0000

    - added conditional upgrade

--- sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
@@ -1,10 +1,33 @@
 
 -- Add URLs for object type "Timesheet Task"
 
-insert into im_biz_object_urls (object_type, url_type, url) values (
-'im_timesheet_task','view','/intranet/projects/view?project_id=');
-insert into im_biz_object_urls (object_type, url_type, url) values (
-'im_timesheet_task','edit','/intranet/projects/new?project_id=');
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count                 integer;
+begin
+        select  count(*)
+        into    v_count
+        from    im_biz_object_urls
+        where   object_type = 'im_timesheet_task'
+		and url_type = 'view';
+
+        if v_count = 1 then
+            return 0;
+        end if;
+
+	insert into im_biz_object_urls (object_type, url_type, url) values (
+	'im_timesheet_task','view','/intranet/projects/view?project_id=');
+	insert into im_biz_object_urls (object_type, url_type, url) values (
+	'im_timesheet_task','edit','/intranet/projects/new?project_id=');
+
+        return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 
 
 -- Counter without number restriction
@@ -53,18 +76,63 @@ end;' language 'plpgsql';
 -- Configuration Management:
 -- Add "enabled_p" to menus and components
 --
-alter table im_menus add enabled_p char(1);
-alter table im_menus alter enabled_p set default 't';
-update im_menus set enabled_p = 't';
-alter table im_menus add constraint im_menus_enabled_ck
-	check (enabled_p in ('t','f'));
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count                 integer;
+begin
+        select  count(*)
+        into    v_count
+        from    user_tab_columns
+        where   lower(table_name) = ''im_menus''
+                and lower(column_name) = ''enabled_p'';
+
+        if v_count = 1 then
+            return 0;
+        end if;
+
+	alter table im_menus add enabled_p char(1);
+	alter table im_menus alter enabled_p set default 't';
+	update im_menus set enabled_p = 't';
+	alter table im_menus add constraint im_menus_enabled_ck
+		check (enabled_p in ('t','f'));
+
+        return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
 
 
-alter table im_component_plugins add enabled_p char(1);
-alter table im_component_plugins alter enabled_p set default 't';
-update im_component_plugins set enabled_p = 't';
-alter table im_component_plugins add constraint im_comp_plugin_enabled_ck
-	check (enabled_p in ('t','f'));
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count                 integer;
+begin
+        select  count(*)
+        into    v_count
+        from    user_tab_columns
+        where   lower(table_name) = ''im_component_plugins''
+                and lower(column_name) = ''enabled_p'';
+
+        if v_count = 1 then
+            return 0;
+        end if;
+
+	alter table im_component_plugins add enabled_p char(1);
+	alter table im_component_plugins alter enabled_p set default 't';
+	update im_component_plugins set enabled_p = 't';
+	alter table im_component_plugins add constraint im_comp_plugin_enabled_ck
+		check (enabled_p in ('t','f'));
+
+        return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
 
 
 
