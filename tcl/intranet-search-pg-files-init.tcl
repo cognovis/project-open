f6b489f6359af776a895ac78d3e2ea297aacdb07
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-09 14:53:13 +0000

    - added incremental searching

--- tcl/intranet-search-pg-files-init.tcl
+++ tcl/intranet-search-pg-files-init.tcl
@@ -1,1 +1,1 @@
-ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-search-pg-files -parameter SearchIndexerInterval -default 60 ] intranet_search_pg_files_search_indexer
+ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-search-pg-files -parameter SearchIndexerInterval -default 300 ] intranet_search_pg_files_search_indexer
--- tcl/intranet-search-pg-files-procs.tcl
+++ tcl/intranet-search-pg-files-procs.tcl
@@ -39,27 +39,28 @@ ad_proc -public intranet_search_pg_files_index_object {
     Index the files of a single objec such as a project, company or user.
     Returns the number of new files.
 } {
+    set debug 0
     set object_type [db_string otype "
 	select object_type 
 	from acs_objects 
 	where object_id = :object_id
     " -default ""]
 
-    ns_log Notice "intranet_search_pg_files_index_object($object_type, $object_id)"
-    if {"" == $object_type} { return 0 }
+    ns_log Notice "im_ftio($object_type, $object_id)"
+    if {"" == $object_type} { 
+	# It's possible that the object has been deleted,
+	# so delete the entry from the queue.
+	# The main routine makes sure that all existing objects
+	# are queued.
+	db_dml del_queue "
+		delete from im_search_pg_file_biz_objects 
+		where object_id = :object_id
+	"
+	return 0 
+    }
 
     set admin_user_id [im_sysadmin_user_default]
     set find_cmd [im_filestorage_find_cmd]
-
-    # Delete all files from the DB associated with this object
-    db_dml delete_files "
-	delete from im_fs_files
-	where folder_id in (
-		select	folder_id
-		from	im_fs_folders
-		where	object_id = :object_id
-	)
-    "
 
     # Home Filestorage depends on object type...
     switch $object_type {
@@ -67,48 +68,90 @@ ad_proc -public intranet_search_pg_files_index_object {
 	im_company { set home_path [im_filestorage_company_path $object_id] }
 	user { set home_path [im_filestorage_user_path $object_id] }
 	default { 
-	    ns_log Error "intranet_search_pg_files_index_object($object_id): Unknown object type: '$object_type'" 
+	    ns_log Error "im_ftio($object_id): Unknown object type: '$object_type'" 
 	    return 0
 	}
     }
+
+    # Read all files associated with the current objects
+    # into an array for quick comparison:
+    # Filename -> ChangeDate
+    set all_files_sql "
+	select	ff.path as folder_path,
+		f.filename,
+		f.last_modified as db_last_modified
+	from
+		im_fs_folders ff,
+		im_fs_files f
+	where
+		ff.object_id = :object_id
+		and ff.folder_id = f.folder_id
+    "
+    db_foreach all_files $all_files_sql {
+	set key "$folder_path/$filename"
+	set last_modified($key) $db_last_modified
+	ns_log Notice "im_ftio: db_last_modified = $db_last_modified, path='$folder_path/$filename'"
+    }
+
 
     set home_path_len [expr [string length $home_path] + 1]
 
     set files [list]
     if {[catch {
-	set file_list [exec $find_cmd $home_path -type f]
+	set file_list [exec $find_cmd $home_path -type f -printf "%h/%f\t%T@\n"]
 	set files [lsort [split $file_list "\n"]]
     } errmsg]} {
 	ns_log Notice "Unable to get list of files for '$home_path':\n$errmsg"
 	return 0
     }
 
+    # Go through the list of all files on the hard disk:
     set file_ctr 0
-    foreach file_path $files {
+    foreach file_entry $files {
 
-	ns_log Notice "intranet_search_pg_files_index_object: $file_path"
-
+	ns_log Notice "im_ftio: $file_entry"
+	# Split the file_entry into file_path and file_last_modified
 	# Remove the "home_path" from the returned value
-	set file_path [string range $file_path $home_path_len end]
-
 	# Split the remaining path into folder path and file body
+	# Retreive the "last_modified" information from the database.
+	set file_entries [split $file_entry "\t"]
+	set file_path [lindex $file_entries 0]
+	set file_last_modified [lindex $file_entries 1]
+	set file_path [string range $file_path $home_path_len end]
 	set pieces [split $file_path "/"]
 	set body [lindex $pieces [expr [llength $pieces]-1]]
 	set folder_path [join [lrange $pieces 0 end-1] "/"]
+	if {"" != $folder_path} { set path_sql ":folder_path" } else { set path_sql "''" }
+	set db_last_modified ""
+	set db_last_modified_key "$folder_path/$body"
+	if {[info exists last_modified($db_last_modified_key)]} {
+	    set db_last_modified $last_modified($db_last_modified_key)
+	}
 	
-	# ToDo: What happens if there are more then one folder for
-	# one object, even thought the path is ""?
-	if {"" != $folder_path} {
-	    set path_sql ":folder_path"
-	} else {
-	    set path_sql "''"
+	if {$debug} {
+	    ns_log Notice "im_ftio: file_path=$file_path"
+	    ns_log Notice "im_ftio: pieces=$pieces"
+	    ns_log Notice "im_ftio: body=$body"
+	    ns_log Notice "im_ftio: folder_path=$folder_path"
+	    ns_log Notice "im_ftio: path_sql=$path_sql"
+	    ns_log Notice "im_ftio: file_last_modified=$file_last_modified"
+	    ns_log Notice "im_ftio: db_last_modified=$db_last_modified"
 	}
 
-	ns_log Notice "intranet_search_pg_files_index_object: file_path=$file_path"
-	ns_log Notice "intranet_search_pg_files_index_object: pieces=$pieces"
-	ns_log Notice "intranet_search_pg_files_index_object: body=$body"
-	ns_log Notice "intranet_search_pg_files_index_object: folder_path=$folder_path"
-	ns_log Notice "intranet_search_pg_files_index_object: path_sql=$path_sql"
+	# Remember that the file exists, so that we can delete the
+	# non-existing files later (after the main loop).
+	set file_exists_key "$folder_path/$body"
+	set file_exists($file_exists_key) $file_last_modified
+
+	# Skip adding the file to the database if the modified
+	# date is still the same...
+	if {$db_last_modified == $file_last_modified} { 
+	    ns_log Notice "im_ftio: last_modfied not changed: $db_last_modified"
+	    continue 
+	} 
+	
+	# ------------------ File has changed - Update DB ----------------------
+	ns_log Notice "im_ftio: last_modfied changed: db:$db_last_modified - file:$file_last_modified"
 
 	# Make sure the folder exists...
 	set folder_id [db_string folder_exists "
@@ -121,6 +164,7 @@ ad_proc -public intranet_search_pg_files_index_object {
 	# Create the folder if it doesn't exist yet
 	if {!$folder_id} {
 	    set folder_id [db_nextval im_fs_folder_seq]
+	    ns_log Notice "im_ftio: new folder_id=$folder_id"
 	    db_dml insert_folder_sql "
 		insert into im_fs_folders (
 			folder_id, object_id, path
@@ -129,19 +173,45 @@ ad_proc -public intranet_search_pg_files_index_object {
 		)
 	    "
 	}
+	ns_log Notice "im_ftio: folder_id=$folder_id"
 
-	set file_id [db_nextval im_fs_file_seq]
-	db_dml insert_file "
+	# Check if file exists
+	set file_id [db_string file_id "
+		select	file_id
+		from	im_fs_files
+		where	folder_id = :folder_id
+			and filename = :body
+	" -default 0]
+
+	if {0 == $file_id} {
+
+	    # Insert a new file
+	    set file_id [db_nextval im_fs_file_seq]
+	    db_dml insert_file "
 		insert into im_fs_files (
 			file_id, folder_id,
 			owner_id, filename,
-			exists_p, last_updated
+			last_updated, last_modified
 		) values (
 			:file_id, :folder_id,
 			:admin_user_id, :body,
-			'1', now()
+			now(), :file_last_modified
 		)
-        "
+            "
+	    ns_log Notice "im_ftio: insert: file_id=$file_id"
+
+	} else {
+
+	    # File exists - Update the new modification date
+	    db_dml update_file "
+		update im_fs_files set 
+			ft_indexed_p = '0',
+			last_modified = :file_last_modified
+		where file_id = :file_id
+	    "
+	    ns_log Notice "im_ftio: update: file_id=$file_id, file_last_modified=$file_last_modified"
+
+	}
 
 	# ToDo: Optimize: This update seems to be
 	# necessary in order to trigger indexing
@@ -149,6 +219,46 @@ ad_proc -public intranet_search_pg_files_index_object {
 
 	incr file_ctr
     }   
+
+
+    # ------------------ Check if file have disappreared ----------------------
+    # In this case we need to delete the entries from the DB...
+#   ns_log Notice "im_ftio: last_modified: '[array get file_exists]'"
+    foreach filename [array names last_modified] {
+
+	if {![info exists file_exists($filename)]} {
+
+	    # The filename is present in the DB, but not on disk anymore
+	    # => Delete the DB-entry
+	    ns_log Notice "im_ftio: del: doesn't exist in the FS: '$filename'"
+
+	    # Split the remaining path into folder path and file body
+	    set pieces [split $file_path "/"]
+	    set body [lindex $pieces [expr [llength $pieces]-1]]
+	    set folder_path [join [lrange $pieces 0 end-1] "/"]
+
+	    # Deal with empty string == "null" in the db-interface.
+	    if {"" != $folder_path} {
+		set path_sql ":folder_path"
+	    } else {
+		set path_sql "''"
+	    }
+
+	    # Delete all files from the DB associated with this object
+	    db_dml delete_files "
+		delete	from im_fs_files
+		where	folder_id in (
+				select	folder_id
+				from	im_fs_folders
+				where	object_id = :object_id
+					and path = $path_sql
+			)
+			and filename = :body
+
+            "
+	}
+
+    }
 
     return $file_ctr
 }
@@ -158,7 +268,7 @@ ad_proc -public intranet_search_pg_files_index_all {
 } {
     Index the entire server
 } {
-    set debug "intranet_search_pg_files_index_all"
+    set debug "im_fti_index_all"
     ns_log Notice $debug
 
     # What object types should we index?
@@ -185,13 +295,18 @@ ad_proc -public intranet_search_pg_files_index_all {
 
 
 ad_proc intranet_search_pg_files_search_indexer {
-    {-max_files 100}
+    {-max_files 0}
 } {
     Index the entire server.
     This routine is schedule every 60 seconds or so.
     We use this to determine the "oldest" business object
     to be index and update it.
 } {
+
+    if {0 == $max_files} {
+	set max_files [parameter::get_from_package_key -package_key intranet-search-pg-files -parameter IndexerMaxFiles -default 100]
+    }
+
     set unindexed_projects_sql "
 	select	project_id 
 	from	im_projects
