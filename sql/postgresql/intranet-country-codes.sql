0832ca1af7d623c7b148d228a7a8ac6edbe5cfee
arvato <>
2006-10-12 16:02:10 +0000

    - added im_country_from_code

--- sql/postgresql/intranet-country-codes.sql
+++ sql/postgresql/intranet-country-codes.sql
@@ -36,5 +36,21 @@ create table country_codes (
 
 
 
+create or replace function im_country_from_code (varchar)
+returns varchar as '
+DECLARE
+        p_cc		alias for $1;
+        v_country	varchar;
+BEGIN
+    select country_name
+    into v_country
+    from country_codes
+    where iso = p_cc;
+
+    return v_country;
+END;' language 'plpgsql';
+
+
+
 \i ../common/intranet-country-codes.sql
 
