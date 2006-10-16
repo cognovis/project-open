21c6d0a4cc921aeffb4b22b67ed7dbc7e969ae97
arvato <>
2006-10-16 12:44:20 +0000

    - CSVified

--- www/timesheet-productivity.tcl
+++ www/timesheet-productivity.tcl
@@ -13,6 +13,7 @@ ad_page_contract {
 } {
     { start_date "" }
     { level_of_detail 1 }
+    { output_format "html" }
 }
 
 # ------------------------------------------------------------
@@ -247,33 +248,48 @@ set counters [list \
 # Start formatting the page
 #
 
-ad_return_top_of_page "
-[im_header]
-[im_navbar]
-<form>
-<table border=0 cellspacing=1 cellpadding=1>
-<tr>
-  <td class=form-label>Level of Details</td>
-  <td class=form-widget>
-    [im_select -translate_p 0 level_of_detail $levels $level_of_detail]
-  </td>
-</tr>
-<tr>
-  <td class=form-label>Start Date</td>
-  <td class=form-widget>
-    <input type=textfield name=start_date value=$start_date>
-  </td>
-</tr>
-<tr>
-  <td></td>
-  <td><input type=submit value=Submit></td>
-</tr>
-</table>
-</form>
+# Write out HTTP header, considering CSV/MS-Excel formatting
+im_report_write_http_headers -output_format $output_format
 
-<table border=0 cellspacing=1 cellpadding=1>\n"
+# Add the HTML select box to the head of the page
+switch $output_format {
+    html {
+        ns_write "
+	[im_header]
+	[im_navbar]
+	<form>
+		<table border=0 cellspacing=1 cellpadding=1>
+		<tr>
+		  <td class=form-label>Level of Details</td>
+		  <td class=form-widget>
+		    [im_select -translate_p 0 level_of_detail $levels $level_of_detail]
+		  </td>
+		</tr>
+		<tr>
+		  <td class=form-label>Start Date</td>
+		  <td class=form-widget>
+		    <input type=textfield name=start_date value=$start_date>
+		  </td>
+		</tr>
+                <tr>
+                  <td class=form-label>Format</td>
+                  <td class=form-widget>
+                    [im_report_output_format_select output_format $output_format]
+                  </td>
+                </tr>
+
+		<tr>
+		  <td class=form-label></td>
+		  <td class=form-widget><input type=submit value=Submit></td>
+		</tr>
+		</table>
+	</form>
+	<table border=0 cellspacing=1 cellpadding=1>\n"
+    }
+}
 
 im_report_render_row \
+    -output_format $output_format \
     -row $header0 \
     -row_class "rowtitle" \
     -cell_class "rowtitle"
@@ -285,6 +301,7 @@ set class "rowodd"
 db_foreach sql $sql {
 
 	im_report_display_footer \
+	    -output_format $output_format \
 	    -group_def $report_def \
 	    -footer_array_list $footer_array_list \
 	    -last_value_array_list $last_value_list \
@@ -295,6 +312,7 @@ db_foreach sql $sql {
 	im_report_update_counters -counters $counters
 	
 	set last_value_list [im_report_render_header \
+	    -output_format $output_format \
 	    -group_def $report_def \
 	    -last_value_array_list $last_value_list \
 	    -level_of_detail $level_of_detail \
@@ -303,6 +321,7 @@ db_foreach sql $sql {
         ]
 
         set footer_array_list [im_report_render_footer \
+	    -output_format $output_format \
 	    -group_def $report_def \
 	    -last_value_array_list $last_value_list \
 	    -level_of_detail $level_of_detail \
@@ -312,6 +331,7 @@ db_foreach sql $sql {
 }
 
 im_report_display_footer \
+    -output_format $output_format \
     -group_def $report_def \
     -footer_array_list $footer_array_list \
     -last_value_array_list $last_value_list \
@@ -321,9 +341,12 @@ im_report_display_footer \
     -cell_class $class
 
 im_report_render_row \
+    -output_format $output_format \
     -row $footer0 \
     -row_class $class \
     -cell_class $class
 
 
-ns_write "</table>\n[im_footer]\n"
+switch $output_format {
+    html { ns_write "</table>\n[im_footer]\n" }
+}
