710d7230714f49f6ad6b5a94261456bc8febf9bf
arvato <>
2006-10-16 12:25:05 +0000

    - created update script - empty still

---
+++ sql/postgresql/upgrade/upgrade-3.2.2.0.0-3.2.3.0.0.sql
@@ -0,0 +1,15 @@
+-- upgrade-3.2.2.0.0-3.2.3.0.0.sql
+--
+
+-- Copyright (c) 2003-2006 ]project-open[
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author frank.bergmann@project-open.com
+
+
+
+---------------------------------------------------------
+--
+
