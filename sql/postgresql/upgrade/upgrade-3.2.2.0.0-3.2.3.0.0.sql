41a9d3683b476ff83f1b6ab6bf1a8a4fd10884d0
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-19 16:01:25 +0000

    - upgrading a project-consulting system to V3.2.3: Adding translation   fields to im_projects

--- sql/postgresql/upgrade/upgrade-3.2.2.0.0-3.2.3.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.2.0.0-3.2.3.0.0.sql
@@ -27,3 +27,115 @@ SELECT pg_catalog.setval('im_categories_seq', 10000000, true);
 delete from secret_tokens;
 SELECT pg_catalog.setval('t_sec_security_token_id_seq', 1, true);
 
+
+-- -------------------------------------------------------------
+-- Add field company_project_id if it doesn't exist already
+-- (project-translation)
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_attrib_name           varchar;
+        v_attrib_pretty         varchar;
+	v_table			varchar;
+	v_object		varchar;
+
+        v_acs_attrib_id         integer;
+        v_attrib_id             integer;
+        v_count                 integer;
+begin
+        v_attrib_name := ''company_project_nr'';
+        v_attrib_pretty := ''Customer Project Nr'';
+	v_object := ''im_project'';
+	v_table := ''im_projects'';
+
+        select count(*) into v_count from acs_attributes
+        where attribute_name = v_attrib_name;
+        IF 0 != v_count THEN return 0; END IF;
+
+	select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_project'' and table_name = ''im_projects'';
+	IF v_count = 0 THEN
+		insert into acs_object_type_tables (object_type, table_name, id_column)
+		values (''im_project'', ''im_projects'', ''project_id'');
+	END IF;
+        alter table im_projects add company_project_nr varchar(50);
+
+        v_acs_attrib_id := acs_attribute__create_attribute (
+                v_object,
+                v_attrib_name,
+                ''string'',
+                v_attrib_pretty,
+                v_attrib_pretty,
+                v_table,
+                NULL, NULL, ''0'', ''1'',
+                NULL, NULL, NULL
+        );
+        v_attrib_id := acs_object__new (
+                null,
+                ''im_dynfield_attribute'',
+                now(),
+                null, null, null
+        );
+        insert into im_dynfield_attributes (
+                attribute_id, acs_attribute_id, widget_name, deprecated_p
+        ) values (
+                v_attrib_id, v_acs_attrib_id, ''textbox_medium'', ''f''
+        );
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_attrib_name           varchar;
+        v_attrib_pretty         varchar;
+	v_table			varchar;
+	v_object		varchar;
+
+        v_acs_attrib_id         integer;
+        v_attrib_id             integer;
+        v_count                 integer;
+begin
+        v_attrib_name := ''final_company'';
+        v_attrib_pretty := ''Final Customer'';
+	v_object := ''im_project'';
+	v_table := ''im_projects'';
+
+        select count(*) into v_count from acs_attributes
+        where attribute_name = v_attrib_name;
+        IF 0 != v_count THEN return 0; END IF;
+
+        alter table im_projects add final_company varchar(200);
+
+        v_acs_attrib_id := acs_attribute__create_attribute (
+                v_object,
+                v_attrib_name,
+                ''string'',
+                v_attrib_pretty,
+                v_attrib_pretty,
+                v_table,
+                NULL, NULL, ''0'', ''1'',
+                NULL, NULL, NULL
+        );
+        v_attrib_id := acs_object__new (
+                null,
+                ''im_dynfield_attribute'',
+                now(),
+                null, null, null
+        );
+        insert into im_dynfield_attributes (
+                attribute_id, acs_attribute_id, widget_name, deprecated_p
+        ) values (
+                v_attrib_id, v_acs_attrib_id, ''textbox_medium'', ''f''
+        );
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
