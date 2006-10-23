7e15076409e075f875dc61b9142fc1363c9ec7b3
arvato <>
2006-10-23 12:56:55 +0000

    - fixed bug removing user_map dependency

--- sql/postgresql/intranet-components.sql
+++ sql/postgresql/intranet-components.sql
@@ -195,6 +195,9 @@ BEGIN
             from im_component_plugins
             where package_name = p_module_name
 	loop
+	    delete from im_component_plugin_user_map
+	    where plugin_id = row.plugin_id;
+
 	    PERFORM im_component_plugin__delete(row.plugin_id);
 	end loop;
 
