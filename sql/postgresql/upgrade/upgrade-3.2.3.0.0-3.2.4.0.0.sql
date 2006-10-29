53725daa20fac7973143a3115bd63ba19d9becaa
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-29 17:32:04 +0000

    - added update script to add timesheet permissions   so that customers and freelancers don't see TS by   default

---
+++ sql/postgresql/upgrade/upgrade-3.2.3.0.0-3.2.4.0.0.sql
@@ -0,0 +1,23 @@
+-- upgrade-3.2.3.0.0-3.2.4.0.0.sql
+
+
+------------------------------------------------------
+-- Permissions and Privileges
+--
+
+-- view_timesheet_tasks actually is more of an obligation then a privilege...
+select acs_privilege__create_privilege(
+	'view_timesheet_tasks',
+	'View Timesheet Task',
+	'View Timesheet Task'
+);
+select acs_privilege__view_child('admin', 'view_timesheet_tasks');
+
+
+select im_priv_create('view_timesheet_tasks', 'Accounting');
+select im_priv_create('view_timesheet_tasks', 'Employees');
+select im_priv_create('view_timesheet_tasks', 'P/O Admins');
+select im_priv_create('view_timesheet_tasks', 'Project Managers');
+select im_priv_create('view_timesheet_tasks', 'Sales');
+select im_priv_create('view_timesheet_tasks', 'Senior Managers');
+
