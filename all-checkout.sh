ec4ac3359b679413aa8deadb175bb55162cae167
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-10-29 15:03:44 +0000

    - updated all-checkout for new intranet-bug-tracker package

--- all-checkout.sh
+++ all-checkout.sh
@@ -2,6 +2,7 @@
 
 cvs checkout acs-admin
 cvs checkout acs-api-browser
+# cvs checkout acs_admin_tools
 cvs checkout acs-authentication
 cvs checkout acs-automated-testing
 cvs checkout acs-bootstrap-installer
@@ -30,17 +31,26 @@ cvs checkout ecommerce
 cvs checkout events
 
 cvs checkout intranet-big-brother
+cvs checkout intranet-bug-tracker
 cvs checkout intranet-core
 cvs checkout intranet-cost
 cvs checkout intranet-crm-tracking
+# cvs checkout intranet-cust-baselkb
+cvs checkout intranet-cvs-integration
 cvs checkout intranet-dw-light
 cvs checkout intranet-dynfield
 cvs checkout intranet-expenses
 cvs checkout intranet-exchange-rate
+# cvs checkout intranet-expfind2-ora
+# cvs checkout intranet-expfind2-pg
+# cvs checkout intranet-expfind-ora
+# cvs checkout intranet-expfind-pg
+# cvs checkout intranet-filestorage
 cvs checkout intranet-filestorage
 cvs checkout intranet-forum
 cvs checkout intranet-freelance
 cvs checkout intranet-freelance-invoices
+# cvs checkout intranet-freelance-recruiting
 cvs checkout intranet-ganttproject
 cvs checkout intranet-hr
 cvs checkout intranet-html2pdf
@@ -48,29 +58,39 @@ cvs checkout intranet-invoices
 cvs checkout intranet-invoices-templates
 cvs checkout intranet-mail-import
 cvs checkout intranet-material
+# cvs checkout intranet-notes
 cvs checkout intranet-ophelia
 cvs checkout intranet-otp
 cvs checkout intranet-payments
+# cvs checkout intranet-planning
+# cvs checkout intranet-procedures
 cvs checkout intranet-reporting
 cvs checkout intranet-reporting-tutorial
-cvs checkout intranet-reporting-dashboard
-cvs checkout intranet-riskmanagement
+# cvs checkout intranet-reporting-dashboard
+# cvs checkout intranet-riskmanagement
+# cvs checkout intranet-search
+# cvs checkout intranet-search-ora
+# cvs checkout intranet-search-oracle
 cvs checkout intranet-search-pg
-cvs checkout intranet-secure-login
+cvs checkout intranet-search-pg-files
+# cvs checkout intranet-secure-login
 cvs checkout intranet-security-update-client
-cvs checkout intranet-security-update-server
+# cvs checkout intranet-security-update-server
 cvs checkout intranet-simple-survey
-cvs checkout intranet-spam
-cvs checkout intranet-sql-selectors
+# cvs checkout intranet-spam
+# cvs checkout intranet-sql-selectors
+# cvs checkout intranet-timesheet
 cvs checkout intranet-timesheet2
 cvs checkout intranet-timesheet2-invoices
+# cvs checkout intranet-timesheet2-task-popup
 cvs checkout intranet-timesheet2-tasks
-cvs checkout intranet-timesheet2-task-popup
-cvs checkout intranet-translation
 cvs checkout intranet-trans-invoices
+# cvs checkout intranet-trans-invoices-vaw
+cvs checkout intranet-translation
 cvs checkout intranet-trans-quality
+# cvs checkout intranet-travel
 cvs checkout intranet-update-client
-cvs checkout intranet-update-server
+# cvs checkout intranet-update-server
 cvs checkout intranet-wiki
 cvs checkout intranet-workflow
 cvs checkout intranet-xmlrpc
