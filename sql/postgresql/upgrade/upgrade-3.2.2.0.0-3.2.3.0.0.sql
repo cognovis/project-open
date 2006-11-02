f6c0b05cf1e00006dbbb29335815651adab1fda4
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-11-02 15:47:52 +0000

    - added fix to im_expense__name

---
+++ sql/postgresql/upgrade/upgrade-3.2.2.0.0-3.2.3.0.0.sql
@@ -0,0 +1,18 @@
+-- /packages/intranet-expenses/sql/postgresql/upgrade/upgrade-3.2.2.0.0-3.2.2.0.0.sql
+
+
+
+create or replace function im_expense__name (integer)
+returns varchar as '
+DECLARE
+        p_expenses_id  alias for $1;    -- expense_id
+        v_name  varchar(40);
+begin
+        select  cost_name
+        into    v_name
+        from    im_costs
+        where   cost_id = p_expenses_id;
+
+        return v_name;
+end;' language 'plpgsql';
+
