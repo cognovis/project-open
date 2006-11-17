b2eee1cdc466187fc5425017a6d5e1dc4a2a5666
arvato <>
2006-11-17 18:06:11 +0000

    - added DeliveryNotes

--- sql/common/projects-export.sql
+++ sql/common/projects-export.sql
@@ -123,6 +123,10 @@ insert into im_view_columns (column_id, view_id, group_id, column_name, column_r
 extra_select, extra_where, sort_order, visible_for) values (
 2488,24,NULL,'TimeEstim','$cost_timesheet_planned_cache','','',88,'');
 
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (
+2489,24,NULL,'DelNotes','$cost_delivery_notes_cache','','',89,'');
+
 
 
 
