4faf096db635468fa4f1c4727b3c8675ab341dca
Frank Bergmann <frank.bergmann@project-open.com>
2006-11-26 21:08:23 +0000

    - first page working with reasonable "master"

---
+++ www/master.tcl
@@ -0,0 +1,15 @@
+# /packages/intranet-sysconfig/www/master.tcl
+#
+# Copyright (c) 2003-2006 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+
+# ---------------------------------------------------------------
+# Frequently used variables
+# ---------------------------------------------------------------
+
+set bg "/intranet/images/girlongrass.600x400.jpg"
+set po "<span class=brandsec>&\\#93;</span><span class=brandfirst>project-open</span><span class=brandsec>&\\#91;</span>"
+
---
+++ www/segment/index.tcl
@@ -0,0 +1,30 @@
+# /packages/intranet-sysconfig/www/sector/index.tcl
+#
+# Copyright (c) 2003-2006 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+# ---------------------------------------------------------------
+# Page Contract
+# ---------------------------------------------------------------
+
+ad_page_contract {
+    
+} {
+
+}
+
+
+# ---------------------------------------------------------------
+# Frequently used variables
+# ---------------------------------------------------------------
+
+set page_title "[lang::message::lookup "" intranet-sysconfig.System_Configuration_Wizard "System Configuration Wizard"]"
+set context_bar [im_context_bar $page_title]
+
+
+set bg "/intranet/images/girlongrass.600x400.jpg"
+set po "<span class=brandsec>&\#93;</span><span class=brandfirst>project-open</span><span class=brandsec>&\#91;</span>"
+
+
