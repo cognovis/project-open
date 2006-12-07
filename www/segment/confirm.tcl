aad3e2486b0887ff8c4520bc5e2d7e0dd263c04a
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-12-07 20:01:17 +0000

    - added security (SysAdmin only)

--- www/segment/confirm.tcl
+++ www/segment/confirm.tcl
@@ -21,6 +21,11 @@ ad_page_contract {
 # ---------------------------------------------------------------
 
 set current_user_id [ad_maybe_redirect_for_registration]
+set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $current_user_id]
+if {!$user_is_admin_p} {
+    ad_return_complaint 1 "<li>[_ intranet-core.lt_You_need_to_be_a_syst]"
+    return
+}
 
 set bg "/intranet/images/girlongrass.600x400.jpg"
 set po "<span class=brandsec>&\#93;</span><span class=brandfirst>project-open</span><span class=brandsec>&\#91;</span>"
