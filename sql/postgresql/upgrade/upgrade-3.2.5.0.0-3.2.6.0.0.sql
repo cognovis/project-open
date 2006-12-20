6c8973aafd6b1c7748439221ac2336b6d37cfc0c
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-12-20 14:15:45 +0000

    - added journal component for WF

---
+++ sql/postgresql/upgrade/upgrade-3.2.5.0.0-3.2.6.0.0.sql
@@ -0,0 +1,42 @@
+-- upgrade-3.2.5.0.0-3.2.6.0.0.sql
+
+
+-- Project WF Display
+--
+SELECT im_component_plugin__new (
+        null,                           -- plugin_id
+        'acs_object',                   -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        'Project Workflow Graph',       -- plugin_name
+        'intranet-workflow',            -- package_name
+        'right',                        -- location
+        '/intranet/projects/view',     -- page_url
+        null,                           -- view_name
+        20,                              -- sort_order
+        'im_workflow_graph_component -object_id $project_id'
+);
+
+
+-- Project WF Journal
+--
+SELECT im_component_plugin__new (
+        null,                           -- plugin_id
+        'acs_object',                   -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        'Project Workflow Journal',     -- plugin_name
+        'intranet-workflow',            -- package_name
+        'bottom',                       -- location
+        '/intranet/projects/view',      -- page_url
+        null,                           -- view_name
+        60,                             -- sort_order
+        'im_workflow_journal_component -object_id $project_id'
+);
+
+
+
