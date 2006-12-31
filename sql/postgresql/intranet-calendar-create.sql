c942550b4a904870d17ae9a53d5972e2a8e7c330
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2006-12-31 23:58:50 +0000

    - fixing weekly view to calendar - update calendar to OpenACS 5.3

--- sql/postgresql/intranet-calendar-create.sql
+++ sql/postgresql/intranet-calendar-create.sql
@@ -39,70 +39,101 @@ SELECT calendar__new(
 
 
 drop trigger im_projects_calendar_update_tr on im_projects;
-drop function im_projects_calendar_update_tr();
+-- drop function im_projects_calendar_update_tr();
 
 create or replace function im_projects_calendar_update_tr () returns trigger as '
 declare
-	v_old_event_id		integer;	
+	v_cal_item_id		integer;	
+
 	v_timespan_id		integer;
 	v_interval_id		integer;
 	v_calendar_id		integer;
 	v_activity_id		integer;
 	v_recurrence_id		integer;
-	v_cal_item_id		integer;
 begin
-	-- Check if the entry already exists
-	v_old_event_id := null;
+	-- -------------- Skip if start or end date are null ------------
+	IF new.start_date is null OR new.end_date is null THEN
+		return new;
+	END IF;
+
+	-- -------------- Check if the entry already exists ------------
+	v_cal_item_id := null;
+
 	SELECT	event_id
-	INTO	v_old_event_id
+	INTO	v_cal_item_id
 	FROM	acs_events
 	WHERE	related_object_id = new.project_id;
 
-	IF v_old_event_id is not null THEN
-		UPDATE	acs_events 
-		SET	name = new.project_name,
-			description = new.description
-		WHERE	event_id = v_old_event_id;
-		-- ToDo: Update the timepan/interval		
-	    return new;
+	-- --------------------- Create entry if it isnt there -------------
+	IF v_cal_item_id is null THEN
+
+		v_timespan_id := timespan__new(new.end_date, new.end_date);
+		RAISE NOTICE ''im_projects_calendar_update_tr: timespan_id=%'', v_timespan_id;
+	
+		v_activity_id := acs_activity__new(
+			null, 
+			new.project_name,
+			new.description, 
+			''f'', 
+			'''', 
+			''acs_activity'', now(), null, ''0.0.0.0'', null
+		);
+		RAISE NOTICE ''im_projects_calendar_update_tr: v_activity_id=%'', v_activity_id;
+	
+		SELECT	calendar_id
+		INTO 	v_calendar_id
+		FROM	calendars
+		WHERE	calendar_name = ''Global Calendar'';
+	
+		v_recurrence_id := NULL;
+		v_cal_item_id := cal_item__new (
+			null,			-- cal_item_id
+			v_calendar_id,		-- on_which_calendar
+			new.project_name,	-- name
+			new.description,	-- description
+			''f'',			-- html_p
+			'''',			-- status_summary
+			v_timespan_id,		-- timespan_id
+			v_activity_id,		-- activity_id
+			v_recurrence_id,	-- recurrence_id
+			''cal_item'', null, now(), null, ''0.0.0.0''	
+		);
+		RAISE NOTICE ''im_projects_calendar_update_tr: cal_id=%'', v_cal_item_id;
+
 	END IF;
 
-	v_timespan_id := timespan__new(new.start_date, new.end_date);
-	RAISE NOTICE ''im_projects_calendar_update_tr: timespan_id=%'', v_timespan_id;
+	-- --------------------- Update the entry --------------------
+	SELECT	activity_id	INTO v_activity_id	FROM acs_events	WHERE	event_id = v_cal_item_id;
+	SELECT	timespan_id	INTO v_timespan_id	FROM acs_events	WHERE	event_id = v_cal_item_id;
+	SELECT	recurrence_id	INTO v_recurrence_id	FROM acs_events	WHERE	event_id = v_cal_item_id;
 
-	v_activity_id := acs_activity__new(
-		null, 
-		new.project_name,
-		new.description, 
-		''f'', 
-		'''', 
-		''acs_activity'', now(), null, ''0.0.0.0'', null
-	);
-	RAISE NOTICE ''im_projects_calendar_update_tr: v_activity_id=%'', v_activity_id;
+	-- Update the event
+	UPDATE	acs_events 
+	SET	name = new.project_name,
+		description = new.description,
+		related_object_id = new.project_id,
+		related_link_url = ''/intranet/projects/view?project_id=''||new.project_id,
+		related_link_text = new.project_name || '' Project'',
+		redirect_to_rel_link_p = ''t''
+	WHERE	event_id = v_cal_item_id;
 
-	SELECT	calendar_id
-	INTO 	v_calendar_id
-	FROM	calendars
-	WHERE	calendar_name = ''Global Calendar'';
+	-- Update the activity - same as event
+	UPDATE	acs_activities
+	SET	name = new.project_name,
+		description = new.description
+	WHERE	activity_id = v_activity_id;
 
-	v_recurrence_id := NULL;
-	v_cal_item_id := cal_item__new (
-		null,			-- cal_item_id
-		v_calendar_id,		-- on_which_calendar
-		new.project_name,	-- name
-		new.description,	-- description
-		''f'',			-- html_p
-		'''',			-- status_summary
-		v_timespan_id,		-- timespan_id
-		v_activity_id,		-- activity_id
-		v_recurrence_id,	-- recurrence_id
-		''cal_item'', null, now(), null, ''0.0.0.0''	
-	);
-	RAISE NOTICE ''im_projects_calendar_update_tr: cal_id=%'', v_cal_item_id;
+	-- Update the timespan. Make sure there is only one interval
+	-- in this timespan (there may be multiples)
+	SELECT	interval_id	INTO v_interval_id	FROM timespans	WHERE	timespan_id = v_timespan_id;
 
-	update	acs_events
-	set	related_object_id = new.project_id
-	where	event_id = v_cal_item_id;
+	RAISE NOTICE ''cal_update_tr: cal_item:%, activity:%, timespan:%, recurrence:%, interval:%'', 
+			v_cal_item_id, v_activity_id, v_timespan_id, v_recurrence_id, v_interval_id;
+
+	UPDATE	time_intervals
+	SET	start_date = new.end_date,
+		end_date = new.end_date
+	WHERE	interval_id = v_interval_id;
 
 	return new;
 end;' language 'plpgsql';
@@ -111,7 +142,7 @@ create trigger im_projects_calendar_update_tr after insert or update
 on im_projects for each row
 execute procedure im_projects_calendar_update_tr ();
 
-update im_projects set start_date = start_date::date where project_nr like '2006_0051';
+update im_projects set start_date = start_date::date where project_nr like '2005_0134';
 -- update im_projects set start_date = start_date::date where project_nr like '2006%';
 
 select count(*) from acs_events;
