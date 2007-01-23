8bac9fd47ffa24e8dabac437918bd2fbae4f723e
cdamian <>
2007-01-23 15:53:17 +0000

    - really delete the tasks using /intranet-timesheet2-tasks/task-action - /task-actions does the permission check & deletion

--- www/delete-task.tcl
+++ www/delete-task.tcl
@@ -1,12 +1,25 @@
 ad_page_contract {
 
 } {
-    project_id:multiple,optional
+    { task_id:optional,multiple "" }
+    project_id
     return_url
 }
 
-#TODO: really delete task and check permissions
+#
+# Using "/intranet-timesheet2-tasks/task-action"
+# 
 
-ad_returnredirect $return_url
+# /task-action expects the task_id as an array
+set tmp $task_id
+unset task_id
+array set task_id {}
+foreach i $tmp {
+    set task_id($i) $i
+}
+
+set vars [export_vars -url {task_id:array project_id return_url}]
+
+ad_returnredirect "/intranet-timesheet2-tasks/task-action?action=delete&$vars"
 
 
