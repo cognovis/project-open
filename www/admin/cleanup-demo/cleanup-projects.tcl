e94c7802011727c0e613b6ac00954c280c78c1ae
baselkb <>
2007-02-09 15:18:12 +0000

    - nothing

--- www/admin/cleanup-demo/cleanup-projects.tcl
+++ www/admin/cleanup-demo/cleanup-projects.tcl
@@ -116,7 +116,8 @@ db_multirow -extend {project_url parent_project_url} projects get_projects "
 			proj.project_id
 		) subp on (p.project_id = subp.project_id)
 	where	1=1
-	order by p.project_id
+	order by p.project_id DESC
+        limit 300
 " {
 	set project_url [export_vars -base "/intranet/projects/view" {project_id return_url}]
 	set parent_project_url [export_vars -base "/intranet/projects/view" {parent_id return_url}]
