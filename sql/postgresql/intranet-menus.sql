644571dac481efe769698de20dc3007ecf5a8fbf
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-02-15 16:58:55 +0000

    - changes in projop

--- sql/postgresql/intranet-menus.sql
+++ sql/postgresql/intranet-menus.sql
@@ -109,6 +109,14 @@ declare
 
 	v_menu_id	  im_menus.menu_id%TYPE;
 begin
+
+	select	menu_id
+	into	v_menu_id
+	from	im_menus m
+	where	m.label = p_label;
+
+	IF v_menu_id is not null THEN return v_menu_id; END IF;
+
 	v_menu_id := acs_object__new (
                 p_menu_id,    -- object_id
                 p_object_type,  -- object_type
--- sql/postgresql/upgrade/upgrade-3.2.6.0.0-3.2.7.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.6.0.0-3.2.7.0.0.sql
@@ -2,6 +2,61 @@
 
 
 -------------------------------------------------------------
+-- Make im_menu__new double-creation tollerant
+
+create or replace function im_menu__new (integer, varchar, timestamptz, integer, varchar, integer,
+varchar, varchar, varchar, varchar, integer, integer, varchar) returns integer as '
+declare
+        p_menu_id         alias for $1;   -- default null
+        p_object_type     alias for $2;   -- default ''acs_object''
+        p_creation_date   alias for $3;   -- default now()
+        p_creation_user   alias for $4;   -- default null
+        p_creation_ip     alias for $5;   -- default null
+        p_context_id      alias for $6;   -- default null
+        p_package_name    alias for $7;
+        p_label           alias for $8;
+        p_name            alias for $9;
+        p_url             alias for $10;
+        p_sort_order      alias for $11;
+        p_parent_menu_id  alias for $12;
+        p_visible_tcl     alias for $13;  -- default null
+
+        v_menu_id         im_menus.menu_id%TYPE;
+begin
+
+        select  menu_id
+        into    v_menu_id
+        from    im_menus m
+        where   m.label = p_label;
+
+        IF v_menu_id is not null THEN return v_menu_id; END IF;
+
+        v_menu_id := acs_object__new (
+                p_menu_id,    -- object_id
+                p_object_type,  -- object_type
+                p_creation_date,        -- creation_date
+                p_creation_user,        -- creation_user
+                p_creation_ip,  -- creation_ip
+                p_context_id    -- context_id
+        );
+
+        insert into im_menus (
+                menu_id, package_name, label, name,
+                url, sort_order, parent_menu_id, visible_tcl
+        ) values (
+                v_menu_id, p_package_name, p_label, p_name, p_url,
+                p_sort_order, p_parent_menu_id, p_visible_tcl
+        );
+        return v_menu_id;
+end;' language 'plpgsql';
+
+
+
+
+
+
+
+-------------------------------------------------------------
 -- Slow query for Employees (the most frequent one...)
 -- because of missing outer-join reordering in PG 7.4...
 -- Now adding the "im_employees" (in extra-from/extra-where)
