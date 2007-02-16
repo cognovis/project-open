31dfbd4334799920cc6ab0aaed6cca977f16f2e8
Frank Bergmann <frank.bergmann@project-open.com>
2007-02-16 21:36:40 +0000

    - added upgrade script

--- sql/postgresql/intranet-ganttproject-create.sql
+++ sql/postgresql/intranet-ganttproject-create.sql
@@ -20,8 +20,8 @@ add percentage numeric(8,2);
 -- delete potentially existing menus and plugins if this 
 -- file is sourced multiple times during development...
 
-select im_component_plugin__del_module('intranet-ganttproject');
-select im_menu__del_module('intranet-ganttproject');
+-- select im_component_plugin__del_module('intranet-ganttproject');
+-- select im_menu__del_module('intranet-ganttproject');
 
 
 -- Show the ganttproject component in project page
@@ -41,6 +41,25 @@ SELECT im_component_plugin__new (
         10,                            -- sort_order
 	'im_ganttproject_component -project_id $project_id -current_page_url $current_url -return_url $return_url -export_var_list [list project_id]',
 	'lang::message::lookup "" intranet-ganttproject.Scheduling "Scheduling"'
+);
+
+
+
+SELECT im_component_plugin__new (
+        null,                           -- plugin_id
+        'acs_object',                   -- object_type
+        now(),                          -- creation_date
+        null,                           -- creation_user
+        null,                           -- creation_ip
+        null,                           -- context_id
+        'Project Gantt Resource Assignations',      -- plugin_name
+        'intranet-ganttproject',               -- package_name
+        'bottom',                        -- location
+        '/intranet/projects/view',      -- page_url
+        null,                           -- view_name
+        10,                            -- sort_order
+	'im_ganttproject_resource_cube -project_id $project_id -level_of_detail 2 -return_url $return_url -export_var_list [list project_id]',
+	'lang::message::lookup "" intranet-ganttproject.Project_Gantt_Resource_Assignations "Project Gantt Resource Assignations"'
 );
 
 
