7e7d64dc0833c30f8bd5b08c77199cc06bc73892
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-02-20 21:01:26 +0000

    - added create/drop scripts

---
+++ sql/postgresql/intranet-cost-center-drop.sql
@@ -0,0 +1,12 @@
+-- /packages/intranet-cost-center/sql/postgresql/intranet-cost-center-drop.sql
+--
+-- Project/Open Profit Center Management Extension
+--
+-- Copyright (c) 2004-2006 ]project-open[
+--
+-- All rights including reserved. To inquire license terms please
+-- refer to http://www.project-open.com/modules/<module-key>
+
+
+
+select im_menu__del_module('intranet-cost-center');
