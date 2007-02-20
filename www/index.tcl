2d9152a8a552751a797b8e6e1ae1c9e4b087b2fc
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-02-20 23:41:36 +0000

    - added wiki_mount

--- www/index.tcl
+++ www/index.tcl
@@ -20,24 +20,46 @@ set return_url "[ad_conn url]?[ad_conn query]"
 set parent_var :folder_id
 set page_title "Active Wikis"
 
-# Redirect to wiki if there is exactly one..
-set folder_ids [db_list folder_ids "
-	select f.folder_id 
-	from apm_packages p, cr_folders f 
-	where	p.package_id = f.package_id 
-		and p.package_key = 'wiki'
-"]
 
-if {[llength $folder_ids] == 1} {
-    set folder_id [lindex $folder_ids 0]
-    ad_returnredirect [export_vars -base "/wiki/admin/index?" {folder_id}]
+# ----------------------------------------------------
+# Redirect to admin if there is exactly one wiki...
+set wikis_sql "
+        select
+                ap.package_id,
+                cf.folder_id,
+                cr.title as wiki_title,
+                sn.name as wiki_mount
+        from
+                apm_packages ap,
+                cr_folders cf,
+                cr_items ci,
+                cr_revisions cr,
+                site_nodes sn
+        where
+                ap.package_key = 'wiki'
+                and cf.package_id = ap.package_id
+                and ci.parent_id = cf.folder_id
+                and ci.name = 'index'
+                and cr.revision_id = ci.live_revision
+                and sn.object_id = ap.package_id
+"
+
+set ctr 0
+db_foreach wikis $wikis_sql {
+    incr ctr
+}
+
+if {1 == $ctr} {
+    ad_returnredirect [export_vars -base "/$wiki_mount/admin/index?" {folder_id}]
     ad_script_abort
 }
 
 
+# ----------------------------------------------------
+# Show pages
 
 set wiki_component [im_wiki_home_component]
-    
+
 # Get the list of currently existing Wiki installations
 set wikis_sql "
         select
@@ -83,6 +105,7 @@ db_foreach wikis $wikis_sql {
 if {0 == $ctr} {
     append page_list "<tr><td colspan=99>No Wiki Pages</td></tr>\n"
 }
+
 
 append page_list "
 </table>
