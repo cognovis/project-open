aa338e7bf16076d461c37ba891140cb18385d5dd
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-02-20 22:21:26 +0000

    - edited info files for version information

--- www/segment/profiles.tcl
+++ www/segment/profiles.tcl
@@ -29,17 +29,42 @@ set bg "/intranet/images/girlongrass.600x400.jpg"
 set po "<span class=brandsec>&\#93;</span><span class=brandfirst>project-open</span><span class=brandsec>&\#91;</span>"
 set po_short "<span class=brandsec>&\#93;</span><span class=brandfirst>po</span><span class=brandsec>&\#91;</span>"
 
+
+
+
 array set profiles_array {}           
 set myset [ad_conn form]
+set profile_array_size 0
+
 for {set i 0} {$i < [ns_set size $myset]} {incr i} {
     set key [ns_set key $myset $i]
     set value [ns_set value $myset $i]
 
-    set a [lindex [split $key "."] 0]
-    if {$a != "profiles_array"} {
-	continue
-    }
-    set b [lindex [split $key "."] 1]
-   
-    set profiles_array($b) $value
+    set var_key [lindex [split $key "."] 0]
+    if {$var_key != "profiles_array"} { continue }
+
+    set var_value [lindex [split $key "."] 1]
+    set profiles_array($var_value) $value
+    incr profile_array_size
+}
+
+# Defaults if there is nothing specified
+if {0 == $profile_array_size} {
+    set key "employees,all_projects"
+    set profiles_array($key) "on"
+
+    set key "project_managers,all_projects"
+    set profiles_array($key) "on"
+
+    set key "project_managers,all_companies"
+    set profiles_array($key) "on"
+
+    set key "senior_managers,all_projects"
+    set profiles_array($key) "on"
+
+    set key "senior_managers,all_companies"
+    set profiles_array($key) "on"
+
+    set key "senior_managers,finance"
+    set profiles_array($key) "on"
 }
