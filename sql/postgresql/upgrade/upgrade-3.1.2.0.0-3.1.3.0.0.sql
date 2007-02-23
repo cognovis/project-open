4c4cc54d8809cb13dbdf52d8cee2a7ae61fc06f5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-02-23 20:16:30 +0000

    - added bitfromint4 to upgrade script

--- sql/postgresql/upgrade/upgrade-3.1.2.0.0-3.1.3.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.1.2.0.0-3.1.3.0.0.sql
@@ -4,6 +4,38 @@
 -------------------------------------------------------------
 
 
+
+
+create function inline_0 () returns integer as '
+-- Create a bitfromint4(integer) function if it doesn''t exists.
+-- Due to a bug in PG 7.3 this function is absent in PG 7.3.
+declare
+	v_bitfromint4_count integer;
+begin
+	select into v_bitfromint4_count count(*) from pg_proc where proname = ''bitfromint4'';
+	if v_bitfromint4_count = 0 then
+		create or replace function bitfromint4 (integer) returns bit varying as ''
+		begin
+			return $1::bit(32);
+		end;'' language ''plpgsql'' immutable strict;
+	end if;
+	return 1;
+end;' language 'plpgsql';
+select inline_0();
+drop function inline_0();
+
+
+
+-- There were trouble upgrading a Windows 3.1.2/7.4.4 version to V3.2 on PG 8.0.8
+--
+-- create or replace function bitfromint4 (integer) returns bit varying as '
+-- begin
+-- 	return $1::bit(32);
+-- end;' language 'plpgsql' immutable strict;
+
+
+
+
 create or replace function im_day_enumerator (
         date, date
 ) returns setof date as '
@@ -39,12 +71,12 @@ begin
         where privilege = ''edit_companies_all'';
         IF 0 != v_count THEN return 0; END IF;
 
-	select acs_privilege__create_privilege(
+	PERFORM acs_privilege__create_privilege(
 		''edit_companies_all'',
 		''Edit All Companies'',
 		''Edit All Companies''
 	);
-	select acs_privilege__add_child(''admin'', ''edit_companies_all'');
+	PERFORM acs_privilege__add_child(''admin'', ''edit_companies_all'');
 
     return 0;
 end;' language 'plpgsql';
