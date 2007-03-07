2f4f00f14129f1d2c063333a40f26b7d9e32f5e3
Frank Bergmann <frank.bergmann@project-open.com>
2007-03-07 18:24:53 +0000

    - added copyright notice

--- sql/postgresql/notifications-create.sql
+++ sql/postgresql/notifications-create.sql
@@ -1,3 +1,12 @@
+-- /packages/intranet-workflow/sql/oracle/intranet-workflow-create.sql
+--
+-- Copyright (c) 2003-2007 ]project-open[
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author frank.bergmann@project-open.com
+
 
 create function inline_0() 
 returns integer as '
