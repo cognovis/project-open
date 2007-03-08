a48481ebd35c08aaf72f831290a2dba2bf18e3c8
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-03-08 01:19:30 +0000

    - added acs-lang extensions

--- sql/postgresql/upgrade/upgrade-3.2.7.0.0-3.2.8.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.7.0.0-3.2.8.0.0.sql
@@ -116,3 +116,127 @@ insert into im_biz_object_role_map values ('im_timesheet_task',85,1300);
 create index im_project_treesort_idx on im_projects(tree_sortkey);
 
 
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------
+-- Determine the default locale for the user
+create or replace function acs_lang_get_locale_for_user (integer) returns text as '
+declare
+	p_user_id	alias for $1;
+
+	v_workflow_key		varchar(100);
+	v_transition_key	varchar(100);
+	v_acs_lang_package_id	integer;
+	v_locale		varchar(10);
+begin
+	-- Get the users local from preferences
+	select	locale into v_locale
+	from	user_preferences
+	where	user_id = p_user_id;
+
+	-- Get users locale from global default
+	IF v_locale is null THEN
+		select	package_id
+		into	v_acs_lang_package_id
+		from	apm_packages
+		where	package_key = ''acs-lang'';
+
+		v_locale := apm__get_value (v_acs_lang_package_id, ''SiteWideLocale'');
+	END IF;
+
+	-- Partial locale - lookup complete one
+	IF length(v_locale) = 2 THEN
+		select	locale into v_locale
+		from	ad_locales
+		where	language = v_locale
+			and enabled_p = ''t''
+			and (default_p = ''t''
+			   or (select count(*) from ad_locales where language = v_locale) = 1
+			);
+	END IF;
+
+	-- Default: English
+	IF v_locale is null THEN
+		v_locale := ''en_US'';
+	END IF;
+
+	return v_locale;
+end;' language 'plpgsql';
+
+
+-- Determine the message string for (locale, package_key, message_key):
+create or replace function acs_lang_lookup_message (text, text, text) returns text as '
+declare
+	p_locale		alias for $1;
+	p_package_key		alias for $2;
+	p_message_key		alias for $3;
+	v_message		text;
+	v_locale		text;
+	v_acs_lang_package_id	integer;
+begin
+	-- --------------------------------------------
+	-- Check full locale
+	select	message into v_message
+	from	lang_messages
+	where	message_key = p_message_key
+		and package_key = p_package_key
+		and locale = p_locale;
+	IF v_message is not null THEN return v_message; END IF;
+
+	-- --------------------------------------------
+	-- Partial locale - lookup complete one
+	v_locale := substring(p_locale from 1 for 2);
+
+	select	locale into v_locale
+	from	ad_locales
+	where	language = v_locale
+		and enabled_p = ''t''
+		and (default_p = ''t'' or
+		(select count(*) from ad_locales where language = v_locale) = 1);
+
+	select	message into v_message
+	from	lang_messages
+	where	message_key = p_message_key
+		and package_key = p_package_key
+		and locale = v_locale;
+	IF v_message is not null THEN return v_message; END IF;
+
+	-- --------------------------------------------
+	-- Try System Locale
+	select	package_id into	v_acs_lang_package_id
+	from	apm_packages
+	where	package_key = ''acs-lang'';
+	v_locale := apm__get_value (v_acs_lang_package_id, ''SiteWideLocale'');
+
+	select	message into v_message
+	from	lang_messages
+	where	message_key = p_message_key
+		and package_key = p_package_key
+		and locale = v_locale;
+	IF v_message is not null THEN return v_message; END IF;
+
+	-- --------------------------------------------
+	-- Try with English...
+	v_locale := ''en_US'';
+	select	message into v_message
+	from	lang_messages
+	where	message_key = p_message_key
+		and package_key = p_package_key
+		and locale = v_locale;
+	IF v_message is not null THEN return v_message; END IF;
+
+	-- --------------------------------------------
+	-- Nothing found...
+	v_message := ''MISSING '' || p_locale || '' TRANSLATION for '' || p_package_key || ''.'' || p_message_key;
+	return v_message;	
+
+end;' language 'plpgsql';
+
+
