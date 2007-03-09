95dc249a7e3ea15f7d1aea44f7f830bb9ff3a0f7
arvato <>
2007-03-09 22:35:14 +0000

    - added expenses to ProjectsCSV

--- sql/postgresql/upgrade/upgrade-3.2.7.0.0-3.2.8.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.7.0.0-3.2.8.0.0.sql
@@ -4,3 +4,7 @@ insert into im_view_columns (column_id, view_id, group_id, column_name, column_r
 extra_select, extra_where, sort_order, visible_for) values (
 2478,24,NULL,'LoggedHours','$reported_hours_cache','','',78,'');
 
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (
+2491,24,NULL,'Expenses','$cost_expense_logged_cache','','',91,'');
+
