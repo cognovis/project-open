d79e12f5276ee97248af391bd86fb7b7465aae17
arvato <>
2007-03-09 23:23:03 +0000

    - fixed reporting bug excluding all "closed" projects - added caching improvements

--- www/project-trans-tasks.tcl
+++ www/project-trans-tasks.tcl
@@ -139,7 +139,7 @@ if {"" != $project_id && 0 != $project_id} {
 	where
 		parent_p.project_id = :project_id
 		and p.tree_sortkey between parent_p.tree_sortkey and tree_right(parent_p.tree_sortkey)
-		and p.project_status_id not in (select child_id from im_category_hierarchy where parent_id = [im_project_status_closed])
+		and p.project_status_id not in ([im_project_status_deleted])
     )"
 }
 
@@ -204,7 +204,7 @@ set sql "
 		and p.end_date >= to_date(:start_date, 'YYYY-MM-DD')
 		and p.end_date < to_date(:end_date, 'YYYY-MM-DD')
 		and p.end_date::date < to_date(:end_date, 'YYYY-MM-DD')
-		and p.project_status_id not in (select child_id from im_category_hierarchy where parent_id = [im_project_status_closed])
+		and p.project_status_id not in ([im_project_status_deleted])
 		and p.company_id = cust.company_id
 		and children.project_id = t.project_id
 		$where_clause
