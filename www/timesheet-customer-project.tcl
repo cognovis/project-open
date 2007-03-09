ba5c535937f4ffb1b526ed9a852c3f3274ae9565
arvato <>
2007-03-09 23:23:03 +0000

    - fixed reporting bug excluding all "closed" projects - added caching improvements

--- www/timesheet-customer-project.tcl
+++ www/timesheet-customer-project.tcl
@@ -148,7 +148,7 @@ if {[info exists project_id]} {
 	where
 		parent_p.project_id = :project_id
 		and p.tree_sortkey between parent_p.tree_sortkey and tree_right(parent_p.tree_sortkey)
-		and p.project_status_id not in (select child_id from im_category_hierarchy where parent_id = [im_project_status_closed])
+		and p.project_status_id not in ([im_project_status_deleted])
     )"
 }
 
@@ -178,7 +178,7 @@ from
 	cc_users u
 where
 	h.project_id = p.project_id
-	and p.project_status_id not in (select child_id from im_category_hierarchy where parent_id = [im_project_status_closed])
+	and p.project_status_id not in ([im_project_status_deleted])
 	and h.user_id = u.user_id
 	and h.day >= to_date(:start_date, 'YYYY-MM-DD')
 	and h.day < to_date(:end_date, 'YYYY-MM-DD')
@@ -207,7 +207,7 @@ from
 	cc_users u
 where
 	s.user_id = u.user_id
-	and p.project_status_id not in (select child_id from im_category_hierarchy where parent_id = [im_project_status_closed])
+	and p.project_status_id not in ([im_project_status_deleted])
 	and s.company_id = c.company_id
 	and s.project_id = p.project_id
 order by
