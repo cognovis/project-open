1c64660423c668b7de6a6e7d82957cd3454746c2
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-03-10 23:07:34 +0000

    - added im_cost_cache_sweeper

--- tcl/intranet-cost-init.tcl
+++ tcl/intranet-cost-init.tcl
@@ -13,5 +13,5 @@ ad_library {
 nsv_set intranet_cost_sweeper sweeper_p 0
 
 # Check for changed files every X minutes
-ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-cost -parameter CostCacheSweeperInterval -default 57 ] im_cost_cache_sweeper
+ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-cost -parameter CostCacheSweeperInterval -default 57] im_cost_cache_sweeper
 
