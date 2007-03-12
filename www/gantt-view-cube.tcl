9d48b678a32dc7bad900a2061e2187f1988e9f54
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-03-12 04:31:08 +0000

    - improved parametrization

--- www/gantt-view-cube.tcl
+++ www/gantt-view-cube.tcl
@@ -24,8 +24,8 @@ ad_page_contract {
     { user_name_link_opened "" }
     { opened_projects "" }
     { zoom "" }
-    { max_col 100 }
-    { max_row 1000 }
+    { max_col 25 }
+    { max_row 100 }
 }
 
 # ---------------------------------------------------------------
@@ -56,6 +56,8 @@ set page_title [lang::message::lookup "" intranet-reporting.Gantt_Resources "Gan
 
 
 set html [im_ganttproject_gantt_component \
+	-auto_open 1 \
+	-export_var_list [list project_id] \
 	-start_date $start_date \
 	-end_date $end_date \
 	-top_vars $top_vars \
