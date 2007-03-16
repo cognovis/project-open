68e2c6f3e7965f49b91b6ddab839c743acb6510d
arvato <>
2007-03-16 15:42:30 +0000

    - added Calendar Menu item

--- sql/postgresql/intranet-calendar-create.sql
+++ sql/postgresql/intranet-calendar-create.sql
@@ -432,3 +432,66 @@ SELECT im_component_plugin__new (
 	'im_calendar_home_component',
 	'lang::message::lookup "" intranet-calendar.Calendar "Calendar"'
 );
+
+
+
+
+---------------------------------------------------------
+-- Setup the Calendar main menu entry
+--
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        -- Menu IDs
+        v_menu                  integer;
+	v_main_menu		integer;
+
+        -- Groups
+        v_employees             integer;
+        v_accounting            integer;
+        v_senman                integer;
+        v_companies             integer;
+        v_freelancers           integer;
+        v_proman                integer;
+        v_admins                integer;
+BEGIN
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_senman from groups where group_name = ''Senior Managers'';
+    select group_id into v_proman from groups where group_name = ''Project Managers'';
+    select group_id into v_accounting from groups where group_name = ''Accounting'';
+    select group_id into v_employees from groups where group_name = ''Employees'';
+    select group_id into v_companies from groups where group_name = ''Customers'';
+    select group_id into v_freelancers from groups where group_name = ''Freelancers'';
+
+    select menu_id
+    into v_main_menu
+    from im_menus
+    where label=''main'';
+
+    v_menu := im_menu__new (
+        null,                   -- p_menu_id
+        ''acs_object'',         -- object_type
+        now(),                  -- creation_date
+        null,                   -- creation_user
+        null,                   -- creation_ip
+        null,                   -- context_id
+        ''intranet-calendar'',      -- package_name
+        ''calendar'',               -- label
+        ''Calendar'',               -- name
+        ''/calendar/'',    -- url
+        74,                     -- sort_order
+        v_main_menu,            -- parent_menu_id
+        null                    -- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
