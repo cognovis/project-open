efe29cd9c12ebfd59ceae763fc03b04498f801b0
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-03-20 19:46:15 +0000

    - duplicated update to remove double-"e" error in /intranet/users

--- sql/postgresql/upgrade/upgrade-3.2.8.0.0-3.2.9.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.8.0.0-3.2.9.0.0.sql
@@ -6,3 +6,15 @@ alter table persons add demo_password varchar(50);
 
 
 
+
+
+-------------------------------------------------------------
+-- Slow query for Employees (the most frequent one...)
+-- because of missing outer-join reordering in PG 7.4...
+-- Now adding the "im_employees" (in extra-from/extra-where)
+-- INSIDE the basic query.
+
+update im_view_columns set extra_from = null, extra_where = null where column_id = 5500;
+
+
+
