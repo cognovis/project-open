717ad34342303cda01097a9937a43da10416379f
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-05-02 20:39:38 +0000

    - added a clean error message for the case of duplicate calendar names

--- www/admin/calendar-edit.tcl
+++ www/admin/calendar-edit.tcl
@@ -20,9 +20,22 @@ ad_form -name calendar -form {
 } -edit_request {
     set calendar_name [calendar::name $calendar_id]
 } -new_data {
-    calendar::new \
-        -owner_id [ad_conn user_id] \
-        -calendar_name $calendar_name 
+
+    if {[catch {
+	calendar::new \
+	    -owner_id [ad_conn user_id] \
+	    -calendar_name $calendar_name 
+    } errmsg]} {
+	ad_return_complaint 1 "
+		<b>[lang::message::lookup "" calendar.Duplicate_Calendar_Name "Duplicate Calendar Name:"]</b><p>
+		[lang::message::lookup "" calendar.Duplicate_Calendar_Name_Message "
+			A calendar with name '%calendar_name%' already exists.<br>
+			Please choose a different name.<p>
+			Here is the error message for reference:<p>
+			<pre>%errmsg%</pre>
+		"]
+	"
+    }
 } -edit_data {
     calendar::rename \
         -calendar_id $calendar_id \
