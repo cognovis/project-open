224ffd8c55a960ed48b9c8a768c0519423dd0061
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-05-14 20:09:54 +0000

    - updated calendar drop script

--- sql/postgresql/intranet-calendar-drop.sql
+++ sql/postgresql/intranet-calendar-drop.sql
@@ -8,6 +8,21 @@
 -- @author frank.bergmann@project-open.com
 
 
+
+-----------------------------------------------------
+-- Drop menus and components defined by the module
+
+select im_menu__del_module('intranet-calendar');
+select im_component_plugin__del_module('intranet-calendar');
+
+
+
 drop trigger im_projects_calendar_update_tr on im_projects;
+drop trigger im_trans_tasks_calendar_update_tr on im_trans_tasks;
+drop trigger im_forum_topics_calendar_update_tr on im_forum_topics;
+
+
 drop function im_projects_calendar_update_tr();
+drop function im_trans_tasks_calendar_update_tr();
+drop function im_forum_topics_calendar_update_tr();
 
