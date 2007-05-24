bb6932b137620a5311f0dd493d2880d3e70685dc
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-05-24 17:36:39 +0000

    - BaselKB new dereferencing functions included

--- sql/postgresql/intranet-core-create.sql
+++ sql/postgresql/intranet-core-create.sql
@@ -425,6 +425,35 @@ END;' language 'plpgsql';
 
 
 
+create or replace function im_integer_from_id(integer)
+returns varchar as '
+DECLARE
+        v_result        alias for $1;
+BEGIN
+        return v_result::varchar;
+END;' language 'plpgsql';
+
+
+
+create or replace function im_integer_from_id(varchar)
+returns varchar as '
+DECLARE
+        v_result        alias for $1;
+BEGIN
+        return v_result;
+END;' language 'plpgsql';
+
+
+
+
+create or replace function im_integer_from_id(numeric)
+returns varchar as '
+DECLARE
+        v_result        alias for $1;
+BEGIN
+        return v_result::varchar;
+END;' language 'plpgsql';
+
 
 
 
