671f93e0f64a671d1244bba5eee17e5a1ae37fba
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-06-18 10:58:53 +0000

    - relationship between contacts and companies

--- import_sql/import.lexcelera.sql
+++ import_sql/import.lexcelera.sql
@@ -1,6 +1,6 @@
 
 ---------------------------------------------------------------------------------
--- Offices
+-- Companies and Offices
 ---------------------------------------------------------------------------------
 
 -- Check for duplicate Company Names
@@ -172,6 +172,128 @@ drop function inline_0 ();
 --  MatUserID			smallint	
 --  MatDte			timestamp without time zone
 --  JobDir			varchar		-- company_path = replace(row."JobDir", ''/'', ''''),
+
+
+
+
+
+---------------------------------------------------------------------------------
+-- tblCon* - Contacts
+---------------------------------------------------------------------------------
+
+
+create or replace function inline_0 ()
+returns integer as '
+DECLARE
+        row			RECORD;
+	v_contact_name		varchar;
+	v_contact_email		varchar;
+	v_user_id		integer;
+	v_member_p		integer;
+	v_ucont_exists_p	integer;
+BEGIN
+    for row in
+	select	*
+	from	"tblContMaster"
+	where	"FirstNm" is not null and "LastNm" is not null
+	LIMIT 1000000
+    loop
+	v_contact_name = row."FirstNm"|| '' '' ||row."LastNm";
+	v_contact_email := lower(row."FirstNm"||''.''||row."LastNm"||''@nowhere.com'');
+
+	select	person_id into v_user_id from persons p
+	where	lower(trim(p.first_names)) = lower(trim(row."FirstNm")) and
+		lower(trim(p.last_name)) = lower(trim(row."LastNm"));
+
+	-- Create User or get the existing user based on email
+	IF v_user_id is NULL THEN
+	    -- Create the new user without a reasonable password
+	    select acs__add_user(
+		null, ''user'', now(), null, ''0.0.0.0'', null,
+		row."FirstNm" || '' '' || row."LastNm" || ''.'' || row."ContID",
+		v_contact_email, null, row."FirstNm", row."LastNm",
+		''password'', ''salt'',
+		row."FirstNm" || '' '' || row."LastNm" || ''.'' || row."ContID",
+		''f'', ''approved''
+	    ) into v_user_id;
+
+	    select count(*) into v_ucont_exists_p from users_contact
+	    where user_id = v_user_id;
+	    IF 0 = v_ucont_exists_p THEN 
+		insert into users_contact(user_id) values (v_user_id);
+	    END IF;
+
+	    select count(*) into v_member_p from group_distinct_member_map 
+	    where member_id = v_user_id and group_id=461;
+            IF 0 = v_member_p THEN
+                PERFORM membership_rel__new(461, v_user_id);
+            END IF;
+
+	    RAISE NOTICE ''Created the customer contact: %'', v_user_id;
+	END IF;	
+
+	-- Update the person contact_id to find it later.
+	update persons set
+		lxc_contact_id = row."ContID"
+	where person_id = v_user_id;
+
+    end loop;
+    return 0;
+END;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+-- Relationship between company and contact
+create or replace function inline_0 ()
+returns integer as '
+DECLARE
+        row			RECORD;
+	v_user_id		integer;
+	v_company_id		integer;
+	v_rel_exists_p		integer;
+BEGIN
+    for row in
+	select	*
+	from	"tblContComp" cc,
+		"tblContMaster" m
+	where	
+		cc."ContID" != 0
+		and cc."ContID" = m."ContID"
+		and m."FirstNm" is not null
+		and m."LastNm" is not null
+    loop
+	select	person_id into v_user_id
+	from	persons where lxc_contact_id = row."ContID";
+
+        select  company_id into v_company_id
+        from    im_companies
+        where	lxc_company_id = row."CompID";
+
+	select	count(*) into v_rel_exists_p
+	from	acs_rels
+	where	object_id_one = v_company_id
+		and object_id_two = v_user_id;
+
+	IF 0 = v_rel_exists_p AND v_user_id is not NULL THEN
+	    RAISE NOTICE ''Create new rel: comp=%, user=%/% '', v_company_id, row."ContID", v_user_id;
+	    perform im_biz_object_member__new (
+		null,
+		''im_biz_object_member'',
+		v_company_id,
+		v_user_id,
+		1300,
+		0, ''0.0.0.0''
+	    );
+	END IF;
+
+    end loop;
+    return 0;
+END;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 
 
 
@@ -636,11 +758,6 @@ BEGIN
 END;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
-
-
-
-
-
 
 
 -- Translator
