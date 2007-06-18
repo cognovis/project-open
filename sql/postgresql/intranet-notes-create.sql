a22051a3f524f282600a90d49557dfb187f192af
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-06-18 11:42:47 +0000

    - added user note component

--- sql/postgresql/intranet-notes-create.sql
+++ sql/postgresql/intranet-notes-create.sql
@@ -240,6 +240,22 @@ SELECT im_component_plugin__new (
 	'im_notes_project_component -object_id $company_id'	-- component_tcl
 );
 
+SELECT im_component_plugin__new (
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'User Notes',			-- plugin_name
+	'intranet-notes',		-- package_name
+	'right',			-- location
+	'/intranet/users/view',		-- page_url
+	null,				-- view_name
+	90,				-- sort_order
+	'im_notes_project_component -object_id $user_id'	-- component_tcl
+);
+
 
 
 -----------------------------------------------------------
