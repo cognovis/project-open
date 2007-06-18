bf12fbf710a6379f2a5a7ffce0b9285bb0ee3106
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-06-18 20:44:02 +0000

    - Lexcelera import

--- import_sql/import.lexcelera.sql
+++ import_sql/import.lexcelera.sql
@@ -922,7 +922,7 @@ BEGIN
 		t."TranID" = a."TranID"
 		and "LastNm" is not NULL 
 		and "FirstNm" is not NULL
-	LIMIT 0
+	LIMIT 10000000
     loop
 	v_user_email := lower(row."FirstNm"||''.''||row."LastNm"||''@nowhere.com'');
 
@@ -1091,7 +1091,6 @@ drop function inline_0 ();
 
 
 -- Translator
--- "tblTranEvaluation";
 -- "tblTranFileType";
 -- "tblTranHardware";
 -- "tblTranLang";
@@ -1127,7 +1126,7 @@ BEGIN
         select	*
 	from	"tblTranWeb"
 	order by "TranID"
-	LIMIT 10000000
+	LIMIT 0
     loop
 	select	company_id into v_company_id
 	from	im_companies 
@@ -1222,7 +1221,7 @@ BEGIN
         select	*
 	from	"tblTranPhon"
 	where	"CoordTypeID" != 0
-	LIMIT 10
+	LIMIT 0
     loop
 	select	company_id into v_company_id
 	from	im_companies 
@@ -1301,7 +1300,7 @@ BEGIN
 	RAISE NOTICE ''Note: comp=%, cid=%'', 
 	row."TranID", v_company_id;
 
-	IF v_topic_id is NULL THEN
+	IF v_company_id is not null AND v_topic_id is NULL THEN
 	    insert into im_forum_topics (
 		topic_id, object_id,
 		topic_type_id, topic_status_id,
@@ -1320,6 +1319,143 @@ BEGIN
 		substring(v_note_text for 60),
 		v_note_text,
 		row."FollowUpDte"
+	    );
+	END IF;
+
+    end loop;
+    return 0;
+END;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+
+
+---------------------------------------------------------------------------------
+-- tblTranEvaluation - Notes for companies
+---------------------------------------------------------------------------------
+
+create or replace function inline_0 ()
+returns integer as '
+DECLARE
+        row			RECORD;
+	v_company_id		integer;
+	v_topic_id		integer;
+	v_note_text		text;
+	v_subject		text;
+	v_person_id		integer;
+BEGIN
+    for row in
+        select	*
+	from	"tblTranEvaluation" e,
+		"lktblTranEvaluation" v
+	where
+		e."EvaluationID" = v."EvaluationID"
+	LIMIT 10
+    loop
+	select	company_id into v_company_id
+	from	im_companies where lxc_company_id = row."TranID";
+
+	select	person_id into v_person_id
+	from	persons where lxc_user_id = row."MatUserID";
+	IF v_person_id is NULL THEN v_person_id = 624; END IF;
+
+	v_note_text := row."EvaluationNm" || '' '' || row."Rating"::varchar;
+
+	select	topic_id into v_topic_id
+	from	im_forum_topics
+	where	object_id = v_company_id
+		and message = v_note_text;
+
+	RAISE NOTICE ''Note: comp=%, cid=%'', 
+	row."TranID", v_company_id;
+
+	IF v_company_id is not null AND v_topic_id is NULL THEN
+	    insert into im_forum_topics (
+		topic_id, object_id,
+		topic_type_id, topic_status_id,
+		posting_date,
+		owner_id,
+		scope,
+		subject,
+		message
+	    ) values (
+		nextval(''im_forum_topics_seq''), v_company_id,
+		1108, 1200,
+		row."MatDte"::date,
+		v_person_id,
+		''group'',
+		substring(v_note_text for 60),
+		v_note_text
+	    );
+	END IF;
+
+    end loop;
+    return 0;
+END;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
+
+
+---------------------------------------------------------------------------------
+-- tblTranReview - Review of translator's results per job
+---------------------------------------------------------------------------------
+
+create or replace function inline_0 ()
+returns integer as '
+DECLARE
+        row			RECORD;
+	v_company_id		integer;
+	v_topic_id		integer;
+	v_note_text		text;
+	v_subject		text;
+	v_person_id		integer;
+BEGIN
+    for row in
+        select	*
+	from	"tblTranReview"
+	LIMIT 10
+    loop
+	select	company_id into v_company_id
+	from	im_companies where lxc_company_id = row."TranID";
+
+	select	person_id into v_person_id
+	from	persons where lxc_user_id = row."MatUserID";
+	IF v_person_id is NULL THEN v_person_id = 624; END IF;
+
+	v_note_text := ''Evaluation for Job #'' || row."JobID" || '': '' || row."TranReviewNotes";
+
+	select	topic_id into v_topic_id
+	from	im_forum_topics
+	where	object_id = v_company_id
+		and message = v_note_text;
+
+	RAISE NOTICE ''Note: comp=%, cid=%'', 
+	row."TranID", v_company_id;
+
+	IF v_company_id is not null AND v_topic_id is NULL THEN
+	    insert into im_forum_topics (
+		topic_id, object_id,
+		topic_type_id, topic_status_id,
+		posting_date,
+		owner_id,
+		scope,
+		subject,
+		message
+	    ) values (
+		nextval(''im_forum_topics_seq''), v_company_id,
+		1108, 1200,
+		row."MatDte"::date,
+		v_person_id,
+		''group'',
+		substring(v_note_text for 60),
+		v_note_text
 	    );
 	END IF;
 
