bc791cea0e0cac7e3f10f89a8415884d4b507349
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-06-23 17:41:02 +0000

    - starting to get trans-rfqs going

--- sql/postgresql/intranet-freelance-create.sql
+++ sql/postgresql/intranet-freelance-create.sql
@@ -47,7 +47,7 @@ create table im_freelancers (
 );
 
 -----------------------------------------------------------
--- Skills
+-- Freelance Skills
 --
 -- We want to say something like: This user claims he is excellent 
 -- at translating into Spanish, but we haven't checked it yet.
@@ -91,6 +91,44 @@ create or replace view im_freelance_skill_types as
 select category_id as skill_type_id, category as skill_type
 from im_categories 
 where category_type = 'Intranet Skill Type';
+
+
+
+
+
+-----------------------------------------------------------
+-- Skills Associated with other Objects
+--
+-- We want to say: For this RFQ you need to translate 
+-- from English into Spanish (required condition) and be 
+-- preferably specialized in "Legal" or "Business".
+
+
+create table im_object_freelance_skill_map (
+	object_id		integer not null 
+				constraint im_o_skills_user_fk
+				references acs_objects,
+	skill_id		integer not null 
+				constraint im_o_skills_skill_fk
+				references im_categories,
+	skill_type_id		integer not null 
+				constraint im_o_skills_skill_type_fk
+				references im_categories,
+	skill_weight		integer
+				constraint im_o_skills_claimed_ck
+				check (skill_weight > 0 and skill_weight <= 100),
+	skill_required_p	char(1) default('f')
+                                constraint im_o_skills_required_p
+                                check (skill_required_p in ('t','f')),
+	-- "map" type of table
+	constraint im_o_skills_pk
+	primary key (object_id, skill_id)
+);
+
+create index im_object_freelance_skillsmap_idx on im_object_freelance_skill_map(object_id);
+
+
+
 
 -----------------------------------------------------------
 -- Menu Modifications
