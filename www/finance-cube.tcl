b6bbc08ee0564cccd70f9fe59d35ee2889587fb5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-07-08 22:49:38 +0000

    - now more or less working with generic component

--- www/finance-cube.tcl
+++ www/finance-cube.tcl
@@ -11,35 +11,93 @@ ad_page_contract {
 } {
     { start_date "" }
     { end_date "" }
-    { top_vars "year quarter_of_year" }
-    { top_scale1 "" }
-    { top_scale2 "" }
-    { left_scale1 "cost_type" }
-    { left_scale2 "customer_type" }
-    { left_scale3 "" }
+    { top_var1 "year quarter_of_year" }
+    { top_var2 "" }
+    { top_var3 "" }
+    { left_var1 "customer_name" }
+    { left_var2 "" }
+    { left_var3 "" }
     { cost_type_id:multiple "3700 3704 3722 3718" }
     { customer_type_id:integer 0 }
     { customer_id:integer 0 }
+
+    { left_vars "" }
+    { top_vars "" }
 }
 
 # ------------------------------------------------------------
 # Define Dimensions
 
 # Left Dimension - defined by users selects
-set left_vars [list]
-if {"" != $left_scale1} { lappend left_vars $left_scale1 }
-if {"" != $left_scale2} { lappend left_vars $left_scale2 }
-if {"" != $left_scale3} { lappend left_vars $left_scale3 }
+set left {}
+if {"" != $left_vars} {
+    # override left vars with elements from list
+    set left_var1 [lindex $left_vars 0]
+    set left_var2 [lindex $left_vars 1]
+    set left_var3 [lindex $left_vars 2]
+}
+if {"" != $left_var1} { lappend left $left_var1 }
+if {"" != $left_var2} { lappend left $left_var2 }
+if {"" != $left_var3} { lappend left $left_var3 }
+
 
 # Top Dimension
-set top_vars [ns_urldecode $top_vars]
-if {"" != $top_scale1} { lappend top_vars $top_scale1 }
-if {"" != $top_scale2} { lappend top_vars $top_scale2 }
+set top_var1 [ns_urldecode $top_var1]
+
+
+if {"" != $top_vars} {
+    # override top vars with elements from list
+    # Special logic for top_var1 for date dimension - very ugly...
+    set top_var1 [lindex $top_vars 0]
+
+    if {[lrange $top_vars 0 3] == {year quarter_of_year month_of_year day_of_month}} {
+	set top_var1 [lrange $top_vars 0 3]
+	set top_vars [lrange $top_vars 3 end]
+    }
+    if {[lrange $top_vars 0 2] == {year quarter_of_year month_of_year}} {
+	set top_var1 [lrange $top_vars 0 2]
+	set top_vars [lrange $top_vars 2 end]
+    }
+    if {[lrange $top_vars 0 1] == {year quarter_of_year}} {
+	set top_var1 [lrange $top_vars 0 1]
+	set top_vars [lrange $top_vars 1 end]
+    }
+    if {[lrange $top_vars 0 1] == {year month_of_year}} {
+	set top_var1 [lrange $top_vars 0 1]
+	set top_vars [lrange $top_vars 1 end]
+    }
+    if {[lrange $top_vars 0 1] == {year week_of_year}} {
+	set top_var1 [lrange $top_vars 0 1]
+	set top_vars [lrange $top_vars 1 end]
+    }
+    if {[lrange $top_vars 0 1] == {quarter_of_year year}} {
+	set top_var1 [lrange $top_vars 0 1]
+	set top_vars [lrange $top_vars 1 end]
+    }
+    if {[lrange $top_vars 0 1] == {month_of_year year}} {
+	set top_var1 [lrange $top_vars 0 1]
+	set top_vars [lrange $top_vars 1 end]
+    }
+
+    set top_var2 [lindex $top_vars 1]
+    set top_var3 [lindex $top_vars 2]
+}
+
+
+set top {}
+if {"" != $top_var1} { lappend top $top_var1 }
+if {"" != $top_var2} { lappend top $top_var2 }
+if {"" != $top_var3} { lappend top $top_var3 }
+
+# Flatten lists - kinda dirty...
+regsub -all {[\{\}]} $top "" top
+regsub -all {[\{\}]} $left "" left
+
 
 # The complete set of dimensions - used as the key for
 # the "cell" hash. Subtotals are calculated by dropping on
 # or more of these dimensions
-set dimension_vars [concat $top_vars $left_vars]
+set dimension_vars [concat $top $left]
 
 # Check for duplicate variables
 set unique_dimension_vars [lsort -unique $dimension_vars]
@@ -266,33 +324,33 @@ ns_write "
 	<tr>
 	  <td class=form-label>Left 1</td>
 	  <td class=form-widget>
-	    [im_select -translate_p 0 left_scale1 $left_scale_options $left_scale1]
+	    [im_select -translate_p 0 left_var1 $left_scale_options $left_var1]
 	  </td>
 
 	  <td class=form-label>Date Dimension</td>
 	    <td class=form-widget>
-	      [im_select -translate_p 0 top_vars $top_vars_options $top_vars]
+	      [im_select -translate_p 0 top_var1 $top_vars_options $top_var1]
 	  </td>
 	</tr>
 	<tr>
 	  <td class=form-label>Left 2</td>
 	  <td class=form-widget>
-	    [im_select -translate_p 0 left_scale2 $left_scale_options $left_scale2]
+	    [im_select -translate_p 0 left_var2 $left_scale_options $left_var2]
 	  </td>
 
 	  <td class=form-label>Top 1</td>
 	  <td class=form-widget>
-	    [im_select -translate_p 0 top_scale1 $left_scale_options $top_scale1]
+	    [im_select -translate_p 0 top_var2 $left_scale_options $top_var2]
 	  </td>
 	</tr>
 	<tr>
 	  <td class=form-label>Left 3</td>
 	  <td class=form-widget>
-	    [im_select -translate_p 0 left_scale3 $left_scale_options $left_scale3]
+	    [im_select -translate_p 0 left_var3 $left_scale_options $left_var3]
 	  </td>
 	  <td class=form-label>Top 2</td>
 	  <td class=form-widget>
-	    [im_select -translate_p 0 top_scale2 $left_scale_options $top_scale2]
+	    [im_select -translate_p 0 top_var3 $left_scale_options $top_var3]
 	  </td>
 	</tr>
 	<tr>
@@ -313,352 +371,47 @@ ns_write "
 </tr>
 </form>
 </table>
-<table border=0 cellspacing=1 cellpadding=1>
 "
 
 
-
 # ------------------------------------------------------------
-# Conditional SQL Where-Clause
+# Get the cube data
 #
 
-set criteria [list]
+set cube_array [im_reporting_cubes_cube \
+    -cube_name "finance" \
+    -start_date $start_date \
+    -end_date $end_date \
+    -left_vars $left \
+    -top_vars $top \
+    -cost_type_id $cost_type_id \
+    -customer_type_id $customer_type_id \
+    -customer_id $customer_id \
+]
 
-if {"" != $customer_id && 0 != $customer_id} {
-    lappend criteria "c.customer_id = :customer_id"
-}
+array set cube $cube_array
 
-if {1} {
-    lappend criteria "c.cost_type_id in ([join [im_sub_categories $cost_type_id] ", "])"
-}
-
-if {"" != $customer_type_id && 0 != $customer_type_id} {
-    lllappend criteria "pcust.company_type_id in ([join [im_sub_categories $customer_type_id] ", "])
-}
-
-set where_clause [join $criteria " and\n\t\t\t"]
-if { ![empty_string_p $where_clause] } {
-    set where_clause " and $where_clause"
-}
-
-
-# ------------------------------------------------------------
-# Define the report - SQL, counters, headers and footers 
-#
-
-# Inner - Try to be as selective as possible and select
-# the relevant data from the fact table.
-set inner_sql "
-		select
-			p.project_name as sub_project_name,
-			p.project_nr as sub_project_nr,
-			p.project_type_id as sub_project_type_id,
-			p.project_status_id as sub_project_status_id,
-			tree_ancestor_key(p.tree_sortkey, 1) as main_project_sortkey,
-			trunc((c.paid_amount * 
-			  im_exchange_rate(c.effective_date::date, c.currency, 'EUR')) :: numeric
-			  , 2) as paid_amount_converted,
-			trunc((c.amount * 
-			  im_exchange_rate(c.effective_date::date, c.currency, 'EUR')) :: numeric
-			  , 2) as amount_converted,
-			c.*
-		from
-			im_costs c
-			LEFT OUTER JOIN im_projects p ON (c.project_id = p.project_id)
-		where
-			1=1
-			and c.cost_type_id in ([join $cost_type_id ", "])
-			and c.effective_date::date >= to_date(:start_date, 'YYYY-MM-DD')
-			and c.effective_date::date < to_date(:end_date, 'YYYY-MM-DD')
-			and c.effective_date::date < to_date(:end_date, 'YYYY-MM-DD')
-"
-
-# Aggregate additional/important fields to the fact table.
-set middle_sql "
-	select
-		c.*,
-		im_category_from_id(c.cost_type_id) as cost_type,
-		im_category_from_id(c.cost_status_id) as cost_status,
-		to_char(c.effective_date, 'YYYY') as year,
-		to_char(c.effective_date, 'MM') as month_of_year,
-		to_char(c.effective_date, 'Q') as quarter_of_year,
-		to_char(c.effective_date, 'IW') as week_of_year,
-		to_char(c.effective_date, 'DD') as day_of_month,
-		substring(c.cost_name, 1, 14) as cost_name_cut,
-
-		im_category_from_id(c.sub_project_type_id) as sub_project_type,
-		im_category_from_id(c.sub_project_status_id) as sub_project_status,
-
-		mainp.project_name as main_project_name,
-		mainp.project_nr as main_project_nr,
-		mainp.project_type_id as main_project_type_id,
-		im_category_from_id(mainp.project_type_id) as main_project_type,
-		mainp.project_status_id as main_project_status_id,
-		im_category_from_id(mainp.project_status_id) as main_project_status,
-
-		cust.company_name as customer_name,
-		cust.company_path as customer_path,
-		cust.company_type_id as customer_type_id,
-		im_category_from_id(cust.company_type_id) as customer_type,
-		cust.company_status_id as customer_status_id,
-		im_category_from_id(cust.company_status_id) as customer_status,
-
-		prov.company_name as provider_name,
-		prov.company_path as provider_path,
-		prov.company_type_id as provider_type_id,
-		im_category_from_id(prov.company_type_id) as provider_type,
-		prov.company_status_id as provider_status_id,
-		im_category_from_id(prov.company_status_id) as provider_status
-
-	from
-		($inner_sql) c
-		LEFT OUTER JOIN im_projects mainp ON (c.main_project_sortkey = mainp.tree_sortkey)
-		LEFT OUTER JOIN im_companies cust ON (c.customer_id = cust.company_id)
-		LEFT OUTER JOIN im_companies prov ON (c.provider_id = prov.company_id)
-	where
-		1 = 1
-		$where_clause
-"
-
-set sql "
-select
-	sum(c.amount_converted) as amount_converted,
-	sum(c.paid_amount) as paid_amount,
-	[join $dimension_vars ",\n\t"]
-from
-	($middle_sql) c
-group by
-	[join $dimension_vars ",\n\t"]
-"
+# Extract the variables from cube
+set left_scale $cube(left_scale)
+set top_scale $cube(top_scale)
+array set hash $cube(hash_array)
 
 
 # ------------------------------------------------------------
-# Create upper date dimension
+# Display the Cube Table
 
-# Top scale is a list of lists such as {{2006 01} {2006 02} ...}
-# The last element of the list the grand total sum.
-
-# No top dimension at all gives an error...
-if {![llength $top_vars]} { set top_vars [list year] }
-
-set top_scale_plain [db_list_of_lists top_scale "
-	select distinct	[join $top_vars ", "]
-	from		($middle_sql) c
-	order by	[join $top_vars ", "]
-"]
-lappend top_scale_plain [list $sigma $sigma $sigma $sigma $sigma $sigma]
-
-
-# Insert subtotal columns whenever a scale changes
-set top_scale [list]
-set last_item [lindex $top_scale_plain 0]
-foreach scale_item $top_scale_plain {
-    for {set i [expr [llength $last_item]-2]} {$i >= 0} {set i [expr $i-1]} {
-
-	set last_var [lindex $last_item $i]
-	set cur_var [lindex $scale_item $i]
-	if {$last_var != $cur_var} {
-	    set item [lrange $last_item 0 $i]
-	    while {[llength $item] < [llength $last_item]} { lappend item $sigma }
-	    lappend top_scale $item
-	}
-    }
-    lappend top_scale $scale_item
-    set last_item $scale_item
-}
-
-
-# ------------------------------------------------------------
-# Create a sorted left dimension
-
-# No top dimension at all gives an error...
-if {![llength $left_vars]} {
-    ns_write "
-	<p>&nbsp;<p>&nbsp;<p>&nbsp;<p><blockquote>
-	[lang::message::lookup "" intranet-reporting.No_left_dimension "No 'Left' Dimension Specified"]:<p>
-	[lang::message::lookup "" intranet-reporting.No_left_dimension_message "
-		You need to specify atleast one variable for the left dimension.
-	"]
-	</blockquote><p>&nbsp;<p>&nbsp;<p>&nbsp;
-    "
-    ns_write "</table>\n[im_footer]\n"
-    return
-}
-
-# Scale is a list of lists. Example: {{2006 01} {2006 02} ...}
-# The last element is the grand total.
-set left_scale_plain [db_list_of_lists left_scale "
-	select distinct	[join $left_vars ", "]
-	from		($middle_sql) c
-	order by	[join $left_vars ", "]
-"]
-set last_sigma [list]
-foreach t [lindex $left_scale_plain 0] {
-    lappend last_sigma $sigma
-}
-lappend left_scale_plain $last_sigma
-
-
-# Add subtotals whenever a "main" (not the most detailed) scale changes
-set left_scale [list]
-set last_item [lindex $left_scale_plain 0]
-foreach scale_item $left_scale_plain {
-
-    for {set i [expr [llength $last_item]-2]} {$i >= 0} {set i [expr $i-1]} {
-	set last_var [lindex $last_item $i]
-	set cur_var [lindex $scale_item $i]
-	if {$last_var != $cur_var} {
-
-	    set item [lrange $last_item 0 $i]
-	    while {[llength $item] < [llength $last_item]} { lappend item $sigma }
-	    lappend left_scale $item
-	}
-    }
-    lappend left_scale $scale_item
-    set last_item $scale_item
-}
-
-
-
-# ------------------------------------------------------------
-# Display the Table Header
-
-# Determine how many date rows (year, month, day, ...) we've got
-set first_cell [lindex $top_scale 0]
-set top_scale_rows [llength $first_cell]
-set left_scale_size [llength [lindex $left_scale 0]]
-
-set header ""
-for {set row 0} {$row < $top_scale_rows} { incr row } {
-
-    append header "<tr class=rowtitle>\n"
-    append header "<td colspan=$left_scale_size></td>\n"
-
-    for {set col 0} {$col <= [expr [llength $top_scale]-1]} { incr col } {
-
-	set scale_entry [lindex $top_scale $col]
-	set scale_item [lindex $scale_entry $row]
-
-	# Check if the previous item was of the same content
-	set prev_scale_entry [lindex $top_scale [expr $col-1]]
-	set prev_scale_item [lindex $prev_scale_entry $row]
-
-	# Check for the "sigma" sign. We want to display the sigma
-	# every time (disable the colspan logic)
-	if {$scale_item == $sigma} { 
-	    append header "\t<td class=rowtitle>$scale_item</td>\n"
-	    continue
-	}
-
-	# Prev and current are same => just skip.
-	# The cell was already covered by the previous entry via "colspan"
-	if {$prev_scale_item == $scale_item} { continue }
-
-	# This is the first entry of a new content.
-	# Look forward to check if we can issue a "colspan" command
-	set colspan 1
-	set next_col [expr $col+1]
-	while {$scale_item == [lindex [lindex $top_scale $next_col] $row]} {
-	    incr next_col
-	    incr colspan
-	}
-	append header "\t<td class=rowtitle colspan=$colspan>$scale_item</td>\n"	    
-
-    }
-    append header "</tr>\n"
-}
-ns_write $header
-
-
-# ------------------------------------------------------------
-# Execute query and aggregate values into a Hash array
-
-db_foreach query $sql {
-
-    # Get all possible permutations (N out of M) from the dimension_vars
-    set perms [im_report_take_all_ordered_permutations $dimension_vars]
-
-    # Add the invoice amount to ALL of the variable permutations.
-    # The "full permutation" (all elements of the list) corresponds
-    # to the individual cell entries.
-    # The "empty permutation" (no variable) corresponds to the
-    # gross total of all values.
-    # Permutations with less elements correspond to subtotals
-    # of the values along the missing dimension. Clear?
-    #
-    foreach perm $perms {
-
-	# Calculate the key for this permutation
-	# something like "$year-$month-$customer_id"
-	set key_expr "\$[join $perm "-\$"]"
-	set key [eval "set a \"$key_expr\""]
-
-	# Sum up the values for the matrix cells
-	set sum 0
-	if {[info exists hash($key)]} { set sum $hash($key) }
-	
-	if {"" == $amount_converted} { set amount_converted 0 }
-	set sum [expr $sum + $amount_converted]
-	set hash($key) $sum
-    }
-}
-
-
-# ------------------------------------------------------------
-# Display the table body
-
-set ctr 0
-foreach left_entry $left_scale {
-
-    set class $rowclass([expr $ctr % 2])
-    incr ctr
-
-    # Start the row and show the left_scale values at the left
-    ns_write "<tr class=$class>\n"
-    foreach val $left_entry { ns_write "<td>$val</td>\n" }
-
-    # Write the left_scale values to their corresponding local 
-    # variables so that we can access them easily when calculating
-    # the "key".
-    for {set i 0} {$i < [llength $left_vars]} {incr i} {
-	set var_name [lindex $left_vars $i]
-	set var_value [lindex $left_entry $i]
-	set $var_name $var_value
-    }
-    
-    foreach top_entry $top_scale {
-
-	# Write the top_scale values to their corresponding local 
-	# variables so that we can access them easily for $key
-	for {set i 0} {$i < [llength $top_vars]} {incr i} {
-	    set var_name [lindex $top_vars $i]
-	    set var_value [lindex $top_entry $i]
-	    set $var_name $var_value
-	}
-
-	# Calculate the key for this permutation
-	# something like "$year-$month-$customer_id"
-	set key_expr_list [list]
-	foreach var_name $dimension_vars {
-	    set var_value [eval set a "\$$var_name"]
-	    if {$sigma != $var_value} { lappend key_expr_list $var_name }
-	}
-	set key_expr "\$[join $key_expr_list "-\$"]"
-	set key [eval "set a \"$key_expr\""]
-
-	set val "&nbsp;"
-	if {[info exists hash($key)]} { set val $hash($key) }
-
-	ns_write "<td>$val</td>\n"
-
-    }
-    ns_write "</tr>\n"
-}
+ns_write [im_reporting_cubes_display \
+	      -hash_array [array get hash] \
+	      -left_vars $left \
+	      -top_vars $top \
+	      -top_scale $top_scale \
+	      -left_scale $left_scale \
+]
 
 
 # ------------------------------------------------------------
 # Finish up the table
 
-ns_write "</table>\n[im_footer]\n"
+ns_write "[im_footer]\n"
 
 
