df10cfef464f133e8211ef14c194d8972c0bdd86
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-07-14 18:15:47 +0000

    - updated license information

--- sql/postgresql/intranet-reporting-dashboard-create.sql
+++ sql/postgresql/intranet-reporting-dashboard-create.sql
@@ -70,3 +70,28 @@ SELECT im_component_plugin__new (
 	'lang::message::lookup "" intranet-reporting-dashboard.All_Time_Top_Services "All-Time Top Services"'
 );
 
+
+
+
+-- Project Status Histogram
+--
+SELECT im_component_plugin__new (
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Home Project Queue',		-- plugin_name
+	'intranet-reporting-dashboard',	-- package_name
+	'left',				-- location
+	'/intranet/index',		-- page_url
+	null,				-- view_name
+	110,				-- sort_order
+	'im_dashboard_active_projects_status_histogram',
+	'lang::message::lookup "" intranet-reporting-dashboard.Project_Queue "Project Queue"'
+);
+
+
+
+
