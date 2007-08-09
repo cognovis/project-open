f33fa5a935edb74fc12c9ff1b8bf711b093790eb
podemo33 <>
2007-08-09 17:01:33 +0000

    - GanttProject checking day

--- tcl/reply-sweep-init.tcl
+++ tcl/reply-sweep-init.tcl
@@ -14,7 +14,11 @@ set scan_replies_p [parameter::get \
 			-package_id [apm_package_id_from_key notifications] \
 			-parameter EmailQmailQueueScanP -default 0]
 
+set scan_interval [parameter::get \
+     -package_id [apm_package_id_from_key notifications] \
+     -parameter EmailQmailQueueScanInterval -default 300]
+
 if { $scan_replies_p == 1 } {
-    ad_schedule_proc -thread t 60 notification::reply::sweep::scan_all_replies
-    ad_schedule_proc -thread t 60 notification::reply::sweep::process_all_replies
+    ad_schedule_proc -thread t $scan_interval notification::reply::sweep::scan_all_replies
+    ad_schedule_proc -thread t $scan_interval notification::reply::sweep::process_all_replies
 }
