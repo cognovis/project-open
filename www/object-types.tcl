590823a1e36b7d5e72422e8f1ab7fbc93a77a9b3
podemo33 <>
2007-08-12 21:02:26 +0000

    - integration with OCS Inventory Completed

--- www/object-types.tcl
+++ www/object-types.tcl
@@ -72,7 +72,11 @@ db_multirow -extend { object_attributes_url } object_types select_object_types {
 	select	object_type,
     		pretty_name
 	from	acs_object_types
-	where	object_type in ('im_company', 'im_project', 'person', 'im_freelance_rfq', 'im_freelance_rfq_answer')
+	where	object_type in (
+			'im_company', 'im_project', 'person', 
+			'im_freelance_rfq', 'im_freelance_rfq_answer',
+			'im_conf_item'
+		)
 	order by
 		lower(pretty_name)
 } {
