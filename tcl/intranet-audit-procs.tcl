f17fcfcf87c3ef4e35818655ce92f396c320c7a2
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-08-17 18:46:01 +0000

    - Finished basic project audit functionality

--- tcl/intranet-audit-procs.tcl
+++ tcl/intranet-audit-procs.tcl
@@ -19,6 +19,65 @@ ad_library {
     @author frank.bergmann@project-open.com
 }
 
+
+# -------------------------------------------------------------------
+# Audit Sweeper - Make a copy of all "active" projects
+# -------------------------------------------------------------------
+
+ad_proc -public im_core_audit_sweeper  {
+} {
+    Make a copy of all "active" projects
+} {
+    # Make sure that only one thread is sweeping at a time
+    if {[nsv_incr intranet_core audit_sweep_semaphore] > 1} {
+        nsv_incr intranet_core audit_sweep_semaphore -1
+        ns_log Notice "im_core_audit_sweeper: Aborting. There is another process running"
+        return "busy"
+    }
+
+    set debug ""
+    set err_msg ""
+    set counter 0
+    catch {
+	set interval_hours [parameter::get_from_package_key \
+		-package_key intranet-core \
+		-parameter AuditProjectProgressIntervalHours \
+		-default 0 \
+        ]
+
+	set interval_hours 1
+
+	# Select all "active" (=not deleted or canceled) main projects
+	# without an update in the last X hours
+	set project_sql "
+	select	project_id
+	from	im_projects
+	where	parent_id is null and
+		project_status_id not in (
+			[im_project_status_deleted], 
+			[im_project_status_canceled]
+		) and
+		project_id not in (
+			select	distinct project_id
+			from	im_projects_audit
+			where	last_modified > (now() - '$interval_hours hours'::interval)
+		)
+	LIMIT 10
+        "
+	db_foreach audit $project_sql {
+	    append debug [im_project_audit $project_id]
+	    lappend debug $project_id
+	    incr counter
+	}
+    } err_msg
+
+    # Free the semaphore for next use
+    nsv_incr intranet_core audit_sweep_semaphore -1
+
+    return [string trim "$counter $debug $err_msg"]
+}
+
+
 # -------------------------------------------------------------------
 # 
 # -------------------------------------------------------------------
@@ -29,15 +88,38 @@ ad_proc -public im_project_audit  {
 } {
     Creates an audit entry of the specified project
 } {
+    # No audit for non-existing projects
+    if {"" == $project_id} { return "project_id is empty" }
+    if {0 == $project_id} { return "project_id = 0" }
+
+    # Make sure the table exists (compatibility)
     set audit_exists_p [util_memoize "db_table_exists im_projects_audit"]
-    if {!$audit_exists_p} { return }
+    if {!$audit_exists_p} { return "Audit table doesn't exist" }
 
-    set modifying_user [ad_get_user_id]
-    set modifying_ip [ad_conn peeraddr]
+    # No audit for tasks
+    set project_type_id [util_memoize "db_string ptype \"select project_type_id from im_projects where project_id=$project_id\" -default 0"]
+    if {$project_type_id == [im_project_type_task]} { return "Project is a task" }
 
-    db_dml audit "
-	insert into im_projects_audit (
-		modifying_action,	last_modified,		last_modifying_user,		last_modifying_ip,
+    # Parameter to enable/disable project audit
+    set audit_projects_p [parameter::get_from_package_key -package_key "intranet-core" -parameter "AuditProjectsP" -default 1]
+    if {!$audit_projects_p} { return "Audit not enabled" }
+
+
+    # Who is modifying? Use empty values when called from schedules proc sweeper
+    if {[ad_conn -connected_p]} {
+	set modifying_user [ad_get_user_id]
+	set modifying_ip [ad_conn peeraddr]
+    } else {
+	set modifying_user ""
+	set modifying_ip ""
+    }
+
+    catch {
+
+	db_dml audit "
+	    insert into im_projects_audit (
+		modifying_action,
+		last_modified,		last_modifying_user,		last_modifying_ip,
 		project_id,		project_name,			project_nr,
 		project_path,		parent_id,			company_id,
 		project_type_id,	project_status_id,		description,
@@ -45,10 +127,15 @@ ad_proc -public im_project_audit  {
 		supervisor_id,		project_budget,			corporate_sponsor,
 		percent_completed,	on_track_status_id,		project_budget_currency,
 		project_budget_hours,	end_date,			start_date,
-		company_contact_id,	company_project_nr,		final_company
-	) 
-	select
-		:action,		now(),			:modifying_user,		:modifying_ip,
+		company_contact_id,	company_project_nr,		final_company,
+		cost_invoices_cache,	cost_quotes_cache,		cost_delivery_notes_cache,
+		cost_bills_cache,	cost_purchase_orders_cache,	reported_hours_cache,
+		cost_timesheet_planned_cache,	cost_timesheet_logged_cache,
+		cost_expense_planned_cache,	cost_expense_logged_cache
+	    ) 
+	    select
+		:action,		
+		now(),			:modifying_user,		:modifying_ip,
 		project_id,		project_name,			project_nr,
 		project_path,		parent_id,			company_id,
 		project_type_id,	project_status_id,		description,
@@ -56,9 +143,18 @@ ad_proc -public im_project_audit  {
 		supervisor_id,		project_budget,			corporate_sponsor,
 		percent_completed,	on_track_status_id,		project_budget_currency,
 		project_budget_hours,	end_date,			start_date,
-		company_contact_id,	company_project_nr,		final_company
-	from	im_projects
-	where	project_id = :project_id
-    "
+		company_contact_id,	company_project_nr,		final_company,
+		cost_invoices_cache,	cost_quotes_cache,		cost_delivery_notes_cache,
+		cost_bills_cache,	cost_purchase_orders_cache,	reported_hours_cache,
+		cost_timesheet_planned_cache,	cost_timesheet_logged_cache,
+		cost_expense_planned_cache,	cost_expense_logged_cache
+	    from	im_projects
+	    where	project_id = :project_id
+        "
+    } err_msg
+
+    return $err_msg
 }
+
+
 
