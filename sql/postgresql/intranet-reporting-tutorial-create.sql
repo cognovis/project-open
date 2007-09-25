ab36b47f7e3430a74b8fd888be91ee974d19932b
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-09-25 19:48:08 +0000

    - added integration for ad-hoc reports

--- sql/postgresql/intranet-reporting-tutorial-create.sql
+++ sql/postgresql/intranet-reporting-tutorial-create.sql
@@ -247,6 +247,29 @@ BEGIN
     PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
 
 
+
+	v_menu := im_menu__new (
+		null,				-- p_menu_id
+		''acs_object'',			-- object_type
+		now(),				-- creation_date
+		null,				-- creation_user
+		null,				-- creation_ip
+		null,				-- context_id
+		''intranet-reporting-tutorial'',	-- package_name
+		''reporting-tutorial-transtasks01'',	-- label
+		''Translation Tasks AdHoc Report 01'',	-- name
+		''/intranet-reporting-tutorial/trans-tasks-adhoc-01'', -- url
+		170,				-- sort_order
+		v_main_menu,			-- parent_menu_id
+		null				-- p_visible_tcl
+	);
+
+	PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
+
+
+
     return 0;
 end;' language 'plpgsql';
 select inline_0 ();
