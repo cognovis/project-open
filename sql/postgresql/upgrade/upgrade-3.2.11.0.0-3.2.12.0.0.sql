d379b3657b7c98809b778fe8a7ff1ca206bef9f4
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-09-25 13:20:15 +0000

    - added im_dynfield_attribute__new procedure definition to fix canned_note storage

--- sql/postgresql/upgrade/upgrade-3.2.11.0.0-3.2.12.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.11.0.0-3.2.12.0.0.sql
@@ -10,6 +10,77 @@ values ('im_invoice', 'im_invoices', 'invoice_id');
 
 
 
+
+create or replace function im_dynfield_attribute__new (
+        integer, varchar, timestamptz, integer, varchar, integer,
+        varchar, varchar, integer, integer, varchar,
+        varchar, varchar, varchar, varchar, char, char
+) returns integer as '
+DECLARE
+        p_attribute_id          alias for $1;
+        p_object_type           alias for $2;
+        p_creation_date         alias for $3;
+        p_creation_user         alias for $4;
+        p_creation_ip           alias for $5;
+        p_context_id            alias for $6;
+
+        p_attribute_object_type alias for $7;
+        p_attribute_name        alias for $8;
+        p_min_n_values          alias for $9;
+        p_max_n_values          alias for $10;
+        p_default_value         alias for $11;
+
+        p_datatype              alias for $12;
+        p_pretty_name           alias for $13;
+        p_pretty_plural         alias for $14;
+        p_widget_name           alias for $15;
+        p_deprecated_p          alias for $16;
+        p_already_existed_p     alias for $17;
+
+        v_acs_attribute_id      integer;
+        v_attribute_id          integer;
+        v_table_name            varchar;
+BEGIN
+        select table_name into v_table_name
+        from acs_object_types where object_type = p_attribute_object_type;
+
+        v_acs_attribute_id := acs_attribute__create_attribute (
+                p_attribute_object_type,
+                p_attribute_name,
+                p_datatype,
+                p_pretty_name,
+                p_pretty_plural,
+                v_table_name,           -- table_name
+                null,                   -- column_name
+                p_default_value,
+                p_min_n_values,
+                p_max_n_values,
+                null,                   -- sort order
+                ''type_specific'',      -- storage
+                ''f''                   -- static_p
+        );
+
+        v_attribute_id := acs_object__new (
+                p_attribute_id,
+                p_object_type,
+                p_creation_date,
+                p_creation_user,
+                p_creation_ip,
+                p_context_id
+        );
+
+        insert into im_dynfield_attributes (
+                attribute_id, acs_attribute_id, widget_name,
+                deprecated_p, already_existed_p
+        ) values (
+                v_attribute_id, v_acs_attribute_id, p_widget_name,
+                p_deprecated_p, p_already_existed_p
+        );
+        return v_attribute_id;
+end;' language 'plpgsql';
+
+
+
 select im_dynfield_attribute__new (
         null,                   -- widget_id
         'im_dynfield_attribute', -- object_type
