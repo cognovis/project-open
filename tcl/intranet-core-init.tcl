38da35c3771dd60e3848dee5dab9f83c7fa05d2d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-10-24 17:54:15 +0000

    - fixed typo

--- tcl/intranet-core-init.tcl
+++ tcl/intranet-core-init.tcl
@@ -13,7 +13,7 @@ nsv_set intranet_core audit_sweep_semaphore 0
 
 # Schedule the audit function (creates a copy of all active main projects...) every few hours
 #
-set interval [parameter::get_from_package_key -package_key intranet-core -parameter AuditProjectProgressIntervalHours -default 0]
+set interval_hours [parameter::get_from_package_key -package_key intranet-core -parameter AuditProjectProgressIntervalHours -default 0]
 if {0 != $interval_hours} {
     ad_schedule_proc -thread t 10 im_core_audit_sweeper
 }
