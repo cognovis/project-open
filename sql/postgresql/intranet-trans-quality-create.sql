6fc230026318ce63536ebd66b507cdf369db1d06
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2007-10-24 18:44:21 +0000

    - added v_count around creating new privileges

--- sql/postgresql/intranet-trans-quality-create.sql
+++ sql/postgresql/intranet-trans-quality-create.sql
@@ -29,7 +29,7 @@
 
 create sequence quality_report_id start 1;
 create table im_trans_quality_reports (
-        report_id		integer 
+	report_id		integer 
 				constraint im_transq_reports_pk
 				primary key,
 	task_id			integer
@@ -106,19 +106,19 @@ END;' language 'plpgsql';
 -- Project Quality Component
 --
 select im_component_plugin__new (
-        null,                           -- plugin_id
-        'acs_object',                   -- object_type
-        now(),                          -- creation_date
-        null,                           -- creation_user
-        null,                           -- creation_ip
-        null,                           -- context_id
-        'Project Quality Component',    -- plugin_name
-        'intranet-trans-quality',       -- package_name
-        'right',                        -- location
-        '/intranet/projects/view',      -- page_url
-        null,                           -- view_name
-        10,                             -- sort_order
-        'im_quality_project_component -project_id $project_id -return_url $return_url'
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Project Quality Component',	-- plugin_name
+	'intranet-trans-quality',	-- package_name
+	'right',			-- location
+	'/intranet/projects/view',	-- page_url
+	null,				-- view_name
+	10,				-- sort_order
+	'im_quality_project_component -project_id $project_id -return_url $return_url'
 );
 
 
@@ -127,10 +127,33 @@ select im_component_plugin__new (
 -- -----------------------------------------------------
 -- Add privileges for trans_quality
 --
-select acs_privilege__create_privilege('add_trans_quality','Add Trans Quality','Add Trans Quality');
-select acs_privilege__add_child('admin', 'add_trans_quality');
-select acs_privilege__create_privilege('view_trans_quality','View Trans Quality','View Trans Quality');
-select acs_privilege__add_child('admin', 'view_trans_quality');
+
+
+
+
+create or replace function inline_0 ()
+returns integer as '
+DECLARE
+	v_count		 integer;
+BEGIN
+	select	count(*) into v_count
+	from	acs_privileges
+	where	lower(privilege) = ''add_trans_quality'';
+	IF v_count > 0 THEN return 0; END IF;
+
+	select acs_privilege__create_privilege(''add_trans_quality'',''Add Trans Quality'',''Add Trans Quality'');
+	select acs_privilege__add_child(''admin'', ''add_trans_quality'');
+	select acs_privilege__create_privilege(''view_trans_quality'',''View Trans Quality'',''View Trans Quality'');
+	select acs_privilege__add_child(''admin'', ''view_trans_quality'');
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0();
+drop function inline_0();
+
+
+
+
 
 
 -- Set preliminary privileges to setup the
