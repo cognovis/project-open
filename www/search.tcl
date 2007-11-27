0ff40f00e9c1efff23fcc5980d867e5606c5033a
emmar <emmar>
2007-11-27 22:16:26 +0000

    HTML Strict cleanup

--- www/search.tcl
+++ www/search.tcl
@@ -1,7 +1,7 @@
 ad_page_contract {
     @author Neophytos Demetriou <k2pts@cytanet.com.cy>
     @creation-date September 01, 2001
-    @cvs-id $Id: search.tcl,v 1.29 2007/10/21 12:28:43 gustafn Exp $
+    @cvs-id $Id: search.tcl,v 1.30 2007/11/27 22:16:26 emmar Exp $
 } {
     q:trim
     {t:trim ""}
@@ -201,7 +201,6 @@ if { $num > 0 } {
     append url_previous "&num=$num"
     append url_next "&num=$num"
 }
-set ol_start [expr {$offset + 1}]
 
 template::multirow create results_paginator item link
 for { set __i $from_result_page } { $__i <= $to_result_page} { incr __i } {
@@ -221,13 +220,6 @@ if {[llength $search_the_web]} {
 }
 
 # header stuffs
-if {![template::multirow exists link]} {
-    template::multirow create link rel type href title lang media
-}
-template::multirow append link \
-    stylesheet \
-    "text/css" \
-    "/resources/search/search.css" \
-    "" \
-    "" \
-    "all"
+template::head::add_css \
+    -href "/resources/search/search.css" \
+    -media "all"
