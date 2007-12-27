e1968bea34aa11a07e03f1dd2aae9f39498e5ad3
podesign <>
2007-12-27 14:05:41 +0000

    - added dynfields for absences

--- sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.3.1.2.0.sql
+++ sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.3.1.2.0.sql
@@ -9,8 +9,12 @@ extra_select, extra_where, sort_order, visible_for) values (20001,200,NULL,'Name
 '"<a href=$absence_view_url>$absence_name_pretty</a>"','','',1,'');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (20003,200,NULL,'Date',
-'"$start_date_pretty - $end_date_pretty"','','',3,'');
+extra_select, extra_where, sort_order, visible_for) values (20003,200,NULL,'Start',
+'"$start_date_pretty"','','',3,'');
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (20004,200,NULL,'End',
+'"$end_date_pretty"','','',4,'');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
 extra_select, extra_where, sort_order, visible_for) values (20005,200,NULL,'User',
@@ -32,3 +36,58 @@ insert into im_view_columns (column_id, view_id, group_id, column_name, column_r
 extra_select, extra_where, sort_order, visible_for) values (20011,200,NULL,'Contact',
 '"$contact_info_pretty"','','',11,'');
 
+
+
+
+-- Add DynFields
+--
+select im_dynfield_attribute__new (
+	null,				-- widget_id
+	'im_dynfield_attribute',	-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip	
+	null,				-- context_id
+
+	'im_user_absence',		-- attribute_object_type
+	'description',			-- attribute name
+	0,				-- min_n_values
+	1,				-- max_n_values
+	null,				-- default_value
+	'date',				-- ad_form_datatype
+	'#intranet-timesheet2.Description#',	-- pretty name
+	'#intranet-timesheet2.Description#',	-- pretty plural
+	'textarea_small',		-- widget_name
+	'f',				-- deprecated_p
+	't'				-- already_existed_p
+);
+
+update acs_attributes set sort_order = 50
+where attribute_name = 'description' and object_type = 'im_user_absence';
+
+
+-- Add DynFields
+--
+select im_dynfield_attribute__new (
+	null,				-- widget_id
+	'im_dynfield_attribute',	-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip	
+	null,				-- context_id
+
+	'im_user_absence',		-- attribute_object_type
+	'contact_info',			-- attribute name
+	0,				-- min_n_values
+	1,				-- max_n_values
+	null,				-- default_value
+	'date',				-- ad_form_datatype
+	'#intranet-timesheet2.Contact#',	-- pretty name
+	'#intranet-timesheet2.Contact#',	-- pretty plural
+	'textarea_small',		-- widget_name
+	'f',				-- deprecated_p
+	't'				-- already_existed_p
+);
+
+update acs_attributes set sort_order = 60
+where attribute_name = 'contact_info' and object_type = 'im_user_absence';
