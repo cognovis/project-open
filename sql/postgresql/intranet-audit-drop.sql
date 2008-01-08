73a2aa1184ab10b00acbac1abe9767f625bd7202
cambridge <>
2008-01-08 09:25:35 +0000

    - Cambridge changes during the "merging" of changes into the dev environment

---
+++ sql/postgresql/intranet-audit-drop.sql
@@ -0,0 +1,8 @@
+-- /packages/intranet-audit/sql/postgresql/intranet-audit-create.sql
+--
+-- Copyright (c) 2007 ]project-open[
+--
+-- All rights including reserved. To inquire license terms please 
+-- refer to http://www.project-open.com/modules/<module-key>
+
+
