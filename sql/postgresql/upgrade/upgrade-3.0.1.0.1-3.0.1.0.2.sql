958c10dbe7fd870913b823c170c86c9f96e01235
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-02-13 20:33:33 +0000

    - removed upgrade scripts from 3.0.x - We only support upgrades   from V3.1 on

--- sql/postgresql/upgrade/upgrade-3.0.1.0.1-3.0.1.0.2.sql
+++
@@ -1,9 +0,0 @@
-
-
--- using "last_visit_formatted" instead of "last_visit"
-
-delete from im_view_columns where column_id = 1315;
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
-extra_select, extra_where, sort_order, visible_for) values (1315,13,NULL,'Last Visit',
-'$last_visit_formatted','','',15,'');
-
