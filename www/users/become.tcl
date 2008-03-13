551283677d3a0e39220ca2a5b7fc346da2b21704
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-03-13 10:34:29 +0000

    - fixed security issue in become.tcl

--- www/users/become.tcl
+++ www/users/become.tcl
@@ -20,8 +20,17 @@ ad_page_contract {
     @author mobin@mit.edu (Usman Y. Mobin)
     @author frank.bergmann@project-open.com
 } {
-    { return_url "" }
+    { return_url "/intranet/" }
     user_id:integer,notnull
+}
+
+
+# Check the permissions that the current_user has on user_id
+set current_user_id [ad_maybe_redirect_for_registration]
+im_user_permissions $current_user_id $user_id view read write admin
+if {!$admin} {
+    ad_return_complaint 1 "<li>[_ intranet-core.lt_You_are_not_authorize]"
+    ad_script_abort
 }
 
 ad_user_login $user_id
