7337e6f449e2c894b55bc55e2a9fd9534a209f1b
cdamian <>
2008-06-02 11:31:54 +0000

    - move the xml fields into extension tables:   im_gantt_projects   im_gantt_persons

--- sql/postgresql/intranet-ganttproject-create.sql
+++ sql/postgresql/intranet-ganttproject-create.sql
@@ -370,3 +370,56 @@ BEGIN
 end;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
+
+
+	create table im_gantt_projects (
+	        project_id      integer
+	                        primary key,
+	        xml_elements    varchar(1000)
+	                        not null
+	);
+
+	alter table only im_gantt_projects
+		add constraint im_gantt_projects_project_id_fk 
+		foreign key (project_id) references im_projects(project_id);
+
+	select acs_object_type__create_type (
+	        'im_gantt_project',	-- object_type
+        	'GanttProject',         -- pretty_name
+	        'GanttProjects',        -- pretty_plural
+	        'im_project',        	-- supertype
+        	'im_gantt_projects',    -- table_name
+	        'project_id',           -- id_column
+	        'im_gantt_project',     -- package_name
+	        'f',                    -- abstract_p
+	        null,                   -- type_extension_table
+	        'im_project__name'      -- name_method
+	);
+
+	create table im_gantt_persons (
+	        person_id      integer
+	                       primary key,
+	        xml_elements   varchar(1000)
+	                       not null
+	);
+
+	alter table only im_gantt_persons
+		add constraint im_gantt_persons_person_id_fk 
+		foreign key (person_id) references persons(person_id);
+
+	select acs_object_type__create_type (
+        	'im_gantt_person',	-- object_type
+	        'GanttPerson',          -- pretty_name
+	        'GanttPersons',         -- pretty_plural
+        	'person', 	       	-- supertype
+	        'im_gantt_persons',     -- table_name
+	        'person_id',           	-- id_column
+	        'im_gantt_person',      -- package_name
+	        'f',                    -- abstract_p
+	        null,                   -- type_extension_table
+	        'im_person__name'      	-- name_method
+	);
+
+
+
+
