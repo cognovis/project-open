bf15d1e3d4c1991a1d19faf691436acf4e7c72cb
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-08-29 17:13:58 +0000

    - added upgrade for "enable" calls to set_status_id

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.3.0-3.4.0.3.1.sql
@@ -0,0 +1,12 @@
+-- upgrade-3.4.0.3.0-3.4.0.3.1.sql
+
+-- compatibility for WF calls
+CREATE OR REPLACE FUNCTION im_biz_object__set_status_id (integer, varchar, integer) RETURNS integer AS '
+DECLARE
+	p_object_id		alias for $1;
+	p_dummy			alias for $2;
+	p_status_id		alias for $3;
+BEGIN
+	return im_biz_object__set_status_id (p_object_id, p_status_id::integer);
+END;' language 'plpgsql';
+
