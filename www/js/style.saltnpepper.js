4956054b99c2b488bb5f0b7079f9123a47eeb7e5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-10-25 13:04:54 +0000

    -- nothing

--- www/js/style.saltnpepper.js
+++ www/js/style.saltnpepper.js
@@ -11,68 +11,56 @@ var opacityDuration = 1500;
 Structure in original: 
 
 <div id="sideBar">
-     <a href="#" id="sideBarTab"><img src="images/slide-button.gif"
-     alt="sideBar" title="sideBar" /></a>
-     <div id="sideBarContents" style="width:0px;">
-	  <div id="sideBarContentsInner">
-		<h2>side<span>bar</span></h2> ... 
-					      
+	<a href="#" id="sideBarTab"><img src="images/slide-button.gif" alt="sideBar" title="sideBar" /></a>
+	<div id="sideBarContents" style="width:0px;">
+		<div id="sideBarContentsInner">
+			<h2>side<span>bar</span></h2> ... 
+		
      sideBar = filter-list
-      sideBarTab = a href -> button
-       sideBarContents ->filter 
-        sideBarContentsInner -> filter-block
+  	 sideBarTab = a href -> button
+  	 sideBarContents ->filter 
+  	 sideBarContentsInner -> filter-block
 */
 
 function extendContract(){
-	 if(isExtended == 0){
-		       sideBarSlide(0, height, 0, width);
-				       sideBarOpacity(0, 1);
-							 isExtended = 1;
-								    // move
-	 main part
-	      jQuery(".fullwidth-list").animate({marginLeft: "234px"},
-	 slideDuration );
-		       // make expand tab arrow image face left (inwards)
-			  $('#sideBarTab').children().get(0).src =
-	 $('#sideBarTab').children().get(0).src.replace(/(\.[^.]+)$/,
-	 '-active$1');
-	 }
-	 else{
+	if(isExtended == 0){
+		sideBarSlide(0, height, 0, width);
+		sideBarOpacity(0, 1);
+		isExtended = 1;
+		// move main part
+		jQuery(".fullwidth-list").animate({marginLeft: "234px"}, slideDuration );
+		// make expand tab arrow image face left (inwards)
+		$('#sideBarTab').children().get(0).src = $('#sideBarTab').children().get(0).src.replace(/(\.[^.]+)$/, '-active$1');
+	}
+	else{
 		sideBarSlide(height, 0, width, 0);
-				     sideBarOpacity(1, 0);
-						       isExtended = 0;
-								  jQuery(".fullwidth-list").animate({marginLeft: "24px"}, slideDuration );
-														 // make expand tab arrow image face right (outwards)
-														    $('#sideBarTab').children().get(0).src = $('#sideBarTab').children().get(0).src.replace(/-active(\.[^.]+)$/, '$1');
-														    }
+		sideBarOpacity(1, 0);
+		isExtended = 0;
+		jQuery(".fullwidth-list").animate({marginLeft: "24px"}, slideDuration );
+		// make expand tab arrow image face right (outwards)
+		$('#sideBarTab').children().get(0).src = $('#sideBarTab').children().get(0).src.replace(/-active(\.[^.]+)$/, '$1');
+	}
 }
 
 function sideBarSlide(fromHeight, toHeight, fromWidth, toWidth) {
-	  // alert('in: sideBarSlide');
-	  // $("sideBarContents").css ({'height': fromHeight, 'width':
-	  fromWidth});
-	  // $("#sideBarContents").animate( { 'height': toHeight, 'width':
-	  toWidth }, { 'queue': false, 'duration': slideDuration }, "linear"
-	  );
-	  $("sidbar").css ({'height': fromHeight, 'width': fromWidth});
-	  $("#sidebar").animate( { 'height': toHeight, 'width': toWidth }, {
-	  'queue': false, 'duration': slideDuration }, "linear" );
+	 // alert('in: sideBarSlide');
+	// $("sideBarContents").css ({'height': fromHeight, 'width': fromWidth});
+	// $("#sideBarContents").animate( { 'height': toHeight, 'width': toWidth }, { 'queue': false, 'duration': slideDuration }, "linear" );
+	$("sidbar").css ({'height': fromHeight, 'width': fromWidth});
+	$("#sidebar").animate( { 'height': toHeight, 'width': toWidth }, { 'queue': false, 'duration': slideDuration }, "linear" );
 
 }
 
 function sideBarOpacity(from, to){
-	 // $("#sideBarContents").animate( { 'opacity': to }, opacityDuration,
-	 "linear" );
-	 $("#filter").animate( { 'opacity': to }, opacityDuration, "linear" );
+	// $("#sideBarContents").animate( { 'opacity': to }, opacityDuration, "linear" );
+	$("#filter").animate( { 'opacity': to }, opacityDuration, "linear" );
 
 }
 
 $(function(){
-	// Document is ready
-	/* $('#sideBarTab').click( function() { extendContract(); return
-	false; }); */
-	/* $('#sideBarTab').click( function() { extendContract(); return
-	false; }); */
+  	// Document is ready
+	/* $('#sideBarTab').click( function() { extendContract(); return false; }); */
+	/* $('#sideBarTab').click( function() { extendContract(); return false; }); */
 });
 
 
@@ -84,8 +72,7 @@ jQuery.noConflict();
 jQuery().ready(function(){
     // sliding filters 
 
-    jQuery(".filter > .filter-block:first").prepend('<div
-    class="filter-button"></div>');
+    jQuery(".filter > .filter-block:first").prepend('<div class="filter-button"></div>');
 
     if (poGetCookie("filterState")=="hidden") {
        jQuery(".filter").css("left","-240px");
@@ -98,7 +85,7 @@ jQuery().ready(function(){
           "background","url('arrow_comp_left.png') no-repeat"
        );
     }
-    
+	
   
     jQuery(".filter-button").click(function(){
         if (jQuery(".filter").css("left")!="0px") {
@@ -164,8 +151,7 @@ jQuery().ready(function(){
 	/*In order to make this work we need to re-order DIVs*/
 	var node_insert_after=document.getElementById("slave");
 	var node_to_move=document.getElementById("fullwidth-list");
-	document.getElementById("monitor_frame").insertBefore(node_to_move,
-	node_insert_after.nextSibling);
+	document.getElementById("monitor_frame").insertBefore(node_to_move, node_insert_after.nextSibling);
 	alert("here")
 });
 
