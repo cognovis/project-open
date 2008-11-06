0df6a1f4abe756a141d1a8b21e892e41e12805d0
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-11-06 14:45:45 +0000

    - Genedata changes

--- sql/postgresql/upgrade/upgrade-3.4.0.2.0-3.4.0.2.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.2.0-3.4.0.2.1.sql
@@ -9,3 +9,13 @@ alter table im_invoice_items
 add constraint im_invoice_items_un 
 unique (item_name, invoice_id, project_id, sort_order, item_uom_id);
 
+
+-- Update the link to the report to show included timesheet hours
+update apm_parameter_values
+set attr_value = '/intranet-reporting/timesheet-invoice-hours'
+where parameter_id in (
+	select	parameter_id
+	from	apm_parameters
+	where	package_key = 'intranet-invoices' and parameter_name = 'TimesheetInvoiceReport'
+);
+
