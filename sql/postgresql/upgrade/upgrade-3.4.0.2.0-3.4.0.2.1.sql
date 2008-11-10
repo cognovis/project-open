8c5876b786129ebd0f141e803f665912074370ab
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-11-10 22:36:31 +0000

    - Changes after Genedata project

--- sql/postgresql/upgrade/upgrade-3.4.0.2.0-3.4.0.2.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.2.0-3.4.0.2.1.sql
@@ -1,2 +1,5 @@
 -- upgrade-3.4.0.2.0-3.4.0.2.1.sql
 
+SELECT im_category_new (3726,'Timesheet Planned Cost','Intranet Cost Type');
+SELECT im_category_new (3728,'Expense Planned Cost','Intranet Cost Type');
+
