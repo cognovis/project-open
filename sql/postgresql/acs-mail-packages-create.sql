caca8add5cd19551fcc26fc0430232a2a6778880
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-01-31 13:53:56 +0000

    - fixed syntax error of missing "declare"

--- sql/postgresql/acs-mail-packages-create.sql
+++ sql/postgresql/acs-mail-packages-create.sql
@@ -127,17 +127,18 @@ end;
 
 create or replace function acs_mail_body__body_p(integer) 
 returns boolean as '
+declare
 	p_object_id		alias for $1;
-    v_check_body_id integer;
+	v_check_body_id		integer;
 begin
 	select count(body_id) into v_check_body_id
-	  from acs_mail_bodies where body_id = p_object_id;
+	from acs_mail_bodies where body_id = p_object_id;
 
-     if v_check_body_id <> 0 then
-         return ''t'';
-     else
-         return ''f'';
-     end if;
+	if v_check_body_id <> 0 then
+		return ''t'';
+	else
+		return ''f'';
+	end if;
 end;
 ' language 'plpgsql' stable;
 
