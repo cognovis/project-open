027c910ea00e6e575dc93e48fac0e2706778ab12
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-03 17:28:56 +0000

    - added help_text to im_dynfield_type_attribute_map

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.4.0-3.4.0.5.0.sql
@@ -0,0 +1,21 @@
+-- upgrade-3.4.0.2.0-3.4.0.3.0.sql
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	select	count(*) into v_count
+	from	user_tab_columns 
+	where	lower(table_name) = ''im_dynfield_type_attribute_map'' and 
+		lower(column_name) = ''help_text'';
+	IF 0 != v_count THEN return 0; END IF;
+
+	alter table im_dynfield_type_attribute_map
+	add column help_text text;
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
