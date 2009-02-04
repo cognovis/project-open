9974e1cabb99b45c48ac63fddf06c558232ed6fb
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-04 15:01:36 +0000

    - Added 'Intranet User Type' category for DynField User Management   to upgrade script

--- sql/postgresql/upgrade/upgrade-3.4.0.4.0-3.4.0.5.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.4.0-3.4.0.5.0.sql
@@ -1,4 +1,4 @@
--- upgrade-3.4.0.2.0-3.4.0.3.0.sql
+-- upgrade-3.4.0.4.0-3.4.0.4.0.sql
 
 
 create or replace function inline_0 ()
@@ -59,3 +59,38 @@ begin
 end;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
+
+
+-- 22000-22999 Intranet User Type
+SELECT im_category_new(22000, 'Registered Users', 'Intranet User Type');
+SELECT im_category_new(22010, 'The Public', 'Intranet User Type');
+SELECT im_category_new(22020, 'P/O Admins', 'Intranet User Type');
+SELECT im_category_new(22030, 'Customers', 'Intranet User Type');
+SELECT im_category_new(22040, 'Employees', 'Intranet User Type');
+SELECT im_category_new(22050, 'Freelancers', 'Intranet User Type');
+SELECT im_category_new(22060, 'Project Managers', 'Intranet User Type');
+SELECT im_category_new(22070, 'Senior Managers', 'Intranet User Type');
+SELECT im_category_new(22080, 'Accounting', 'Intranet User Type');
+SELECT im_category_new(22090, 'Sales', 'Intranet User Type');
+SELECT im_category_new(22100, 'HR Managers', 'Intranet User Type');
+SELECT im_category_new(22110, 'Freelance Managers', 'Intranet User Type');
+
+-- Set Object Subtypes field
+update acs_object_types set type_category_type = 'Intranet Absence Type' where object_type = 'im_user_absence';
+update acs_object_types set type_category_type = 'Intranet Company Type' where object_type = 'im_company';
+update acs_object_types set type_category_type = 'Intranet Cost Center Type' where object_type = 'im_cost_center';
+update acs_object_types set type_category_type = 'Intranet Cost Type' where object_type = 'im_cost';
+update acs_object_types set type_category_type = 'Intranet Expense Type' where object_type = 'im_expense';
+update acs_object_types set type_category_type = 'Intranet Freelance RFQ Type' where object_type = 'im_freelance_rfq';
+update acs_object_types set type_category_type = 'Intranet Investment Type' where object_type = 'im_investment';
+update acs_object_types set type_category_type = 'Intranet Cost Type' where object_type = 'im_invoice';
+update acs_object_types set type_category_type = 'Intranet Material Type' where object_type = 'im_material';
+update acs_object_types set type_category_type = 'Intranet Office Type' where object_type = 'im_office';
+update acs_object_types set type_category_type = 'Intranet Payment Type' where object_type = 'im_payment';
+update acs_object_types set type_category_type = 'Intranet Project Type' where object_type = 'im_project';
+update acs_object_types set type_category_type = 'Intranet Report Type' where object_type = 'im_report';
+update acs_object_types set type_category_type = 'Intranet Timesheet2 Conf Type' where object_type = 'im_timesheet_conf_object';
+update acs_object_types set type_category_type = 'Intranet Timesheet Task Type' where object_type = 'im_timesheet_task';
+update acs_object_types set type_category_type = 'Intranet Topic Type' where object_type = 'im_forum_topic';
+update acs_object_types set type_category_type = 'Intranet User Type' where object_type = 'person';
+
