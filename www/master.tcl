177d1ec107c5d3fc7b0377e320c182ac78eef473
phast <>
2009-02-05 17:09:17 +0000

    - added Xinha JS

--- www/master.tcl
+++ www/master.tcl
@@ -3,3 +3,15 @@
 if { ![info exists header_stuff] } {
     set header_stuff {}
 }
+
+
+append header_stuff "
+
+<script type=\"text/javascript\">
+      _editor_url = \"/resources/acs-templating/xinha-nightly/\";
+      _editor_lang = \"en\";
+</script>
+<script type=\"text/javascript\" src=\"/resources/acs-templating/xinha-nightly/htmlarea.js\"></script>
+"
+
+
