b624e6d85b45fab0c237afdb12cc333b474a4d23
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-06 16:43:29 +0000

    - added default classes

--- tcl/99-create-class-procs.tcl
+++ tcl/99-create-class-procs.tcl
@@ -7,10 +7,28 @@ ad_library {
     @cvs-id 
 }
 
-# Initialize all object types which are linked from im_dynfield_type_attribute_map
+# Initialize all object types which are linked from im_dynfield_attributes,
+# plus some standard classes
 
 # First get the object_types
-set object_types [db_list object_types "select distinct object_type from acs_attributes aa, im_dynfield_attributes ida, im_dynfield_type_attribute_map tam where aa.attribute_id = ida.acs_attribute_id and ida.attribute_id = tam.attribute_id "]
+set object_types [db_list object_types "
+	select distinct 
+		object_type 
+	from	acs_attributes aa, 
+		im_dynfield_attributes ida
+	where	aa.attribute_id = ida.acs_attribute_id
+UNION	select 'im_office'
+UNION	select 'im_company'
+UNION	select 'im_project'
+UNION	select 'im_conf_item'
+UNION	select 'im_timesheet_task'
+UNION	select 'im_ticket'
+UNION	select 'im_expense_bundle'
+UNION	select 'im_material'
+UNION	select 'im_report'
+UNION	select 'im_user_absence'
+UNION	select 'person'
+"]
 
 # Now we need to go up for each of these and initialize the class
 
