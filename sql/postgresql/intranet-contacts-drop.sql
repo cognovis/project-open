ccbdf196b1b478d799bee5060558ff9ff0455fb7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-08 23:30:19 +0000

    - fixed drop script

--- sql/postgresql/intranet-contacts-drop.sql
+++ sql/postgresql/intranet-contacts-drop.sql
@@ -11,19 +11,25 @@ drop table contact_owner_rels;
 drop table contact_list_members;
 drop table contact_lists;
 
+drop view contact_messages;
 drop table contact_privacy;
 drop table contact_message_log;
 drop table contact_message_items;
 drop table contact_message_types;
 
+
+-- Searches
+
+select acs_object__delete(search_id) from contact_searches;
+select acs_object_type__drop_type('contact_search','t');
+select acs_object_type__drop_type ('contact_list','t');
+
+drop table contact_search_log;
 drop table contact_search_extend_map;
 drop table contact_search_conditions;
 drop table contact_searches;
 drop table contact_extend_options;
-select drop_package('contact_search');
-select acs_object__delete(search_id) from contact_searches;
-select acs_object_type__drop_type('contact_search','t');
-select acs_object_type__drop_type ('contact_list','t');
+
 
 drop view contact_rel_types;
 drop table contact_signatures;
@@ -32,10 +38,12 @@ drop table contact_rels;
 drop table organization_rels;
 drop table contact_complaint_track;
 
+
 select content_type__drop_type ('contact_party_revision','t','t');
 --drop table contact_party_revisions;
 select acs_rel_type__drop_type('contact_owner','t');
 select acs_rel_type__drop_type('organization_rel','t');
+select acs_rel_type__drop_type('main_office_rel','t');
 select acs_rel_type__drop_type(object_type,'t') from acs_object_types where supertype = 'contact_rel';
 select acs_rel_type__drop_type('contact_rel','t');
 
@@ -47,3 +55,11 @@ select drop_package('contact_list');
 select drop_package('contact_owner');
 
 drop sequence contact_extend_search_seq;
+
+drop table contact_groups_allowed_rels;
+drop table contact_deleted_history;
+
+
+select drop_package('contact_search');
+
+
