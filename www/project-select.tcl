523ce13124a0a109e6c8ca3a478c30278bdfa64e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-16 23:22:33 +0000

    - Updated copyright headers

--- www/project-select.tcl
+++ www/project-select.tcl
@@ -1,6 +1,6 @@
 # /packages/intranet-freelance-invoices/www/project-select.tcl
 #
-# Copyright (C) 2003-2005 Project/Open
+# Copyright (C) 2003 - 2009 ]project-open[
 #
 # All rights reserved. Please check
 # http://www.project-open.com/license/ for details.
