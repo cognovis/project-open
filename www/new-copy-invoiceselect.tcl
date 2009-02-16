4dbc866438c0387a2d1105625d4ee607e84772f6
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-16 23:22:34 +0000

    - Updated copyright headers

--- www/new-copy-invoiceselect.tcl
+++ www/new-copy-invoiceselect.tcl
@@ -1,6 +1,6 @@
 # /packages/intranet-invoices/www/new-copy-invoiceselect.tcl
 #
-# Copyright (C) 2003-2005 Project/Open
+# Copyright (C) 2003 - 2009 ]project-open[
 #
 # All rights reserved. Please check
 # http://www.project-open.com/license/ for details.
