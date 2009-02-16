585b7787b9660cc43fadaac9054c8da2abad757d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-16 22:06:52 +0000

    - Copied V3.3 upgrade scripts into V3.5

--- sql/postgresql/upgrade/upgrade-3.1.0.0.0-3.1.0.1.0.sql
+++ sql/postgresql/upgrade/upgrade-3.1.0.0.0-3.1.0.1.0.sql
@@ -1,4 +1,9 @@
+-- upgrade-3.1.0.0.0-3.1.0.1.0.sql
 
+SELECT acs_log__debug('/packages/intranet-freelance/sql/postgresql/upgrade/upgrade-3.1.0.0.0-3.1.0.1.0.sql','');
+
+
+\i ../../../../intranet-core/sql/postgresql/upgrade/upgrade-3.0.0.0.first.sql
 
 
 -- -----------------------------------------------------
