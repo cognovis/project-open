fc809575069bf88fd7765221a575ebff3bd2e494
Frank Bergmann <frank.bergmann@project-open.com>
2009-02-16 23:22:34 +0000

    - Updated copyright headers

--- tcl/intranet-ganttproject-procs.tcl
+++ tcl/intranet-ganttproject-procs.tcl
@@ -1,6 +1,6 @@
 # /packages/intranet-ganttproject/tcl/intranet-ganttproject.tcl
 #
-# Copyright (C) 2003-2006 Project/Open
+# Copyright (C) 2003 - 2009 ]project-open[
 #
 # All rights reserved. Please check
 # http://www.project-open.com/license/ for details.
