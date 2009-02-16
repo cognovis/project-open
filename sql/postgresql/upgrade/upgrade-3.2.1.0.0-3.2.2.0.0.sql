d64f27886bb09847a7e8859ce2e3eceb8c9935a1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-16 22:06:52 +0000

    - Copied V3.3 upgrade scripts into V3.5

--- sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql
@@ -1,5 +1,7 @@
 -- upgrade-3.2.1.0.0-3.2.2.0.0.sql
 
+SELECT acs_log__debug('/packages/intranet-freelance-invoices/sql/postgresql/upgrade/upgrade-3.2.1.0.0-3.2.2.0.0.sql','');
+
 update im_menus 
 set visible_tcl = '[im_cost_type_write_p $user_id 3704]'
 where label = 'invoices_freelance_new_prov_invoice';
