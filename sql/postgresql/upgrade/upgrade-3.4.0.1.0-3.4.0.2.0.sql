23efe2a7da6a285c0d944868864b01a6ddef0dfc
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 22:57:05 +0000

    - Fixing more upgrade scripts

--- sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
@@ -2,8 +2,23 @@
 
 SELECT acs_log__debug('/packages/intranet-search-pg-files/sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql','');
 
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	select	count(*) into v_count 
+	from	pg_constraint
+	where	conname = ''im_fs_files_owner_fk'';
+	if v_count = 0 then return 0; end if;
 
-alter table im_fs_files drop constraint im_fs_files_owner_fk;
+	alter table im_fs_files drop constraint im_fs_files_owner_fk;
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 
 create or replace function inline_0 ()
 returns integer as '
