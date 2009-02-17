263d2581b71ad35a35899266d9729da73bbcba1c
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:28 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/intranet-security-update-client-drop.sql
+++ sql/postgresql/intranet-security-update-client-drop.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-security-update-client/sql/postgres/intranet-security-update-client-drop.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
