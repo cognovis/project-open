7f1eff1428210cf22c14adacaa89f4a8b464eccf
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:24 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/intranet-ganttproject-drop.sql
+++ sql/postgresql/intranet-ganttproject-drop.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-ganttproject/sql/postgresql/intranet-ganttproject-drop.sql
 --
--- Copyright (c) 2003-2006 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
