6b4e01795645f61eadd56dfe3232b78b51f76a42
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:28 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
@@ -1,7 +1,9 @@
 -- upgrade-3.4.0.1.0-3.4.0.2.0.sql
 
-alter table im_fs_files drop constraint im_fs_files_owner_fk;
+SELECT acs_log__debug('/packages/intranet-search-pg-files/sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql','');
 
+
+alter table im_fs_files drop constraint im_fs_files_owner_fk;
 
 create or replace function inline_0 ()
 returns integer as '
