a7900ef5275433093a724d28d0f225a4eef9ba12
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:30 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/intranet-timesheet2-workflow-drop.sql
+++ sql/postgresql/intranet-timesheet2-workflow-drop.sql
@@ -1,6 +1,6 @@
 -- /package/intranet-timesheet2-workflow/sql/intranet-timesheet2-workflow-drop.sql
 --
--- Copyright (c) 2003-2007 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
