fa641e8647c48e588a9386989aa424dc21e28d2c
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 19:13:24 +0000

    - fixed acs_log information that the script has been run

--- sql/postgresql/upgrade/upgrade-3.4.0.3.0-3.4.0.3.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.3.0-3.4.0.3.1.sql
@@ -1,5 +1,8 @@
 -- upgrade-3.4.0.3.0-3.4.0.3.1.sql
 
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.0.3.0-3.4.0.3.1.sql','');
+
+
 CREATE OR REPLACE FUNCTION im_category__new (
         integer, varchar, varchar, varchar
 ) RETURNS integer as '
