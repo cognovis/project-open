591b9f5dbc38e55d991c2b44b5f02343957aada8
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:26 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/intranet-material-drop.sql
+++ sql/postgresql/intranet-material-drop.sql
@@ -1,6 +1,6 @@
 -- /package/intranet-material/sql/intranet-material-drop.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
