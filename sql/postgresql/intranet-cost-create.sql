f2a3419b0744db4bb03d8e331e30207ee7b8900f
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 14:49:04 +0000

    - V3.3 merging stuff from projop

--- sql/postgresql/intranet-cost-create.sql
+++ sql/postgresql/intranet-cost-create.sql
@@ -1606,6 +1606,30 @@ select im_component_plugin__new (
 
 
 
+-- ------------------------------------------------
+-- Return the final customer name for a cost item
+--
+
+create or replace function im_cost_get_final_customer_name(integer)
+returns varchar as '
+DECLARE
+        v_cost_id       alias for $1;
+        v_company_name  varchar;
+BEGIN
+	select	company_name into v_company_name
+	from 	im_companies
+	where 	company_id in ( 
+	        select  company_id
+	        from    im_projects
+	        where   project_id in (
+        	        select  project_id
+                	from    im_costs c
+	                where   c.cost_id = v_cost_id
+        	        )
+		);
+        return v_company_name;
+END;' language 'plpgsql';
+
 
 -------------------------------------------------------------
 -- Helper functions to make our queries easier to read
