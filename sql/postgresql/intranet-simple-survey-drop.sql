97372e38f1b4e5c27e8700488d40f3e229d00164
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-17 17:20:28 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/postgresql/intranet-simple-survey-drop.sql
+++ sql/postgresql/intranet-simple-survey-drop.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-simple-survey/sql/postgres/intranet-simple-survey-drop.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
