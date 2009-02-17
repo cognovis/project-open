d24f9a74423a5aceba6f8b26f5fded35276ec53e
Frank Bergmann <frank.bergmann@project-open.com>
2009-02-17 17:20:23 +0000

    - First round of V3.4 upgrade script fixes - Updated copyright notices

--- sql/common/intranet-filestorage-common.sql
+++ sql/common/intranet-filestorage-common.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-filestorage/sql/common/intranet-filestorage-create.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
--- sql/postgresql/intranet-filestorage-drop.sql
+++ sql/postgresql/intranet-filestorage-drop.sql
@@ -1,6 +1,6 @@
 -- /packages/intranet-filestorage/sql/oracle/intranet-filestorage-drop.sql
 --
--- Copyright (c) 2003-2004 Project/Open
+-- Copyright (c) 2003 - 2009 ]project-open[
 --
 -- All rights reserved. Please check
 -- http://www.project-open.com/license/ for details.
