7000f2cd93bce289e077397aaea982ea221763da
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-20 13:43:09 +0000

    - added upgrade to add translation language as a DnyField to allow   for dynamic filtering

---
+++ sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.4.0.5.0.sql
@@ -0,0 +1,22 @@
+-- upgrade-3.3.1.1.0-3.4.0.5.0.sql
+
+SELECT acs_log__debug('/packages/intranet-translation/sql/postgresql/upgrade/upgrade-3.3.1.1.0-3.4.0.5.0.sql','');
+
+
+select im_dynfield_widget__new (
+        null,                                   -- widget_id
+        'im_dynfield_widget',                   -- object_type
+        now(),                                  -- creation_date
+        null,                                   -- creation_user
+        null,                                   -- creation_ip
+        null,                                   -- context_id
+        'translation_languages',                -- widget_name
+        '#intranet-translation.Trans_Langs#',   -- pretty_name
+        '#intranet-translation.Trans_Langs#',   -- pretty_plural
+        10007,                                  -- storage_type_id
+        'integer',                              -- acs_datatype
+        'im_category_tree',                     -- widget
+        'integer',                              -- sql_datatype
+        '{custom {category_type "Intranet Translation Language"}}' -- parameters
+);
+
