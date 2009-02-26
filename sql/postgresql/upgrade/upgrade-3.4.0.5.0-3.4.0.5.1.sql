b83f591f17f5cee8804539556fd2ea9c2731d3d5
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-02-26 18:24:46 +0000

    - changed "Start Block" to "Effective Date"

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql
@@ -0,0 +1,11 @@
+-- upgrade-3.4.0.5.0-3.4.0.5.1.sql
+
+SELECT acs_log__debug('/packages/intranet-cost/sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql','');
+
+
+update im_view_columns set
+	column_name = 'Effective Date',
+	column_render_tcl = '$effective_date_formatted'
+where
+	column_id = 22013;
+
