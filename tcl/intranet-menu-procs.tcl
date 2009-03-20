4bbb06f65992ce4cd4b83247f697d22970ed987e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-03-20 19:36:52 +0000

    - added caching for im_menu_ul_list

--- tcl/intranet-menu-procs.tcl
+++ tcl/intranet-menu-procs.tcl
@@ -117,6 +117,19 @@ ad_proc -public im_menu_ul_list {
     to be added to index screens (costs) etc. 
 } {
     set user_id [ad_get_user_id]
+    set result [util_memoize [list im_menu_ul_list_helper $user_id $no_uls $parent_menu_label $bind_vars]]
+    return $result
+}
+
+ad_proc -public im_menu_ul_list_helper {
+    user_id
+    no_uls
+    parent_menu_label 
+    bind_vars 
+} {
+    Returns all subitems of a menus as LIs, suitable
+    to be added to index screens (costs) etc. 
+} {
     set parent_menu_id [db_string parent_admin_menu "select menu_id from im_menus where label=:parent_menu_label" -default 0]
 
     set menu_select_sql "
