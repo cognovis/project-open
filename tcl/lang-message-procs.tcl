9198b03d4efaaac9ed82ab10e074aea95a373611
cvs <cvs>
2009-03-21 18:46:23 +0000

    - set localization server to new berlin location

--- tcl/lang-message-procs.tcl
+++ tcl/lang-message-procs.tcl
@@ -12,7 +12,7 @@ ad_library {
     @author Bruno Mattarollo (bruno.mattarollo@ams.greenpeace.org)
     @author Peter Marklund (peter@collaboraid.biz)
     @author Lars Pind (lars@collaboraid.biz)
-    @cvs-id $Id: lang-message-procs.tcl,v 1.3 2009/02/09 16:40:18 cvs Exp $
+    @cvs-id $Id: lang-message-procs.tcl,v 1.4 2009/03/21 18:46:23 cvs Exp $
 }
 
 namespace eval lang::message {}
@@ -40,7 +40,7 @@ ad_proc -public lang::message::register_remote {
 	set sender_email [db_string sender_email "select email as sender_email from parties where party_id = [ad_get_user_id]" -default $system_owner_email]
 	set sender_first_names [db_string sender_email "select first_names from persons where person_id = [ad_get_user_id]" -default "System"]
 	set sender_last_name [db_string sender_email "select last_name from persons where person_id = [ad_get_user_id]" -default "Administrator"]
-	set lang_server_base_url "http://berlin.dnsalias.com/acs-lang-server/lang-message-register"
+	set lang_server_base_url "http://berlin.project-open.net/acs-lang-server/lang-message-register"
 	set lang_server_base_url [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerURL" -default $lang_server_base_url]
 	set lang_server_timeout [parameter::get_from_package_key -package_key "acs-lang" -parameter "LangServerTimeout" -default 5]
 	set lang_server_url [export_vars -base $lang_server_base_url {locale package_key message_key message comment package_version sender_email sender_first_names sender_last_name}]
