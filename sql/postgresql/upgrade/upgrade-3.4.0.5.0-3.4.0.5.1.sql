cdc50e8ba0664ea2c4ad213f5ee4e06920338021
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-03-21 19:11:20 +0000

    - fixed url of upgrade script

--- sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql
@@ -1,6 +1,6 @@
 -- upgrade-3.4.0.5.0-3.4.0.5.1.sql
 
-SELECT acs_log__debug('/packages/intranet-reporting/sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql','');
+SELECT acs_log__debug('/packages/intranet-reporting-finance/sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql','');
 
 -- Fix reinisch wrong report name
 update im_menus set
