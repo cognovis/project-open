e18a979beeaf4c2588a380226d411faf3e306dd1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-03-21 12:30:57 +0000

    - fixed issue at reinisch with wrong naming of report

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql
@@ -0,0 +1,13 @@
+-- upgrade-3.4.0.5.0-3.4.0.5.1.sql
+
+SELECT acs_log__debug('/packages/intranet-reporting/sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.5.1.sql','');
+
+-- Fix reinisch wrong report name
+update im_menus set
+	package_name = 'intranet-reporting-finance',
+	url = '/intranet-reporting-finance/finance-expenses'
+where
+	url = '/intranet-reporting/finance-expenses'
+;
+
+
