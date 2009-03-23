4741246ecf63cbd4c2626051f1f3a2069735f4d4
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-03-23 01:23:45 +0000

    - Reinisch performance optimization - implemented im_profile cache

--- tcl/intranet-trans-procs.tcl
+++ tcl/intranet-trans-procs.tcl
@@ -659,7 +659,7 @@ ad_proc -public im_translation_task_permissions {user_id task_id view_var read_v
     set project_id [db_string project_id "select project_id from im_trans_tasks where task_id = :task_id"]
 
     set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $user_id]
-    set user_is_wheel_p [ad_user_group_member [im_wheel_group_id] $user_id]
+    set user_is_wheel_p [im_profile::member_p -profile_id [im_wheel_group_id] -user_id $user_id]
     set user_is_group_member_p [im_biz_object_member_p $user_id $project_id]
     set user_is_group_admin_p [im_biz_object_admin_p $user_id $project_id]
     set user_is_employee_p [im_user_is_employee_p $user_id]
