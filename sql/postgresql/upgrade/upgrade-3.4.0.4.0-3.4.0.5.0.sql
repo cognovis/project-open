f9ba2d8171a9f0058c8a95b44a895607cae542bf
cognovis <>
2009-04-15 17:43:27 +0000

    Fixed issue with attribute__new not checking for existing acs_attribute

--- sql/postgresql/upgrade/upgrade-3.4.0.4.0-3.4.0.5.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.4.0-3.4.0.5.0.sql
@@ -295,36 +295,62 @@ update acs_object_types set type_category_type = 'Intranet User Type' where obje
 
 
 
-create or replace function im_dynfield_widget__new (
+create or replace function im_dynfield_attribute__new (
 	integer, varchar, timestamptz, integer, varchar, integer,
-	varchar, varchar, varchar, integer, varchar, varchar, 
-	varchar, varchar
+	varchar, varchar, integer, integer, varchar, 
+	varchar, varchar, varchar, varchar, char, char
 ) returns integer as '
 DECLARE
-	p_widget_id		alias for $1;
+	p_attribute_id		alias for $1;
 	p_object_type		alias for $2;
 	p_creation_date 	alias for $3;
 	p_creation_user 	alias for $4;
 	p_creation_ip		alias for $5;
 	p_context_id		alias for $6;
 
-	p_widget_name		alias for $7;
-	p_pretty_name		alias for $8;
-	p_pretty_plural		alias for $9;
-	p_storage_type_id	alias for $10;
-	p_acs_datatype		alias for $11;
-	p_widget		alias for $12;
-	p_sql_datatype		alias for $13;
-	p_parameters		alias for $14;
+	p_attribute_object_type	alias for $7;
+	p_attribute_name	alias for $8;
+	p_min_n_values		alias for $9;
+	p_max_n_values		alias for $10;
+	p_default_value		alias for $11;
 
-	v_widget_id		integer;
+	p_datatype		alias for $12;
+	p_pretty_name		alias for $13;
+	p_pretty_plural		alias for $14;
+	p_widget_name		alias for $15;
+	p_deprecated_p		alias for $16;
+	p_already_existed_p	alias for $17;
+
+	v_acs_attribute_id	integer;
+	v_attribute_id		integer;
+	v_table_name		varchar;
 BEGIN
-	select widget_id into v_widget_id from im_dynfield_widgets
-	where widget_name = p_widget_name;
-	if v_widget_id is not null then return v_widget_id; end if;
+	select table_name into v_table_name
+	from acs_object_types where object_type = p_attribute_object_type;
 
-	v_widget_id := acs_object__new (
-		p_widget_id,
+	select attribute_id into v_acs_attribute_id from acs_attributes
+	where object_type = p_attribute_object_type and attribute_name = p_attribute_name;
+
+	IF v_acs_attribute_id is null THEN
+		v_acs_attribute_id := acs_attribute__create_attribute (
+		p_attribute_object_type,
+		p_attribute_name,
+		p_datatype,
+		p_pretty_name,
+		p_pretty_plural,
+		v_table_name,		-- table_name
+		null,			-- column_name
+		p_default_value,
+		p_min_n_values,
+		p_max_n_values,
+		null,			-- sort order
+		''type_specific'',	-- storage
+		''f''			-- static_p
+		);
+	END IF;
+
+	v_attribute_id := acs_object__new (
+		p_attribute_id,
 		p_object_type,
 		p_creation_date,
 		p_creation_user,
@@ -332,17 +358,31 @@ BEGIN
 		p_context_id
 	);
 
-	insert into im_dynfield_widgets (
-		widget_id, widget_name, pretty_name, pretty_plural,
-		storage_type_id, acs_datatype, widget, sql_datatype, parameters
+	insert into im_dynfield_attributes (
+		attribute_id, acs_attribute_id, widget_name,
+		deprecated_p, already_existed_p
 	) values (
-		v_widget_id, p_widget_name, p_pretty_name, p_pretty_plural,
-		p_storage_type_id, p_acs_datatype, p_widget, p_sql_datatype, p_parameters
+		v_attribute_id, v_acs_attribute_id, p_widget_name,
+		p_deprecated_p, p_already_existed_p
 	);
-	return v_widget_id;
+
+	-- By default show the field for all object types
+	insert into im_dynfield_type_attribute_map (attribute_id, object_type_id, display_mode)
+	select	ida.attribute_id,
+		c.category_id,
+		''edit''
+	from	im_dynfield_attributes ida,
+		acs_attributes aa,
+		acs_object_types aot,
+		im_categories c
+	where	ida.acs_attribute_id = aa.attribute_id and
+		aa.object_type = aot.object_type and
+		aot.type_category_type = c.category_type and
+		aot.object_type = p_attribute_object_type and
+		aa.attribute_name = p_attribute_name;
+
+	return v_attribute_id;
 end;' language 'plpgsql';
-
-
 
 
 select im_dynfield_widget__new (
