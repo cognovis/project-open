91ef616acbad99a3e1dd7154941ac9455db6b354
lup <>
2009-04-22 14:55:16 +0000

    - added comments together with Marc Fleischer

--- sql/postgresql/intranet-notes-create.sql
+++ sql/postgresql/intranet-notes-create.sql
@@ -34,6 +34,32 @@ SELECT acs_object_type__create_type (
 					-- returns the name of the object.
 );
 
+-- Add additional meta information to allow DynFields to extend the im_note object.
+update acs_object_types set
+        status_type_table = 'im_notes',		-- which table contains the status_id field?
+        status_column = 'note_status_id',	-- which column contains the status_id field?
+        type_column = 'note_type_id'		-- which column contains the type_id field?
+where object_type = 'im_note';
+
+-- Object Type Tables contain the lists of all tables (except for
+-- acs_objects...) that contain information about an im_note object.
+-- This way, developers can add "extension tables" to an object to
+-- hold additional DynFields, without changing the program code.
+insert into acs_object_type_tables (object_type,table_name,id_column)
+values ('im_note', 'im_notes', 'note_id');
+
+
+
+-- Generic URLs to link to an object of type "im_note".
+-- These URLs are used by the Full-Text Search Engine and the Workflow
+-- to show links to the object type.
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_note','view','/intranet-notes/new?display_mode=display&note_id=');
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_note','edit','/intranet-notes/new?display_mode=edit&note_id=');
+
+
+
 -- This table stores one object per row. Links to super-type "acs_object" 
 -- using the "note_id" field, which contains the same object_id as 
 -- acs_objects.object_id.
@@ -203,69 +229,96 @@ end;' language 'plpgsql';
 
 
 -----------------------------------------------------------
--- Type and Status
+-- Categories for Type and Status
 --
 -- Create categories for Notes type and status.
--- Status acutally is not use, so we just define "active"
-
--- Here are the ranges for the constants as defined in
--- /intranet-core/sql/common/intranet-categories.sql
+-- Status acutally is not used by the application, 
+-- so we just define "active" and "deleted".
+-- Type contains information on how to format the data
+-- in the im_notes.note field. For example, a "HTTP"
+-- note is shown as a link.
 --
--- Please contact support@project-open.com if you need to
--- reserve a range of constants for a new module.
+-- The categoriy_ids for status and type are used as a
+-- global unique constants and defined in 
+-- /packages/intranet-core/sql/common/intranet-categories.sql.
+-- Please send an email to support@project-open.com with
+-- the subject line "Category Range Request" in order to
+-- request a range of constants for your own modules.
 --
 -- 11400-11499  Intranet Notes Status
 -- 11500-11599  Intranet Notes Status
 
+-- Status
+SELECT im_category_new (11400, 'Active', 'Intranet Notes Status');
+SELECT im_category_new (11402, 'Deleted', 'Intranet Notes Status');
 
-insert into im_categories(category_id, category, category_type) 
-values (11400, 'Active', 'Intranet Notes Status');
-insert into im_categories(category_id, category, category_type) 
-values (11402, 'Deleted', 'Intranet Notes Status');
-
-
-insert into im_categories(category_id, category, category_type) 
-values (11500, 'Address', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11502, 'Email', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11504, 'Http', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11506, 'Ftp', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11508, 'Phone', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11510, 'Fax', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11512, 'Mobile', 'Intranet Notes Type');
-insert into im_categories(category_id, category, category_type) 
-values (11514, 'Other', 'Intranet Notes Type');
+-- Type
+SELECT im_category_new (11500, 'Address', 'Intranet Notes Type');
+SELECT im_category_new (11502, 'Email', 'Intranet Notes Type');
+SELECT im_category_new (11504, 'Http', 'Intranet Notes Type');
+SELECT im_category_new (11506, 'Ftp', 'Intranet Notes Type');
+SELECT im_category_new (11508, 'Phone', 'Intranet Notes Type');
+SELECT im_category_new (11510, 'Fax', 'Intranet Notes Type');
+SELECT im_category_new (11512, 'Mobile', 'Intranet Notes Type');
+SELECT im_category_new (11514, 'Other', 'Intranet Notes Type');
 
 
 -----------------------------------------------------------
 -- Create views for shortcut
 --
+-- These views are optional.
 
 create or replace view im_note_status as
 select	category_id as note_status_id, category as note_status
 from	im_categories
 where	category_type = 'Intranet Notes Status'
-	and (enabled_p is null or enabled_p = 't');
+	and enabled_p = 't';
 
 create or replace view im_note_types as
 select	category_id as note_type_id, category as note_type
 from	im_categories
 where	category_type = 'Intranet Notes Type'
-	and (enabled_p is null or enabled_p = 't');
+	and enabled_p = 't';
 
+
+
+
+-------------------------------------------------------------
+-- Permissions and Privileges
+--
+
+-- A "privilege" is a kind of parameter that can be set per group
+-- in the Admin -> Profiles page. This way you can define which
+-- users can see notes.
+-- In the default configuration below, only Employees have the
+-- "privilege" to "view" notes.
+-- The "acs_privilege__add_child" line below means that "view_notes"
+-- is a sub-privilege of "admin". This way the SysAdmins always
+-- have the right to view notes.
+
+select acs_privilege__create_privilege('view_notes','View Notes','View Notes');
+select acs_privilege__add_child('admin', 'view_notes');
+
+-- Allow all employees to view notes. You can add new groups in 
+-- the Admin -> Profiles page.
+select im_priv_create('view_notes','Employees');
 
 
 -----------------------------------------------------------
--- Component Plugin
+-- Plugin Components
 --
+-- Plugins are these grey boxes that appear in many pages in 
+-- the system. The plugin shows the list of notes that are
+-- associated with the specific object.
+-- This way we can add notes to projects, users companies etc.
+-- with only a single TCL/ADP page.
+--
+-- You can add/modify these plugin definitions in the Admin ->
+-- Plugin Components page
+
+
+
 -- Create a Notes plugin for the ProjectViewPage.
-
-
 SELECT im_component_plugin__new (
 	null,				-- plugin_id
 	'acs_object',			-- object_type
@@ -279,7 +332,7 @@ SELECT im_component_plugin__new (
 	'/intranet/projects/view',	-- page_url
 	null,				-- view_name
 	90,				-- sort_order
-	'im_notes_project_component -object_id $project_id'	-- component_tcl
+	'im_notes_component -object_id $project_id'	-- component_tcl
 );
 
 update im_component_plugins 
@@ -287,6 +340,7 @@ set title_tcl = 'lang::message::lookup "" intranet-notes.Project_Notes "Project 
 where plugin_name = 'Project Notes';
 
 
+-- Create a notes plugin for the CompanyViewPage
 SELECT im_component_plugin__new (
 	null,				-- plugin_id
 	'acs_object',			-- object_type
@@ -300,7 +354,7 @@ SELECT im_component_plugin__new (
 	'/intranet/companies/view',	-- page_url
 	null,				-- view_name
 	90,				-- sort_order
-	'im_notes_project_component -object_id $company_id'	-- component_tcl
+	'im_notes_component -object_id $company_id'	-- component_tcl
 );
 
 update im_component_plugins 
@@ -309,6 +363,7 @@ where plugin_name = 'Company Notes';
 
 
 
+-- Create a notes plugin for the UserViewPage
 SELECT im_component_plugin__new (
 	null,				-- plugin_id
 	'acs_object',			-- object_type
@@ -322,7 +377,7 @@ SELECT im_component_plugin__new (
 	'/intranet/users/view',		-- page_url
 	null,				-- view_name
 	90,				-- sort_order
-	'im_notes_project_component -object_id $user_id'	-- component_tcl
+	'im_notes_component -object_id $user_id'	-- component_tcl
 );
 
 update im_component_plugins 
@@ -335,8 +390,8 @@ where plugin_name = 'User Notes';
 -----------------------------------------------------------
 -- Menu for Notes
 --
--- Create a menu item and set some default permissions
--- for various groups who whould be able to see the menu.
+-- Create a menu item in the main menu bar and set some default 
+-- permissions for various groups who should be able to see the menu.
 
 
 create or replace function inline_0 ()
@@ -348,21 +403,15 @@ declare
 
 	-- Groups
 	v_employees		integer;
-	v_accounting		integer;
 	v_senman		integer;
-	v_companies		integer;
+	v_customers		integer;
 	v_freelancers		integer;
-	v_proman		integer;
-	v_admins		integer;
 	v_reg_users		integer;
 BEGIN
 	-- Get some group IDs
-	select group_id into v_admins from groups where group_name = ''P/O Admins'';
 	select group_id into v_senman from groups where group_name = ''Senior Managers'';
-	select group_id into v_proman from groups where group_name = ''Project Managers'';
-	select group_id into v_accounting from groups where group_name = ''Accounting'';
 	select group_id into v_employees from groups where group_name = ''Employees'';
-	select group_id into v_companies from groups where group_name = ''Customers'';
+	select group_id into v_customers from groups where group_name = ''Customers'';
 	select group_id into v_freelancers from groups where group_name = ''Freelancers'';
 	select group_id into v_reg_users from groups where group_name = ''Registered Users'';
 
@@ -388,20 +437,18 @@ BEGIN
 		null			-- p_visible_tcl
 	);
 
-	-- Grant read permissions to most of the system
-	PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+	-- Grant some groups the read permission for the main "Notes" tab.
+	-- These permissions are independent from the user`s permission to
+	-- read the actual notes.
 	PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
 	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_companies, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_customers, ''read'');
 	PERFORM acs_permission__grant_permission(v_menu, v_freelancers, ''read'');
 	PERFORM acs_permission__grant_permission(v_menu, v_reg_users, ''read'');
 
 	return 0;
 end;' language 'plpgsql';
-
--- Execute and drop the function
+-- Execute and then drop the function
 select inline_0 ();
 drop function inline_0 ();
 
