ed2a5ec05d5052e78598850d58e859533e8d36d1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-04-23 12:09:06 +0000

    - fixed drop script

--- sql/postgresql/intranet-reporting-indicators-drop.sql
+++ sql/postgresql/intranet-reporting-indicators-drop.sql
@@ -9,3 +9,18 @@
 
 
 select im_menu__del_module('intranet-reporting-indicators');
+select im_component_plugin__del_module('intranet-reporting-indicators');
+drop view im_indicator_sections;
+drop function im_indicator__delete(integer);
+drop function im_indicator__new (
+        integer, varchar, timestamptz, integer, varchar, integer,
+        varchar, varchar, integer, integer, text,
+        double precision, double precision, integer
+);
+drop function im_indicator__name(integer);
+drop sequence im_indicator_results_seq;
+drop table im_indicator_results;
+drop table im_indicators;
+delete from im_categories where category_type = 'Intranet Indicator Section';
+SELECT acs_object_type__delete_type('im_indicator');
+
