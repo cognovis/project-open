31fb68737c04dcb2e8f15673fce93a6bebe0c2b4
cambridge <>
2009-05-04 19:40:50 +0000

    - upgrade fixes from CTP

--- sql/postgresql/upgrade/upgrade-3.4.0.0.0-3.4.0.1.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.0.0-3.4.0.1.0.sql
@@ -363,33 +363,18 @@ SELECT  im_component_plugin__new (
 create or replace function inline_0 ()
 returns integer as '
 declare
-        -- Menu IDs
+
         v_menu                  integer;
         v_main_menu             integer;
-
-        -- Groups
         v_employees             integer;
-        v_accounting            integer;
-        v_senman                integer;
         v_companies             integer;
         v_freelancers           integer;
-        v_proman                integer;
-        v_admins                integer;
-        v_reg_users             integer;
 BEGIN
-    select group_id into v_admins from groups where group_name = ''P/O Admins'';
-    select group_id into v_senman from groups where group_name = ''Senior Managers'';
-    select group_id into v_proman from groups where group_name = ''Project Managers'';
-    select group_id into v_accounting from groups where group_name = ''Accounting'';
     select group_id into v_employees from groups where group_name = ''Employees'';
     select group_id into v_companies from groups where group_name = ''Customers'';
     select group_id into v_freelancers from groups where group_name = ''Freelancers'';
-    select group_id into v_reg_users from groups where group_name = ''Registered Users'';
 
-    select menu_id
-    into v_main_menu
-    from im_menus
-    where label=''main'';
+    select menu_id into v_main_menu from im_menus where label=''main'';
 
     v_menu := im_menu__new (
         null,                   -- p_menu_id
@@ -407,10 +392,6 @@ BEGIN
         null                    -- p_visible_tcl
     );
 
-    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
-    PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
-    PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
-    PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
     PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
     PERFORM acs_permission__grant_permission(v_menu, v_companies, ''read'');
     PERFORM acs_permission__grant_permission(v_menu, v_freelancers, ''read'');
