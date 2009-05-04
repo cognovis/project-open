0225f614537c0229f4881aa1c89ef0a0a071200b
cambridge <>
2009-05-04 18:03:31 +0000

    - added an improved version of acs_privilege__add_child   to avoid errors with non-existing profiles

--- sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.1.0-3.4.0.2.0.sql
@@ -4,6 +4,56 @@ SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.
 
 
 
+
+create or replace function acs_privilege__create_privilege (varchar,varchar,varchar)
+returns integer as '
+declare
+	create_privilege__privilege             alias for $1;  
+	create_privilege__pretty_name           alias for $2;  -- default null  
+	create_privilege__pretty_plural         alias for $3;  -- default null
+	v_count					integer;
+begin
+	select count(*) into v_count from acs_privileges
+	where privilege = create_privilege__privilege;
+	IF v_count > 0 THEN return 0; END IF;
+
+	insert into acs_privileges (
+		privilege, pretty_name, pretty_plural
+	) values (
+		create_privilege__privilege, 
+		create_privilege__pretty_name, 
+		create_privilege__pretty_plural
+	);
+
+    return 0; 
+end;' language 'plpgsql';
+
+
+
+
+create or replace function acs_privilege__add_child (varchar,varchar)
+returns integer as '
+declare
+	add_child__privilege            alias for $1;  
+	add_child__child_privilege      alias for $2;  
+	v_count				integer;
+begin
+	select count(*) into v_count from acs_privilege_hierarchy
+	where privilege = add_child__privilege and child_privilege = add_child__child_privilege;
+	IF v_count > 0 THEN return 0; END IF;
+	
+	insert into acs_privilege_hierarchy (
+		privilege, child_privilege
+	) values (
+		add_child__privilege, add_child__child_privilege
+	);
+
+    return 0; 
+end;' language 'plpgsql';
+
+
+
+
 select acs_privilege__create_privilege('edit_project_status','Edit Project Status','Edit Project Status');
 select acs_privilege__add_child('admin', 'edit_project_status');
 
