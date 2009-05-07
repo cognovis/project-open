bc43cd0db2d6461d03c223a29c41a41ebe709c24
cambridge <>
2009-05-07 13:22:57 +0000

    - fixing permission setting

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.6.0.sql
@@ -0,0 +1,38 @@
+-- upgrade-3.4.0.5.0-3.4.0.6.0.sql
+
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.0.5.0-3.4.0.6.0.sql','');
+
+
+-------------------------------------------------------------
+-- Updated version of grant_permission that deals with the
+-- case that the grantee doesnt exist.
+-------------------------------------------------------------
+
+create or replace function acs_permission__grant_permission (integer, integer, varchar)
+returns integer as '
+declare
+	grant_permission__object_id		alias for $1;
+	grant_permission__grantee_id		alias for $2;
+	grant_permission__privilege		alias for $3;
+	exists_p				boolean;
+begin
+	lock table acs_permissions_lock;
+
+	select count(*) > 0 into exists_p from acs_permissions
+	where object_id = grant_permission__object_id and 
+		grantee_id = grant_permission__grantee_id and
+		privilege = grant_permission__privilege;
+
+	IF grant_permission__grantee_id is not NULL AND not exists_p THEN
+		insert into acs_permissions (
+		       	object_id, grantee_id, privilege
+		) values (
+			grant_permission__object_id, 
+			grant_permission__grantee_id, 
+			grant_permission__privilege
+		);
+	END IF;
+
+	return 0; 
+end;' language 'plpgsql';
+
