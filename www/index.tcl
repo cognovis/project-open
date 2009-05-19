d9c7d3536023d0a44b77c2bb1deab8d0a65fcb55
Frank Bergmann <frank.bergmann@project-open.com>
2009-05-19 19:13:36 +0000

    - fixed lincense information in .info files - no ideas about the indicators changes...

--- www/index.tcl
+++ www/index.tcl
@@ -17,7 +17,7 @@ ad_page_contract {
 set user_id [auth::require_login]
 set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $user_id]
 if {!$user_is_admin_p} {
-    ad_return_complaint 1 "<li>[_ intranet-core.lt_You_need_to_be_a_syst]">
+    ad_return_complaint 1 "<li>[_ intranet-core.lt_You_need_to_be_a_syst]"
     return
 }
 
