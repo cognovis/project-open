c958dba19e9721bd9cc3d7cd27764b3150897fb4
Frank Bergmann <frank.bergmann@project-open.com>
2009-06-03 13:26:04 +0000

    - added v_count around the update of acs_object_type_tables for im_expense and im_expense_bundle

--- sql/postgresql/upgrade/upgrade-3.4.0.6.0-3.4.0.6.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.6.0-3.4.0.6.1.sql
@@ -2,24 +2,67 @@
 
 SELECT acs_log__debug('/packages/intranet-expenses/sql/postgresql/upgrade/upgrade-upgrade-3.4.0.6.0-3.4.0.6.1.sql','');
 
-delete from acs_object_type_tables where object_type = 'im_expense';
-
--- im_expense is a sub-type of im_costs, so it needs to define both
--- tables as "extension tables".
-insert into acs_object_type_tables (object_type,table_name,id_column)
-values ('im_expense', 'im_expenses', 'expense_id');
-insert into acs_object_type_tables (object_type,table_name,id_column)
-values ('im_expense', 'im_costs', 'cost_id');
 
 
 
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count                 integer;
+begin
+	-- im_expense is a sub-type of im_costs, so it needs to define both
+	-- tables as "extension tables".
 
-delete from acs_object_type_tables where object_type = 'im_expense_bundle';
+        select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_expense'' and table_name = ''im_expenses'';
+        IF v_count = 0 THEN
 
--- im_expense_bundle is a sub-type of im_costs, so it needs to define
--- both tables as "extension tables".
-insert into acs_object_type_tables (object_type,table_name,id_column)
-values ('im_expense_bundle', 'im_expense_bundles', 'bundle_id');
-insert into acs_object_type_tables (object_type,table_name,id_column)
-values ('im_expense_bundle', 'im_costs', 'cost_id');
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values (''im_expense'', ''im_expenses'', ''expense_id'');
 
+	END IF;
+
+        select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_expense'' and table_name = ''im_costs'';
+        IF v_count = 0 THEN
+
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values (''im_expense'', ''im_costs'', ''cost_id'');
+
+	END IF;
+
+
+        select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_expense'' and table_name = ''im_costs'';
+        IF v_count = 0 THEN
+
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values (''im_expense'', ''im_costs'', ''cost_id'');
+
+	END IF;
+
+
+	-- im_expense_bundle is a sub-type of im_costs, so it needs to define
+	-- both tables as "extension tables".
+        select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_expense_bundle'' and table_name = ''im_expense_bundles'';
+        IF v_count = 0 THEN
+
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values (''im_expense_bundle'', ''im_expense_bundles'', ''bundle_id'');
+
+	END IF;
+
+        select count(*) into v_count from acs_object_type_tables
+	where object_type = ''im_expense_bundle'' and table_name = ''im_costs'';
+        IF v_count = 0 THEN
+
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values (''im_expense_bundle'', ''im_costs'', ''cost_id'');
+
+	END IF;
+
+        return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
