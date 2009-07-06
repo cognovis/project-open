e75b74614a1aeccabd470a87c40743f13e8602d4
Frank Bergmann <frank.bergmann@project-open.com>
2009-07-06 13:43:15 +0000

    - added low and high level watermarks for indicators

--- sql/postgresql/intranet-reporting-indicators-indicators.sql
+++ sql/postgresql/intranet-reporting-indicators-indicators.sql
@@ -1017,3 +1017,28 @@ end;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
 
+
+
+
+-- Update low and high level watermarks
+
+update im_indicators set
+	indicator_low_critical=5, indicator_low_warn=10, indicator_high_warn=40, indicator_high_critical=45
+where indicator_id in (select report_id from im_reports where report_code = 'active_customers_last_month');
+
+update im_indicators set
+	indicator_low_critical=2, indicator_low_warn=5, indicator_high_warn=null, indicator_high_critical=null
+where indicator_id in (select report_id from im_reports where report_code = 'new_countsers_per_month');
+
+update im_indicators set
+	indicator_low_critical=10, indicator_low_warn=20, indicator_high_warn=null, indicator_high_critical=null
+where indicator_id in (select report_id from im_reports where report_code = 'new_project_per_month');
+
+update im_indicators set
+	indicator_low_critical=10000, indicator_low_warn=20000, indicator_high_warn=null, indicator_high_critical=null
+where indicator_id in (select report_id from im_reports where report_code = 'revenues');
+
+update im_indicators set
+	indicator_low_critical=3, indicator_low_warn=4, indicator_high_warn=6, indicator_high_critical=8
+where indicator_id in (select report_id from im_reports where report_code = 'open_projects_per_PM');
+
