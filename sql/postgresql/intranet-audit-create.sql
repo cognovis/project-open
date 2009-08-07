78c7dedd82399d1c04ca36522800d9c8df27fda7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-08-07 18:10:35 +0000

    - separated the creation of the foreign key into a separate   line in order to allow dropping the table im_audits without   having to recompile the acs_objects triggers

--- sql/postgresql/intranet-audit-create.sql
+++ sql/postgresql/intranet-audit-create.sql
@@ -13,90 +13,95 @@
 
 create sequence im_audit_seq;
 
-create table im_audit (
+create table im_audits (
 	audit_id		integer
-				constraint im_audit_pk
+				constraint im_audits_pk
 				primary key,
 	audit_object_id		integer
-				constraint im_audit_object_fk
+				constraint im_audits_object_fk
 				references acs_objects
-				constraint im_audit_object_nn
+				constraint im_audits_object_nn
 				not null,
 	audit_user_id		integer
-				constraint im_audit_user_fk
+				constraint im_audits_user_fk
 				references users
-				constraint im_audit_user_nn
+				constraint im_audits_user_nn
 				not null,
 	audit_date		timestamptz
-				constraint im_audit_date_nn
+				constraint im_audits_date_nn
 				not null,
 	audit_ip		varchar(50)
-				constraint im_audit_ip_nn
+				constraint im_audits_ip_nn
 				not null,
 	audit_last_id		integer
-				constraint im_audit_last_fk
-				references im_audit,
+				constraint im_audits_last_fk
+				references im_audits,
 	audit_ref_object_id	integer
-				constraint im_audit_reference_fk
+				constraint im_audits_reference_fk
 				references acs_objects,
 	audit_value		text
-				constraint im_audit_value_nn
+				constraint im_audits_value_nn
 				not null,
 	audit_diff		text
-				constraint im_audit_diff_nn
+				constraint im_audits_diff_nn
 				not null,
 	audit_note		text,
 	audit_hash		text
 );
 
 -- Add a link for every object to the ID of the last audit entry
-alter table acs_objects add column last_audit_id integer references im_audit;
+alter table acs_objects add column last_audit_id integer;
 
-# Create an index for fast access of the changes of an object
-create index im_audit_audit_object_id_idx on im_audit(audit_object_id);
+-- Add a foreign key constraint on last_audit_id:
+alter table acs_objects 
+add constraint acs_objects_last_audit_id_fkey 
+foreign key (last_audit_id) references im_audits;
 
-comment on table im_audit is '
+-- Create an index for fast access of the changes of an object
+create index im_audits_audit_object_id_idx on im_audits(audit_object_id);
+
+comment on table im_audits is '
  Generic audit table. A new row is created everytime that the value
  of the object is updated.
 ';
 
 
-comment on column im_audit.audit_user_id is '
+comment on column im_audits.audit_user_id is '
  Who has performed the change?
 ';
 
-comment on column im_audit.audit_date is '
+comment on column im_audits.audit_date is '
  When was the change performed?
 ';
 
-comment on column im_audit.audit_ip is '
+comment on column im_audits.audit_ip is '
  IP address of the connection initiating the change.
 ';
 
-comment on column im_audit.audit_last_id is '
+comment on column im_audits.audit_last_id is '
  Pointer to the last last audit of the object or NULL
  before the first update. Used to quickly find the old
  values for calculating a diff.
 ';
 
-comment on column im_audit.audit_ref_object_id is '
+comment on column im_audits.audit_ref_object_id is '
  Optional reference to an object who initiated the change.
 ';
 
-comment on column im_audit.audit_value is '
+comment on column im_audits.audit_value is '
  List of the object fields after the update.
 ';
 
-comment on column im_audit.audit_diff is '
+comment on column im_audits.audit_diff is '
  Difference between the audit_value of the audit_value
  of the audit_last_id and the new audit_value.
 ';
 
-comment on column im_audit.audit_note is '
+comment on column im_audits.audit_note is '
  Additional note by the user. Optional.
 ';
 
-comment on column im_audit.audit_hash is '
+comment on column im_audits.audit_hash is '
  Crypto hash to ensure the integrity of the audit log.
  The hash value includes the hash of the audit_last_id,
  so that any modification in the audit log can be 
