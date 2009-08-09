ee85e99e33430d04a5358cf8e93547b80b66bcf0
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-08-09 20:17:01 +0000

    - unified parameters of im_audit

--- www/gantt-upload-2.tcl
+++ www/gantt-upload-2.tcl
@@ -49,7 +49,7 @@ set reassign_title [lang::message::lookup "" intranet-ganttproject.Delete_Gantt_
 set resource_title [lang::message::lookup "" intranet-ganttproject.Resource_Title "Resources not Found"]
 
 # Write audit trail
-im_project_audit $project_id
+im_project_audit -project_id $project_id
 
 # -------------------------------------------------------------------
 # Get the file from the user.
