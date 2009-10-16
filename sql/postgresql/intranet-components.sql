8b3107fd09a84128725958d0d33e006dbd073eef
Frank Bergmann <frank.bergmann@project-open.com>
2009-10-16 13:46:25 +0000

    - added Browser Warning component

--- sql/postgresql/intranet-components.sql
+++ sql/postgresql/intranet-components.sql
@@ -490,6 +490,25 @@ SELECT  im_component_plugin__new (
 );
 
 
+
+-- BrowserWarning Component on HomePage
+SELECT  im_component_plugin__new (
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Browser Warning',		-- plugin_name
+	'intranet-core',		-- package_name
+	'top',				-- location
+	'/intranet/index',		-- page_url
+	null,				-- view_name
+	10,				-- sort_order
+	'im_browser_warning_component'	-- component_tcl
+);
+
+
 ------------------------------------------------------------------
 -- Set permissions on all Plugin Components for Employees, Freelancers and Customers.
 create or replace function inline_0 ()
@@ -529,4 +548,6 @@ BEGIN
 END;' language 'plpgsql';
 select inline_0();
 drop function inline_0();
+
+
 
