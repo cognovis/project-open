7c7a567fb3675b4490d4958ccfa772ce25752f45
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-10-25 10:49:22 +0000

    - fix: sanity check for update

--- sql/postgresql/upgrade/upgrade-3.4.0.7.5-3.4.0.7.6.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.7.5-3.4.0.7.6.sql
@@ -2,12 +2,23 @@
 
 SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.0.7.5-3.4.0.7.6.sql','');
 
-
-
-
 -- Fix the "table" attribute of the "url" attribute of party.
 
-update acs_attributes set table_name = 'parties' where object_type = 'party' and attribute_name = 'url';
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count         integer;
+begin
+
+	select count(*) into v_count from acs_attributes where object_type = ''party'' and attribute_name = ''url'';
+	IF v_count != 0 THEN
+		update acs_attributes set table_name = ''parties'' where object_type = ''party'' and attribute_name = ''url'';
+	END IF;
+        RETURN 0;
+
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
 
 
 -- Returns a TCL list of company_id suitable to stuff into a
