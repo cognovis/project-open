aaa2795ce9d8a308304e58491a5c82685898a6b6
Frank Bergmann <frank.bergmann@project-open.com>
2009-11-02 21:32:57 +0000

    - allowing User admins to enable/disable users

--- www/users/view.tcl
+++ www/users/view.tcl
@@ -500,18 +500,18 @@ case $member_state {
 	default { set user_state $member_state }
 }
 
-if { [im_user_is_admin_p $current_user_id ]} {
+set activate_link ""
+set delete_link ""
+if {$admin} {
+
     set activate_link "<a href=/acs-admin/users/member-state-change?member_state=approved&[export_url_vars user_id return_url]>[_ intranet-core.activate]</a>"
     set delete_link "<a href=/acs-admin/users/member-state-change?member_state=banned&[export_url_vars user_id return_url]>[_ intranet-core.delete]</a>"
-} else {
-    set activate_link "<a href=/intranet/users/member-state-change?member_state=approved&[export_url_vars user_id return_url]>[_ intranet-core.activate]</a>"
-    set delete_link "<a href=/intranet/users/member-state-change?member_state=banned&[export_url_vars user_id return_url]>[_ intranet-core.delete]</a>"
-}
-
-if {$admin} {
     append admin_links "<li>[_ intranet-core.lt_Member_state_user_sta]"
+
 } else {
+
     append admin_links "<li>[_ intranet-core.User_state]: $user_state"
+
 }
 
 set change_pwd_url "/intranet/users/password-update?[export_url_vars user_id return_url]"
