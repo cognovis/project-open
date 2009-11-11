def693cb4b7727d278a877801f6f431ad52d16a1
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-11-11 20:23:52 +0000

    - nothing changed

--- tcl/intranet-timesheet2-procs.tcl
+++ tcl/intranet-timesheet2-procs.tcl
@@ -596,6 +596,14 @@ ad_proc im_timesheet_absences_sum {
     set num_absences [db_string absences_sum "
 	select	count(*)
 	from	(
+		select	count(*)
+		from
+			im_user_absences a,
+			im_day_enumerator(now()::date - '7'::integer, now()::date) d
+		where
+			owner_id = :user_id
+			and a.start_date <= d.d
+			and a.end_date >= d.d
 		) ttt
     " -default 0]
     if {"" == $num_absences} { set num_absences 0}
