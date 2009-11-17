341f18e8a1fad1c7bd3709432bf8cd3f4be3ca28
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2009-11-17 07:39:46 +0000

    - EconoMedic changes 	- Implemented company-wide bank holidays 	- Extended invoices to check the CostCenter before 	  creating an invoice 	- Implemented custom number circuits for EconoMedic 	- Implemented controlling by day 	- Implemented invoicing by day 	- Implemented conversion function from hours to days, 	  limiting the number of days logged per consultant 	  to 1. 	- Implemented OpenOffice invoicing templates 	- Implemented Template maintenance page in /intranet/admin/ 	- Improved filtering in AbsenceListPage

--- www/invoices/new-3.tcl
+++ www/invoices/new-3.tcl
@@ -95,7 +95,7 @@ set button_text "[_ intranet-trans-invoices.Create_Invoice]"
 set page_title "[_ intranet-trans-invoices.New_Invoice]"
 set context_bar [im_context_bar [list /intranet/invoices/ "[_ intranet-trans-invoices.Finance]"] $page_title]
 set invoice_id [im_new_object_id]
-set invoice_nr [im_next_invoice_nr -invoice_type_id $target_cost_type_id]
+set invoice_nr [im_next_invoice_nr -cost_type_id $target_cost_type_id]
 set invoice_date $todays_date
 set default_payment_days [ad_parameter -package_id [im_package_cost_id] "DefaultCompanyInvoicePaymentDays" "" 30] 
 set enable_file_type_p [parameter::get_from_package_key -package_key intranet-trans-invoices -parameter "EnableFileTypeInTranslationPriceList" -default 0]
