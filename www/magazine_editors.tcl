235753a0f9aacbf880e64dfb56e84c59cc166198
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2010-01-20 16:09:32 +0000

    - fixed permissions

--- www/magazine_editors.tcl
+++ www/magazine_editors.tcl
@@ -12,6 +12,26 @@ set today [db_string today "select to_char(sysdate, 'YYYYMMDD.HHmm') from dual"]
 
 set row_class "rowtitle"
 
+
+
+# ------------------------------------------------------------
+# Security
+
+# Label: Provides the security context for this report
+# because it identifies unquely the report's Menu and
+# its permissions.
+set menu_label "reporting-other-magazine-editors"
+set current_user_id [ad_maybe_redirect_for_registration]
+set read_p [db_string report_perms "
+        select  im_object_permission_p(m.menu_id, :current_user_id, 'read')
+        from    im_menus m
+        where   m.label = :menu_label
+" -default 'f']
+if {![string equal "t" $read_p]} {
+    ad_return_complaint 1 [lang::message::lookup "" intranet-reporting.You_dont_have_permissions "You don't have the necessary permissions to view this page"]
+    ad_script_abort
+}
+
 # ------------------------------------------------------------
 # Define the report - SQL, counters, headers and footers 
 #
