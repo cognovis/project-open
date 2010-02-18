8aafa27c22199afcba6f243fe574ebe159fb3ac4
moravia <>
2010-02-18 15:39:35 +0000

    - moved object creation functionality to intranet-rest-procs-create.tcl

--- tcl/intranet-rest-procs.tcl
+++ tcl/intranet-rest-procs.tcl
@@ -238,11 +238,11 @@ ad_proc -private im_rest_page {
     ns_log Notice "im_rest_index_page: rest_otype=$rest_otype, query_hash=$query_hash_pairs"
 
     set params [list \
-                    [list rest_otype $rest_otype] \
-                    [list rest_oid $rest_oid] \
-                    [list format $format] \
-                    [list user_id $user_id] \
-                    [list query_hash_pairs $query_hash_pairs] \
+		    [list rest_otype $rest_otype] \
+		    [list rest_oid $rest_oid] \
+		    [list format $format] \
+		    [list user_id $user_id] \
+		    [list query_hash_pairs $query_hash_pairs] \
     ]
 
     set result [ad_parse_template -params $params "/packages/intranet-rest/www/$rest_otype"]
@@ -276,17 +276,17 @@ ad_proc -private im_rest_get_object {
     # to table columns and values 
     array set result_hash {}
     db_with_handle db {
-        set selection [db_exec select $db query $sql 1]
-        while { [db_getrow $db $selection] } {
-            set col_names [ad_ns_set_keys $selection]
-            set this_result [list]
-            for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
+	set selection [db_exec select $db query $sql 1]
+	while { [db_getrow $db $selection] } {
+	    set col_names [ad_ns_set_keys $selection]
+	    set this_result [list]
+	    for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
 		set var [lindex $col_names $i]
 		set val [ns_set value $selection $i]
 		if {[lsearch $index_columns $var] >= 0} { continue }
 		set result_hash($var) $val
-            }
-        }
+	    }
+	}
     }
     db_release_unused_handles
 
@@ -347,16 +347,16 @@ ad_proc -private im_rest_get_im_category {
     # corresponding to table columns and values 
     array set result_hash {}
     db_with_handle db {
-        set selection [db_exec select $db query $sql 1]
-        while { [db_getrow $db $selection] } {
-            set col_names [ad_ns_set_keys $selection]
-            set this_result [list]
-            for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
+	set selection [db_exec select $db query $sql 1]
+	while { [db_getrow $db $selection] } {
+	    set col_names [ad_ns_set_keys $selection]
+	    set this_result [list]
+	    for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
 		set var [lindex $col_names $i]
 		set val [ns_set value $selection $i]
 		set result_hash($var) $val
-            }
-        }
+	    }
+	}
     }
     db_release_unused_handles
 
@@ -421,16 +421,16 @@ ad_proc -private im_rest_get_im_invoice_item {
     # corresponding to table columns and values 
     array set result_hash {}
     db_with_handle db {
-        set selection [db_exec select $db query $sql 1]
-        while { [db_getrow $db $selection] } {
-            set col_names [ad_ns_set_keys $selection]
-            set this_result [list]
-            for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
+	set selection [db_exec select $db query $sql 1]
+	while { [db_getrow $db $selection] } {
+	    set col_names [ad_ns_set_keys $selection]
+	    set this_result [list]
+	    for { set i 0 } { $i < [ns_set size $selection] } { incr i } {
 		set var [lindex $col_names $i]
 		set val [ns_set value $selection $i]
 		set result_hash($var) $val
-            }
-        }
+	    }
+	}
     }
     db_release_unused_handles
 
@@ -727,7 +727,7 @@ ad_proc -private im_rest_post_object_type {
     set content [ns_conn content]
 
     # Switch to object specific procedures for handling new object creation
-    # Check if the procedure exists
+    # Check if the procedure exists.
     if {0 != [llength [info commands im_rest_post_$rest_otype]]} {
 	
 	set rest_oid [eval [list im_rest_post_$rest_otype \
@@ -744,7 +744,7 @@ ad_proc -private im_rest_post_object_type {
 		<tr class=rowtitle><td class=rowtitle>Object ID</td></tr>
 		<tr<td>$rest_oid</td></tr>
 		</table>[im_footer]
-	        "
+		"
 	    }
 	    xml {  doc_return 200 "text/xml" "<?xml version='1.0'?>\n<object_id>$rest_oid</object_id>\n" }
 	    default {
@@ -809,152 +809,6 @@ ad_proc -private im_rest_post_object {
     }
 
 }
-
-
-
-ad_proc -private im_rest_post_im_project {
-    { -format "xml" }
-    { -user_id 0 }
-    { -content "" }
-} {
-    Create a new project and returns the project_id.
-} {
-    ns_log Notice "im_rest_post_im_project: user_id=$user_id"
-
-    # store the key-value pairs into a hash array
-    if {[catch {set doc [dom parse $content]} err_msg]} {
-	return [im_rest_error -http_status 406 -message "Unable to parse XML: 'err_msg'."]
-    }
-
-    set root_node [$doc documentElement]
-    foreach child [$root_node childNodes] {
-	set nodeName [$child nodeName]
-	set nodeText [$child text]
-	
-	# Store the values
-	set hash($nodeName) $nodeText
-	set $nodeName $nodeText
-    }
-
-    # Check for duplicate
-    set parent_sql "parent_id = :parent_id"
-    if {"" == $parent_id} { set parent_sql "parent_id is NULL" }
-
-    set dup_sql "
-                select  count(*)
-                from    im_projects
-                where   $parent_sql and
-                        (       upper(trim(project_name)) = upper(trim(:project_name)) OR
-                                upper(trim(project_nr)) = upper(trim(:project_nr)) OR
-                                upper(trim(project_path)) = upper(trim(:project_path))
-                        )
-    "
-    if {[db_string duplicates $dup_sql]} {
-	return [im_rest_error -http_status 406 -message "Duplicate Project: Your project name or project path already exists for the specified parent_id."]
-    }
-
-    if {[catch {
-        set rest_oid [project::new \
-			-creation_user	    $user_id \
-			-context_id	    "" \
-			-project_name       $hash(project_name) \
-			-project_nr         $hash(project_nr) \
-			-project_path       $hash(project_path) \
-			-company_id         $hash(company_id) \
-			-parent_id          $hash(parent_id) \
-			-project_type_id    $hash(project_type_id) \
-			-project_status_id  $hash(project_status_id) \
-        ]
-    } err_msg]} {
-	return [im_rest_error -http_status 406 -message "Error creating project: 'err_msg'."]
-    }
-
-    if {[catch {
-	im_rest_object_type_update_sql \
-	    -rest_otype "im_project" \
-	    -rest_oid $rest_oid \
-	    -hash_array [array get hash]
-
-    } err_msg]} {
-	return [im_rest_error -http_status 406 -message "Error updating project: 'err_msg'."]
-    }
-    
-    return $rest_oid
-}
-
-
-
-ad_proc -private im_rest_post_im_trans_task {
-    { -format "xml" }
-    { -user_id 0 }
-    { -content "" }
-} {
-    Create a new Translation Task and return the task_id.
-} {
-    ns_log Notice "im_rest_post_im_trans_task: user_id=$user_id"
-
-    # store the key-value pairs into a hash array
-    if {[catch {set doc [dom parse $content]} err_msg]} {
-	return [im_rest_error -http_status 406 -message "Unable to parse XML: '$err_msg'."]
-    }
-
-    set root_node [$doc documentElement]
-    foreach child [$root_node childNodes] {
-	set nodeName [$child nodeName]
-	set nodeText [$child text]
-	
-	# Store the values
-	set hash($nodeName) $nodeText
-	set $nodeName $nodeText
-    }
-
-    # Check for duplicate
-    set dup_sql "
-                select  count(*)
-                from    im_trans_tasks
-                where   project_id = :project_id and
-			task_name = :task_name and
-			target_language_id = :target_language_id
-    "
-    if {[db_string duplicates $dup_sql]} {
-	return [im_rest_error -http_status 406 -message "Duplicate Translation Task: Your translation task name or translation task path already exists for the specified parent_id."]
-    }
-
-    if {[catch {
-        set rest_oid [db_string new_trans_task "
-		select im_trans_task__new (
-			null,			-- task_id
-			'im_trans_task',	-- object_type
-			now(),			-- creation_date
-			:user_id,		-- creation_user
-			'[ns_conn peeraddr]',	-- creation_ip	
-			null,			-- context_id	
-
-			:project_id,		-- project_id	
-			:task_type_id,		-- task_type_id	
-			:task_status_id,	-- task_status_id
-			:source_language_id,	-- source_language_id
-			:target_language_id,	-- target_language_id
-			:task_uom_id		-- task_uom_id
-		)
-	"]
-    } err_msg]} {
-	return [im_rest_error -http_status 406 -message "Error creating translation task: 'err_msg'."]
-    }
-
-    if {[catch {
-	im_rest_object_type_update_sql \
-	    -rest_otype "im_trans_task" \
-	    -rest_oid $rest_oid \
-	    -hash_array [array get hash]
-
-    } err_msg]} {
-	return [im_rest_error -http_status 406 -message "Error updating translation task: 'err_msg'."]
-    }
-    
-    return $rest_oid
-}
-
 
 # --------------------------------------------------------
 # Auxillary functions
