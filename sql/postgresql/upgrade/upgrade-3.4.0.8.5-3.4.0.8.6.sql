bbe004ed5c28bdaf9fcb57ce6c81b199828ee484
moravia <>
2010-03-12 12:50:00 +0000

    - fixed upgrade script

--- sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
@@ -3,16 +3,6 @@
 SELECT acs_log__debug('/packages/intranet-dynfield/sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql','');
 
 
-
--- Groups of projects = "program"
--- To be added to the ProjectNewPage via DynField
-alter table im_projects add
-program_id                      integer
-                                constraint im_projects_program_id
-                                references im_projects
-;
-
-
 SELECT im_dynfield_widget__new (
 	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
 	'program_projects', 'Program Projects', 'Program Projects',
