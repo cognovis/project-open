8dee650e8d2cd00991cf3c5cb83bcd65d0471514
economedic <>
2010-03-16 18:53:56 +0000

    -- added menu item

--- sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
@@ -93,3 +93,48 @@ FROM
 	return number_biz_days;
 end;'
 language 'plpgsql';
+
+
+-- Create menu item and set permissions
+-- 
+
+create or replace function inline_1 ()
+returns integer as '
+declare
+        v_menu                  integer;
+        v_parent_menu    	integer;
+        v_admins                integer;
+	v_managers		integer;
+	v_hr_managers		integer;
+begin
+
+    select group_id into v_admins from groups where group_name = ''P/O Admins'';
+    select group_id into v_managers from groups where group_name = ''Senior Managers'';
+    select group_id into v_hr_managers from groups where group_name = ''HR Managers'';
+
+    select menu_id into v_parent_menu from im_menus where label=''timesheet2_absences'';
+
+    v_menu := im_menu__new (
+        null,                   -- p_menu_id
+        ''acs_object'',         -- object_type
+        now(),                  -- creation_date
+        null,                   -- creation_user
+        null,                   -- creation_ip
+        null,                   -- context_id
+        ''intranet-timesheet2'', -- package_name
+        ''capacity-planning'',  -- label
+        ''Capacity Planning'',  -- name
+        ''/intranet-timesheet2/capacity-planning'', -- url
+        500,                    -- sort_order
+        v_parent_menu,           -- parent_menu_id
+        null                    -- p_visible_tcl
+    );
+
+    PERFORM acs_permission__grant_permission(v_menu, v_admins, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_managers, ''read'');
+    PERFORM acs_permission__grant_permission(v_menu, v_hr_managers, ''read'');
+
+    return 0;
+end;' language 'plpgsql';
+select inline_1 ();
+drop function inline_1();
