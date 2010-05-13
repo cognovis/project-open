ab5cba9024cf2ec6331ee88c76e690caf1398c57
moravia <>
2010-05-13 16:46:12 +0000

    - added upgrade script

---
+++ sql/postgresql/upgrade/upgrade-3.4.0.8.9-3.4.1.0.0.sql
@@ -0,0 +1,8 @@
+-- upgrade-3.4.0.8.9-3.4.1.0.0.sql
+
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.0.8.9-3.4.1.0.0.sql','');
+
+
+
+SELECT im_lang_add_message('en_US','intranet-core','Category_Type','Category Type');
+
