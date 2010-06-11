604feeb1daeec53882b67a881a83fc82b544cc24
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2010-06-11 17:41:43 +0000

    - updated ticket table definition

--- sql/postgresql/intranet-helpdesk-create.sql
+++ sql/postgresql/intranet-helpdesk-create.sql
@@ -110,6 +110,14 @@ create table im_tickets (
 	ticket_done_date		timestamptz,
 	-- Customer confirms ticket is done
 	ticket_signoff_date		timestamptz
+
+	ticket_description		text,
+	ticket_customer_deadline	timestamptz,
+	ticket_quoted_days		numeric(12,2),
+	ticket_quote_comment		text,
+	ticket_telephony_request_type_id integer,
+	ticket_telephony_old_number 	text,
+	ticket_telephony_new_number 	text,
 );
 
 
@@ -1463,35 +1471,38 @@ order by
 
 
 
+SELECT im_dynfield_widget__new (
+	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
+	'service_level_agreements', 'Service Level Agreements', 'Service Level Agreements',
+	10007, 'integer', 'generic_sql', 'integer',
+	'{custom {sql {
+		select	
+			p.project_id,
+			p.project_name
+		from 
+			im_projects p
+		where 
+			p.project_type_id = 2502 and
+			p.project_status_id in (select * from im_sub_categories(76))
+		order by 
+			lower(project_name) 
+	}}}'
+);
 
--- select
--- 	p.package_id,
--- 	substring(v.package_key ||' - '||coalesce(v.summary, '') for 40)
--- from
--- 	apm_packages p,
--- 	apm_package_versions v,
--- 	(select package_key, max(version_name) as version_name from apm_package_versions group by package_key) g
--- where
--- 	p.package_key = v.package_key and
--- 	v.package_key = g.package_key and
--- 	v.version_name = g.version_name and
--- 	v.package_key like 'intranet%'
--- order by
--- 	v.package_key
--- 
+
 
 
 
 
 -----------------------------------------------------------
--- DynField Fields
---
-
------------------------------------------------------------
--- Hard Coded fields
+-- Hard Coded DynFields
 --
 SELECT im_dynfield_attribute_new (
-	'im_ticket', 'project_name', 'Name', 'textbox_medium', 'string', 'f', 00, 't', 'im_projects'
+	'im_ticket', 'project_name', 'Name', 'textbox_medium', 'string', 'f', 0, 't', 'im_projects'
+);
+SELECT im_dynfield_attribute_new (
+	'im_ticket', 'parent_id', 'Service Level Agreement', 'service_level_agreements', 
+	'integer', 'f', 10, 't', 'im_projects'
 );
 SELECT im_dynfield_attribute_new (
 	'im_ticket', 'ticket_status_id', 'Status', 'ticket_status', 'integer', 'f', 20, 't', 'im_tickets'
@@ -1503,76 +1514,41 @@ SELECT im_dynfield_attribute_new (
 
 
 -----------------------------------------------------------
--- Priority
+-- Other fields
+--
+
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_prio_id', 'Priority', 'ticket_priority', 'integer', 'f');
-
------------------------------------------------------------
--- Assignee
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_assignee_id', 'Assignee', 'ticket_assignees', 'integer', 'f');
-
------------------------------------------------------------
--- Note
-
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_note', 'Note', 'textarea_small', 'string', 'f');
-
------------------------------------------------------------
--- ]po[ Component
-
-alter table im_tickets add ticket_component_id integer;
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_component_id', 'Software Component', 'ticket_po_components', 'integer', 'f');
-
-
------------------------------------------------------------
--- Hardware Item
-
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_conf_item_id', 'Hardware Component', 'conf_items_servers', 'integer', 'f');
-
-
------------------------------------------------------------
--- Description
-
-alter table im_tickets add ticket_description text;
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_description', 'Description', 'textarea_small', 'string', 'f');
-
-update acs_attributes
-set min_n_values=1
-where	object_type = 'im_ticket' and
-	attribute_name = 'ticket_description'
-;
-
-
------------------------------------------------------------
--- Desired Date
-
-alter table im_tickets add ticket_customer_deadline date;
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_customer_deadline', 'Desired Customer End Date', 'date', 'date', 'f');
-
------------------------------------------------------------
--- Quote results
-
-alter table im_tickets add ticket_quoted_days numeric(12,2);
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_quoted_days', 'Quoted Days', 'numeric', 'integer', 'f');
-
-
-alter table im_tickets add ticket_quote_comment text;
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_quote_comment', 'Quote Comment', 'textarea_small_nospell', 'string', 'f');
-
-
------------------------------------------------------------
--- Telephony Request special fields
------------------------------------------------------------
-
--- Type of telephony request
-alter table im_tickets add ticket_telephony_request_type_id integer;
--- im_dynfield_attribute_new(object_type, column_name, pretty_name, widget_name, datatype, required_p, pos_y, also_hard_coded_p);
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_telephony_request_type_id', 'Telephony Request Type', 'telephony_request_type', 'integer', 'f');
-
--- Old and new telephone number
-alter table im_tickets add ticket_telephony_old_number text;
-alter table im_tickets add ticket_telephony_new_number text;
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_telephony_old_number', 'Old Number/ Location', 'textbox_medium', 'string', 'f');
 SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_telephony_new_number', 'New Number/ Location', 'textbox_medium', 'string', 'f');
+SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_customer_contact_id', 'Customer Contact', 'customer_contact', 'integer', 'f');
+SELECT im_dynfield_attribute_new ('im_ticket', 'ticket_dept_id', 'Department', 'cost_centers', 'integer', 'f');
 
+
+-----------------------------------------------------------
+-- Unused fields
+--
+
+-- ticket_service_id                | integer                  |
+-- ticket_hardware_id               | integer                  |
+-- ticket_application_id            | integer                  |
+-- ticket_queue_id                  | integer                  |
+-- ticket_alarm_date                | timestamp with time zone |
+-- ticket_alarm_action              | text                     |
+-- ticket_creation_date             | timestamp with time zone |
+-- ticket_reaction_date             | timestamp with time zone |
+-- ticket_confirmation_date         | timestamp with time zone |
+-- ticket_done_date                 | timestamp with time zone |
+-- ticket_signoff_date              | timestamp with time zone |
+-- ocs_software_id                  | integer                  |
 
 
 
