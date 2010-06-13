bad436cfa49267f02d418ea1f3262da0222d1f2a
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2010-06-13 18:26:23 +0000

    - added dynfields

--- sql/postgresql/upgrade/upgrade-3.4.1.0.0-3.4.1.0.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.0-3.4.1.0.1.sql
@@ -9,22 +9,22 @@ create or replace function im_project__new (
 	varchar, varchar, varchar, integer, integer, integer, integer
 ) returns integer as '
 DECLARE
-	p_project_id	alias for $1;
-	p_object_type	alias for $2;
-	p_creation_date   alias for $3;
-	p_creation_user   alias for $4;
-	p_creation_ip	alias for $5;
-	p_context_id	alias for $6;
+	p_project_id		alias for $1;
+	p_object_type		alias for $2;
+	p_creation_date 	alias for $3;
+	p_creation_user		alias for $4;
+	p_creation_ip		alias for $5;
+	p_context_id		alias for $6;
 
-	p_project_name	alias for $7;
-	p_project_nr	alias for $8;
-	p_project_path	alias for $9;
-	p_parent_id	alias for $10;
-	p_company_id	alias for $11;
+	p_project_name		alias for $7;
+	p_project_nr		alias for $8;
+	p_project_path		alias for $9;
+	p_parent_id		alias for $10;
+	p_company_id		alias for $11;
 	p_project_type_id	alias for $12;
-	p_project_status_id alias for $13;
+	p_project_status_id	alias for $13;
 
-	v_project_id	integer;
+	v_project_id		integer;
 BEGIN
 	v_project_id := acs_object__new (
 		p_project_id,
@@ -57,4 +57,86 @@ where	project_id not in (
 		from	im_biz_objects
 	)
 ;
+
+
+
+
+
+-----------------------------------------------------------
+-- Widgets
+--
+
+SELECT im_dynfield_widget__new (
+	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
+	'offices', 'Offices', 'Offices',
+	10007, 'integer', 'generic_sql', 'integer',
+	'{custom {sql {
+		select	
+			p.office_id,
+			p.office_name
+		from 
+			im_offices p
+		where 
+			p.office_status_id not in (161)
+		order by 
+			lower(office_name) 
+	}}}'
+);
+
+
+-----------------------------------------------------------
+-- Hard coded fields
+--
+SELECT im_dynfield_attribute_new (
+	'im_company', 'company_name', 'Name', 'textbox_medium', 'string', 'f', 0, 't', 'im_companies'
+);
+SELECT im_dynfield_attribute_new (
+	'im_company', 'company_path', 'Path', 'textbox_medium', 'string', 'f', 10, 't', 'im_companies'
+);
+SELECT im_dynfield_attribute_new (
+	'im_company', 'main_office_id', 'Main Office', 'offices', 'integer', 'f', 20, 't', 'im_companies'
+);
+SELECT im_dynfield_attribute_new (
+	'im_company', 'company_status_id', 'Status', 'category_company_status', 
+	'integer', 'f', 30, 't', 'im_companies'
+);
+SELECT im_dynfield_attribute_new (
+	'im_company', 'company_type_id', 'Type', 'category_company_type', 
+	'integer', 'f', 40, 't', 'im_companies'
+);
+
+
+-----------------------------------------------------------
+-- Hard coded fields
+--
+
+SELECT im_dynfield_attribute_new ('im_company', 'primary_contact_id', 'Primary Contact', 'customer_contact', 'integer', 'f', 100, 'f', 'im_companies');
+SELECT im_dynfield_attribute_new ('im_company', 'accounting_contact_id', 'Accounting Contact', 'customer_contact', 'integer', 'f', 100, 'f', 'im_companies');
+
+
+-- note                        | character varying(4000) |
+-- referral_source             | character varying(1000) |
+-- annual_revenue_id           | integer                 |
+-- status_modification_date    | date                    |
+-- old_company_status_id       | integer                 |
+-- billable_p                  | character(1)            | default 'f'::bpchar
+-- site_concept                | character varying(100)  |
+-- manager_id                  | integer                 |
+-- contract_value              | integer                 |
+-- start_date                  | date                    |
+-- vat_number                  | character varying(100)  |
+-- default_vat                 | numeric(12,1)           | default 0
+-- default_invoice_template_id | integer                 |
+-- default_payment_method_id   | integer                 |
+-- default_payment_days        | integer                 |
+-- default_bill_template_id    | integer                 |
+-- default_po_template_id      | integer                 |
+-- default_delnote_template_id | integer                 |
+-- invoice_template_id         | integer                 |
+-- payment_method_id           | integer                 |
+-- payment_days                | integer                 |
+-- default_quote_template_id   | integer                 |
+-- company_group_id            | integer                 |
+-- business_sector_id          | integer                 |
+-- default_tax                 | numeric(12,1)           | default 0
 
