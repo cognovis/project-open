e10b15b18da6d2653ee68a6b7155ad5bd4bf4f02
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2010-06-15 10:12:48 +0000

    - added im_dynfield_widget__new before the upgrade   script to avoid duplicates

--- sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
+++ sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql
@@ -3,6 +3,59 @@
 SELECT acs_log__debug('/packages/intranet-dynfield/sql/postgresql/upgrade/upgrade-3.4.0.8.5-3.4.0.8.6.sql','');
 
 
+-- Make sure we got the new version with duplicate detection
+create or replace function im_dynfield_widget__new (
+	integer, varchar, timestamptz, integer, varchar, integer,
+	varchar, varchar, varchar, integer, varchar, varchar, 
+	varchar, varchar
+) returns integer as '
+DECLARE
+	p_widget_id		alias for $1;
+	p_object_type		alias for $2;
+	p_creation_date 	alias for $3;
+	p_creation_user 	alias for $4;
+	p_creation_ip		alias for $5;
+	p_context_id		alias for $6;
+
+	p_widget_name		alias for $7;
+	p_pretty_name		alias for $8;
+	p_pretty_plural		alias for $9;
+	p_storage_type_id	alias for $10;
+	p_acs_datatype		alias for $11;
+	p_widget		alias for $12;
+	p_sql_datatype		alias for $13;
+	p_parameters		alias for $14;
+
+	v_widget_id		integer;
+BEGIN
+	select widget_id into v_widget_id from im_dynfield_widgets
+	where widget_name = p_widget_name;
+	if v_widget_id is not null then return v_widget_id; end if;
+
+	v_widget_id := acs_object__new (
+		p_widget_id,
+		p_object_type,
+		p_creation_date,
+		p_creation_user,
+		p_creation_ip,
+		p_context_id
+	);
+
+	insert into im_dynfield_widgets (
+		widget_id, widget_name, pretty_name, pretty_plural,
+		storage_type_id, acs_datatype, widget, sql_datatype, parameters
+	) values (
+		v_widget_id, p_widget_name, p_pretty_name, p_pretty_plural,
+		p_storage_type_id, p_acs_datatype, p_widget, p_sql_datatype, p_parameters
+	);
+	return v_widget_id;
+end;' language 'plpgsql';
+
+
+
+
+
+
 SELECT im_dynfield_widget__new (
 	null, 'im_dynfield_widget', now(), 0, '0.0.0.0', null,
 	'program_projects', 'Program Projects', 'Program Projects',
