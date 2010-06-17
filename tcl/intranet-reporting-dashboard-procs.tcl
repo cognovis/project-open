5e883f1f173159dbb0ef9ddcd7ac9e684b009bc6
Frank Bergmann <frank.bergmann@project-open.com>
2010-06-17 18:07:41 +0000

    - added project EVA

--- tcl/intranet-reporting-dashboard-procs.tcl
+++ tcl/intranet-reporting-dashboard-procs.tcl
@@ -561,5 +561,344 @@ ad_proc im_dashboard_histogram {
 }
 
 
+# ----------------------------------------------------------------------
+# Project EVA
+# ---------------------------------------------------------------------
+
+ad_proc im_dashboard_project_eva_create_audit_all {
+} {
+    Create project audit entries for all projects
+} {
+   set project_ids [db_list pids "select project_id from im_projects where parent_id is null"]
+   foreach pid $project_ids {
+	im_dashboard_project_eva_create_audit -project_id $pid
+   }   
+}
+
+ad_proc im_dashboard_project_eva_create_audit {
+    -project_id
+} {
+    Creates im_projects_audit entries for the project
+    by splitting the time between start_date and end_date
+    in $intervals pieces and calculates new im_projects_audit
+    entries for the dates based on the information of
+    timesheet hours and financial documents
+} {
+    # Delete the entire history
+    db_dml del_audit "delete from im_projects_audit where project_id = :project_id"
+
+    # Get start- and end date in Julian format (as integer)
+    db_1row start_end "
+        select	p.*,
+		to_char(start_date, 'J') as start_date_julian,
+		to_char(end_date, 'J') as end_date_julian,
+		to_char(now(), 'J') as now_julian,
+		to_char(end_date, 'J')::integer - to_char(start_date, 'J')::integer as duration_days
+        from	im_projects p
+        where	project_id = :project_id
+    "
+    
+    set modifying_action "update"
+#    if {$now_julian < $end_date_julian} { set end_date_julian $now_julian }
+#    if {$now_julian < $start_date_julian } { set start_date_julian [expr $now_julian - 10] }
+    set duration_days [expr $end_date_julian - $start_date_julian]
+    set increment [expr round($duration_days / 50)]
+
+    set default_hourly_rate [ad_parameter -package_id [im_package_cost_id] "DefaultTimesheetHourlyCost" "" "30.0"]
+
+    # Loop for every day and calculate the sum of all cost types
+    # per cost type
+    for { set i $start_date_julian} {$i < $end_date_julian} { set i [expr $i + 1 + $increment] } {
+
+	set cost_timesheet_planned_cache 0.0
+	set cost_expense_logged_cache 0.0
+	set cost_expense_planned_cache 0.0
+	set cost_quotes_cache 0.0
+	set cost_bills_cache 0.0
+	set cost_purchase_orders_cache 0.0
+	set cost_delivery_notes_cache 0.0
+	set cost_invoices_cache 0.0
+	set cost_timesheet_logged_cache 0.0
+
+	set cost_sql "
+		select	sum(c.amount) as amount,
+			c.cost_type_id
+		from	im_costs c
+		where	c.effective_date < to_date(:i, 'J') and
+			c.project_id in (
+				select	child.project_id
+				from	im_projects parent,
+					im_projects child
+				where	parent.project_id = :project_id and
+					child.tree_sortkey 
+						between parent.tree_sortkey 
+						and tree_right(parent.tree_sortkey)
+			)
+		group by
+			cost_type_id
+	"
+	db_foreach costs $cost_sql {
+	    switch $cost_type_id {
+		 3726 { set cost_timesheet_planned_cache $amount }
+		 3722 { set cost_expense_logged_cache $amount }
+		 3728 { set cost_expense_planned_cache $amount }
+		 3702 { set cost_quotes_cache $amount }
+		 3704 { set cost_bills_cache $amount }
+		 3706 { set cost_purchase_orders_cache $amount }
+		 3724 { set cost_delivery_notes_cache $amount }
+		 3700 { set cost_invoices_cache $amount }
+		 3718 { set cost_timesheet_logged_cache $amount }
+	    }
+	}
+	set ts_sql "
+		select	sum(h.hours) as hours
+		from	im_hours h
+		where	h.day < to_date(:i, 'J') and
+			h.project_id in (
+				select	child.project_id
+				from	im_projects parent,
+					im_projects child
+				where	parent.project_id = :project_id and
+					child.tree_sortkey 
+						between parent.tree_sortkey 
+						and tree_right(parent.tree_sortkey)
+			)
+	"
+	set reported_hours_cache [db_string hours $ts_sql]
+
+	db_dml insert "
+		insert into im_projects_audit (
+			modifying_action,
+			last_modified,
+			last_modifying_user,
+			last_modifying_ip,
+			project_id,
+			project_name,
+			project_nr,
+			project_path,
+			parent_id,
+			company_id,
+			project_type_id,
+			project_status_id,
+			description,
+			billing_type_id,
+			note,
+			project_lead_id,
+			supervisor_id,
+			project_budget,
+			corporate_sponsor,
+			percent_completed,
+			on_track_status_id,
+			project_budget_currency,
+			project_budget_hours,
+			end_date,
+			start_date,
+			company_contact_id,
+			company_project_nr,
+			final_company,
+			cost_invoices_cache,
+			cost_quotes_cache,
+			cost_delivery_notes_cache,
+			cost_bills_cache,
+			cost_purchase_orders_cache,
+			cost_timesheet_planned_cache,
+			cost_timesheet_logged_cache,
+			cost_expense_planned_cache,
+			cost_expense_logged_cache,
+			reported_hours_cache
+		) values (
+			:modifying_action,
+			to_date(:i,'J'),
+			'[ad_get_user_id]',
+			'[ns_conn peeraddr]',
+			:project_id,
+			:project_name,
+			:project_nr,
+			:project_path,
+			:parent_id,
+			:company_id,
+			:project_type_id,
+			:project_status_id,
+			:description,
+			:billing_type_id,
+			:note,
+			:project_lead_id,
+			:supervisor_id,
+			:project_budget,
+			:corporate_sponsor,
+			:percent_completed,
+			:on_track_status_id,
+			:project_budget_currency,
+			:project_budget_hours,
+			:end_date,
+			:start_date,
+			:company_contact_id,
+			:company_project_nr,
+			:final_company,
+			:cost_invoices_cache,
+			:cost_quotes_cache,
+			:cost_delivery_notes_cache,
+			:cost_bills_cache,
+			:cost_purchase_orders_cache,
+			:cost_timesheet_planned_cache,
+			:cost_timesheet_logged_cache,
+			:cost_expense_planned_cache,
+			:cost_expense_logged_cache,
+			:reported_hours_cache
+		)
+	"		    
+    }
+}
+
+
+ad_proc im_dashboard_project_eva {
+    -project_id
+    { -name "" }
+    { -histogram_values {} }
+    { -diagram_width 300 }
+    { -diagram_height 200 }
+    { -font_color "000000" }
+    { -diagram_color "0080FF" }
+    { -font_size 8 }
+    { -font_style "font-family:Verdana;font-weight:normal;line-height:10pt;font-size:8pt;" }
+    { -bar_color "0080FF" }
+    { -outer_distance 2 }
+    { -left_distance 35 }
+    { -bottom_distance 20 }
+    { -widget_bins 5 }
+} {
+    Returns a formatted HTML text to display a timeline of dots
+    based on Lutz Tautenhahn' "Javascript Diagram Builder", v3.3.
+    @param values Contains a list of "element" lists.
+} {
+    set date_format "YYYY-MM-DD HH:MI:SS"
+    set oname "D[expr round(rand()*1000000000.0)]"
+    set today [db_string today "select to_char(now(), :date_format)"]
+    set default_currency [ad_parameter -package_id [im_package_cost_id] "DefaultCurrency" "" "EUR"]
+
+    if {"" == $name} { set name [db_string name "select project_name from im_projects where project_id = :project_id"] }
+    # Extract first and last date
+    db_1row start_end "
+	select  to_char(min(last_modified), :date_format) as first_date,
+		to_char(max(last_modified), :date_format) as last_date,
+		max(trunc((project_budget * im_exchange_rate(start_date::date, project_budget_currency, :default_currency))::numeric, 2)) as project_budget_max,
+		-- The cost side - thee different types - ts, bills and expenses
+		max(cost_timesheet_logged_cache) as cost_timesheet_logged_cache_max,
+		max(cost_bills_cache) as cost_bills_cache_max,
+		max(cost_expense_logged_cache) as cost_expense_logged_cache_max,
+		-- The income side - quotes and invoices
+		max(cost_invoices_cache) as cost_invoices_cache_max,
+		max(cost_quotes_cache) as cost_quotes_cache_max,
+		-- Delivered value
+		max(cost_delivery_notes_cache) as cost_delivery_notes_cache_max
+        from    im_projects_audit
+        where   project_id = :project_id
+    "
+    regexp {([0-9]*)\-([0-9]*)\-([0-9]*) ([0-9]*)\:([0-9]*)\:([0-9]*)} $first_date match year0 month0 day0 hour0 min0 sec0
+    regexp {([0-9]*)\-([0-9]*)\-([0-9]*) ([0-9]*)\:([0-9]*)\:([0-9]*)} $last_date match year9 month9 day9 hour9 min9 sec9
+
+    set widget_min 0
+    set cost_max [expr $cost_timesheet_logged_cache_max + $cost_bills_cache_max + $cost_expense_logged_cache_max]
+    set widget_max $cost_max
+    if {$project_budget_max > $widget_max} { set widget_max $project_budget_max}
+
+    # Draw dots and lines from point to point.
+    set sql "
+	select	*,
+		to_char(last_modified, 'YYYY-MM-DD HH:MI:SS') as date,
+		trunc((project_budget * im_exchange_rate(start_date::date, project_budget_currency, :default_currency))::numeric, 2) as project_budget_converted
+	from	im_projects_audit pa
+	where	project_id = :project_id
+	order by
+		last_modified
+    "
+
+    set last_v [list]
+    set last_date $first_date
+    set diagram_html ""
+
+    set titles { "Date" "Budget" "Expenses" "Expenses + Bills" "Expenses + Bills + Timesheet" }
+    #set start_color [im_dashboard_color_saltnpepper -type "start_color"]
+    #set end_color [im_dashboard_color_saltnpepper -type "end_color"]
+    set start_color "0000FF" 
+    set end_color "00FF00"
+    array set colors [im_dashboard_pie_colors -start_color $start_color -end_color $end_color -max_entries 4]
+
+    db_foreach project_eva $sql {
+
+	regexp {([0-9]*)\-([0-9]*)\-([0-9]*) ([0-9]*)\:([0-9]*)\:([0-9]*)} $date match year month day hour min sec
+	regexp {([0-9]*)\-([0-9]*)\-([0-9]*) ([0-9]*)\:([0-9]*)\:([0-9]*)} $last_date match last_year last_month last_day last_hour last_min last_sec
+
+        set v [list $date \
+		$project_budget_converted \
+		[expr $cost_expense_logged_cache] \
+		[expr $cost_expense_logged_cache + $cost_bills_cache] \
+		[expr $cost_expense_logged_cache + $cost_bills_cache + $cost_timesheet_logged_cache] \
+	]
+
+        if {"" == $last_v} { set last_v $v }
+
+	for {set k 1} {$k < [llength $v]} {incr k} {
+	    set val [lindex $v $k]
+	    set last_val [lindex $last_v $k]
+	    set color_key [expr $k-1]
+	    set color $colors($color_key)
+	    set title [lindex $titles $k]
+	    append diagram_html "
+		var x = $oname.ScreenX(Date.UTC($year, $month, $day, $hour, $min, $sec));
+		var last_x = $oname.ScreenX(Date.UTC($last_year, $last_month, $last_day, $last_hour, $last_min, $last_sec));
+		var y = $oname.ScreenY($val);
+		var last_y = $oname.ScreenY($last_val);
+		new Line(last_x, last_y, x, y, \"$color\", 1, \"$title\");
+		new Dot(x, y, 4, 3, \"$color\", \"$title - $val\");
+
+	    "
+	}
+	set last_v $v
+	set last_date $date
+    }
+
+    set y_grid_delta [expr ($widget_max - $widget_min) / $widget_bins]
+    set y_grid_delta [im_diagram_round_to_next_nice_number $y_grid_delta]
+
+    set border "border:1px solid blue; "
+    set border ""
+
+    set histogram_html "
+	<div style='$border position:relative;top:0px;height:${diagram_height}px;width:${diagram_width}px;'>
+	<SCRIPT Language=JavaScript>
+	document.open();
+	var $oname=new Diagram();
+
+	$oname.Font=\"$font_style\";
+	_BFont=\"$font_style\";
+
+	$oname.SetFrame(
+		$outer_distance + $left_distance, $outer_distance, 
+		$diagram_width - $outer_distance, $diagram_height - $outer_distance - $bottom_distance
+	);
+	$oname.SetBorder(
+		Date.UTC($year0, $month0, $day0, $hour0, $min0, $sec0),
+		Date.UTC($year9, $month9, $day9, $hour9, $min9, $sec9),
+		$widget_min, $widget_max
+	);
+	$oname.XScale=4;
+	$oname.YScale=1;
+
+	$oname.GetXGrid();
+	$oname.XGridDelta=$oname.XGrid\[1\]*3;
+	$oname.XGridDelta=$oname.XGrid\[1\];
+	$oname.YGridDelta=$y_grid_delta;
+
+	$oname.Draw(\"\", \"$diagram_color\", false);
+	$oname.SetText(\"\",\"\", \"<B>$name</B>\");
+	$diagram_html
+	document.close();
+	</SCRIPT>
+	</div>
+    "
+
+    return $histogram_html
+}
 
 
