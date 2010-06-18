6dd4fef718908da3004488a9e9470f17c6d1bbc9
Frank Bergmann <frank.bergmann@project-open.com>
2010-06-18 19:50:07 +0000

    - removed Earned Value

--- sql/postgresql/intranet-reporting-dashboard-create.sql
+++ sql/postgresql/intranet-reporting-dashboard-create.sql
@@ -296,24 +296,3 @@ SELECT im_component_plugin__new (
 	'lang::message::lookup "" intranet-reporting-dashboard.Sales_Pipeline "Sales<br>Pipeline"'
 );
 
-
--- Project Earned Value
---
-SELECT im_component_plugin__new (
-	null,					-- plugin_id
-	'im_component_plugin',			-- object_type
-	now(),					-- creation_date
-	null,					-- creation_user
-	'0.0.0.0',				-- creation_ip
-	null,					-- context_id
-	'Earned Value',				-- plugin_name
-	'intranet-reporting-dashboard',		-- package_name
-	'right',				-- location
-	'/intranet/projects/view',		-- page_url
-	null,					-- view_name
-	200,					-- sort_order
-	'im_dashboard_project_eva -project_id $project_id',
-	'lang::message::lookup "" intranet-reporting-dashboard.Earned_Value "Earned Value"'
-);
-
-
