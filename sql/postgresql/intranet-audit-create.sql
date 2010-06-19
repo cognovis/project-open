210cae95940d1da6fc01f0b642c308c3284ab06f
Frank Bergmann <frank.bergmann@project-open.com>
2010-06-19 16:59:47 +0000

    - added Earned Value portlet to audit package

--- sql/postgresql/intranet-audit-create.sql
+++ sql/postgresql/intranet-audit-create.sql
@@ -369,3 +369,24 @@ SELECT acs_permission__grant_permission(
 	'read'
 );
 
+
+
+-- Project Earned Value
+--
+SELECT im_component_plugin__new (
+	null,					-- plugin_id
+	'im_component_plugin',			-- object_type
+	now(),					-- creation_date
+	null,					-- creation_user
+	'0.0.0.0',				-- creation_ip
+	null,					-- context_id
+	'Earned Value',				-- plugin_name
+	'intranet-reporting-dashboard',		-- package_name
+	'right',				-- location
+	'/intranet/projects/view',		-- page_url
+	null,					-- view_name
+	200,					-- sort_order
+	'im_dashboard_project_eva -project_id $project_id',
+	'lang::message::lookup "" intranet-reporting-dashboard.Earned_Value "Earned Value"'
+);
+
