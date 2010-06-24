67a48358e9f3b89764dd41c4e5d2ee46a982ec13
Frank Bergmann <frank.bergmann@project-open.com>
2010-06-24 10:01:15 +0000

    - fixed object type for many menus and component plugins created

--- sql/postgresql/upgrade/upgrade-3.2.5.0.0-3.2.6.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.5.0.0-3.2.6.0.0.sql
@@ -35,7 +35,7 @@ begin
 
     v_menu := im_menu__new (
         null,                   -- p_menu_id
-        ''acs_object'',         -- object_type
+        ''im_menu'',         -- object_type
         now(),                  -- creation_date
         null,                   -- creation_user
         null,                   -- creation_ip
