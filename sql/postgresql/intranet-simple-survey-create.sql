371d5895ef2e1af2afeb2e30192a8f8bd2d40db1
Frank Bergmann <frank.bergmann@project-open.com>
2010-06-24 10:01:19 +0000

    - fixed object type for many menus and component plugins created

--- sql/postgresql/intranet-simple-survey-create.sql
+++ sql/postgresql/intranet-simple-survey-create.sql
@@ -428,55 +428,39 @@ drop function inline_0 ();
 
 
 
-
 -- Setup the Simple Survey Report in Menus
 --
 create or replace function inline_0 ()
 returns integer as '
 declare
-	-- Menu IDs
 	v_menu			integer;
-	v_reporting_other_menu 		integer;
-
-	-- Groups
+	v_project_menu 		integer;
 	v_employees		integer;
-	v_accounting		integer;
-	v_senman		integer;
-	v_customers		integer;
-	v_freelancers		integer;
-	v_proman		integer;
-	v_admins		integer;
 begin
-	select group_id into v_senman from groups where group_name = ''Senior Managers'';
-	select group_id into v_proman from groups where group_name = ''Project Managers'';
-	select group_id into v_accounting from groups where group_name = ''Accounting'';
-
-	select menu_id into v_reporting_other_menu from im_menus where label=''reporting-other'';
+	select group_id into v_employees from groups where group_name = ''Employees'';
+	select menu_id into v_project_menu from im_menus where label=''projects'';
 
 	v_menu := im_menu__new (
 		null,							-- menu_id
-		''acs_object'',						-- object_type
+		''im_menu'',						-- object_type
 		now(),							-- creation_date
 		null,							-- creation_user
 		null,							-- creation_ip
 		null,							-- context_id
 		''intranet-simple-survey'',				-- package_name
-		''reporting_survsimp_project_reports'',			-- label
-		''Simple Survey Project Reports & Reminders'',		-- name
+		''project_reports'',					-- label
+		''Project Reports'',					-- name
 		''/intranet-simple-survey/reporting/project-reports'',	-- url
-		20,							-- sort_order
-		v_reporting_other_menu,					-- parent_menu_id
+		-5,							-- sort_order
+		v_project_menu,						-- parent_menu_id
 		null							-- visible_tcl
 	);
 
-	PERFORM acs_permission__grant_permission(v_menu, v_accounting, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_senman, ''read'');
-	PERFORM acs_permission__grant_permission(v_menu, v_proman, ''read'');
+	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
 
 	return 0;
 end;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
-
 
 
