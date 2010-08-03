b23f2d591422763dd56b8f3a83844f4e09785955
po34devklaus <>
2010-08-03 11:24:38 +0000

    -- patch Malte - menu item

--- sql/postgresql/intranet-portfolio-management-create.sql
+++ sql/postgresql/intranet-portfolio-management-create.sql
@@ -153,3 +153,23 @@ insert into im_view_columns (column_id, view_id, group_id, column_name, column_r
 extra_select, extra_where, sort_order, visible_for) values (92010,920,NULL,'Project',
 '"<nobr>$indent_html$gif_html<a href=[export_vars -base $project_base_url {project_id}]>$project_name</a></nobr>"','','',10,'');
 
+
+-- -------------------------------
+-- Create the menu item
+-- -------------------------------
+
+SELECT im_menu__new (
+         null,                           -- p_menu_id
+         'acs_object',                   -- object_type
+         now(),                          -- creation_date
+         null,                           -- creation_user
+         null,                           -- creation_ip
+         null,                           -- context_id
+         'intranet-portfolio-management',            -- package_name
+         'department_planner',             -- label
+         'Department Planner',                      -- name
+         '/intranet-portfolio-management/department-planner/index?view_name=portfolio_department_planner_list',       -- url
+         10,                             -- sort_order
+         (select menu_id from im_menus where label = 'projects'),
+         null                            -- p_visible_tcl
+);
