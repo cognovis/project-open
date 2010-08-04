91ff0df558bc5b4882869303360c486409f58d30
po34devklaus <>
2010-08-04 06:43:16 +0000

    -- initial c

---
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.4-3.4.1.0.5.sql
@@ -0,0 +1,13 @@
+-- upgrade-3.4.1.0.4-3.4.1.0.5.sql
+
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.1.0.4-3.4.1.0.5.sql','');
+
+SELECT im_lang_add_message('en_US','intranet-expenses','Expense_Report','Expense Report');
+SELECT im_lang_add_message('en_US','intranet-core','Date_submitted','Date submitted');
+SELECT im_lang_add_message('en_US','intranet-core','Amount_due','Amount due');
+SELECT im_lang_add_message('en_US','intranet-core','Print_date','Print date');
+SELECT im_lang_add_message('en_US','intranet-cost','Number_billable_expenses','Number billable expenses');
+
+
+
+
