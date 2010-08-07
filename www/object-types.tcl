162c3e7ab9205a39d3d015afa69577fae6b8dfa6
Frank Bergmann <frank.bergmann@project-open.com>
2010-08-07 17:50:33 +0000

    - added im_cost_center as a dynfieldified object type

--- www/object-types.tcl
+++ www/object-types.tcl
@@ -75,7 +75,7 @@ db_multirow -extend { object_attributes_url } object_types select_object_types {
 	where	object_type in (
 			'im_company', 'im_office', 'im_project', 'im_ticket', 'person', 
 			'im_freelance_rfq', 'im_freelance_rfq_answer',
-			'im_conf_item', 'im_note', 'im_user_absence',
+			'im_conf_item', 'im_cost_center', 'im_note', 'im_user_absence',
 			'im_invoice', 'im_expense', 'im_expense_bundle',
 			'im_material', 'im_timesheet_task'
 		)
