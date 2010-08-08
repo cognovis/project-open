ec25526756f6c313e38ec8de4f3134220a278603
Frank Bergmann <frank.bergmann@project-open.com>
2010-08-08 20:02:11 +0000

    - added permissions to make the department planner readable for all employees

--- sql/postgresql/intranet-portfolio-management-create.sql
+++ sql/postgresql/intranet-portfolio-management-create.sql
@@ -173,17 +173,26 @@ extra_select, extra_where, sort_order, visible_for) values (92010,920,NULL,'Proj
 -- -------------------------------
 
 SELECT im_menu__new (
-         null,                           -- p_menu_id
-         'acs_object',                   -- object_type
-         now(),                          -- creation_date
-         null,                           -- creation_user
-         null,                           -- creation_ip
-         null,                           -- context_id
-         'intranet-portfolio-management',            -- package_name
-         'department_planner',             -- label
-         'Department Planner',                      -- name
-         '/intranet-portfolio-management/department-planner/index?view_name=portfolio_department_planner_list',       -- url
-         10,                             -- sort_order
-         (select menu_id from im_menus where label = 'projects'),
-         null                            -- p_visible_tcl
+	null,					-- p_menu_id
+	'acs_object',				-- object_type
+	now(),					-- creation_date
+	null,					-- creation_user
+	null,					-- creation_ip
+	null,					-- context_id
+	'intranet-portfolio-management',		-- package_name
+	'department_planner',			-- label
+	'Department Planner',			-- name
+	'/intranet-portfolio-management/department-planner/index?view_name=portfolio_department_planner_list',	-- url
+	10,					-- sort_order
+	(select menu_id from im_menus where label = 'projects'),
+	null					-- p_visible_tcl
 );
+
+
+
+SELECT acs_permission__grant_permission(
+	(select menu_id from im_menus where label = 'department_planner'),
+	(select group_id from groups where group_name = 'Employees'),
+	'read'
+);
+
