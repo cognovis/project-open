64b813caf834f4a882e033a5099e0272c50f28f5
Frank Bergmann <frank.bergmann@project-open.com>
2010-08-13 16:06:02 +0000

    - added acs_object_type_tables for im_timesheet_task

--- sql/postgresql/upgrade/upgrade-3.4.1.0.0-3.4.1.0.1.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.0-3.4.1.0.1.sql
@@ -228,6 +228,37 @@ SELECT im_dynfield_attribute_new ('im_timesheet_task', 'project_status_id', 'Sta
 'project_status', 'integer', 'f', 30, 't', 'im_projects');
 SELECT im_dynfield_attribute_new ('im_timesheet_task', 'project_type_id', 'Type', 
 'project_type', 'integer', 'f', 40, 't', 'im_projects');
+
+
+
+-- Make sure the acs_object_type_tables exist
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+        v_count                 integer;
+begin
+	select	count(*) into v_count from acs_object_type_tables
+	where	lower(object_type) = 'im_timesheet_task' and lower(table_name) = 'im_timesheet_tasks';
+        IF v_count = 0 THEN
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values ('im_timesheet_task', 'im_timesheet_tasks', 'task_id');
+	END IF;
+
+	select	count(*) into v_count from acs_object_type_tables
+	where	lower(object_type) = 'im_timesheet_task' and lower(table_name) = 'im_projects';
+        IF v_count = 0 THEN
+		insert into acs_object_type_tables (object_type,table_name,id_column)
+		values ('im_timesheet_task', 'im_projects', 'project_id');
+	END IF;
+
+        return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
 SELECT im_dynfield_attribute_new ('im_timesheet_task', 'uom_id', 'Unit of Measure', 
 'units_of_measure', 'integer', 'f', 50, 't', 'im_timesheet_tasks');
 SELECT im_dynfield_attribute_new ('im_timesheet_task', 'material_id', 'Material', 
