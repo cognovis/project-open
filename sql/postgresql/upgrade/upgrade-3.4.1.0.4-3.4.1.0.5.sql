d97d638b3bc6d4bfe6bfe8bdcd3b24cdb3af87c7
Frank Bergmann <frank.bergmann@project-open.com>
2010-08-14 19:47:53 +0000

    - added new Argentinian Peso

--- sql/postgresql/upgrade/upgrade-3.4.1.0.4-3.4.1.0.5.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.4-3.4.1.0.5.sql
@@ -9,5 +9,10 @@ SELECT im_lang_add_message('en_US','intranet-core','Print_date','Print date');
 SELECT im_lang_add_message('en_US','intranet-cost','Number_billable_expenses','Number billable expenses');
 
 
+update currency_codes set
+	currency_name = 'Argentinian Peso (obsolete)'
+where
+	iso = 'ARP';
 
+insert into currency_codes values ('ARS','Argentinian Peso','f','');
 
