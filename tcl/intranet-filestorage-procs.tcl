30ed7492ee5158761443441a247f5f9e07c0f5f8
Frank Bergmann <frank.bergmann@project-open.com>
2010-09-28 10:36:27 +0000

    - fixed issue with bread_crum_path

--- tcl/intranet-filestorage-procs.tcl
+++ tcl/intranet-filestorage-procs.tcl
@@ -1467,7 +1467,7 @@ ad_proc -public im_filestorage_base_component { user_id object_id object_name ba
     # to be incorporated into the local return_url
     ns_set delkey $bind_vars return_url
     set return_url "$current_url_without_vars?[export_url_bind_vars $bind_vars]"
-    if {"" != $bread_crum_path} {
+    if {"" == $bread_crum_path} {
         set bread_crum_path [ns_set get $bind_vars bread_crum_path]
     }
     set base_path_depth [llength [split $base_path "/"]]
