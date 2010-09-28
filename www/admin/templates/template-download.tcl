830f5882cfa6748ed1fcfac9630aa8ae39c9292b
Frank Bergmann <frank.bergmann@project-open.com>
2010-09-28 11:25:53 +0000

    - nothing changed

--- www/admin/templates/template-download.tcl
+++ www/admin/templates/template-download.tcl
@@ -26,6 +26,8 @@ if {!$user_is_admin_p} {
 set path_to_file [parameter::get -package_id [db_string get_view_id "select package_id from apm_packages where package_key = 'intranet-invoices'" -default 0] -parameter "InvoiceTemplatePathUnix" -default ""]
 append path_to_file "/"  $template_name
 
+
+
 if {[catch {
      ns_returnfile 200 "application" $path_to_file
 } err_msg]} {
