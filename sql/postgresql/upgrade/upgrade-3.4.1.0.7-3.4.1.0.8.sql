7f008d804fb493bd3c3172ae5915596524ba564b
Frank Bergmann <frank.bergmann@project-open.com>
2010-09-29 17:12:40 +0000

    - fixed im_biz_object_url for "view" mode to include form_mode=display

--- sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
@@ -4,3 +4,9 @@ SELECT acs_log__debug('/packages/intranet-helpdesk/sql/postgresql/upgrade/upgrad
 
 
 SELECT im_category_new(30540, 'Associate', 'Intranet Ticket Action');
+
+
+update im_biz_object_urls 
+set url = '/intranet-helpdesk/new?form_mode=display&ticket_id='
+where object_type = 'im_ticket' and url_type = 'view';
+
