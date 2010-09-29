87c5bf3e9fc13c54224ca67f056253a3f2da8342
Frank Bergmann <frank.bergmann@project-open.com>
2010-09-29 17:24:43 +0000

    - renamed component form "Related Tickets" to "Related Objects"

--- sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
@@ -10,3 +10,57 @@ update im_biz_object_urls
 set url = '/intranet-helpdesk/new?form_mode=display&ticket_id='
 where object_type = 'im_ticket' and url_type = 'view';
 
+
+
+
+-- ------------------------------------------------------
+-- Show related objects
+--
+SELECT	im_component_plugin__new (
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Related Objects',		-- plugin_name
+	'intranet-helpdesk',		-- package_name
+	'right',			-- location
+	'/intranet-helpdesk/new',	-- page_url
+	null,				-- view_name
+	91,				-- sort_order
+	'im_helpdesk_related_objects_component -ticket_id $ticket_id',
+	'lang::message::lookup "" intranet-helpdesk.Related_Objects "Related Objects"'
+);
+
+SELECT acs_permission__grant_permission(
+        (select plugin_id from im_component_plugins where plugin_name = 'Related Objects' and package_name = 'intranet-helpdesk'),
+        (select group_id from groups where group_name = 'Employees'),
+        'read'
+);
+
+
+
+-- Delete the old "Related Tickets" component
+create or replace function inline_0 ()
+returns integer as $$
+declare
+	v_component_id		integer;
+begin
+	select	plugin_id into v_component_id
+	from	im_component_plugins
+	where	package_name = 'intranet-helpdesk' and
+		plugin_name = 'Related Objects';
+
+	IF v_component_id IS NULL THEN return 1; END IF;
+
+	PERFORM im_component_plugin__delete(v_component_id);
+
+	return 0;
+end;$$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
