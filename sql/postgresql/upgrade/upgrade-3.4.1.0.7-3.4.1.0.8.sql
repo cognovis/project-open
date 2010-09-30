f20f7352926f5e43a0f5a51f611e62e30c64af13
Frank Bergmann <frank.bergmann@project-open.com>
2010-09-30 12:54:29 +0000

    - fixed upgrade version number

--- sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql
@@ -1,6 +1,6 @@
 -- upgrade-3.4.1.0.6-3.4.1.0.8.sql
 
-SELECT acs_log__debug('/packages/intranet-confdb/sql/postgresql/upgrade/upgrade-3.4.1.0.6-3.4.1.0.8.sql','');
+SELECT acs_log__debug('/packages/intranet-confdb/sql/postgresql/upgrade/upgrade-3.4.1.0.7-3.4.1.0.8.sql','');
 
 
 SELECT im_category_new(30540, 'Associate', 'Intranet Ticket Action');
