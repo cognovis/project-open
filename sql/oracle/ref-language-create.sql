8bb7cc39113286996525952958e341ef8be592aa
Frank Bergmann <frank.bergmann@project-open.com>
2010-10-23 13:00:17 +0000

    - OpenACS 5.6

--- sql/oracle/ref-language-create.sql
+++ sql/oracle/ref-language-create.sql
@@ -6,29 +6,26 @@
 --
 
 
--- ISO 639
+-- ISO 639-1
 create table language_codes (
     language_id char(2)
         constraint language_codes_language_id_pk
         primary key,
     name varchar(100)
-        constraint language_codes_name_uq
-        unique
         constraint language_codes_name_nn
         not null
 );
 
 comment on table language_codes is '
-    This is data from the ISO 639 standard on language codes.
+    This is data from the ISO 639-1 standard on language codes.
 ';
 
 comment on column language_codes.language_id is '
-    This is the ISO standard language code
+    This is the ISO standard language 2 char code
 ';
 
 comment on column language_codes.name is '
     This is the English version of the language name. 
-    I don''t want to get crazy here!
 ';
 
 -- now register this table with the repository
@@ -37,10 +34,20 @@ declare
 begin
     v_id := acs_reference.new(
         table_name     => upper('language_codes'),
-        source         => 'ISO 639',
+        source         => 'ISO 639-1',
         source_url     => 'http://www.iso.ch',
         effective_date => sysdate
     );
 commit;
 end;
 /
+
+-- Languages ISO-639-2 codes
+
+create table language_639_2_codes (
+       iso_639_2            char(3) constraint language_codes_iso_639_2_pk primary key,
+       iso_639_1            char(2),
+       label                varchar(200)
+);
+
+comment on table language_639_2_codes is 'Contains ISO-639-2 language codes and their corresponding ISO-639-1 when it exists.';
