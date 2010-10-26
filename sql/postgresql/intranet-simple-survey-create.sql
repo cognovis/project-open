37cc8360c6cc95937461612ac8a28d8c0cc0f2fb
Frank Bergmann <frank.bergmann@project-open.com>
2010-10-26 14:29:49 +0000

    - added ticket survey component

--- sql/postgresql/intranet-simple-survey-create.sql
+++ sql/postgresql/intranet-simple-survey-create.sql
@@ -248,7 +248,7 @@ declare
 begin
 	v_plugin := im_component_plugin__new (
 		null,					-- plugin_id
-		''acs_object'',				-- object_type
+		''im_component_plugin'',		-- object_type
 		now(),					-- creation_date
 		null,					-- creation_user
 		null,					-- creation_ip
@@ -274,7 +274,7 @@ declare
 begin
 	v_plugin := im_component_plugin__new (
 		null,					-- plugin_id
-		''acs_object'',				-- object_type
+		''im_component_plugin'',		-- object_type
 		now(),					-- creation_date
 		null,					-- creation_user
 		null,					-- creation_ip
@@ -321,6 +321,31 @@ drop function inline_0 ();
 
 
 
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_plugin		integer;
+begin
+	v_plugin := im_component_plugin__new (
+		null,					-- plugin_id
+		''im_component_plugin'',		-- object_type
+		now(),					-- creation_date
+		null,					-- creation_user
+		null,					-- creation_ip
+		null,					-- context_id
+		''Ticket Survey Component'',		-- plugin_name
+		''intranet-simple-survey'',		-- package_name
+		''right'',				-- location
+		''/intranet-helpdesk/new'',		-- page_url
+		null,					-- view_name
+		120,					-- sort_order
+		''im_survsimp_component $ticket_id''	-- component_tcl
+	);
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
 
 
 
