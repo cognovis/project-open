4948b90fe7cc5af436a7be8486269d58701a851f
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-12 13:14:39 +0000

    - added v_count

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -3,10 +3,31 @@
 SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql','');
 
 
-alter table apm_package_types
-add column inherit_templates_p char(1);
 
-alter table apm_package_types
-add column implements_subsite_p char(1);
+
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+        v_count                 integer;
+begin
+        select count(*) into v_count from user_tab_columns
+	where lower(table_name) = 'apm_package_types' and lower(column_name) = 'inherit_templates_p';
+        IF v_count = 0 THEN
+		alter table apm_package_types
+		add column inherit_templates_p char(1);
+	END IF;
+
+        select count(*) into v_count from user_tab_columns
+	where lower(table_name) = 'apm_package_types' and lower(column_name) = 'implements_subsite_p';
+        IF v_count = 0 THEN
+		alter table apm_package_types
+		add column implements_subsite_p char(1);
+	END IF;
+
+        return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
 
 
