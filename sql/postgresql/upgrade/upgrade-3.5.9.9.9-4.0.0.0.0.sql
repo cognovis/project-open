55a678337524fdf9146f6bd995aa42f6097e5e1f
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-13 12:20:08 +0000

    - Removed XoTCL files from product - started implementing Versia ITSM extensions

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -14,15 +14,31 @@ begin
 	where lower(table_name) = 'apm_package_types' and lower(column_name) = 'inherit_templates_p';
         IF v_count = 0 THEN
 		alter table apm_package_types
-		add column inherit_templates_p char(1);
+		add column inherit_templates_p boolean default 't'
+			constraint inherit_templates_p_nn
+			not null;
 	END IF;
 
         select count(*) into v_count from user_tab_columns
 	where lower(table_name) = 'apm_package_types' and lower(column_name) = 'implements_subsite_p';
         IF v_count = 0 THEN
 		alter table apm_package_types
-		add column implements_subsite_p char(1);
+		add column implements_subsite_p boolean default 'f'
+			constraint apm_packages_impl_subs_p_nn
+			not null;
 	END IF;
+
+        select count(*) into v_count from user_tab_columns
+	where lower(table_name) = 'apm_package_types' and lower(column_name) = 'implements_subsite_p';
+        IF v_count = 0 THEN
+		alter table apm_parameters
+		add column scope varchar(10) default 'instance'
+			constraint apm_parameters_scope_ck
+			check (scope in ('global','instance'))
+			constraint apm_parameters_scope_nn
+			not null;
+	END IF;
+
 
         return 0;
 end;$body$ language 'plpgsql';
