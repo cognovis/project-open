42fb4652b14c81412c5f0e8e910f565742b244c6
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-14 17:33:15 +0000

    - removed earned value diagram

--- sql/postgresql/intranet-audit-create.sql
+++ sql/postgresql/intranet-audit-create.sql
@@ -20,7 +20,7 @@ create table im_audits (
 	audit_object_id		integer
 				constraint im_audits_object_nn
 				not null,
-	audit_status_id		integer
+	audit_object_status_id	integer
 				constraint im_audits_status_fk
 				references im_categories,
 	audit_action		text
@@ -371,32 +371,4 @@ SELECT acs_permission__grant_permission(
 	(select group_id from groups where group_name = 'Employees'),
 	'read'
 );
-
-
-
--- Project Earned Value
---
-SELECT im_component_plugin__new (
-	null,					-- plugin_id
-	'im_component_plugin',			-- object_type
-	now(),					-- creation_date
-	null,					-- creation_user
-	'0.0.0.0',				-- creation_ip
-	null,					-- context_id
-	'Earned Value',				-- plugin_name
-	'intranet-audit',			-- package_name
-	'left',					-- location
-	'/intranet/projects/view',		-- page_url
-	null,					-- view_name
-	50,					-- sort_order
-	'im_audit_project_eva_diagram -project_id $project_id',
-	'lang::message::lookup "" intranet-reporting-dashboard.Earned_Value "Earned Value"'
-);
-
-SELECT acs_permission__grant_permission(
-	(select plugin_id from im_component_plugins where plugin_name = 'Earned Value'),
-	(select group_id from groups where group_name = 'Employees'),
-	'read'
-);
-
 
