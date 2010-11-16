66587782080fa5b1a2c0dd77cadf091fbb028bf1
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-16 21:12:47 +0000

    - fixed apm procedure

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -468,42 +468,6 @@ begin
    
 end;' language 'plpgsql' stable strict;
 
-create or replace function apm__get_value (integer,varchar)
-returns varchar as '
-declare
-  get_value__package_id             alias for $1;  
-  get_value__parameter_name         alias for $2;  
-  v_parameter_id                    apm_parameter_values.parameter_id%TYPE;
-  value                             apm_parameter_values.attr_value%TYPE;
-begin
-    v_parameter_id := apm__id_for_name (get_value__package_id, get_value__parameter_name);
-
-    select attr_value into value from apm_parameter_values v
-    where v.package_id = get_value__package_id
-    and parameter_id = get_value__parameter_id;
-
-    return value;
-   
-end;' language 'plpgsql' stable strict;
-
-create or replace function apm__get_value (varchar,varchar)
-returns varchar as '
-declare
-  get_value__package_key            alias for $1;  
-  get_value__parameter_name         alias for $2;  
-  v_parameter_id                    apm_parameter_values.parameter_id%TYPE;
-  value                             apm_parameter_values.attr_value%TYPE;
-begin
-    v_parameter_id := apm__id_for_name (get_value__package_key, get_value__parameter_name);
-
-    select attr_value into value from apm_parameter_values v
-    where v.package_id is null
-    and parameter_id = get_value__parameter_id;
-
-    return value;
-   
-end;' language 'plpgsql' stable strict;
-
 create or replace function apm__set_value (integer,varchar,varchar)
 returns integer as '
 declare
@@ -1600,3 +1564,45 @@ begin
 end;' language 'plpgsql';
 
 
+
+
+
+
+
+
+
+create or replace function apm__get_value (integer,varchar)
+returns varchar as '
+declare
+  p_get_value__package_id             alias for $1;
+  p_get_value__parameter_name         alias for $2;
+  v_parameter_id                    apm_parameter_values.parameter_id%TYPE;
+  value                             apm_parameter_values.attr_value%TYPE;
+begin
+    v_parameter_id := apm__id_for_name (p_get_value__package_id, p_get_value__parameter_name);
+
+    select attr_value into value from apm_parameter_values v
+    where v.package_id = p_get_value__package_id
+    and parameter_id = v_parameter_id;
+
+    return value;
+
+end;' language 'plpgsql' stable strict;
+
+create or replace function apm__get_value (varchar,varchar)
+returns varchar as '
+declare
+  get_value__package_key            alias for $1;
+  get_value__parameter_name         alias for $2;
+  v_parameter_id                    apm_parameter_values.parameter_id%TYPE;
+  value                             apm_parameter_values.attr_value%TYPE;
+begin
+    v_parameter_id := apm__id_for_name (get_value__package_key, get_value__parameter_name);
+
+    select attr_value into value from apm_parameter_values v
+    where v.package_id is null
+    and parameter_id = get_value__parameter_id;
+
+    return value;
+
+end;' language 'plpgsql' stable strict;
