033d240162578a85c13ed58af74e9533724ad721
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-18 08:52:28 +0000

    - implemented semaphore for SLA resolution time sweeper

--- tcl/intranet-sla-management-init.tcl
+++ tcl/intranet-sla-management-init.tcl
@@ -10,7 +10,7 @@ ad_library {
 # Initialize the search "semaphore" to 0.
 # There should be only one thread sweeping at any time, even if the
 # thread should be very slow...
-nsv_set intranet_sla_parameter_sweeper sweeper_p 0
+nsv_set intranet_sla_management sweeper_p 0
 
 # Re-calculate the "solution time" of open tickets every minute
 ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-sla-management -parameter SLASolutionTimeSweeperInterval -default 59] im_sla_ticket_solution_time
