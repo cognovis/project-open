f1aa107596228d4f11fe3c6169dd5feb4e2f6ad0
Frank Bergmann <frank.bergmann@project-open.com>
2010-11-30 13:39:35 +0000

    - 2nd version of SLA parameter indicators, now in a unified table

--- www/sla-parameter-indicator-component.adp
+++ www/sla-parameter-indicator-component.adp
@@ -1,6 +1,12 @@
 <if @sla_read@>
 <if @indicator_cnt@>
 <script language="JavaScript" type="text/javascript" src="/resources/diagram/diagram/diagram.js"></script>
-<listtemplate name="report_list"></listtemplate>
+<form action=sla-parameter-indicator-action method=POST>
+<table>
+@header_html;noquote@
+@body_html;noquote@
+</table>
+</form>
+@footer_html;noquote@
 </if>
 </if>
--- www/sla-parameter-indicator-component.tcl
+++ www/sla-parameter-indicator-component.tcl
@@ -1,4 +1,4 @@
-# /packages/intranet-sla-management/www/indicator_component.tcl
+# /packages/intranet-sla-management/www/sla-parameter-indicator_component.tcl
 #
 # Copyright (c) 2010 ]project-open[
 #
@@ -27,86 +27,76 @@ set current_user_id [ad_maybe_redirect_for_registration]
 set add_reports_p [im_permission $current_user_id "add_reports"]
 set view_reports_all_p [im_permission $current_user_id "view_reports_all"]
 
+set bgcolor(0) " class=roweven "
+set bgcolor(1) " class=rowodd "
+
 if {"" == $return_url} { set return_url [ad_conn url] }
-set page_title [lang::message::lookup "" intranet-reporting.Indicators "Indicators"]
+set page_title [lang::message::lookup "" intranet-sla-parameters.SLA_Parameters "SLA Parameters"]
 set context_bar [im_context_bar $page_title]
 set context ""
-
-if {![info exists project_id]} { ad_return_complaint 1 "sla-parameter-indicator-component.tcl: variable project_id not defined" }
 
 # Evaluate indicators every X hours:
 set eval_interval_hours [parameter::get_from_package_key -package_key "intranet-reporting-indicators" -parameter "IndicatorEvaluationIntervalHours" -default 24]
 
 # Permissions: Check read permissions on the SLA
+if {![info exists project_id]} { ad_return_complaint 1 "sla-parameter-indicator-component.tcl: variable project_id not defined" }
 im_project_permissions $current_user_id $project_id sla_view sla_read sla_write sla_admin
 # $read is queries in the .adp template
-
-
-# ------------------------------------------------------
-# List creation
-# ------------------------------------------------------
-set elements_list {}
-
-lappend elements_list \
-	param {
-	    label "Param"
-	    display_template {
-		<a href=@reports.param_view_url@>@reports.param_name@</a>
-	    }
-	}
-
-lappend elements_list \
-	value {
-	    label "Value"
-	    display_template {
-		<b><font color=@reports.indicator_color@>@reports.value_html;noquote@</font></b>
-	    }
-	}
-
-lappend elements_list \
-	diagram {
-	    label "Diagram"
-	    display_template { @reports.diagram_html;noquote@ }
-	}
-
-lappend elements_list \
-    name {
-	label $page_title
-	display_template {
-	    <a href=@reports.report_view_url@>@reports.report_name@</a>
-	    @reports.help_gif;noquote@
-	}
-    }
-
-if {$add_reports_p} {
-    lappend elements_list \
-	edit {
-	    label "[im_gif wrench]"
-	    display_template {
-		@reports.edit_html;noquote@
-	    }
-	}
-}
-
-list::create \
-        -name report_list \
-        -multirow reports \
-        -key menu_id \
-        -elements $elements_list \
-        -class "table_indicators" \
-        -filters {
-        	return_url
-        }
 
 set permission_sql "and 't' = im_object_permission_p(r.report_id, :current_user_id, 'read')"
 #if {$view_reports_all_p} { set permission_sql "" }
 
-set indicator_cnt 0
-db_multirow -extend {param_view_url report_view_url report_edit_html value_html edit_html diagram_html help_gif indicator_color} reports get_reports "
+
+
+# ---------------------------------------------------------------
+# Header: We need to show a list of parameter DynFields
+# ---------------------------------------------------------------
+
+set header_html ""
+append header_html "<tr class=rowtitle>\n"
+append header_html "<td><input type=checkbox name=_dummy onclick=\"acs_ListCheckAll('param',this.checked)\"></td>\n"
+append header_html "<td class=rowtitle>[lang::message::lookup "" intranet-sla-management.Parameter_Name "Name"]</td>\n"
+
+# Loop through DynFilds of the im_sla_parameter
+set column_sql "
+        select  w.deref_plpgsql_function,
+                aa.attribute_name,
+                aa.pretty_name
+        from    im_dynfield_widgets w,
+                im_dynfield_attributes a,
+                acs_attributes aa
+        where   a.widget_name = w.widget_name and
+                a.acs_attribute_id = aa.attribute_id and
+                aa.object_type = 'im_sla_parameter'
+"
+set extra_selects [list "0 as zero"]
+set dynfield_attributes [list]
+db_foreach column_list_sql $column_sql {
+
+    # Select another field
+    lappend extra_selects "${deref_plpgsql_function}($attribute_name) as ${attribute_name}_deref"
+
+    # Show this field in the title
+    append header_html "<td class=rowtitle>[lang::message::lookup "" intranet-sla-management.$attribute_name $pretty_name]</td>\n"
+
+    # Add the field name to the list of DynFields to be shown
+    lappend dynfield_attributes $attribute_name
+}
+set extra_select [join $extra_selects ",\n\t\t"]
+
+append header_html "</tr>\n"
+
+
+# ---------------------------------------------------------------
+# The SLA-Parameter + Indicator SQL
+# ---------------------------------------------------------------
+
+set indicator_sql "
 	select
 		sp.*,
 		r.*,
 		i.*,
+		$extra_select,
 		im_category_from_id(i.indicator_section_id) as section,
 		ir.result
 	from
@@ -132,53 +122,43 @@ db_multirow -extend {param_view_url report_view_url report_edit_html value_html 
 		i.indicator_object_type = 'im_sla_parameter'
 		$permission_sql
 	order by 
-		section
-" {
-    incr indicator_cnt
+		lower(sp.param_name),
+		lower(r.report_name)
+"
 
-    set report_view_url [export_vars -base "/intranet-reporting-indicators/view" {indicator_id return_url}]
-    set report_edit_url [export_vars -base "/intranet-reporting-indicators/new" {indicator_id return_url}]
+
+# ---------------------------------------------------------------
+# Body: We need to show a list of parameter DynFields
+# ---------------------------------------------------------------
+
+# We have a double loop here with:
+#	1st: parameter: We need to show paramer DynFiends and
+#	2nd: indicator: We need to render the indicator
+
+set body_html ""
+set old_param_id ""
+set param_cnt 0
+set indicator_cnt 0
+set colspan [expr [llength $dynfield_attributes] + 2]
+db_foreach param_indicators $indicator_sql {
+
     set param_view_url [export_vars -base "/intranet-sla-management/new" {param_id {form_mode display} return_url}]
 
+    # Check if the parameter has changed.
+    # In this case show the param plus DynFields
+    if {$old_param_id != $param_id} {
 
-    set perms_url [export_vars -base "/intranet-reporting-indicators/perms" {{object_id $indicator_id} return_url}]
-    set edit_html "
-	<a href='$report_edit_url'>[im_gif "wrench"]</a>
-	<a href='$perms_url'>[im_gif "lock"]</a>
-    "
-    set help_gif [im_gif help $report_description]
-    set indicator_color "black"
-
-    if {"" == $result} {
-	set result "error"
-	set error_occured [catch {set result [db_string value $report_sql]} err_msg]
-
-	if {$error_occured} {
-	    set result "<pre>$err_msg</pre>" 
-	} else {
-	    if {"" != $result} {
-		db_dml insert "
-			insert into im_indicator_results (
-				result_id,result_indicator_id,result_date,result
-			) values (
-				nextval('im_indicator_results_seq'),:report_id,now(),:result
-			)
-	        "
-	    }
+	set row_html ""
+	append row_html "<td><input type=checkbox name=param value=$param_id id=\"param,$param_id\"></td>\n"
+	append row_html "<td><a href='$param_view_url'>$param_name</a></td>\n"
+	# Append the values of the dynfield attributes
+	foreach da $dynfield_attributes {
+	    append row_html "<td>[eval "set a \$${da}_deref"]</td>\n"
 	}
-    }
 
-    if {"" != $result} {
-	if {"" != $result} {
-	    if {"" != $indicator_low_warn} { if {$result < $indicator_low_warn} { set indicator_color "\#DF8F00" } }
-	    if {"" != $indicator_low_critical} { if {$result < $indicator_low_critical} { set indicator_color "red" } }
-	    if {"" != $indicator_high_warn} { if {$result > $indicator_high_warn} { set indicator_color "orange" } }
-	    if {"" != $indicator_high_critical} { if {$result > $indicator_high_critical} { set indicator_color "red" } }
-	}
+	append body_html $row_html
+	incr param_cnt
     }
-
-    set value_html "min=$indicator_widget_min, res=$result, max=$indicator_widget_max"
-    set value_html $result
 
     set diagram_html [im_indicator_horizontal_bar \
 			  -name "test" \
@@ -190,5 +170,34 @@ db_multirow -extend {param_view_url report_view_url report_edit_html value_html 
 			  -widget_max_red $indicator_high_critical \
 			  -widget_max $indicator_widget_max \
 			 ]
+
+    set row_html ""
+    append row_html "<tr $bgcolor([expr $indicator_cnt % 2])>\n"
+    append row_html "<td colspan=$colspan>$diagram_html</td>\n"
+    append row_html "</tr>\n"
+    append body_html $row_html
+
+    incr indicator_cnt
 }
+
+set footer_html ""
+set action_html ""
+if {$sla_write} {
+
+    set submit_msg [lang::message::lookup "" intranet-core.Submit Submit]
+    append action_html "<tr><td colspan=$colspan>\n"
+    append action_html "<select name=action>\n"
+    append action_html "<option name=''></option>\n"
+    append action_html "</select>\n"
+    append action_html "<input type=submit name=submit value=$submit_msg>\n"
+    append action_html "</td><tr>\n"
+    set action_html ""
+
+    append footer_html "<ul>\n"
+    append footer_html "<li><a href=''>[lang::message::lookup "" intranet-sla-management.Create_a_New_SLA_Parameter "Create a New SLA Parameter"]</a></li>\n"
+    append footer_html "<li><a href='http://www.project-open.org/documentation/process_sla_management'>[lang::message::lookup "" intranet-sla-management.SLA_Management_Help "SLA Management Help"]</a></li>\n"
+    append footer_html "</ul>\n"
+}
+
+append body_html $action_html
 
