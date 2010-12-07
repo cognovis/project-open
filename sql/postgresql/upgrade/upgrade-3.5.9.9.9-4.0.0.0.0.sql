affd46720a265b54d8ddda40d56f4fde7f772486
Frank Bergmann <frank.bergmann@project-open.com>
2010-12-07 19:38:50 +0000

    - added created_from_item_id field in im_invoice_items

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -5,3 +5,29 @@ SELECT acs_log__debug('/packages/intranet-invoices/sql/postgresql/upgrade/upgrad
 -- Move the "Sel" column to the left
 update im_view_columns set sort_order = 0 where column_id = 3115;
 
+
+
+
+-- Drop the old version of the constraint if exists
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count         integer;
+begin
+        select count(*) into v_count from user_tab_columns
+        where lower(table_name) = ''im_invoice_items'' and lower(column_name) = ''created_from_item_id'';
+        if v_count > 0 then return 0; end if;
+
+	-- Reference for cases where we want to link a copy
+	-- back to the original
+	alter table im_invoice_items add
+	created_from_item_id    integer
+	                        constraint im_invoice_items_created_from_fk
+	                        references im_invoice_items;
+
+        return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
