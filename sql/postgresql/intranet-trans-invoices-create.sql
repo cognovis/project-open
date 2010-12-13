ba13662ce6a6fc2a44555b142e338287603485cc
Frank Bergmann <frank.bergmann@project-open.com>
2010-12-13 13:04:03 +0000

    - fixed comment

--- sql/postgresql/intranet-trans-invoices-create.sql
+++ sql/postgresql/intranet-trans-invoices-create.sql
@@ -266,7 +266,7 @@ create unique index im_trans_price_idx on im_trans_prices (
 -- Views to Business Objects
 --
 
--- Calculate a match value between a price list item and an invoice_item
+-- Calculate a match value between a price list item and a task
 -- The higher the match value the better the fit.
 
 
