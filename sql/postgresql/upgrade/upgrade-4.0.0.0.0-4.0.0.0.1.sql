45ac4b16d5a191e2389470463daeabbc15e04267
Frank Bergmann <frank.bergmann@project-open.com>
2011-01-05 18:00:14 +0000

    - Fixed issue deleting authorities by   deleting parameters now

--- sql/postgresql/upgrade/upgrade-4.0.0.0.0-4.0.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.0.0.0.0-4.0.0.0.1.sql
@@ -9,3 +9,24 @@ DECLARE
 BEGIN
 	return v_result::varchar;
 END;' language 'plpgsql';
+
+
+
+-- Fixed issue deleting authorities by
+-- deleting parameters now
+--
+create or replace function authority__del (integer)
+returns integer as '
+declare
+  p_authority_id            alias for $1;
+begin
+
+  delete from auth_driver_params
+  where authority_id = p_authority_id;
+
+  perform acs_object__delete(p_authority_id);
+
+  return 0; 
+end;' language 'plpgsql';
+
+
