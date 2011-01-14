b214051b105cb608f05cf0d1f2d4daf6a00696c6
po35devklaus <>
2011-01-14 15:06:28 +0000

    - added personell_number

---
+++ sql/postgresql/upgrade/upgrade-3.5.0.0.0-3.5.0.0.1.sql
@@ -0,0 +1,22 @@
+-- upgrade-3.5.0.0.0-3.5.0.0.1.sql
+
+SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-3.5.0.0.0-3.5.0.0.1.sql','');
+
+
+create or replace function inline_0 ()
+returns integer as '
+declare
+        v_count         integer;
+begin
+
+	select count(*) into v_count from user_tab_columns where table_name = ''im_employees'' and column_name = ''personnel_number'';
+        IF v_count > 0 THEN return 1; END IF;
+
+        alter table im_employees add column personell_number character varying(10);
+        RETURN 0;
+
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
