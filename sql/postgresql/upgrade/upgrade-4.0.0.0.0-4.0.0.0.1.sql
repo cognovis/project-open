e4a75f0732525b0d91aa4c1fa6ed7ba560d3fdcb
kpmg <>
2011-01-27 19:24:42 +0000

    - added new actions

--- sql/postgresql/upgrade/upgrade-4.0.0.0.0-4.0.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.0.0.0.0-4.0.0.0.1.sql
@@ -5,3 +5,7 @@ SELECT acs_log__debug('/packages/intranet-helpdesk/sql/postgresql/upgrade/upgrad
 SELECT im_category_new(30560, 'Resolved', 'Intranet Ticket Action');
 
 
+-- Add new actions
+SELECT im_category_new(30540, 'Associate', 'Intranet Ticket Action');
+SELECT im_category_new(30550, 'Escalate', 'Intranet Ticket Action');
+SELECT im_category_new(30552, 'Close Escalated Tickets', 'Intranet Ticket Action');
