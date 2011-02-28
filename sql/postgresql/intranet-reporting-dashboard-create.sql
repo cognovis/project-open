490324c9ee1a2b9b15de50fec0d244bccdde3178
Frank Bergmann <frank.bergmann@project-open.com>
2011-02-28 11:47:22 +0000

    - Merged stuff from Malte

--- sql/postgresql/intranet-reporting-dashboard-create.sql
+++ sql/postgresql/intranet-reporting-dashboard-create.sql
@@ -162,6 +162,26 @@ SELECT im_component_plugin__new (
 );
 
 
+-- Project Status Histogram
+--
+SELECT im_component_plugin__new (
+        null,                                   -- plugin_id
+        'im_component_plugin',                  -- object_type
+        now(),                                  -- creation_date
+        null,                                   -- creation_user
+        null,                                   -- creation_ip
+        null,                                   -- context_id
+        'Project Queue',                        -- plugin_name
+        'intranet-reporting-dashboard',         -- package_name
+        'right',                                -- location
+        '/intranet-cost/index',                 -- page_url
+        null,                                   -- view_name
+        40,                                     -- sort_order
+        'im_dashboard_active_projects_status_histogram',
+        'lang::message::lookup "" intranet-reporting-dashboard.Project_Queue "Project Queue"'
+);
+
+
 -- Tickets Histograms
 --
 
