55a41bfce47947e061b55064529ea1f41ae32877
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-03-02 18:42:12 +0000

    - init

---
+++ sql/postgresql/intranet-cust-koernigweber-create.sql
@@ -0,0 +1,166 @@
+-- /packages/intranet-cust-kw/sql/postgres/intranet-cust-kw-create.sql
+--
+-- Copyright (C) 1999-2011 various parties
+--
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author	klaus.hofeditz@project-open.com
+
+-------------------------------------------------------------
+
+select acs_object_type__create_type (
+        'project_approval2_wf',           -- object_type
+        'Project Close Approval',              -- pretty_name
+        'Project Close Approval',            -- pretty_plural
+        'workflow',        -- supertype
+        'project_approval2_wf_cases',         -- table_name
+        'case_id',           -- id_column
+        'project_approval2_wf',           -- package_name
+        'f',                    -- abstract_p
+        null,                   -- type_extension_table
+        'null'      -- name_method
+);
+
+insert into acs_object_type_tables (object_type,table_name,id_column) values ('project_approval2_wf', 'project_approval2_wf_cases', 'case_id');
+
+
+create function im_workflow__assign_to_project_manager(int4, text) returns int4 as '
+ declare
+        p_task_id               alias for $1;
+        p_custom_arg            alias for $2;
+
+        v_transition_key        varchar;
+        v_object_type           varchar;
+        v_case_id               integer;
+        v_object_id             integer;
+        v_creation_user         integer;
+        v_creation_ip           varchar;
+        v_project_manager_id    integer;
+        v_project_manager_name  varchar;
+
+        v_journal_id            integer;
+
+ begin
+        -- Get information about the transition and the ''environment''
+        select  tr.transition_key, t.case_id, c.object_id, o.creation_user, o.creation_ip, o.object_type
+        into    v_transition_key, v_case_id, v_object_id, v_creation_user, v_creation_ip, v_object_type
+        from    wf_tasks t, wf_cases c, wf_transitions tr, acs_objects o
+        where   t.task_id = p_task_id
+                and t.case_id = c.case_id
+                and o.object_id = t.case_id
+                and t.workflow_key = tr.workflow_key
+                and t.transition_key = tr.transition_key;
+
+        select  project_lead_id into v_project_manager_id from im_projects
+        where   project_id = v_object_id;
+
+        select im_name_from_id(v_project_manager_id) into v_project_manager_name;
+
+        IF v_project_manager_id is not null THEN
+                v_journal_id := journal_entry__new(
+                    null, v_case_id,
+                    v_transition_key || '' assign_to_project_manager '' || v_project_manager_name,
+                    v_transition_key || '' assign_to_project_manager '' || v_project_manager_name,
+                    now(), v_creation_user, v_creation_ip,
+                    ''Assigning to user'' || v_project_manager_name
+                );
+                PERFORM workflow_case__add_task_assignment(p_task_id, v_project_manager_id, ''f'');
+                PERFORM workflow_case__notify_assignee (p_task_id, v_project_manager_id, null, null,
+                        ''wf_'' || v_object_type || ''_assignment_notif'');
+        END IF;
+        return 0;
+end;' language 'plpgsql';
+
+
+-- create table to manage Employee/Customer Price Matrix 
+
+create table im_emp_cust_price_list (
+        id                      integer
+                                primary key,
+        user_id			integer
+				constraint im_employee_customer_price_list_user_fk
+                                references users,
+        company_id              integer not null
+                                constraint im_employee_customer_price_list_company_fk 
+				references im_companies,
+        amount                  numeric(12,3),
+        currency                char(3)
+                                constraint im_costs_currency_fk
+                                references currency_codes(iso),
+	unique(user_id, company_id)
+);
+
+create index emp_cust_price_list_idx on im_emp_cust_price_list(id);
+
+select acs_object_type__create_type (
+        'im_employee_customer_price',           -- object_type
+        'Employee Customer Price',              -- pretty_name
+        'Employee Customer Price',             	-- pretty_plural
+        'im_biz_object',        		-- supertype
+        'im_emp_cust_price_list',      		-- table_name
+        'id',           			-- id_column
+        'im_emp_cust_price_list',      		-- package_name
+        'f',                  			-- abstract_p
+        null,                  			-- type_extension_table
+        'im_emp_cust_price__name'      		-- name_method
+);
+
+insert into acs_object_type_tables (object_type,table_name,id_column)
+values ('im_employee_customer_price', 'im_emp_cust_price_list', 'id');
+
+-- check if required
+-- update acs_object_types set
+--        status_type_table = 'im_projects',
+--        status_column = 'project_status_id',
+--        type_column = 'project_type_id'
+-- where object_type = 'im_project';
+
+create function im_employee_customer_price__new(int4,varchar,timestamptz,int4,varchar,int4,int,int,numeric,varchar) returns int4 as '
+        DECLARE
+        	p_id 		  alias for $1;
+        	p_object_type     alias for $2;
+        	p_creation_date   alias for $3;
+        	p_creation_user   alias for $4;
+        	p_creation_ip     alias for $5;
+        	p_context_id      alias for $6;
+        
+        	p_user_id	  alias for $1;
+        	p_company_id      alias for $2;
+        	p_amount          alias for $3;
+        	p_currency	  alias for $4;
+		v_id		  integer;
+
+        BEGIN
+        	v_id := acs_object__new (
+        		p_id,
+        		p_object_type,
+        		p_creation_date,
+        		p_creation_user,
+        		p_creation_ip,
+        		p_context_id
+        	);
+        
+        	insert into im_emp_cust_price_list (
+        		id, user_id, company_id, amount, currency 
+        	) values (
+        		v_id, p_user_id, p_company_id, p_amount, p_currency
+        	);
+        
+        	return v_id;
+
+end;' language 'plpgsql';
+
+-- insert into im_employee_customer_price_list (id, )
+
+
+        select im_employee_customer_price__new (
+                null, ''im_employee_customer_price'', now()::date,
+                0, ''0.0.0.0'', null,
+                v_organisation_name,
+                v_organisation_name,
+                v_office_id,
+                57,
+                46
+        ) into v_company_id;
