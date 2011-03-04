aff6d716302f8ac57945199cbeeae9460807e29c
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-03-04 11:49:17 +0000

    -- dev

--- tcl/intranet-cust-koernigweber-procs.tcl
+++ tcl/intranet-cust-koernigweber-procs.tcl
@@ -118,46 +118,46 @@ ad_proc -public im_group_member_component_employee_customer_price_list {
     # now takes care that a user is assigned only once to a group.
     # We neeed this if we want to show the role of the user.
     #
+
     set sql_query "
-	select
-		u.user_id, 
-		u.user_id as party_id,
-		pl.*,
-		im_email_from_user_id(u.user_id) as email,
-		im_name_from_user_id(u.user_id) as name,
-		im_category_from_id(c.category_id) as member_role,
-		c.category_gif as role_gif,
-		c.category_description as role_description
-		$bo_rels_percentage_sql
-	from
-		users u,
-		acs_rels rels
-		LEFT OUTER JOIN im_biz_object_members bo_rels ON (rels.rel_id = bo_rels.rel_id)
-		LEFT OUTER JOIN im_categories c ON (c.category_id = bo_rels.object_role_id),
-		group_member_map m,
-		membership_rels mr,
-		im_employee_customer_price_list pl
-	where
-		rels.object_id_one = $object_id
-		and rels.object_id_two = u.user_id
-		and mr.member_state = 'approved'
-		and u.user_id = m.member_id
-		and mr.member_state = 'approved'
-		and m.group_id = acs__magic_object_id('registered_users'::character varying)
-		and m.rel_id = mr.rel_id
-		and m.container_id = m.group_id
-		and m.rel_type = 'membership_rel'
-		and pl.company_id = $object_id
-		$limit_to_group_id_sql 
-		$dont_allow_sql
-	order by lower(im_name_from_user_id(u.user_id))
+        select
+                u.user_id,
+                u.user_id as party_id,
+                im_email_from_user_id(u.user_id) as email,
+                im_name_from_user_id(u.user_id) as name,
+                im_category_from_id(c.category_id) as member_role,
+                c.category_gif as role_gif,
+                c.category_description as role_description,
+                (select amount from im_emp_cust_price_list where company_id=object_id_one and user_id = u.user_id) as amount
+                $bo_rels_percentage_sql
+        from
+                users u,
+                acs_rels rels
+                LEFT OUTER JOIN im_biz_object_members bo_rels ON (rels.rel_id = bo_rels.rel_id)
+                LEFT OUTER JOIN im_categories c ON (c.category_id = bo_rels.object_role_id),
+                group_member_map m,
+                membership_rels mr
+        where
+                rels.object_id_one = $object_id
+                and rels.object_id_two = u.user_id
+                and mr.member_state = 'approved'
+                and u.user_id = m.member_id
+                and mr.member_state = 'approved'
+                and m.group_id = acs__magic_object_id('registered_users'::character varying)
+                and m.rel_id = mr.rel_id
+                and m.container_id = m.group_id
+                and m.rel_type = 'membership_rel'
+                $limit_to_group_id_sql
+                $dont_allow_sql
+        order by lower(im_name_from_user_id(u.user_id))
     "
 
     # ------------------ Format the table header ------------------------
-    set colspan 1
+    set colspan 2
     set header_html "
       <tr> 
 	<td class=rowtitle align=middle>[_ intranet-core.Name]</td>
+	<td class=rowtitle align=middle>[lang::message::lookup "" intranet-core.Price "Price"]</td>
     "
     if {$show_percentage_p} {
         incr colspan
@@ -178,43 +178,28 @@ ad_proc -public im_group_member_component_employee_customer_price_list {
     set body_html ""
     db_foreach users_in_group $sql_query {
 
-	# Make up a GIF with ALT text to explain the role (Member, Key 
-	# Account, ...
-	set descr $role_description
-	if {"" == $descr} { set descr $member_role }
-	set descr_tr [lang::util::suggest_key $descr]
-	set profile_gif [im_gif $role_gif $descr_tr]
-
-	incr count
-	if { $current_user_id == $user_id } { set found 1 }
-
-	# determine how to show the user: 
-	# -1: Show name only, 0: don't show, 1:Show link
 	set show_user [im_show_user_style $user_id $current_user_id $object_id]
 	if {$debug} { ns_log Notice "im_group_member_component: user_id=$user_id, show_user=$show_user" }
-
 	if {$show_user == 0} { continue }
 
 	append body_html "
-<tr $td_class([expr $count % 2])>
-  <input type=hidden name=member_id value=$user_id>
-  <td>"
-	if {$show_user > 0} {
-append body_html "<A HREF=/intranet/users/view?user_id=$user_id>$name</A>"
-	} else {
-append body_html $name
-	}
+		<tr $td_class([expr $count % 2])>
+		  <input type=hidden name=member_id value=$user_id>
+  		<td>"
+			if {$show_user > 0} {
+				append body_html "<A HREF=/intranet/users/view?user_id=$user_id>$name</A>"
+			} else {
+				append body_html $name
+			}
 
-	append body_html "$profile_gif</td>"
-	if {$show_percentage_p} {
-	    append body_html "
-		  <td align=middle>
-		    <input type=input size=4 maxlength=4 name=\"percentage.$user_id\" value=\"$percentage\">
-		  </td>
-	    "
-	}
+	append body_html "</td>"
 
-        append body_html "$profile_gif</td>"
+            append body_html "
+                  <td align=middle>
+                    <input type=input size=4 maxlength=4 name=\"amount.$user_id\" value=\"$amount\">
+                  </td>
+            "
+        append body_html "</td>"
         if {$show_percentage_p} {
             append body_html "
                   <td align=middle>
@@ -239,47 +224,13 @@ append body_html $name
 
     # ------------------ Format the table footer with buttons ------------
     set footer_html ""
-    if {$add_admin_links} {
-
-	set spam_members_html ""
-	if {[im_table_exists spam_messages]} {
-	    set spam_members_html "<li><A HREF=\"[spam_base]spam-add?[export_url_vars object_id sql_query]\">[_ intranet-core.Spam_Members]</A>&nbsp;"
-	    set spam_members_html "<option value=spam_members>[_ intranet-core.Spam_Members]</option>\n"
-	}
-
-
-	append footer_html "
-	    <tr>
-	      <td align=left>
-		<ul>
-		<li><A HREF=\"/intranet/member-add?[export_url_vars object_id also_add_to_group_id return_url]\">[_ intranet-core.Add_member]</A>
-		</ul>
-	      </td>
-	"
-
 	append footer_html "
 	    <tr>
 	      <td align=right colspan=$colspan>
-		<select name=action>
-	"
-#		<option value=add_member>[_ intranet-core.Add_a_new_member]</option>
-
-
-	if {$show_percentage_p} {
-	    append footer_html "
-		<option value=update_members>[_ intranet-core.Update_members]</option>
-	    "
-	}
-	append footer_html "
-		<option value=del_members>[_ intranet-core.Delete_members]</option>
-		$spam_members_html
-		</select>
-		<input type=submit value='[_ intranet-core.Apply]' name=submit_apply></td>
+		<input type=submit value='[lang::message::lookup "" intranet-core.Save "Save"]' name=submit_apply></td>
 	      </td>
 	    </tr>
-	"
-    }
-
+	    "
     # ------------------ Join table header, body and footer ----------------
     set html "
 	<form method=POST action=/intranet/member-update>
@@ -295,15 +246,11 @@ append body_html $name
 }
 
 
-
 ad_proc -public im_employee_customer_price_list_new {
-
     user_id:integer,notnull
     company_id:integer,notnull
-    user_id:integer,notnull
     amount
     currency 
-
 } {
         select im_employee_customer_price__new (
                 null, ''im_employee_customer_price'', now()::date,
