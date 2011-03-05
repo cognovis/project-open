acd8d8a3c80552710dab5d7522f73125929114a9
Iuri Sampaio <iuri.sampaio@gmail.com>
2011-03-05 17:19:12 +0100

    fix home-tasks view psql script

--- packages/intranet-cognovis/sql/postgresql/intranet-cognovis-create.sql
+++ packages/intranet-cognovis/sql/postgresql/intranet-cognovis-create.sql
@@ -345,26 +345,25 @@ insert into im_views (view_id, view_name, visible_for) values (950, 'im_timeshee
 
 
 -- Task_id
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92100,950,NULL, 'TaskID','@tasks.task_id;noquote@','','',0,'');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92100,950,NULL, 'TaskID','<center>@tasks.task_id;noquote@</center>','','',0,'');
 
 -- Task Name
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92101,950,NULL,'Name','<nobr>@tasks.gif_html;noquote@<a href=@tasks.object_url;noquote@>@tasks.task_name;noquote@</a></nobr>','','',1,'');
 
 -- Planned Units
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92102,950,NULL,'PLN','<a href=@tasks.object_url;noquote@>@tasks.planned_units;noquote@</a>','','',2,'');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92102,950,NULL,'PLN','<center><a href=@tasks.object_url;noquote@>@tasks.planned_units;noquote@</a></center>','','',2,'');
 
 -- Start Date
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92103,950,NULL,'Start', '@tasks.start_date;noquote@','','',3,'');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92103,950,NULL,'Start', '<center>@tasks.start_date;noquote@</center>','','',3,'');
 
 -- End Date
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92104,950,NULL,'End', '@tasks.end_date;noquote@','','',4,'');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92104,950,NULL,'End', '<center>@tasks.end_date;noquote@</center>','','',4,'');
 
 -- Log Hours
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92105,950,NULL,'Log','<p align=right><a href=@tasks.timesheet_report_url;noquote@">@tasks.planned_unit;noquote@</a></p>','','',5,'');
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92105,950,NULL,'Log','<center><a href=@tasks.timesheet_report_url;noquote@">@tasks.logged_hours;noquote@</a></center>','','',5,'');
 
 -- Task Prio
-insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92106,950,NULL, 'Prio','@tasks.task_prio;noquote@','','',6,'');
-
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92106,950,NULL, 'Prio','<center>@tasks.task_prio;noquote@</center>','','',6,'');
 
 -- Checkbox
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl, extra_select, extra_where, sort_order, visible_for) values (92107,950,NULL,'<input type=checkbox name=_dummy onclick=acs_ListCheckAll(\"tasks\",this.checked)>','<input type=checkbox name=task_id.@tasks.task_id@ id=tasks,@tasks.task_id@>', '', '', 7, '');
