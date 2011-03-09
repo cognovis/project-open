855f58ce4d54de47a86eec1dce5ab1ec7c117eb2
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-03-09 12:46:09 +0000

    -- dev

--- www/monthly-report-wf-extended.tcl
+++ www/monthly-report-wf-extended.tcl
@@ -37,7 +37,7 @@ ad_page_contract {
     @author Klaus Hofeditz (klaus.hofeditz@project-open.com)
 } {
     { owner_id:integer "" }
-    { project_id:integer }
+    { project_id:integer 0 }
     { report_year_month:integer "" }
     { report_month:integer "" }
     { report_year:integer "" }
@@ -57,21 +57,23 @@ set current_user_id [ad_maybe_redirect_for_registration]
 
 set todays_date [db_string todays_date "select to_char(now(), :date_format) from dual" -default ""]
 
-if { [empty_string_p report_month] } {
+if { [empty_string_p $report_month] } {
     set report_month [string range $todays_date 5 6] 
 }
 
-if { [empty_string_p report_year] } {
+if { [empty_string_p $report_year] } {
     set report_year [string range $todays_date 0 3] 
 }
 
-if { [empty_string_p report_year] } {
+if { ([empty_string_p $report_year] && [empty_string_p $report_month]) || [empty_string_p $report_year_month] } {
 	set report_year_month "[string range $todays_date 0 3]-[string range $todays_date 5 6]"
 }
 
 set first_day_of_month "$report_year-$report_month-01"
 set duration [db_string get_number_days_month "SELECT date_part('day','$first_day_of_month'::date + '1 month'::interval - '1 day'::interval)" -default 0]
-   
+
+# set duration 2
+
 if { $owner_id != $user_id && ![im_permission $user_id "view_hours_all"] } {
     ad_return_complaint 1 "<li>[_ intranet-timesheet2.lt_You_have_no_rights_to]"
     return
@@ -110,7 +112,7 @@ set filter_form_html "
                 <tr>
                   <td class=form-label>Month</td>
                   <td class=form-widget>
-                    <input type=textfield name=start_date value=$start_date>
+                    <input type=textfield name='report_year_month' value='$report_year_month'>
                   </td>
                 </tr>
                 <tr>
@@ -137,72 +139,52 @@ set admin_html ""
 
 
 # ---------------------------------------------------------------
-# Get the Column Headers and prepare some SQL
-# ---------------------------------------------------------------
-
-set table_header_html "<tr><td class=rowtitle>[_ intranet-timesheet2.Users]</td>"
-set days [list]
-set holydays [list]
-set sql_from [list]
-set sql_from2 [list]
-
-for { set i [expr $duration - 1]  } { $i >= 0 } { incr i -1 } {
-	set col_sql "
-	    select 
-		to_char(sysdate, :date_format) as today_date,
-		to_char(to_date('$start_at', :date_format)-$i, :date_format) as i_date, 
-		to_char((to_date('$start_at', :date_format)-$i), 'Day') as f_date_day,
-		to_char((to_date('$start_at', :date_format)-$i), 'dd') as f_date_dd,
-		to_char((to_date('$start_at', :date_format)-$i), 'Mon') as f_date_mon,
-		to_char((to_date('$start_at', :date_format)-$i), 'yyyy') as f_date_yyyy,    
-		to_char(to_date('$start_at', :date_format)-$i, 'DY') as h_date 
-	    from dual"
-
-	    db_1row get_date $col_sql
-	    lappend days $i_date
-	    if { $h_date == "SAT" || $h_date == "SUN" } {
-		lappend holydays $i_date
-	    }
-
-    #prepare the data to UNION
-    lappend sql_from2 "select to_date($i_date, :date_format) as day from dual\n"
-    set f_date "[_ intranet-timesheet2.[string trim $f_date_day]], $f_date_dd. [_ intranet-timesheet2.$f_date_mon] $f_date_yyyy" 
-
-    append table_header_html "<td class=rowtitle>$f_date</td>"
-}
-
-append table_header_html "</tr>"
-
-
-# ---------------------------------------------------------------
 # Get Column Header & Data 
 # ---------------------------------------------------------------
 
-set table_header_html "<tr><td class=rowtitle>[_ intranet-timesheet2.Users]</td>"
-set inner_sql ""
+set table_header_html "<tr><td class=rowtitle>Projekt</td><td class=rowtitle>Mitarbeiter</td>"
+set inner_sql_list [list]
 
-for { set i 0 } { $i < $duration } { incr i } {
-    if { 1 == [string length i]} { set day_double_digit 0$i }
-    append inner_sql "(select sum(hours) from im_hours h where
-            h.user_id= r.object_id_two
-            and h.project_id in ([im_project_subproject_ids p.project_id])
-            and h.day like '%$report_year-$report_month-$day_double_digit%') as h_$report_year-$report_month-$day_double_digit
-    "
-
-    set h_date [db_string get_view_id "select view_id from im_views where view_name=:view_name" -default 0]
-    if { $h_date == "SAT" || $h_date == "SUN" } {
-            append table_header_html "<td class=rowtitle>$day_double_digit</td>
+for { set i 1 } { $i < $duration + 1 } { incr i } {
+    if { 1 == [string length $i]} { 
+	set day_double_digit 0$i 
     } else {
-            append table_header_html "<td class=rowtitle>$day_double_digit</td>
+        set day_double_digit $i
+    }
+    lappend inner_sql_list "(select sum(hours) from im_hours h where
+            h.user_id= r.object_id_two
+            and h.project_id in (
+		select distinct 
+			children.project_id as subproject_id
+		from
+			im_projects parent,
+			im_projects children
+		where
+			children.tree_sortkey between parent.tree_sortkey and tree_right(parent.tree_sortkey)
+			and parent.project_id = h.project_id
+			)
+            and h.day like '%$report_year-$report_month-$day_double_digit%') as h$report_year$report_month$day_double_digit
+    "
+    set h_date [db_string get_view_id "select to_char(to_date('$report_year-$report_month-$day_double_digit', :date_format)-$i, 'DY') as h_date from dual" -default 0]
+    if { $h_date == "SAT" || $h_date == "SUN" } {
+            append table_header_html "<td class=rowtitle>$day_double_digit</td>"
+    } else {
+            append table_header_html "<td class=rowtitle>$day_double_digit</td>"
     }
 }
 
-append table_header_html "<td>Reminder</td><td>Confirm WF</td></tr>"
+set inner_sql [join $inner_sql_list ", "]
+append table_header_html "<td class=rowtitle>Reminder</td><td class=rowtitle >Confirm WF</td></tr>"
+
+# delete last comma
+set inner_sql [string range $inner_sql 0 [expr [string length $inner_sql]-2]]
 
 set sql "
 	select 
 		p.project_id,
-		r.object_id_two as user,
+		p.project_name,
+		r.object_id_two as user_id,
+		(select im_name_from_user_id(r.object_id_two)) as user_name,
 		$inner_sql
 	from 
 		im_projects p, 
@@ -217,8 +199,6 @@ set sql "
 		project_id
 "
 
-ad_return_complaint 1 $sql
-
 # ---------------------------------------------------------------
 # Get Column Header & Data
 # ---------------------------------------------------------------
@@ -232,96 +212,82 @@ set ctr 0
 
 ns_log notice $sql
 
+set project_name_saved ""
+
+
+set project_url "/intranet/projects/view?project_id="
+set user_url "intranet/users/view?user_id="
+
 db_foreach get_hours $sql {
-    if { $do_user_init == 1 } {
-	set old_owner [list $curr_owner_id $owner_name]
-	set do_user_init 0
+    if { ![string equal $project_name_saved $project_name] } {
+	    append table_body_html "<tr><td><strong><a href='$project_url$project_id'>$project_name</a></strong></td>"
+	    set project_name_saved $project_name
+    } else {
+            append table_body_html "<tr><td></td>"
     }
-    if { [lindex $old_owner 0] != $curr_owner_id } {
-	append table_body_html [im_do_row [array get bgcolor] $ctr [lindex $old_owner 0] [lindex $old_owner 1] $days [array get user_days] [array get user_absences] $holydays $today_date [array get user_ab_descr]]
-	set old_owner [list $curr_owner_id $owner_name]
-	array unset user_days
-	array unset user_absences
+    append table_body_html "<td><a href='$user_url$user_id'>$user_name</a></td>"
+    for { set i 1 } { $i < $duration + 1 } { incr i } {
+	if { 1 == [string length $i]} { set day_double_digit 0$i }
+	set varname h 
+	append varname $report_year$report_month$day_double_digit
+	set value [expr "\$$varname"]
+	append table_body_html "<td>$value</td>"
     }
-    if { $type == "h" } {
-	set user_days($curr_day) $val
+    # are there any unconfirmed hours for this user?  
+    set sql "
+	select count(*) 
+	from im_hours	
+        where conf_object_id is null and project_id in (
+                select distinct
+                        children.project_id as subproject_id
+                from
+                        im_projects parent,
+                        im_projects children
+                where
+                        children.tree_sortkey between parent.tree_sortkey and tree_right(parent.tree_sortkey)
+                        and parent.project_id = :project_id
+	) and user_id=:user_id and day like '%$report_year-$report_month-%'
+    "
+    set ctr_unconfirmed_hrs [db_string get_ctr_unconfirmed_hrs $sql -default 0]
+    if { 0 != $ctr_unconfirmed_hrs } {
+	append table_body_html "<td><a href='/intranet-cust-koernigweber/notify-logged-hours?report_year_month=$report_year_month&user_id=$user_id&project_id=$project_id' class='button'>Erinnern</a></td>"	
+    } else {
+	append table_body_html "<td>&nbsp</td>"
+    }    
+
+    set wf_tasks_sql "
+	select distinct
+		task_id 
+	from 
+		wf_tasks 
+	where 
+		case_id in (
+			select case_id 
+			from wf_cases 
+			where object_id in 
+				(select conf_object_id from im_hours where project_id = :project_id and user_id=:user_id and day like '%$report_year-$report_month-%')
+		)
+    "
+    set task_id_string ""
+    db_foreach  wf_tasks_list_sql $wf_tasks_sql {
+	#result should be always one record 
+	append task_id_string "task_id=$task_id&"  
     }
-    if { $type == "a" } {
-	set user_absences($curr_day) $val
-	set user_ab_descr($val) $descr
-    }
-    set val ""
-    incr ctr
+    append table_body_html "<td><a href='/acs-workflow/task?return_url=/intranet-cust-koernigweber/monthly-report-wf-extended&task_id=$task_id_string/' class='button'>Best&auml;tigen</a></td>"	
 }
 
-set colspan [expr [llength $days]+1]
+# set colspan [expr [llength $days]+1]
 
 # Show a reasonable message when there are no result rows:
-if { [array size user_days] > 0 } {
-    append table_body_html [im_do_row [array get bgcolor] $ctr $curr_owner_id $owner_name $days [array get user_days] [array get user_absences] $holydays $today_date [array get user_ab_descr]]
-} elseif { [empty_string_p $table_body_html] } {
+if { [empty_string_p $table_body_html] } {
     set table_body_html "
 	 <tr><td colspan=$colspan><ul><li><b>
 	[_ intranet-timesheet2.No_Users_found]
 	</b></ul></td></tr>"
 }
 
-# ---------------------------------------------------------------
-# Provide << and >> to see future and past days
-# ---------------------------------------------------------------
-
-set navig_sql "
-    select 
-    	to_char(to_date('$start_at', :date_format) - 7, :date_format) as past_date,
-	to_char(to_date('$start_at', :date_format) + 7, :date_format) as future_date 
-    from 
-    	dual"
-db_1row get_navig_dates $navig_sql
-
-set switch_link_html "<a href=\"weekly_report?[export_url_vars owner_id project_id duration display]"
-
-set switch_past_html "$switch_link_html&start_at=$past_date\">&laquo;</a>"
-set switch_future_html "$switch_link_html&start_at=$future_date\">&raquo;</a>"
-
-# ---------------------------------------------------------------
-# Format Table Continuation and title
-# ---------------------------------------------------------------
-
-set table_continuation_html "
-<tr>
-  <td align=center>
-     $switch_past_html
-  </td>
-  <td colspan=[expr $colspan - 2]></td>
-  <td align=center>
-    $switch_future_html
-  </td>
-</tr>\n"
-
 set page_title "[_ intranet-timesheet2.Timesheet_Summary]"
 set context_bar [im_context_bar $page_title]
-if { $owner_id != "" && [info exists owner_name] } {
-    append page_title " of $owner_name"
-}
-if { $project_id != 0 && [info exists project_name] } {
-    append page_title " by project \"$project_name\""
-}
-
-
-# ---------------------------------------------------------------
-# 
-# ---------------------------------------------------------------
-
-set left_navbar "
-            <div class=\"filter-block\">
-                <div class=\"filter-title\">
-                [lang::message::lookup "" intranet-timesheet2.Admin "Admin Links"]
-                </div>
-		$admin_html
-            </div>
-            <hr/>
-"
-
 
 
 ad_proc im_do_row {
