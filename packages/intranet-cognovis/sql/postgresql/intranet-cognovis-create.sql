fe3ed51f74ef78d4c66b0f6b2fa61ed2553a0115
Iuri Sampaio <iuri.sampaio@gmail.com>
2011-03-15 04:09:06 +0100

    make sure user locale component is readable

--- packages/intranet-cognovis/sql/postgresql/intranet-cognovis-create.sql
+++ packages/intranet-cognovis/sql/postgresql/intranet-cognovis-create.sql
@@ -1072,6 +1072,40 @@ SELECT im_component_plugin__new (
        0,
        'im_user_localization_component $user_id $return_url');
 
+-- Make sure User Locale Component is readable for anybody
+CREATE OR REPLACE FUNCTION inline_0 ()
+RETURNS integer AS '
+DECLARE
+	row		RECORD;
+	v_object_id	INTEGER;
+
+BEGIN
+
+ 	SELECT o.object_id INTO v_object_id 
+	FROM im_component_plugins c, acs_objects o
+	WHERE o.object_id = c.plugin_id
+	AND package_name = ''intranet-core''
+	AND plugin_name = ''User Locale'';
+
+	FOR row IN 
+		SELECT DISTINCT g.group_id
+		FROM acs_objects o, groups g, im_profiles p
+		WHERE g.group_id = o.object_id
+		AND g.group_id = p.profile_id
+		AND o.object_type = ''im_profile''
+	LOOP
+	
+		PERFORM im_grant_permission(v_object_id,row.group_id,''read'');
+
+	END LOOP;
+
+	RETURN 0;
+
+END;' language 'plpgsql';
+
+SELECT inline_0 ();
+DROP FUNCTION inline_0 ();
+
 -- User Portrait Component
 SELECT im_component_plugin__new (
        null,
