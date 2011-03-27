c87b603c4f6a69db332917f87cbaf82bbbd331fa
Frank Bergmann <frank.bergmann@project-open.com>
2011-03-27 14:33:05 +0000

    - included updates for PG 8.4

--- sql/postgresql/intranet-search-pg-create.sql
+++ sql/postgresql/intranet-search-pg-create.sql
@@ -49,6 +49,34 @@ create table im_search_object_types (
 
 
 
+
+-- fraber 110327: PG 8.3 and 8.4 configuration
+-- These versions have a new table "pg_ts_config"
+--
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	-- Check if the table exists
+	select	count(*) into v_count from user_tab_columns
+	where	lower(table_name) = ''pg_ts_config'';
+	if v_count = 0 then return 1; end if;
+
+	-- Check if the entry already exists
+	select	count(*) into v_count from pg_ts_config
+	where	lower(cfgname) = ''default'';
+	if v_count > 0 then return 2; end if;
+
+	CREATE TEXT SEARCH CONFIGURATION public.default (COPY = pg_catalog.english);
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
 -- The main search table with Full Text Index.
 --
 create table im_search_objects (
@@ -737,11 +765,53 @@ update cr_items set name=name;
 
 
 
+-- fraber 080225:
 -- Set the default language to the current system languages
--- fraber 080225
-update pg_ts_cfg set 
-	locale = (select setting from pg_settings where name = 'lc_messages') 
-where ts_name='default';
+-- fraber 110327: This does not work for PG 8.4 somehow...
+--
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	select	count(*) into v_count from user_tab_columns
+	where	lower(table_name) = ''pg_ts_cfg'';
+	if v_count = 0 then return 1; end if;
 
+	update pg_ts_cfg set 
+		locale = (select setting from pg_settings where name = ''lc_messages'')
+	where ts_name = ''default'';
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+-- fraber 110327: PG 8.3 and 8.4 configuration
+-- These versions have a new table "pg_ts_config"
+--
+create or replace function inline_0 ()
+returns integer as '
+declare
+	v_count		integer;
+begin
+	-- Check if the table exists
+	select	count(*) into v_count from user_tab_columns
+	where	lower(table_name) = ''pg_ts_config'';
+	if v_count = 0 then return 1; end if;
+
+	-- Check if the entry already exists
+	select	count(*) into v_count from pg_ts_config
+	where	lower(cfgname) = ''default'';
+	if v_count > 0 then return 2; end if;
+
+	CREATE TEXT SEARCH CONFIGURATION public.default (COPY = pg_catalog.english);
+
+	return 0;
+end;' language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
 
 
