66e41ec7d69772037ef7e652e2902e6bdcaee7ef
Frank Bergmann <frank.bergmann@project-open.com>
2011-04-04 09:59:12 +0000

    - changes for Riskmanagement

--- sql/postgresql/intranet-components.sql
+++ sql/postgresql/intranet-components.sql
@@ -444,13 +444,29 @@ SELECT	im_component_plugin__new (
 	null,				-- creation_user
 	null,				-- creation_ip
 	null,				-- context_id
-	'Home Project Component',	-- plugin_name
+	'Home Project Portlet',		-- plugin_name
 	'intranet-core',		-- package_name
 	'left',				-- location
 	'/intranet/index',		-- page_url
 	null,				-- view_name
 	15,				-- sort_order
 	'im_project_personal_active_projects_component'	-- component_tcl
+);
+
+SELECT	im_component_plugin__new (
+	null,				-- plugin_id
+	'acs_object',			-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Home ToDo Portlet',		-- plugin_name
+	'intranet-core',		-- package_name
+	'left',				-- location
+	'/intranet/index',		-- page_url
+	null,				-- view_name
+	14,				-- sort_order
+	'im_personal_todo_component'	-- component_tcl
 );
 
 
