658099edc9488c5f276009b6470740324969dac0
Frank Bergmann <frank.bergmann@project-open.com>
2011-04-04 10:10:13 +0000

    - Reimplemented Riskmagement

---
+++ sql/postgresql/intranet-riskmanagement-create.sql
@@ -0,0 +1,258 @@
+-- /packages/intranet-riskmanagement/sql/postgresql/intranet-riskmanagement-create.sql
+--
+-- Copyright (c) 2003-2011 ]project-open[
+--
+-- All rights reserved. Please check
+-- http://www.project-open.com/license/ for details.
+--
+-- @author frank.bergmann@project-open.com
+
+-----------------------------------------------------------
+-- Risks
+--
+-- Risks is a simple object that can be added to a project.
+
+-- Create a new object type.
+--
+SELECT acs_object_type__create_type (
+	'im_risk',			-- object_type - only lower case letters and "_"
+	'Risk',				-- pretty_name - Human readable name
+	'Risks',			-- pretty_plural - Human readable plural
+	'acs_object',			-- supertype - "acs_object" is topmost object type.
+	'im_risks',			-- table_name - where to store data for this object?
+	'risk_id',			-- id_column - where to store object_id in the table?
+	'intranet-riskmanagement',	-- package_name - name of this package
+	'f',				-- abstract_p - abstract class or not
+	null,				-- type_extension_table
+	'im_risk__name'			-- name_method - a PL/SQL procedure that
+					-- returns the name of the object.
+);
+
+-- Add additional meta information to allow DynFields to extend the im_risk object.
+update acs_object_types set
+	status_type_table = 'im_risks',		-- which table contains the status_id field?
+	status_column = 'risk_status_id',	-- which column contains the status_id field?
+	type_column = 'risk_type_id'		-- which column contains the type_id field?
+where object_type = 'im_risk';
+
+-- Object Type Tables contain the lists of all tables (except for
+-- acs_objects...) that contain information about an im_risk object.
+insert into acs_object_type_tables (object_type,table_name,id_column)
+values ('im_risk', 'im_risks', 'risk_id');
+
+
+-- Generic URLs to link to an object of type "im_risk".
+-- These URLs are used by the Full-Text Search Engine and the Workflow
+-- to show links to the object type.
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_risk','view','/intranet-riskmanagement/new?display_mode=display&risk_id=');
+insert into im_biz_object_urls (object_type, url_type, url) values (
+'im_risk','edit','/intranet-riskmanagement/new?display_mode=edit&risk_id=');
+
+
+-- This table stores one object per row. Links to super-type "acs_object" 
+-- using the "risk_id" field, which contains the same object_id as 
+-- acs_objects.object_id.
+create table im_risks (
+				-- The object_id: references acs_objects.object_id.
+				-- So we can lookup object metadata such as creation_date,
+				-- object_type etc in acs_objects.
+	risk_id			integer
+				constraint im_risk_id_pk
+				primary key
+				constraint im_risk_id_fk
+				references acs_objects,
+				-- Every risk should be associated with exactly one project.
+	risk_project_id		integer
+				constraint im_risk_project_nn
+				not null
+				constraint im_risk_project_fk
+				references im_projects,
+				-- Every ]po[ object should have a "status_id" to control
+				-- its lifecycle. Status_id reference im_categories, where 
+				-- you can define the list of stati for this object type.
+	risk_status_id		integer 
+				constraint im_risk_status_nn
+				not null
+				constraint im_risk_status_fk
+				references im_categories,
+				-- Every ]po[ object should have a "type_id" to allow creating
+				-- sub-types of the object. Type_id references im_categories
+				-- where you can define the list of subtypes per object type.
+	risk_type_id		integer 
+				constraint im_risk_type_nn
+				not null
+				constraint im_risk_type_fk
+				references im_categories,
+				-- This is the main content of a "risk". Just a piece of text.
+	risk_name		text
+				constraint im_risk_risk_nn
+				not null,
+				-- Probablility of the risk in percent
+	risk_probability_percent numeric(12,1) default 0.0
+				constraint im_risk_probability_ck
+				check(risk_probability_percent >= 0.0 AND risk_probability_percent <= 100.0),
+				-- financial impact measured in default currency
+	risk_impact		numeric(12,1) default 0.0,
+				-- Long description of a risk.
+	risk_description	text
+);
+
+
+-- Speed up (frequent) queries to find all risks for a specific object.
+create index im_risks_project_idx on im_risks(risk_project_id);
+
+-- Don't allow two risks with the same name in the same project.
+create unique index im_risks_project_un on im_risks(risk_project_id, risk_name);
+
+
+-----------------------------------------------------------
+-- PL/SQL functions to Create and Delete risks and to get
+-- the name of a specific risk.
+--
+create or replace function im_risk__name(integer)
+returns varchar as $body$
+DECLARE
+	p_risk_id		alias for $1;
+	v_name			varchar;
+BEGIN
+	select	substring(risk_name)
+	into	v_name from im_risks
+	where	risk_id = p_risk_id;
+
+	return v_name;
+end; $body$ language 'plpgsql';
+
+
+-- Create a new risk.
+create or replace function im_risk__new (
+	integer, varchar, timestamptz,
+	integer, varchar, integer,
+	integer, integer, integer, varchar
+) returns integer as $body$
+DECLARE
+	-- Default 6 parameters that go into the acs_objects table
+	p_risk_id		alias for $1;		-- risk_id  default null
+	p_object_type   	alias for $2;		-- object_type default im_risk
+	p_creation_date 	alias for $3;		-- creation_date default now()
+	p_creation_user 	alias for $4;		-- creation_user default null
+	p_creation_ip   	alias for $5;		-- creation_ip default null
+	p_context_id		alias for $6;		-- context_id default null
+
+	-- Specific parameters with data to go into the im_risks table
+	-- Only those fields that are hard-coded and "not null"
+	p_risk_project_id	alias for $7;		-- project container
+	p_risk_status_id	alias for $8;		-- "active" or "inactive" or for WF stages
+	p_risk_type_id		alias for $9;		-- user defined type of risk. Determines WF.
+	p_risk_name		alias for $10;		-- Unique name of risk per project
+
+	-- This is a variable for the PL/SQL function
+	v_risk_id	integer;
+BEGIN
+	-- Create an acs_object as the super-type of the risk.
+	-- (Do you remember, im_risk is a subtype of acs_object?)
+	-- acs_object__new returns the object_id of the new object.
+	v_risk_id := acs_object__new (
+		p_risk_id,		-- object_id - NULL to create a new id
+		p_object_type,		-- object_type - "im_risk"
+		p_creation_date,	-- creation_date - now()
+		p_creation_user,	-- creation_user - Current user or "0" for guest
+		p_creation_ip,		-- creation_ip - IP from ns_conn, or "0.0.0.0"
+		p_context_id,		-- context_id - NULL, not used in ]po[
+		't'			-- security_inherit_p - not used in ]po[
+	);
+	
+	-- Create an entry in the im_risks table with the same
+	-- v_risk_id from acs_objects.object_id
+	insert into im_risks (
+		risk_id, risk_project_id,
+		risk_status_id, risk_type_id,
+		risk_name
+	) values (
+		v_risk_id, p_risk_project_id,
+		p_risk_status_id, p_risk_type_id,
+		p_risk_name
+	);
+
+	return v_risk_id;
+END;$body$ language 'plpgsql';
+
+
+-- Delete a risk from the system.
+create or replace function im_risk__delete(integer)
+returns integer as $body$
+DECLARE
+	p_risk_id	alias for $1;
+BEGIN
+	-- Delete any data related to the object
+	delete	from im_risks
+	where	risk_id = p_risk_id;
+
+	-- Finally delete the object iself
+	PERFORM acs_object__delete(p_risk_id);
+
+	return 0;
+end;$body$ language 'plpgsql';
+
+
+
+
+-----------------------------------------------------------
+-- Categories for Type and Status
+--
+-- 75000-75999  Intranet Risk Management (1000)
+-- 75000-75099  Intranet Risk Status
+-- 75100-75199  Intranet Risk Type
+
+-- Status
+SELECT im_category_new (75000, 'Active', 'Intranet Risk Status');
+SELECT im_category_new (75002, 'Deleted', 'Intranet Risk Status');
+
+-- Type
+SELECT im_category_new (75100, 'Default', 'Intranet Risk Type');
+
+
+-----------------------------------------------------------
+-- Create views for shortcut
+--
+-- These views are optional.
+
+create or replace view im_risk_status as
+select	category_id as risk_status_id, category as risk_status
+from	im_categories
+where	category_type = 'Intranet Risks Status'
+	and enabled_p = 't';
+
+create or replace view im_risk_types as
+select	category_id as risk_type_id, category as risk_type
+from	im_categories
+where	category_type = 'Intranet Risks Type'
+	and enabled_p = 't';
+
+
+
+-----------------------------------------------------------
+-- Plugin Components
+--
+
+-- List of Risks per project
+SELECT im_component_plugin__new (
+	null,				-- plugin_id
+	'im_component_plugin',		-- object_type
+	now(),				-- creation_date
+	null,				-- creation_user
+	null,				-- creation_ip
+	null,				-- context_id
+	'Project Risks',		-- plugin_name
+	'intranet-riskmanagement',	-- package_name
+	'left',				-- location
+	'/intranet/projects/view',	-- page_url
+	null,				-- view_name
+	30,				-- sort_order
+	'im_risk_project_component -project_id $project_id'	-- component_tcl
+);
+
+update im_component_plugins 
+set title_tcl = 'lang::message::lookup "" intranet-riskmanagement.Project_Risks "Project Risks"'
+where plugin_name = 'Project Risks';
+
---
+++ www/new.tcl
@@ -0,0 +1,361 @@
+# /packages/intranet-riskmanagement/www/new.tcl
+#
+# Copyright (c) 2003-2008 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/license/ for details.
+
+
+# -----------------------------------------------------------
+# Page Head
+#
+# There are two different heads, depending whether it's called
+# "standalone" (TCL-page) or as a Workflow Panel.
+# -----------------------------------------------------------
+
+# Skip if this page is called as part of a Workflow panel
+if {![info exists task]} {
+
+    ad_page_contract {
+	@author frank.bergmann@project-open.com
+    } {
+	risk_id:integer,optional
+	{ risk_name "" }
+	{ risk_project_id "" }
+	{ risk_status_id "" }
+	{ risk_type_id "" }
+	{ mine_p "all" }
+	{ vars_from_url "" }
+	{ form_mode "edit" }
+	{ plugin_id "" }
+	{ view_name "" }
+	{ return_url "/intranet-riskmanagement/" }
+    }
+
+    set show_components_p 1
+    set enable_master_p 1
+
+} else {
+    
+    set task_id $task(task_id)
+    set case_id $task(case_id)
+
+    set vars_from_url ""
+    set return_url [im_url_with_query]
+
+    set risk_id [db_string pid "select object_id from wf_cases where case_id = :case_id" -default ""]
+    set transition_key [db_string transition_key "select transition_key from wf_tasks where task_id = :task_id"]
+    set task_page_url [export_vars -base [ns_conn url] { risk_id task_id return_url}]
+
+    set show_components_p 0
+    set enable_master_p 0
+    set risk_type_id ""
+
+    set plugin_id ""
+    set view_name "standard"
+    set mine_p "all"
+
+    set render_template_id 0
+
+    # Don't show this page in WF panel.
+    # Instead, redirect to this same page, but in TaskViewPage mode.
+    # ad_returnredirect "/intranet-riskmanagement/new?risk_id=$task(object_id)"
+
+    # fraber 20100602: redirecting to return_url leads to an infinite
+    # loop with workflow. Re-activating redirection to the RiskNewPage
+    # ad_returnredirect $return_url
+
+    ad_returnredirect [export_vars -base "/intranet-riskmanagement/new" { {risk_id $task(object_id)} {form_mode display}} ]
+
+}
+
+# ------------------------------------------------------------------
+# Default & Security
+# ------------------------------------------------------------------
+
+set current_user_id [ad_maybe_redirect_for_registration]
+set user_id $current_user_id
+set current_url [im_url_with_query]
+set action_url "/intranet-riskmanagement/new"
+# set focus "riskmanagement_risk.var_name"
+set focus "risk.var_name"
+
+if {[info exists risk_id] && "" == $risk_id} { unset risk_id }
+
+set view_risks_all_p [im_permission $current_user_id "view_risks_all"]
+set copy_from_risk_name ""
+
+# No support for workflow at the moment
+set edit_risk_status_p 1
+
+
+# ----------------------------------------------
+# Page Title
+
+set page_title [lang::message::lookup "" intranet-riskmanagement.New_Risk "New Risk"]
+if {[exists_and_not_null risk_id]} {
+    set page_title [db_string title "select project_name from im_projects where project_id = :risk_id" -default ""]
+}
+if {"" == $page_title && 0 != $risk_type_id} { 
+    set risk_type [im_category_from_id $risk_type_id]
+    set page_title [lang::message::lookup "" intranet-riskmanagement.New_RiskType "New %risk_type%"]
+} 
+
+set context [list $page_title]
+
+
+# ----------------------------------------------
+# Determine risk type
+
+# We need the risk_type_id for page title, dynfields etc.
+# Check if we can deduce the risk_type_id from risk_id
+if {0 == $risk_type_id || "" == $risk_type_id} {
+    if {[exists_and_not_null risk_id]} { 
+	set risk_type_id [db_string ttype_id "select risk_type_id from im_risks where risk_id = :risk_id" -default 0]
+    }
+}
+
+# ----------------------------------------------
+# Calculate form_mode
+
+if {"edit" == [template::form::get_action riskmanagement_action]} { set form_mode "edit" }
+if {![info exists risk_id]} { set form_mode "edit" }
+if {![info exists form_mode]} { set form_mode "display" }
+
+# Show the ADP component plugins?
+if {"edit" == $form_mode} { set show_components_p 0 }
+
+set risk_exists_p 0
+if {[exists_and_not_null risk_id]} {
+    # Check if the risk exists
+    set risk_exists_p [db_string risk_exists_p "select count(*) from im_risks where risk_id = :risk_id"]
+
+    # Write Audit Trail
+    im_project_audit -project_id $risk_id -action before_update
+
+}
+
+
+# ---------------------------------------------
+# The base form. Define this early so we can extract the form status
+# ---------------------------------------------
+
+set title_label [lang::message::lookup {} intranet-riskmanagement.Name {Title}]
+set title_help [lang::message::lookup {} intranet-riskmanagement.Title_Help {Please enter a descriptive name for the new risk.}]
+
+set edit_p [im_permission $current_user_id add_risks_for_customers]
+set delete_p $edit_p
+
+set actions {}
+if {$edit_p} { lappend actions {"Edit" edit} }
+# if {$delete_p} { lappend actions {"Delete" delete} }
+
+ad_form \
+    -name riskmanagement_risk \
+    -cancel_url $return_url \
+    -action $action_url \
+    -actions $actions \
+    -has_edit 1 \
+    -mode $form_mode \
+    -export {next_url return_url} \
+    -form {
+	risk_id:key
+	{risk_name:text(text) {label $title_label} {html {size 50}}}
+	{risk_nr:text(hidden),optional }
+	{start_date:date(hidden),optional }
+	{end_date:date(hidden),optional }
+    }
+
+# ------------------------------------------------------------------
+# Risk Action
+# ------------------------------------------------------------------
+
+set tid [value_if_exists risk_id]
+set risk_action_html "
+<form action=/intranet-riskmanagement/action name=riskmanagement_action>
+[export_form_vars return_url tid]
+<input type=submit value='[lang::message::lookup "" intranet-riskmanagement.Action "Action"]'>
+[im_category_select \
+     -translate_p 1 \
+     -package_key "intranet-riskmanagement" \
+     -plain_p 1 \
+     -include_empty_p 1 \
+     -include_empty_name "" \
+     "Intranet Risk Action" \
+     action_id \
+]
+</form>
+"
+
+# ------------------------------------------------------------------
+# Delete pressed?
+# ------------------------------------------------------------------
+
+set button_pressed [template::form get_action riskmanagement_risk]
+if {"delete" == $button_pressed} {
+     db_dml mark_risk_deleted "
+	update	im_risks
+	set	risk_status_id = [im_risk_status_deleted]
+	where	risk_id = :risk_id
+     "
+
+    # Write Audit Trail
+    im_project_audit -project_id $risk_id -action delete
+
+    ad_returnredirect $return_url
+}
+
+
+# ------------------------------------------------------------------
+# Redirect if risk_type_id is missing
+# ------------------------------------------------------------------
+
+if {"edit" == $form_mode} {
+
+    set redirect_p 0
+    # redirect if risk_type_id is not defined
+    if {("" == $risk_type_id || 0 == $risk_type_id) && ![exists_and_not_null risk_id]} {
+	set all_same_p [im_dynfield::subtype_have_same_attributes_p -object_type "im_risk"]
+	set all_same_p 0
+	if {!$all_same_p} { set redirect_p 1 }
+    }
+
+    if {$redirect_p} {
+	ad_returnredirect [export_vars -base "new-typeselect" {{return_url $current_url} risk_id risk_type_id risk_name risk_project_id}]
+    }
+
+}
+
+
+# ------------------------------------------------------------------
+# 
+# ------------------------------------------------------------------
+
+# Fetch variable values from the HTTP session and write to local variables
+set url_vars_set [ns_conn form]
+foreach var_from_url $vars_from_url {
+    ad_set_element_value -element $var_from_url [im_opt_val $var_from_url]
+}
+
+
+set risk_elements [list]
+set project_options [im_project_options -include_empty 0]
+lappend risk_elements {risk_project_id:text(select) {label "[lang::message::lookup {} intranet-riskmanagement.Project Project]"} {options $project_options}}
+
+lappend risk_elements {risk_type_id:text(im_category_tree) {label "[lang::message::lookup {} intranet-riskmanagement.Type Type]"} {custom {category_type "Intranet Risk Type" translate_p 1 include_empty_p 0 package_key "intranet-riskmanagement"}}}
+
+lappend risk_elements {risk_status_id:text(im_category_tree) {label "[lang::message::lookup {} intranet-riskmanagement.Status Status]"} {custom {category_type "Intranet Risk Status" translate_p 1 include_empty_p 0 package_key "intranet-riskmanagement"}} }
+
+# Extend the form with new fields
+ad_form -extend -name riskmanagement_risk -form $risk_elements
+
+# Add DynFields to the form
+set field_cnt [im_dynfield::append_attributes_to_form \
+		   -object_id [im_opt_val risk_id] \
+		   -form_display_mode $form_mode \
+		   -object_subtype_id [im_opt_val risk_type_id] \
+		   -object_type "im_risk" \
+		   -form_id "riskmanagement_risk" \
+]
+
+# ------------------------------------------------------------------
+# Form Actions
+# ------------------------------------------------------------------
+
+# Fix for problem changing to "edit" form_mode
+set form_action [template::form::get_action "riskmanagement_risk"]
+if {"" != $form_action} { set form_mode "edit" }
+
+ad_form -extend -name riskmanagement_risk -on_request {
+
+    # Populate elements from local variables
+
+} -select_query {
+
+	select	r.*
+	from	im_risks t
+	where	r.risk_id = :risk_id
+
+} -new_data {
+
+    set risk_id [db_string new_risk "
+	select im_risk__new(
+		null::integer,			-- risk_id  default null
+		'im_risk',			-- object_type default im_risk
+		now()::timestamptz,		-- creation_date default now()
+		:current_user_id::integer,	-- creation_user default null
+		'[ad_conn peeraddr]',		-- creation_ip default null
+		null::integer,			-- context_id default null
+
+		:risk_project_id::integer,	-- project container
+		:risk_status_id::integer,	-- active or inactive or for WF stages
+		:risk_type_id::integer,		-- user defined type of risk. Determines WF.
+		:risk_name			-- Unique name of risk per project
+	)
+    "]
+
+    im_dynfield::attribute_store \
+	-object_type "im_risk" \
+	-object_id $risk_id \
+	-form_id riskmanagement_risk
+
+    # Write Audit Trail
+    im_project_audit -project_id $risk_id -action create
+
+    ad_returnredirect $return_url
+#    ad_returnredirect [export_vars -base "/intranet-riskmanagement/new" {risk_id {form_mode display}}]
+    ad_script_abort
+
+} -edit_data {
+
+    set start_date_sql [template::util::date get_property sql_date $start_date]
+    set end_date_sql [template::util::date get_property sql_timestamp $end_date]
+
+    db_dml risk_update {}
+    db_dml project_update {}
+
+    im_dynfield::attribute_store \
+	-object_type "im_risk" \
+	-object_id $risk_id \
+	-form_id riskmanagement_risk
+
+    # Write Audit Trail
+    im_project_audit -project_id $risk_id -action update
+
+} -on_submit {
+
+	ns_log Notice "new: on_submit"
+
+} -after_submit {
+
+	ad_returnredirect $return_url
+	ad_script_abort
+
+} -validate {
+    {risk_name
+	{ [string length $risk_name] < 1000 }
+	"[lang::message::lookup {} intranet-riskmanagement.Risk_name_too_long {Risk Name too long (max 1000 characters).}]" 
+    }
+}
+
+# ---------------------------------------------------------------
+# Risk Menu
+# ---------------------------------------------------------------
+
+# Setup the subnavbar
+set bind_vars [ns_set create]
+if {[info exists risk_id]} { ns_set put $bind_vars risk_id $risk_id }
+
+
+if {![info exists risk_id]} { set risk_id "" }
+
+set risk_parent_menu_id [db_string parent_menu "select menu_id from im_menus where label='riskmanagement'" -default 0]
+set sub_navbar [im_sub_navbar \
+    -components \
+    -current_plugin_id $plugin_id \
+    -base_url "/intranet-riskmanagement/new?risk_id=$risk_id" \
+    -plugin_url "/intranet-riskmanagement/new" \
+    $risk_parent_menu_id \
+    $bind_vars "" "pagedesriptionbar" "riskmanagement_summary"] 
+
+
