a3ec7db65e04cb2213e76b3ad6bf583c67eb55a7
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-04-06 15:30:05 +0000

    -- menu "Rechnung auf Basis Kunden/Mitarbeiter Preisliste anlegen"

--- sql/postgresql/intranet-cust-koernigweber-create.sql
+++ sql/postgresql/intranet-cust-koernigweber-create.sql
@@ -347,6 +347,43 @@ returns integer as '
 select inline_1 ();
 drop function inline_1();
 
+create or replace function inline_1 ()
+returns integer as '
+declare
+        v_menu                  integer;
+        v_parent_menu           integer;
+        v_senior_managers       integer;
+        v_project_managers      integer;
+begin
+        select group_id into v_project_managers from groups where group_name = ''Project Managers'';
+        select group_id into v_senior_managers from groups where group_name = ''Senior Managers'';
+
+        select menu_id into v_parent_menu from im_menus where label = ''invoices_customers'';
+
+        v_menu := im_menu__new (
+                null,                                           -- p_menu_id
+                ''im_menu'',                                    -- object_type
+                now(),                                          -- creation_date
+                null,                                           -- creation_user
+                null,                                           -- creation_ip
+                null,                                           -- context_id
+                ''intranet-cust-koernigweber'',                 -- package_name
+                ''invoices_customer_employee_prices'',          -- label
+                ''Rechnung auf Basis Kunden/Mitarbeiter Preisliste anlegen'',   -- name
+                ''/intranet-cust-koernigweber/invoices/new?target_cost_type_id=3700'', -- url
+                500,                                            -- sort_order
+                v_parent_menu,                                  -- parent_menu_id
+                ''[im_cost_type_write_p $user_id 3700]''        -- p_visible_tcl
+        );
+
+        PERFORM acs_permission__grant_permission(v_menu, v_project_managers, ''read'');
+        PERFORM acs_permission__grant_permission(v_menu, v_senior_managers, ''read'');
+
+        return 0;
+end;' language 'plpgsql';
+select inline_1 ();
+drop function inline_1();
+
 
 
 
