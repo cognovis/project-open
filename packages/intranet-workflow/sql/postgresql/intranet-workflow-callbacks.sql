26d18402a7a198831f5b34cd300b8434c161bc5e
Malte Sussdorff <malte.sussdorff@cognovis.de>
2011-04-12 11:55:13 +0200

    Added assign workflow procedures for assigning to the project_manager or all project_admins

--- packages/intranet-workflow/sql/postgresql/intranet-workflow-callbacks.sql
+++ packages/intranet-workflow/sql/postgresql/intranet-workflow-callbacks.sql
@@ -6,7 +6,7 @@
 -- http://www.project-open.com/license/ for details.
 --
 -- @author frank.bergmann@project-open.com
-
+-- @author Malte Sussdorff (malte.sussdorff)
 -- ------------------------------------------------------------------
 -- Generic Workflow Callbacks that work across Business Objects
 -- ------------------------------------------------------------------
@@ -290,6 +290,56 @@ begin
 end;' language 'plpgsql';
 
 
+CREATE OR REPLACE FUNCTION im_workflow__assign_to_project_manager(integer, text)
+  RETURNS integer AS
+$BODY$
+ declare
+     p_task_id        alias for $1;
+	p_custom_arg		alias for $2;
+     v_transition_key    varchar;    
+    v_object_type        varchar;
+     v_case_id        integer;    
+    v_object_id        integer;
+     v_creation_user        integer;    
+    v_creation_ip        varchar;
+     v_project_manager_id     integer;    
+    v_project_manager_name     varchar;    
+
+     v_journal_id        integer;    
+ 
+ begin
+     -- Get information about the transition and the 'environment'
+     select    tr.transition_key, t.case_id, c.object_id, o.creation_user, o.creation_ip, o.object_type
+     into    v_transition_key, v_case_id, v_object_id, v_creation_user, v_creation_ip, v_object_type
+     from    wf_tasks t, wf_cases c, wf_transitions tr, acs_objects o
+     where    t.task_id = p_task_id
+         and t.case_id = c.case_id
+         and o.object_id = t.case_id
+         and t.workflow_key = tr.workflow_key
+         and t.transition_key = tr.transition_key;
+ 
+     select    p.project_lead_id into v_project_manager_id from im_projects p, im_timesheet_conf_objects co
+     where    p.project_id = co.conf_project_id
+     and co.conf_id = v_object_id;
+ 
+     select im_name_from_id(v_project_manager_id) into v_project_manager_name;
+
+     RAISE NOTICE 'My projectmanager for % is % and called %', v_object_id, v_project_manager_id, v_project_manager_name; 
+     IF v_project_manager_id is not null THEN
+         v_journal_id := journal_entry__new(
+             null, v_case_id,
+             v_transition_key || ' assign_to_project_manager ' || v_project_manager_name,
+             v_transition_key || ' assign_to_project_manager ' || v_project_manager_name,
+             now(), v_creation_user, v_creation_ip,
+             'Assigning to user' || v_project_manager_name
+         );
+         PERFORM workflow_case__add_task_assignment(p_task_id, v_project_manager_id, 'f');
+         PERFORM workflow_case__notify_assignee (p_task_id, v_project_manager_id, null, null, 
+             'wf_' || v_object_type || '_assignment_notif');
+     END IF;
+     return 0;
+end;$BODY$
+  LANGUAGE 'plpgsql' VOLATILE;
 
 -- Assign the transition to project members with role "Project Admin".
 -- (The project manager is automatically assigned as project member with
@@ -347,6 +397,59 @@ begin
 	return 0;
 end;' language 'plpgsql';
 
+
+create or replace function im_workflow__assign_to_project_admins (integer, text)
+returns integer as '
+declare
+        p_task_id               alias for $1;
+        p_custom_arg            alias for $2;
+
+	v_case_id		integer;
+	v_creation_ip		varchar; 	
+    v_creation_user		integer;
+	v_object_id		integer;	v_object_type		varchar;
+	v_journal_id		integer;
+	v_transition_key	varchar;	v_workflow_key		varchar;
+
+	row			RECORD;
+begin
+	-- Select out some frequently used variables of the environment
+	select	tr.transition_key, t.case_id, c.object_id, o.creation_user, o.creation_ip, o.object_type
+	into	v_transition_key, v_case_id, v_object_id, v_creation_user, v_creation_ip, v_object_type
+	from	wf_tasks t, wf_cases c, wf_transitions tr, acs_objects o
+	where	t.task_id = p_task_id
+		and t.case_id = c.case_id
+		and o.object_id = t.case_id
+		and t.workflow_key = tr.workflow_key
+		and t.transition_key = tr.transition_key;
+
+	FOR row IN
+		select	r.object_id_two as user_id, 
+			im_name_from_user_id(r.object_id_two) as user_name
+		from im_timesheet_conf_objects co,
+			im_projects p,
+			acs_rels r,
+			im_biz_object_members bom
+		where	co.conf_id = v_object_id and
+			co.conf_project_id = p.project_id and
+			r.object_id_one = p.project_id and
+			r.rel_id = bom.rel_id and
+			bom.object_role_id = 1301
+	LOOP
+		v_journal_id := journal_entry__new(
+		    null, v_case_id,
+		    v_transition_key || '' assign_to_user '' || row.user_name,
+		    v_transition_key || '' assign_to_user '' || row.user_name,
+		    now(), v_creation_user, v_creation_ip,
+		    ''Assigning to '' || row.user_name
+		);
+		PERFORM workflow_case__add_task_assignment(p_task_id, row.user_id, ''f'');
+		PERFORM workflow_case__notify_assignee (p_task_id, row.user_id, null, null, 
+			''wf_'' || v_object_type || ''_assignment_notif'');
+	END LOOP;
+
+	return 0;
+end;' language 'plpgsql';
 
 
 -- Enable callback that deletes all tokens in the systems except the one
