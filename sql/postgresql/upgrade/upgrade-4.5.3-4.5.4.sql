c74565e225e748fb530e91bcfe8e7bae4e9f072a
po35devklaus <>
2011-05-12 22:18:58 +0000

    -- init

---
+++ sql/postgresql/upgrade/upgrade-4.5.3-4.5.4.sql
@@ -0,0 +1,7 @@
+-- upgrade-4.5.3-4.5.4.sql
+
+-- Localization
+
+SELECT im_lang_add_message('en_US','acs-workflow','Task_Has_Not_Been_Started_Yet','This task has not been started yet.');
+SELECT im_lang_add_message('de_DE','acs-workflow','Task_Has_Not_Been_Started_Yet','Workflow Aufgabe noch nicht gestarted');
+
