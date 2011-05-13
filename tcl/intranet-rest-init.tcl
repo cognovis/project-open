b3115e4871a68dc18b701f88a4679dc34cbf1400
Frank Bergmann <frank.bergmann@project-open.com>
2011-05-13 11:49:43 +0000

    - REST Interface:   Added an "init" script to initialize a cache and   to register execution of procedures

---
+++ tcl/intranet-rest-init.tcl
@@ -0,0 +1,23 @@
+ad_library {
+
+    Initialization for intranet-rest module
+    
+    @author Frank Bergmann (frank.bergmann@project-open.com)
+    @creation-date 10 May, 2011
+    @cvs-id $Id$
+
+}
+
+
+# Register handler procedures for the various HTTP methods
+ad_register_proc GET /intranet-rest/* im_rest_call_get
+ad_register_proc POST /intranet-rest/* im_rest_call_post
+ad_register_proc PUT /intranet-rest/* im_rest_call_put
+# ad_register_proc DELETE /intranet-rest/* im_rest_call_delete
+
+
+# Create a global cache for im_rest entries
+# The cache is bound by global timeout of 1 hour currently.
+ns_cache create im_rest -timeout 3600
+
+
