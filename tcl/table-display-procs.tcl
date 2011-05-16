c075c83fa637a500a397094c80ebee22c5b0e7b1
khofeditz <>
2011-05-16 06:10:04 +0000

    -- fixed /acs-admin/apm/version-parameters - var scope not found

--- tcl/table-display-procs.tcl
+++ tcl/table-display-procs.tcl
@@ -453,6 +453,9 @@ ad_proc -deprecated ad_table {
 
 	    append Thtml [subst $Trow_code]
 	    
+	    # fixing /acs-admin/apm/version-parameters
+	    set scope ""
+
 	    foreach Ti $Tcolumn_list {
 		set Tcol [lindex $Tdatadef $Ti]
 		# If we got some special formatting code we handle it
