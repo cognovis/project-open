1d4fbed53da788308e8954591244ee82cd58e6fc
po34demo <po34demo>
2011-06-03 08:34:43 +0000

    - Sencha Ticket Tracker:   Introduced a TabPanel for viewing the details of a ticket.   This will allow us to add a file storage panel

---
+++ www/TicketTabPanel.js
@@ -0,0 +1,42 @@
+/**
+ * intranet-sencha-ticket-tracker/www/TicketContainer.js
+ * Container for both TicketGrid and TicketForm.
+ *
+ * @author Frank Bergmann (frank.bergmann@project-open.com)
+ * @creation-date 2011-05
+ * @cvs-id $Id: TicketTabPanel.js,v 1.1 2011/06/03 08:34:43 po34demo Exp $
+ *
+ * Copyright (C) 2011, ]project-open[
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+Ext.define('TicketBrowser.TicketTabPanel', {
+    extend: 'Ext.tab.Panel',
+    alias: 'widget.ticketTabPanel',
+    activeTab: 0,
+    tabBar: {
+	border: true
+    },
+    items: [{
+	itemId: 'ticket',
+	xtype: 'ticketInfo',
+	title: 'View Ticket'
+    }, {
+	itemId: 'ticketFilestorage',
+	title: '#intranet-helpdesk.Tickets#',
+	html: '<h1>test</h1>'
+    }]   
+});
--- www/index.adp
+++ www/index.adp
@@ -18,6 +18,7 @@
  	<script type="text/javascript" src="Panels.js"></script>
 	<script type="text/javascript" src="TicketForm.js"></script> 
 	<script type="text/javascript" src="TicketFilterForm.js"></script> 
+	<script type="text/javascript" src="TicketTabPanel.js"></script> 
 	<script type="text/javascript" src="TicketFilterAccordion.js"></script> 
 	<script type="text/javascript" src="PreviewPlugin.js"></script> 
 
