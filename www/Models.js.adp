f574fad42ff844557ccaf07b1c9c68ba4060f186
po34demo <po34demo>
2011-06-06 14:02:15 +0000

    - Sencha Ticket Tracker:   Working on file upload

--- www/Models.js.adp
+++ www/Models.js.adp
@@ -4,7 +4,7 @@
  *
  * @author Frank Bergmann (frank.bergmann@project-open.com)
  * @creation-date 2011-05
- * @cvs-id $Id: Models.js.adp,v 1.2 2011/06/03 11:34:10 po34demo Exp $
+ * @cvs-id $Id: Models.js.adp,v 1.3 2011/06/06 14:02:15 po34demo Exp $
  *
  * Copyright (C) 2011, ]project-open[
  *
@@ -149,10 +149,10 @@ Ext.define('TicketBrowser.Company', {
 Ext.define('TicketBrowser.FileStorage', {
     extend: 'Ext.data.Model',
 
-    idProperty: 'id'	,		// The primary key or object_id of the filestorage
+    idProperty: 'item_id',		// The primary key or object_id of the filestorage
     fields: [
 	// Basic filestorage fields with special meaning
-	'id',				// The primary key or object_id of the filestorage
+	'item_id',			// The primary key or object_id of the filestorage
 	'name',				// The name of the file.
 	'parent_id',			// The ID of the content folder that contains the file
 	'mime_type',			// MIME type of the file, i.e. "image/jpeg", ...
---
+++ www/file-add.tcl
@@ -0,0 +1,58 @@
+ad_page_contract {
+    page to add a new file to the system
+    @author Kevin Scaldeferri (kevin@arsdigita.com)
+    @creation-date 6 Nov 2000
+    @cvs-id $Id: file-add.tcl,v 1.1 2011/06/06 14:02:15 po34demo Exp $
+} {
+    folder_id:integer,optional,notnull
+    upload_file:trim,optional
+    upload_file.tmpfile:tmpfile,optional
+    {title ""}
+    {description "" }
+}
+
+if {![info exists upload_file]} {
+    ns_return 200 "text/html" "{\"result\":{
+	\"success\":false,
+	\"errors\": {\"email\":\"already taken\"}
+    }}"
+    ad_script_abort
+}
+
+set user_id [ad_conn user_id]
+# Get package. ad_conn package_id doesn't seem to work...
+set package_id [db_string package "select min(package_id) from apm_packages where package_key = 'file-storage'"]
+set mime_type [cr_filename_to_mime_type -create -- $upload_file]
+
+ns_log Notice "file-add: folder_id=[im_opt_val folder_id], upload_file=[im_opt_val upload_file], title=$title, upload_file.tmpfile=${upload_file.tmpfile}, mime_type=$mime_type, package_id=$package_id"
+
+permission::require_permission \
+    -object_id $folder_id \
+    -party_id $user_id \
+    -privilege "write"
+
+
+if {"" != $upload_file} {
+    set this_file_id ""
+
+    fs::add_file \
+	-name $upload_file \
+	-parent_id $folder_id \
+	-tmp_filename ${upload_file.tmpfile}\
+	-creation_user $user_id \
+	-creation_ip [ad_conn peeraddr] \
+	-title $title \
+	-description $description \
+	-package_id $package_id \
+	-mime_type $mime_type
+    
+}
+
+db_release_unused_handles
+ad_http_cache_control
+
+ns_return 200 "text/html" "{\"result\":{\"success\":true}}"
+ad_script_abort
+
+
+
