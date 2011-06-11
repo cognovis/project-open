07d89c0873e52e8366fbc1db63e62a2347a0a100
Malte Sussdorff <malte.sussdorff@cognovis.de>
2011-06-11 07:57:43 +0200

    Merge acs-mail-lite as our subdirectory

---
+++ packages/acs-mail-lite/sql/oracle/acs-mail-lite-drop.sql
@@ -0,0 +1,12 @@
+--
+-- A simple mail queue
+--
+-- @author <a href="mailto:eric@openforce.net">eric@openforce.net</a>
+-- @version $Id: acs-mail-lite-drop.sql,v 1.5 2009/03/18 22:41:15 emmar Exp $
+--
+
+drop table acs_mail_lite_queue;
+drop sequence acs_mail_lite_id_seq;
+drop table acs_mail_lite_mail_log; 
+drop table acs_mail_lite_bounce; 
+drop table acs_mail_lite_bounce_notif;
