93535997d8fa57d5dcc37f5fe05b029f1aa97e21
Malte Sussdorff <intranet@cognovis.de>
2011-06-20 13:57:06 +0200

    Fixed display of tasks in the project

---
+++ packages/intranet-cognovis/sql/postgresql/upgrade/upgrade-0.5d18-0.5d19.sql
@@ -0,0 +1,29 @@
+-- 
+-- packages/intranet-cognovis/sql/postgresql/upgrade/upgrade-0.5d18-0.5d19.sql
+-- 
+-- Copyright (c) 2011, cognovís GmbH, Hamburg, Germany
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-- 
+-- @author Malte Sussdorff (malte.sussdorff@cognovis.de)
+-- @creation-date 2011-06-17
+-- @cvs-id $Id$
+--
+
+SELECT acs_log__debug('/packages/intranet-cognovis/sql/postgresql/upgrade/upgrade-0.5d18-0.5d19.sql','');
+
+-- Update the view to correctly display the project and task
+update im_view_columns set column_render_tcl = '"<nobr>$indent_html$gif_html<a href=/intranet-cognovis/tasks/view?[export_url_vars project_id task_id return_url]>$task_name</a></nobr>"' where column_id = 1007;
+
+-- Update to correctly view the tasks with the new status
+update im_component_plugins set component_tcl = 'im_timesheet_task_list_component -restrict_to_project_id $project_id -view_name im_timesheet_task_list_short -restrict_to_status_id 9600' where plugin_name = 'Project Timesheet Tasks';
+
