97a525e32e2aadb044dff1c8c41fff768b8164d8
Frank Bergmann <frank.bergmann@project-open.com>
2011-06-23 12:06:39 +0000

    - File-Storage:   Fixed meta-information for file_storage_object object type

--- sql/postgresql/upgrade/upgrade-4.0.2.0.0-4.0.2.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.0.2.0.0-4.0.2.0.1.sql
@@ -3,7 +3,8 @@
 -- Fix metadata from file_storage_object
 update acs_object_types set 
 	table_name = 'file_storage_objectx', 
-	id_column='object_id' 
+	id_column='object_id',
+	name_method = 'file_storage_object__name'
 where 
 	object_type = 'file_storage_object'
 ;
@@ -14,3 +15,19 @@ delete from cr_text;
 alter table cr_text disable trigger cr_text_tr;
 insert into cr_text (text_data) values ('');
 alter table cr_text enable trigger cr_text_tr;
+
+
+
+create or replace function file_storage_object__name(integer)
+returns varchar as $body$
+DECLARE
+        p_item_id               alias for $1;
+        v_name                  varchar;
+BEGIN
+        select  name into v_name
+        from    file_storage_objectx
+        where   object_id = p_item_id;
+
+        return v_name;
+end; $body$ language 'plpgsql';
+
