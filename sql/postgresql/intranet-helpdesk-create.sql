1550d5b4543609380ce6e9ce312d54523ed8cfa8
Frank Bergmann <frank.bergmann@project-open.com>
2011-06-23 16:51:35 +0000

    - Helpdesk:   Fixed issue with "holes" in the numbers of tickets

--- sql/postgresql/intranet-helpdesk-create.sql
+++ sql/postgresql/intranet-helpdesk-create.sql
@@ -191,7 +191,7 @@ end;' language 'plpgsql';
 
 create or replace function im_ticket__new (
 	integer, varchar, timestamptz, integer, varchar, integer,
-	varchar, integer, integer, integer 
+	varchar, varchar, integer, integer, integer 
 ) returns integer as '
 DECLARE
 	p_ticket_id		alias for $1;		-- ticket_id default null
@@ -201,14 +201,12 @@ DECLARE
 	p_creation_ip		alias for $5;		-- creation_ip default null
 	p_context_id		alias for $6;		-- context_id default null
 	p_ticket_name		alias for $7;		-- ticket_name
-	p_ticket_customer_id	alias for $8;
-	p_ticket_type_id	alias for $9;		
-	p_ticket_status_id	alias for $10;
+	p_ticket_nr		alias for $8;		-- ticket_name
+	p_ticket_customer_id	alias for $9;
+	p_ticket_type_id	alias for $10;		
+	p_ticket_status_id	alias for $11;
 	v_ticket_id		integer;
-	v_ticket_nr		integer;
 BEGIN
-	select nextval(''im_ticket_seq'') into v_ticket_nr;
-
 	v_ticket_id := im_project__new (
 		p_ticket_id,		-- object_id
 		p_object_type,		-- object_type
@@ -217,8 +215,8 @@ BEGIN
 		p_creation_ip,		-- creation_ip
 		p_context_id,		-- context_id
 		p_ticket_name,
-		v_ticket_nr::varchar,
-		v_ticket_nr::varchar,
+		p_ticket_nr::varchar,
+		p_ticket_nr::varchar,
 		null,			-- parent_id
 		p_ticket_customer_id,
 		101,			-- p_ticket_type_id
@@ -237,6 +235,45 @@ BEGIN
 
 	return v_ticket_id;
 END;' language 'plpgsql';
+
+
+
+create or replace function im_ticket__new (
+	integer, varchar, timestamptz, integer, varchar, integer,
+	varchar, integer, integer, integer 
+) returns integer as '
+DECLARE
+	p_ticket_id		alias for $1;		-- ticket_id default null
+	p_object_type		alias for $2;		-- object_type default im_ticket
+	p_creation_date 	alias for $3;		-- creation_date default now()
+	p_creation_user 	alias for $4;		-- creation_user default null
+	p_creation_ip		alias for $5;		-- creation_ip default null
+	p_context_id		alias for $6;		-- context_id default null
+	p_ticket_name		alias for $7;		-- ticket_name
+	p_ticket_customer_id	alias for $8;
+	p_ticket_type_id	alias for $9;		
+	p_ticket_status_id	alias for $10;
+	v_ticket_nr		integer;
+BEGIN
+	select nextval(''im_ticket_seq'') into v_ticket_nr;
+
+	return im_ticket__new (
+		p_ticket_id,
+		p_object_type,
+		p_creation_date,
+		p_creation_user,
+		p_creation_ip,
+		p_context_id,
+		p_ticket_name,
+		v_ticket_nr,
+		p_ticket_customer_id,
+		p_ticket_type_id,
+		p_ticket_status_id	
+	);
+
+END;' language 'plpgsql';
+
+
 
 
 create or replace function im_ticket__delete(integer)
