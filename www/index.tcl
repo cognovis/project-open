5b407d0e7c7ac2cd601c9675722c67dc8b6e1f66
Frank Bergmann <frank.bergmann@project-open.com>
2011-06-28 10:12:17 +0000

    - Sencha TT:   Requiring that all users using the Sencha GUI is Employee

--- www/index.tcl
+++ www/index.tcl
@@ -35,4 +35,10 @@ ad_page_contract {
 # Make sure the user is logged in.
 # Otherwise Sencha will get an error in the REST backend.
 set user_id [auth::require_login]
+set user_is_employee_p [im_user_is_employee_p $user_id]
 
+if {!$user_is_employee_p} {
+    ad_return_complaint 1 "
+	The current user #$user_id is not member of the group Employees
+    "
+}
