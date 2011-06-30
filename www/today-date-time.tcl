04237627cb55b298f0dd9604198b5e0b1fcd6ece
po34demo <po34demo>
2011-06-30 07:49:42 +0000

    - Sencha TT:   fixed date format

--- www/today-date-time.tcl
+++ www/today-date-time.tcl
@@ -1,3 +1,3 @@
 
 # Return the current date
-doc_return 200 "text/html" [db_string date "select to_char(now(), 'YYYY-MM-HH HH24:MI') from dual"]
+doc_return 200 "text/html" [db_string date "select to_char(now(), 'YYYY-MM-DD HH24:MI') from dual"]
