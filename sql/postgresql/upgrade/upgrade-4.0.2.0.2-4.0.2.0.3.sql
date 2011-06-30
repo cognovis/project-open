d150aa978042f10a055a484e92df77420b51c3d0
Frank Bergmann <frank.bergmann@project-open.com>
2011-06-30 11:53:36 +0000

    - Core:   Fixed accounting information on SQL upgrade script

--- sql/postgresql/upgrade/upgrade-4.0.2.0.2-4.0.2.0.3.sql
+++ sql/postgresql/upgrade/upgrade-4.0.2.0.2-4.0.2.0.3.sql
@@ -1,6 +1,6 @@
--- upgrade-4.0.2.0.1-4.0.2.0.2.sql
+-- upgrade--4.0.2.0.2-4.0.2.0.3.sql
 
-SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-4.0.2.0.1-4.0.2.0.2.sql','');
+SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade--4.0.2.0.2-4.0.2.0.3.sql','');
 
 
 
