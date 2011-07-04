a4a84246aeef73c75d7d9aeaa7bef387ccb05e89
po35devklaus <>
2011-07-04 17:51:54 +0000

    -- added value for paramater 'profile' - intranet/user/new.tcl will show only Dynfields applicable for Customer Profile

--- www/companies/primary-contact.tcl
+++ www/companies/primary-contact.tcl
@@ -74,7 +74,8 @@ db_release_unused_handles
 
 
 if { [empty_string_p $contact_info] } {
-    set new_client_contact_link "<A HREF=/intranet/users/new?[export_url_vars return_url]>[_ intranet-core.new_client_contact]</A>"
+    set profile "Customers"
+    set new_client_contact_link "<A HREF=/intranet/users/new?[export_url_vars return_url profile]>[_ intranet-core.new_client_contact]</A>"
     set company_employee_link "<A HREF=\"/intranet/member-add?object_id=$company_id&also_add_to_group_id=461&return_url=/intranet/companies/view?company%5fid=$company_id\">[_ intranet-core.company_employee]</A>"
     set page_body "
 <H3>[_ intranet-core.lt_No_Company_Employees_]</H3>
