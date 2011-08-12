0919b8178d2cc77e2ca4ae1351a568551d78b0ea
Frank Bergmann <frank.bergmann@project-open.com>
2011-08-12 17:05:00 +0000

    - Indicators:   Added a delete script

---
+++ www/delete.tcl
@@ -0,0 +1,20 @@
+ad_page_contract {
+    Delete the indicator
+    @author Frank Bergmann (frank.bergmann@project-open.com)
+    @creation-date 2011-08-13
+    @cvs-id $Id$
+} {
+    indicator_id:integer
+    { return_url "/intranet-reporting-indicators/index" }
+}
+
+set current_user_id [auth::require]
+set admin_p [im_is_user_site_wide_or_intranet_admin $current_user_id]
+if {!$admin_p} { 
+    ad_return_complaint 1 "You are not an Administrator"
+    ad_script_abort
+}
+
+db_string del "select im_indicator__delete(:indicator_id)"
+ad_returnredirect $return_url
+
