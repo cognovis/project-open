aa4764f76247147076d22b20b4bdf5f712724bec
Frank Bergmann <frank.bergmann@project-open.com>
2011-08-12 08:01:36 +0000

    - SLA Management:   Disabled the "SLAs" menu

--- sql/postgresql/intranet-sla-management-create.sql
+++ sql/postgresql/intranet-sla-management-create.sql
@@ -767,48 +767,49 @@ SELECT acs_permission__grant_permission(
 -- Create a menu item in the main menu bar and set some default 
 -- permissions for various groups who should be able to see the menu.
 
+-- Dont create an extra menu...
 
-create or replace function inline_0 ()
-returns integer as '
-declare
-	-- Menu IDs
-	v_menu			integer;
-	v_main_menu		integer;
-	v_employees		integer;
-
-BEGIN
-	-- Get some group IDs
-	select group_id into v_employees from groups where group_name = ''Employees'';
-
-	-- Determine the main menu. "Label" is used to
-	-- identify menus.
-	select menu_id into v_main_menu from im_menus where label=''main'';
-
-	-- Create the menu.
-	v_menu := im_menu__new (
-		null,				-- p_menu_id
-		''im_menu'',			-- object_type
-		now(),				-- creation_date
-		null,				-- creation_user
-		null,				-- creation_ip
-		null,				-- context_id
-		''intranet-sla-management'',	-- package_name
-		''sla-management'',		-- label
-		''SLAs'',			-- name
-		''/intranet-sla-management/'',	-- url
-		85,				-- sort_order
-		v_main_menu,			-- parent_menu_id
-		null				-- p_visible_tcl
-	);
-
-	-- Grant some groups the read permission for the main "SLA-Management" tab.
-	-- These permissions are independent from the user`s permission to
-	-- read the actual sla-management.
-	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
-
-	return 0;
-end;' language 'plpgsql';
--- Execute and then drop the function
-select inline_0 ();
-drop function inline_0 ();
+-- create or replace function inline_0 ()
+-- returns integer as '
+-- declare
+-- 	-- Menu IDs
+-- 	v_menu			integer;
+-- 	v_main_menu		integer;
+-- 	v_employees		integer;
+-- 
+-- BEGIN
+-- 	-- Get some group IDs
+-- 	select group_id into v_employees from groups where group_name = ''Employees'';
+-- 
+-- 	-- Determine the main menu. "Label" is used to
+-- 	-- identify menus.
+-- 	select menu_id into v_main_menu from im_menus where label=''main'';
+-- 
+-- 	-- Create the menu.
+-- 	v_menu := im_menu__new (
+-- 		null,				-- p_menu_id
+-- 		''im_menu'',			-- object_type
+-- 		now(),				-- creation_date
+-- 		null,				-- creation_user
+-- 		null,				-- creation_ip
+-- 		null,				-- context_id
+-- 		''intranet-sla-management'',	-- package_name
+-- 		''sla-management'',		-- label
+-- 		''SLAs'',			-- name
+-- 		''/intranet-sla-management/'',	-- url
+-- 		85,				-- sort_order
+-- 		v_main_menu,			-- parent_menu_id
+-- 		null				-- p_visible_tcl
+-- 	);
+-- 
+-- 	-- Grant some groups the read permission for the main "SLA-Management" tab.
+-- 	-- These permissions are independent from the user`s permission to
+-- 	-- read the actual sla-management.
+-- 	PERFORM acs_permission__grant_permission(v_menu, v_employees, ''read'');
+-- 
+-- 	return 0;
+-- end;' language 'plpgsql';
+-- -- Execute and then drop the function
+-- select inline_0 ();
+-- drop function inline_0 ();
 
