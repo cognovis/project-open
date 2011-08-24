57cf5de4b6a6d94e1712b6674cc8376274aeb1ed
Frank Bergmann <frank.bergmann@project-open.com>
2011-08-24 16:47:02 +0000

    - Audit:   Removed foreign key constraint on audit_object_status_id,   because users don't have a valid ID

---
+++ sql/postgresql/upgrade/upgrade-4.0.2.0.5-4.0.2.0.6.sql
@@ -0,0 +1,41 @@
+-- upgrade-4.0.2.0.5-4.0.2.0.6.sql
+
+SELECT acs_log__debug('/packages/intranet-audit/sql/postgresql/upgrade/upgrade-4.0.2.0.5-4.0.2.0.6.sql','');
+
+
+create or replace function inline_0 ()
+returns integer as $body$
+DECLARE
+	v_count		integer;
+BEGIN
+	select count(*) into v_count from pg_constraint
+	where  lower(conname) = 'im_audits_object_status_fk';
+	IF v_count = 0 THEN return 0; END IF;
+
+	alter table im_audits 
+	drop constraint im_audits_object_status_fk;
+
+	return 0;
+end; $body$ language 'plpgsql';
+select inline_0();
+drop function inline_0();
+
+
+
+create or replace function inline_0 ()
+returns integer as $body$
+DECLARE
+	v_count		integer;
+BEGIN
+	select count(*) into v_count from pg_constraint
+	where  lower(conname) = 'im_audits_status_fk';
+	IF v_count = 0 THEN return 0; END IF;
+
+	alter table im_audits 
+	drop constraint im_audits_status_fk;
+
+	return 0;
+end; $body$ language 'plpgsql';
+select inline_0();
+drop function inline_0();
+
