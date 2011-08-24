c5802a8128f3c0e29206c45d9b3254d38e445c26
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-08-24 13:38:48 +0000

    -- bugfix: Time logged on projects did not show up in report

--- www/timesheet-productivity-compensation.tcl
+++ www/timesheet-productivity-compensation.tcl
@@ -33,9 +33,9 @@ set current_user_id [ad_maybe_redirect_for_registration]
 set use_project_name_p [parameter::get_from_package_key -package_key intranet-reporting -parameter "UseProjectNameInsteadOfProjectNr" -default 0]
 
 set read_p [db_string report_perms "
-	select	im_object_permission_p(m.menu_id, :current_user_id, 'read')
-	from	im_menus m
-	where	m.label = :menu_label
+selectim_object_permission_p(m.menu_id, :current_user_id, 'read')
+fromim_menus m
+wherem.label = :menu_label
 " -default 'f']
 
 if {![string equal "t" $read_p]} {
@@ -74,8 +74,8 @@ set work_days_per_year [parameter::get_from_package_key -package_key "intranet-t
 
 db_1row todays_date "
 select
-	to_char(sysdate::date - :days_in_past::integer, 'YYYY') as todays_year,
-	to_char(sysdate::date - :days_in_past::integer, 'MM') as todays_month
+to_char(sysdate::date - :days_in_past::integer, 'YYYY') as todays_year,
+to_char(sysdate::date - :days_in_past::integer, 'MM') as todays_month
 from dual
 "
 
@@ -135,31 +135,54 @@ set end_date "${year}-${month}-$num_days"
 
 set inner_sql "
 select
-	h.day::date as date,
-	h.user_id,
-	t.task_id,
+h.day::date as date,
+h.user_id,
         p.parent_id as project_id,
-	p2.company_id,
-	h.hours as hours,
-	e.availability,
+p2.company_id,
+h.hours as hours,
+e.availability,
         p.project_type_id
 from
-	im_hours h,
-	im_projects p,
+im_hours h,
+im_projects p,
         im_projects p2,
         im_timesheet_tasks t,
-	users u
-	LEFT OUTER JOIN im_employees e ON (u.user_id = e.employee_id)
+users u
+LEFT OUTER JOIN im_employees e ON (u.user_id = e.employee_id)
 where
-	h.project_id = t.task_id
-	and p2.project_status_id not in ([im_project_status_deleted])
-	and h.user_id = u.user_id
-	and h.day >= to_date(:start_date, 'YYYY-MM')
-	and h.day <= to_date(:end_date, 'YYYY-MM-DD')
-	and :start_date = to_char(h.day, 'YYYY-MM')
+h.project_id = t.task_id
+and p2.project_status_id not in ([im_project_status_deleted])
+and h.user_id = u.user_id
+and h.day >= to_date(:start_date, 'YYYY-MM')
+and h.day <= to_date(:end_date, 'YYYY-MM-DD')
+and :start_date = to_char(h.day, 'YYYY-MM')
         and p2.project_id = p.parent_id
         and t.task_id = p.project_id
-	$where_clause
+$where_clause
+UNION
+select
+h.day::date as date,
+h.user_id,
+        p.project_id as project_id,
+p.company_id,
+h.hours as hours,
+e.availability,
+        p.project_type_id
+from
+im_hours h,
+im_projects p,
+users u
+LEFT OUTER JOIN im_employees e ON (u.user_id = e.employee_id)
+where
+h.project_id = p.project_id
+and p.project_status_id not in ([im_project_status_deleted])
+and h.user_id = u.user_id
+and h.day >= to_date(:start_date, 'YYYY-MM')
+and h.day <= to_date(:end_date, 'YYYY-MM-DD')
+and :start_date = to_char(h.day, 'YYYY-MM')
+and project_type_id != [im_project_type_task]
+$where_clause
+
 "
 
 if {$level_of_detail == 5} {
@@ -185,14 +208,14 @@ select  start_date::date as date,
 from 
         im_user_absences
 where 
-	start_date <= to_date(:end_date, 'YYYY-MM-DD') and
+start_date <= to_date(:end_date, 'YYYY-MM-DD') and
         end_date >= to_date(:start_date, 'YYYY-MM')
         $abs_where_clause
 order by
-	user_name,
-	company_id,
-	project_id,
-	date
+user_name,
+company_id,
+project_id,
+date
 "
 
 } else {
@@ -202,28 +225,28 @@ select
         s.date,
         s.availability,
         s.hours,
-	CASE c.company_id = :internal_company_id WHEN true THEN s.hours ELSE 0 END as hours_intl,
-	CASE c.company_id != :internal_company_id WHEN true THEN s.hours ELSE 0 END as hours_extl,
+CASE c.company_id = :internal_company_id WHEN true THEN s.hours ELSE 0 END as hours_intl,
+CASE c.company_id != :internal_company_id WHEN true THEN s.hours ELSE 0 END as hours_extl,
         CASE p.project_type_id = :no_assignment_project_type_id WHEN true THEN s.hours ELSE 0 END as hours_no_assignment,
         0 as hours_absence,
-	u.user_id,
-	im_name_from_user_id(u.user_id) as user_name,
-	p.project_id,
-	p.project_nr,
-	p.project_name,
-	c.company_id,
-	c.company_path as company_nr,
-	c.company_name
+u.user_id,
+im_name_from_user_id(u.user_id) as user_name,
+p.project_id,
+p.project_nr,
+p.project_name,
+c.company_id,
+c.company_path as company_nr,
+c.company_name
 from
-	($inner_sql) s,
-	im_companies c,
-	im_projects p,
-	cc_users u
+($inner_sql) s,
+im_companies c,
+im_projects p,
+cc_users u
 where
-	s.user_id = u.user_id
-	and p.project_status_id not in ([im_project_status_deleted])
-	and s.company_id = c.company_id
-	and s.project_id = p.project_id
+s.user_id = u.user_id
+and p.project_status_id not in ([im_project_status_deleted])
+and s.company_id = c.company_id
+and s.project_id = p.project_id
 
 UNION
 
@@ -245,183 +268,183 @@ select  start_date::date as date,
 from 
         im_user_absences
 where 
-	start_date <= to_date(:end_date, 'YYYY-MM-DD') and
+start_date <= to_date(:end_date, 'YYYY-MM-DD') and
         end_date >= to_date(:start_date, 'YYYY-MM')
         $abs_where_clause
 ) hours_and_absences
 order by
-	user_name,
-	company_id,
-	project_id,
-	date
+user_name,
+company_id,
+project_id,
+date
 
 "
 }
 
 set report_def [list \
     group_by user_id \
-    header {
-	"\#colspan=9 <b><a href=$user_url$user_id>$user_name</a></b>"
-    } \
-    content [list  \
-	group_by company_id \
-	header {
-	    $user_name
-	    $company_name_pretty
-	} \
-	content [list \
-	    group_by project_id \
-	    header {
-		$user_name
-		""
-		$project_name_pretty
-	    } \
-	    content [list \
 		    header {
-			$user_name
-			""
-			""
-			"" ""
-			$hours_intl
-			$hours_extl
-		        $hours_absence
-			$hours
+			"\\#colspan=9 <b><a href=$user_url$user_id>$user_name</a></b>"
 		    } \
-		    content {} \
-	    ] \
-	    footer {
-		$user_name
-		""
-		""
-		"" ""
-		"<small>$hours_project_intl_subtotal</small>" 
-		"<small>$hours_project_extl_subtotal</small>" 
-		"<small>$hours_project_absence_subtotal</small>" 
-		"<small>$hours_project_subtotal</small>" 
-	    } \
-	] \
-	footer {
-	    $user_name 
-	    "" "" "" ""
-	    "<i>$hours_company_intl_subtotal</i>" 
-	    "<i>$hours_company_extl_subtotal</i>" 
-	    "<i>$hours_company_absence_subtotal</i>" 
-	    "<i>$hours_company_subtotal</i>" 
-	} \
-    ] \
-    footer {
-	$user_name 
-	"" "" "" 
-	"<b>$availability %</b> &nbsp;"
-	"<b>$hours_user_intl_subtotal</b>" 
-	"<b>$hours_user_extl_subtotal</b>" 
-	"" 
-	"<b>$hours_user_subtotal</b>" 
-    } \
-]
+		    content [list  \
+				 group_by company_id \
+				 header {
+				         $user_name
+				         $company_name_pretty
+				 } \
+				 content [list \
+					          group_by project_id \
+					      header {
+						  $user_name
+						  ""
+						  $project_name_pretty
+					      } \
+					      content [list \
+							   header {
+							       $user_name
+							       ""
+							       ""
+							       "" ""
+							       $hours_intl
+							       $hours_extl
+							               $hours_absence
+							       $hours
+							   } \
+							   content {} \
+							   ] \
+					      footer {
+						  $user_name
+						  ""
+						  ""
+						  "" ""
+						  "<small>$hours_project_intl_subtotal</small>" 
+						  "<small>$hours_project_extl_subtotal</small>" 
+						  "<small>$hours_project_absence_subtotal</small>" 
+						  "<small>$hours_project_subtotal</small>" 
+					      } \
+					      ] \
+				 footer {
+				         $user_name 
+				         "" "" "" ""
+				         "<i>$hours_company_intl_subtotal</i>" 
+				         "<i>$hours_company_extl_subtotal</i>" 
+				         "<i>$hours_company_absence_subtotal</i>" 
+				         "<i>$hours_company_subtotal</i>" 
+				 } \
+				 ] \
+		    footer {
+			$user_name 
+			"" "" "" 
+			"<b>$availability %</b> &nbsp;"
+			"<b>$hours_user_intl_subtotal</b>" 
+			"<b>$hours_user_extl_subtotal</b>" 
+			"" 
+			"<b>$hours_user_subtotal</b>" 
+		    } \
+		    ]
 
 # Global header/footer
 set header0 {"Employee" "Customer" "Project" "&nbsp;" Avail "Intl<br>Hours" "Extl<br>Hours" "Absence<br/>Hours" "Total<br>Hours"}
 set footer0 {"" "" "" "" "" "" "" "" ""}
 
 set hours_user_counter [list \
-	pretty_name Hours \
-	var hours_user_subtotal \
-	reset \$user_id \
-	expr \$hours
-]
+			    pretty_name Hours \
+			    var hours_user_subtotal \
+			    reset \$user_id \
+			    expr \$hours
+			]
 
 set hours_user_intl_counter [list \
-	pretty_name HoursIntl \
-	var hours_user_intl_subtotal \
-	reset \$user_id \
-	expr \$hours_intl
-]
+				 pretty_name HoursIntl \
+				 var hours_user_intl_subtotal \
+				 reset \$user_id \
+				 expr \$hours_intl
+			     ]
 
 set hours_user_extl_counter [list \
-	pretty_name HoursExtl \
-	var hours_user_extl_subtotal \
-	reset \$user_id \
-	expr \$hours_extl
-]
+				 pretty_name HoursExtl \
+				 var hours_user_extl_subtotal \
+				 reset \$user_id \
+				 expr \$hours_extl
+			     ]
 
 set hours_user_no_assignment_counter [list \
-	pretty_name HoursNoAssignment \
-	var hours_user_no_assignment_subtotal \
-	reset \$user_id \
-	expr \$hours_no_assignment
-]
+					  pretty_name HoursNoAssignment \
+					  var hours_user_no_assignment_subtotal \
+					  reset \$user_id \
+					  expr \$hours_no_assignment
+				      ]
 
 set hours_company_counter [list \
-	pretty_name Hours \
-	var hours_company_subtotal \
-	reset \$company_id \
-	expr \$hours
-]
+			       pretty_name Hours \
+			       var hours_company_subtotal \
+			       reset \$company_id \
+			       expr \$hours
+			   ]
 
 set hours_company_intl_counter [list \
-	pretty_name HoursIntl \
-	var hours_company_intl_subtotal \
-	reset \$company_id \
-	expr \$hours_intl
-]
+				    pretty_name HoursIntl \
+				    var hours_company_intl_subtotal \
+				    reset \$company_id \
+				    expr \$hours_intl
+				]
 
 set hours_company_extl_counter [list \
-	pretty_name HoursExtl \
-	var hours_company_extl_subtotal \
-	reset \$company_id \
-	expr \$hours_extl
-]
+				    pretty_name HoursExtl \
+				    var hours_company_extl_subtotal \
+				    reset \$company_id \
+				    expr \$hours_extl
+				]
 
 set hours_company_absence_counter [list \
-	pretty_name HoursAbsence \
-	var hours_company_absence_subtotal \
-	reset \$company_id \
-	expr \$hours_absence
-]
+				       pretty_name HoursAbsence \
+				       var hours_company_absence_subtotal \
+				       reset \$company_id \
+				       expr \$hours_absence
+				   ]
 
 set hours_project_counter [list \
-	pretty_name Hours \
-	var hours_project_subtotal \
-	reset \$project_id \
-	expr \$hours
-]
+			       pretty_name Hours \
+			       var hours_project_subtotal \
+			       reset \$project_id \
+			       expr \$hours
+			   ]
 
 set hours_project_intl_counter [list \
-	pretty_name HoursIntl \
-	var hours_project_intl_subtotal \
-	reset \$project_id \
-	expr \$hours_intl
-]
+				    pretty_name HoursIntl \
+				    var hours_project_intl_subtotal \
+				    reset \$project_id \
+				    expr \$hours_intl
+				]
 
 set hours_project_extl_counter [list \
-	pretty_name HoursExtl \
-	var hours_project_extl_subtotal \
-	reset \$project_id \
-	expr \$hours_extl
-]
+				    pretty_name HoursExtl \
+				    var hours_project_extl_subtotal \
+				    reset \$project_id \
+				    expr \$hours_extl
+				]
 
 set hours_project_absence_counter [list \
-	pretty_name HoursAbsence \
-	var hours_project_absence_subtotal \
-	reset \$project_id \
-	expr \$hours_absence
-]
+				       pretty_name HoursAbsence \
+				       var hours_project_absence_subtotal \
+				       reset \$project_id \
+				       expr \$hours_absence
+				   ]
 
 set counters [list \
-	$hours_user_counter \
-	$hours_user_intl_counter \
-	$hours_user_extl_counter \
-	$hours_user_no_assignment_counter \
-	$hours_company_counter \
-	$hours_company_intl_counter \
-	$hours_company_extl_counter \
-	$hours_project_counter \
-	$hours_project_intl_counter \
-	$hours_project_extl_counter \
-	$hours_project_absence_counter \
-	$hours_company_absence_counter \
-]
+		  $hours_user_counter \
+		  $hours_user_intl_counter \
+		  $hours_user_extl_counter \
+		  $hours_user_no_assignment_counter \
+		  $hours_company_counter \
+		  $hours_company_intl_counter \
+		  $hours_company_extl_counter \
+		  $hours_project_counter \
+		  $hours_project_intl_counter \
+		  $hours_project_extl_counter \
+		  $hours_project_absence_counter \
+		  $hours_company_absence_counter \
+		  ]
 
 
 # ------------------------------------------------------------
@@ -435,47 +458,47 @@ im_report_write_http_headers -output_format $output_format
 switch $output_format {
     html {
         ns_write "
-	[im_header]
-	[im_navbar]
-	<form>
-		<table border=0 cellspacing=1 cellpadding=1>
-		<tr>
-		  <td class=form-label>Level of Details</td>
-		  <td class=form-widget>
-		    [im_select -translate_p 0 level_of_detail $levels $level_of_detail]
-		  </td>
-		</tr>
-		<tr>
-		  <td class=form-label>Month</td>
-		  <td class=form-widget>
-		    <input type=textfield name=start_date value=$start_date>
-		  </td>
-		</tr>
-		<tr>
-		  <td class=form-label>Manager</td>
-		  <td class=form-widget>
-		    [im_user_select -include_empty_p 1 -group_id [im_profile_employees] manager_id $manager_id]
-		  </td>
-		</tr>
-		<tr>
-		  <td class=form-label>User</td>
-		  <td class=form-widget>
-		    [im_user_select -include_empty_p 1 user_id $user_id]
-		  </td>
-		</tr>
+[im_header]
+[im_navbar]
+<form>
+<table border=0 cellspacing=1 cellpadding=1>
+<tr>
+  <td class=form-label>Level of Details</td>
+  <td class=form-widget>
+[im_select -translate_p 0 level_of_detail $levels $level_of_detail]
+  </td>
+</tr>
+<tr>
+  <td class=form-label>Month</td>
+  <td class=form-widget>
+    <input type=textfield name=start_date value=$start_date>
+  </td>
+</tr>
+<tr>
+  <td class=form-label>Manager</td>
+  <td class=form-widget>
+[im_user_select -include_empty_p 1 -group_id [im_profile_employees] manager_id $manager_id]
+  </td>
+</tr>
+<tr>
+  <td class=form-label>User</td>
+  <td class=form-widget>
+[im_user_select -include_empty_p 1 user_id $user_id]
+  </td>
+</tr>
                 <tr>
                   <td class=form-label>Format</td>
                   <td class=form-widget>
-                    [im_report_output_format_select output_format "" $output_format]
+[im_report_output_format_select output_format "" $output_format]
                   </td>
                 </tr>
-		<tr>
-		  <td class=form-label></td>
-		  <td class=form-widget><input type=submit value=Submit></td>
-		</tr>
-		</table>
-	</form>
-	<table border=0 cellspacing=1 cellpadding=1>\n"
+<tr>
+  <td class=form-label></td>
+  <td class=form-widget><input type=submit value=Submit></td>
+</tr>
+</table>
+</form>
+<table border=0 cellspacing=1 cellpadding=1>\n"
     }
 }
 
@@ -505,11 +528,11 @@ db_foreach sql $sql {
 
     # Find out if these are absences. Then the company_id is 9999999
     if {$company_id == "9999999"} {
-	set company_name_pretty "\#colspan=8 <b>$company_name</b>"
-	set project_name_pretty "\#colspan=7 <b>$project_name</b>"
+	set company_name_pretty "\\#colspan=8 <b>$company_name</b>"
+	set project_name_pretty "\\#colspan=7 <b>$project_name</b>"
     } else {
-	set company_name_pretty "\#colspan=8 <b><a href=$company_url$company_id>$company_name</a></b>"
-	set project_name_pretty "\#colspan=7 <b><a href=$project_url$project_id>$project_name</a></b>"
+	set company_name_pretty "\\#colspan=8 <b><a href=$company_url$company_id>$company_name</a></b>"
+	set project_name_pretty "\\#colspan=7 <b><a href=$project_url$project_id>$project_name</a></b>"
     }
 
     if {$previous_user_id == ""} {
@@ -535,48 +558,48 @@ db_foreach sql $sql {
 	
 	set hours_diff [expr $hours_user_subtotal - $workable_hours]
 	im_report_render_row \
-	    -output_format $output_format \
+	        -output_format $output_format \
 	    -row [list "$previous_user_name" "" "" "" "" "" "" "" "<b>$hours_diff</b>"] \
-	    -row_class "rowodd" \
-	    -cell_class "rowodd"
+	        -row_class "rowodd" \
+	        -cell_class "rowodd"
 	
 	set compensation_hours [expr $hours_diff - $hours_user_no_assignment_subtotal]
 	im_report_render_row \
-	    -output_format $output_format \
+	        -output_format $output_format \
 	    -row [list "$previous_user_name" "" "" "" "" "" "" "" "<b>$compensation_hours</b>"] \
-	    -row_class "rowodd" \
-	    -cell_class "rowodd"
+	        -row_class "rowodd" \
+	        -cell_class "rowodd"
 	
 	set working_percent [format "%0.2f" [expr (($hours_user_subtotal - $hours_company_absence_subtotal) / $workable_hours)*100]]%
 	
 	im_report_render_row \
-	    -output_format $output_format \
+	        -output_format $output_format \
 	    -row [list "$previous_user_name" "" "" "" "" "" "" "" "<b>$working_percent</b>"] \
-	    -row_class "rowodd" \
-	    -cell_class "rowodd"
+	        -row_class "rowodd" \
+	        -cell_class "rowodd"
 	
     }    
     
     im_report_update_counters -counters $counters
     
     set last_value_list [im_report_render_header \
-			     -output_format $output_format \
-			     -group_def $report_def \
-			     -last_value_array_list $last_value_list \
-			     -level_of_detail $level_of_detail \
-			     -row_class $class \
-			     -cell_class $class
-			]
+			          -output_format $output_format \
+			          -group_def $report_def \
+			          -last_value_array_list $last_value_list \
+			          -level_of_detail $level_of_detail \
+			          -row_class $class \
+			          -cell_class $class
+			 ]
 
     
     set footer_array_list [im_report_render_footer \
-			       -output_format $output_format \
-			       -group_def $report_def \
-			       -last_value_array_list $last_value_list \
-			       -level_of_detail $level_of_detail \
-			       -row_class $class \
-			       -cell_class $class
-			  ]
+			              -output_format $output_format \
+			              -group_def $report_def \
+			              -last_value_array_list $last_value_list \
+			              -level_of_detail $level_of_detail \
+			              -row_class $class \
+			              -cell_class $class
+			   ]
 
 }
 
