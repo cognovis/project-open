8163e20901a882340f8e2cfc378378c438551959
Frank Bergmann <frank.bergmann@project-open.com>
2011-08-31 17:33:32 +0000

    - SQL Selectors:   Integrated into helpdesk

--- www/index.tcl
+++ www/index.tcl
@@ -172,6 +172,19 @@ if {$view_tickets_all_p} {
 lappend mine_p_options [list [lang::message::lookup "" intranet-helpdesk.My_queues "My Queues"] "queue"]
 lappend mine_p_options [list [lang::message::lookup "" intranet-helpdesk.Mine "Mine"] "mine"]
 
+# Add custom searches to drop-down
+if {[im_table_exists im_sql_selectors]} {
+    set selector_sql "
+	select	s.name, s.short_name
+	from	im_sql_selectors s
+	where	s.object_type = 'im_ticket'
+    "
+    db_foreach selectors $selector_sql {
+	lappend mine_p_options [list $name $short_name]
+    }
+}
+
+
 set ticket_member_options [util_memoize "db_list_of_lists ticket_members {
 	select  distinct
 		im_name_from_user_id(object_id_two) as user_name,
@@ -402,10 +415,18 @@ switch $mine_p {
                 )
 	)"
     }
-    "default" { ad_return_complaint 1 "Error:<br>Invalid variable mine_p = '$mine_p'" }
+    "default" { 
+	# The short name of a SQL selector
+	set selector_sql [db_string selector_sql "select selector_sql from im_sql_selectors where short_name = :mine_p" -default ""]
+	if {"" == $selector_sql} {
+	    ad_return_complaint 1 "Error:<br>Invalid variable mine_p = '$mine_p'" 
+	    ad_script_abort
+	}
+
+	lappend criteria "t.ticket_id in ($selector_sql)"
+    }
 }
 
-# ad_return_complaint 1 $order_by
 
 set order_by_clause "order by lower(t.ticket_id) DESC"
 switch [string tolower $order_by] {
