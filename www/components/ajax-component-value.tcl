436f61b919810ff48ac8c2bc26588b39a492ddb2
Frank Bergmann <frank.bergmann@project-open.com>
2011-09-07 14:26:26 +0000

    - Core:   Added a generic TCL for returning HTML portlet contents   for AJAX calls

---
+++ www/components/ajax-component-value.tcl
@@ -0,0 +1,51 @@
+ad_page_contract {
+    Return the value of a portlet for AJAX functions.
+    @author Frank Bergmann (frank.bergmann@project-open.com)
+    @author David Blanco (david.blanco@grupoversia.com)
+    @creation-date 06/09/2011
+    @cvs-id $Id$
+} {
+    {plugin_id:integer ""}
+    {plugin_name ""}
+    {package_name ""}
+}
+
+# -------------------------------------------------------------
+# Defaults & Security
+# -------------------------------------------------------------
+
+# Find out the portlet component if specified
+# by name and package
+if {"" == $plugin_id} {
+    set plugin_id [db_string portlet "
+	select	plugin_id
+	from	im_component_plugins
+	where	plugin_name = :plugin_name and
+		package_name = :package_name
+    " -default ""]
+}
+
+if {"" != $plugin_id} {
+    set result "<pre>
+<b>[lang::message::lookup "" intranet-core.Portlet_not_Specified "Portlet Not Specified"]</b>:
+[lang::message::lookup "" intranet-core.Portlet_not_Specified_msg "You need to specify either 'plugin_id' or 'plugin_name' and 'package_name'.]
+"
+    doc_return 200 "text/html" $result
+    ad_script_abort
+}
+
+
+set perm_p [im_object_permissions -object_id $plugin_id]
+if {!$perm_p} {
+    set result "<pre>[lang::message::lookup "" intranet-core.You_dont_have_permissions_to_access_this_portlet "
+    You don't have sufficient permissions to access this portlet"]"
+    doc_return 200 "text/html" $result
+    ad_script_abort
+}
+
+# -------------------------------------------------------------
+# Return the value
+# -------------------------------------------------------------
+
+set result [im_user_base_info_component -user_id $customer_id]
+doc_return 200 "text/html" $result
