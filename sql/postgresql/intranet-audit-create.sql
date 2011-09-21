5350cd4cc9250c88ad51ccd1e64e633c7e995342
Frank Bergmann <frank.bergmann@project-open.com>
2011-09-21 18:03:40 +0000

    - Audit:   Added a project status change matrix

--- sql/postgresql/intranet-audit-create.sql
+++ sql/postgresql/intranet-audit-create.sql
@@ -440,8 +440,8 @@ SELECT im_component_plugin__new (
 	null,					-- creation_user
 	null,					-- creation_ip
 	null,					-- context_id
-	'Daily Ticket Status Change',		-- plugin_name
-	'intranet-reporting-dashboard',		-- package_name
+	'24h Status Changes',			-- plugin_name
+	'intranet-audit',			-- package_name
 	'right',				-- location
 	'/intranet-helpdesk/index',		-- page_url
 	null,					-- view_name
@@ -472,5 +472,45 @@ SELECT im_component_plugin__new (
 	" -description "Shows the status changes of tickets in the last 24 hours.
 	" -status_list [db_list status_list "select distinct ticket_status_id from im_tickets order by ticket_status_id"]',
 	'lang::message::lookup "" intranet-reporting-dashboard.Daily_Ticket_Status_Change "Daily Ticket Status Change"'
+);
+
+
+-- Project Status Change Matrix
+
+SELECT im_component_plugin__new (
+	null,					-- plugin_id
+	'im_component_plugin',			-- object_type
+	now(),					-- creation_date
+	null,					-- creation_user
+	null,					-- creation_ip
+	null,					-- context_id
+	'30 Day Status Changes',		-- plugin_name
+	'intranet-audit',			-- package_name
+	'right',				-- location
+	'/intranet/projects/index',		-- page_url
+	null,					-- view_name
+	180,					-- sort_order
+	'im_dashboard_status_matrix -max_category_len 3 -sql "
+		select	count(*) as cnt,
+			old_status_id,
+			new_status_id
+		from	(select	parent.project_status_id as new_status_id,
+				max_audit_a.audit_object_status_id as old_status_id
+			from	im_projects parent
+				LEFT OUTER JOIN (
+					select	p.project_id,
+						max(a.audit_date) as max_audit_date
+					from	im_projects p
+						LEFT OUTER JOIN im_audits a ON (p.project_id = a.audit_object_id and a.audit_date < now() - '30 days'::interval)
+					where	p.parent_id is null
+					group by p.project_id, p.project_status_id
+				) max_audit_date ON (parent.project_id = max_audit_date.project_id)
+				LEFT OUTER JOIN im_audits max_audit_a ON (max_audit_a.audit_object_id = parent.project_id and max_audit_a.audit_date = max_audit_date.max_audit_date)
+			where	parent.parent_id is null
+			) t
+		group by old_status_id, new_status_id
+	" -description "Shows the status changes of projects in the last 30 days.
+	" -status_list [db_list status_list "select distinct project_status_id from im_projects order by project_status_id"]',
+	'lang::message::lookup "" intranet-reporting-dashboard.Monthly_Project_Status_Changes "30 Day Status Changes"'
 );
 
