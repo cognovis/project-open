0bb93096e3265114d7a067ca900e4369f028ff0d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-09-28 15:29:50 +0000

    - File Search:   Fixed important performance issue @ Projop

--- tcl/intranet-search-pg-files-procs.tcl
+++ tcl/intranet-search-pg-files-procs.tcl
@@ -460,10 +460,9 @@ ad_proc intranet_search_pg_files_search_indexer {
 	    set unindexed_projects_sql "
 		select	project_id 
 		from	im_projects
-		where	project_id not in (
-				select object_id 
-				from im_search_pg_file_biz_objects
-		)
+		EXCEPT
+		select	object_id
+		from	im_search_pg_file_biz_objects
 	    "
 	    db_foreach unindexed_projects $unindexed_projects_sql {
 		db_dml insert_project "
