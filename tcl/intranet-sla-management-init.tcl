f22ed7c9a15c41d277a92eb1eac13604682a26b6
Frank Bergmann <frank.bergmann@project-open.com>
2011-10-03 18:48:39 +0000

    - SLA Management:   Re-enabling resolution time calculation sweeper and   closed tickets sweeper

--- tcl/intranet-sla-management-init.tcl
+++ tcl/intranet-sla-management-init.tcl
@@ -14,9 +14,9 @@ nsv_set intranet_sla_management sweeper_p 0
 
 # Re-calculate the "solution time" of open tickets every minute
 
-# ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-sla-management -parameter SLASolutionTimeSweeperInterval -default 59] im_sla_ticket_solution_time_sweeper
+ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-sla-management -parameter SLASolutionTimeSweeperInterval -default 59] im_sla_ticket_solution_time_sweeper
 
 
 # Close all tickets that are in status "resolved" for more then a certain period
-# ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-sla-management -parameter CloseResolvedTicketsSweeperInterval -default 179] im_sla_ticket_close_resolved_tickets_sweeper
+ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-sla-management -parameter CloseResolvedTicketsSweeperInterval -default 179] im_sla_ticket_close_resolved_tickets_sweeper
 
