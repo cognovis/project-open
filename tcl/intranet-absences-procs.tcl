4a1a41baa0b869d14e9e38a2ab551087087b936d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-10-19 16:18:20 +0000

    -- Allowed custom colors for absences, to be defined in categories -> aux_string1

--- tcl/intranet-absences-procs.tcl
+++ tcl/intranet-absences-procs.tcl
@@ -272,15 +272,8 @@ ad_proc im_absence_new_page_wf_perm_delete_button {
 ad_proc im_absence_cube_color_list { } {
     Returns the list of colors for the various types of absences
 } {
-    set color_list {	
-	F00000
-	F08000
-	0000F0
-	9900F0
-	F0F000
-	808080
-    }
 
+    # define default color set 
     set color_list {
         EC9559
         E2849B
@@ -290,6 +283,19 @@ ad_proc im_absence_cube_color_list { } {
         CCCCC9
     }
 
+    # Overwrite in case there's a custom color defined 
+    set col_sql "
+        select category_id, aux_string1
+        from    im_categories
+        where   category_type = 'Intranet Absence Type'
+        order by category_id
+    "
+    set ctr 0 
+    db_foreach cols $col_sql {
+	if { "" != $aux_string1 } {
+	    lset color_list $ctr $aux_string1
+	}
+    }
     return $color_list
 }
 
