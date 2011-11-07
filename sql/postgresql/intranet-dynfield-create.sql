060fd48a73b1ed35ed93c0e0e95561531b225676
Frank Bergmann <frank.bergmann@project-open.com>
2011-11-07 21:45:58 +0000

    - Riskmanagement:   Added im_risk to the DynField objects

--- sql/postgresql/intranet-dynfield-create.sql
+++ sql/postgresql/intranet-dynfield-create.sql
@@ -1004,6 +1004,28 @@ SELECT im_menu__new (
 	null,							-- creation_ip
 	null,							-- context_id
 	'intranet-dynfield',					-- package_name
+	'dynfield_otype_risk',					-- label
+	'Risk',							-- name
+	'/intranet-dynfield/object-type?object_type=im_risk',	-- url
+	175,							-- sort_order
+	(select menu_id from im_menus where label = 'dynfield_otype'),	-- parent_menu_id
+	null							-- p_visible_tcl
+);
+
+SELECT acs_permission__grant_permission(
+	(select menu_id from im_menus where label = 'dynfield_otype_risk'),
+	(select group_id from groups where group_name='Employees'), 
+	'read'
+);
+
+SELECT im_menu__new (
+	null,							-- p_menu_id
+	'im_menu',						-- object_type
+	now(),							-- creation_date
+	null,							-- creation_user
+	null,							-- creation_ip
+	null,							-- context_id
+	'intranet-dynfield',					-- package_name
 	'dynfield_otype_ticket',				-- label
 	'Ticket',						-- name
 	'/intranet-dynfield/object-type?object_type=im_ticket',	-- url
--- www/object-types.tcl
+++ www/object-types.tcl
@@ -77,7 +77,7 @@ db_multirow -extend { object_attributes_url } object_types select_object_types {
 			'im_freelance_rfq', 'im_freelance_rfq_answer',
 			'im_conf_item', 'im_cost_center', 'im_note', 'im_user_absence',
 			'im_invoice', 'im_expense', 'im_expense_bundle',
-			'im_material', 'im_timesheet_task',
+			'im_material', 'im_risk', 'im_timesheet_task',
 			'im_sla_parameter'
 		)
 	order by
