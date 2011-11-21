1e22c38afcae462367fe8367e0562026294d20de
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-11-21 12:15:31 +0000

    -- Added callback

--- tcl/intranet-timesheet2-init.tcl
+++ tcl/intranet-timesheet2-init.tcl
@@ -14,3 +14,17 @@ nsv_set intranet_timesheet2 timesheet_synchronizer_p 0
 
 # Check for imports of external im_hours entries every every X minutes
 ad_schedule_proc -thread t [parameter::get_from_package_key -package_key intranet-timesheet2 -parameter SyncHoursInterval -default 59 ] im_timesheet2_sync_timesheet_costs
+
+# Callbacks 
+ad_proc -public -callback absence_on_change {
+    {-absence_id:required}
+    {-absence_type_id:required}
+    {-user_id:required}
+    {-start_date:required}
+    {-end_date:required}
+    {-duration_days:required}
+    {-transaction_type:required}
+} {
+    Callback to be executed after an absence has been created
+} -
+
