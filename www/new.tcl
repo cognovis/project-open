de5fc7eb2d758eba97ac8cce90feaa849320c0fd
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-11-23 16:21:26 +0000

    -- moved from ad_form to to template::form to support dynfields

--- www/new.tcl
+++ www/new.tcl
@@ -33,7 +33,26 @@ ad_page_contract {
     { termination_reason "" }
     { referred_by "0" }
     { vacation_balance:float "0" }
-    { vacation_days_per_year:float "0" }
+    { start_date "" }
+    { end_date "" }
+	{ supervisor_id "" }
+	{ department_id "" }
+	{ dependant_p "" }
+	{ only_job_p "" }
+	{ married_p "" }
+	{ dependants ""}
+	{ head_of_household_p "" }
+	{ skills "" }
+	{ first_experience "" }
+	{ employee_status_id "" }
+	{ voluntary_termination_p "" }
+	{ signed_nda_p "" }
+	{ experience_id "" }
+	{ source_id "" }
+	{ original_job_id "" }
+	{ current_job_id "" }
+	{ qualification_id "" }
+	{ vacation_days_per_year "" }
 }
 
 # ------------------------------------------------------------------
@@ -101,29 +120,29 @@ set rep_cost_ids [db_list rep_costs_exist "
 
 if {[llength $rep_cost_ids] == 0} {
     if [catch {
-	set rep_cost_id [im_cost::new -object_type "im_repeating_cost" -cost_name $employee_id -cost_type_id [im_cost_type_repeating]]
+		set rep_cost_id [im_cost::new -object_type "im_repeating_cost" -cost_name $employee_id -cost_type_id [im_cost_type_repeating]]
 	
-	db_dml update_costs "
-		update im_costs set
-			cause_object_id = :employee_id
-		where
-			cost_id = :rep_cost_id
-	"
+		db_dml update_costs "
+			update im_costs set
+				cause_object_id = :employee_id
+			where
+				cost_id = :rep_cost_id
+		"
 
-	db_dml insert_repeating_costs "
-		insert into im_repeating_costs (
-			rep_cost_id,
-			start_date,
-			end_date
-		) values (
-			:rep_cost_id,
-			to_date(:today,:date_format),
-			to_date(:today,:date_format)
-		)
+		db_dml insert_repeating_costs "
+			insert into im_repeating_costs (
+				rep_cost_id,
+				start_date,
+				end_date
+			) values (
+				:rep_cost_id,
+				to_date(:today,:date_format),
+				to_date(:today,:date_format)
+			)
 	    "
     } err_msg] {
-	ad_return_complaint 1 "<li>[_ intranet-hr.lt_Error_creating_a_new_]<br>
-	<pre>$err_msg</pre>"
+		ad_return_complaint 1 "<li>[_ intranet-hr.lt_Error_creating_a_new_]<br>
+		<pre>$err_msg</pre>"
     }
 }
 
@@ -142,8 +161,7 @@ set supervisor_options [im_employee_options 1]
 set salary_interval_options {{Month month} {Day day} {Week week} {Year year}}
 
 set employee_status_options [db_list_of_lists employee_status_options "
-select state, state_id
-from im_employee_pipeline_states
+	select state, state_id from im_employee_pipeline_states
 "]
 
 
@@ -170,107 +188,85 @@ set end_date_label "[_ intranet-hr.End_date]"
 set voluntary_termination_p_label "[_ intranet-hr.lt_Voluntary_Termination]"
 set termination_reason_label "[_ intranet-hr.Termination_Reason]"
 set signed_nda_p_label "[_ intranet-hr.NDA_Signed]"
+set label_yes [lang::message::lookup "" intranet-core.Yes "Yes"]
+set label_no [lang::message::lookup "" intranet-core.No "No"]
 
 set vacation_days_per_year_label [lang::message::lookup "" intranet-hr.Vacation_Days_Per_Year "Vacation Days per Year"]
 set vacation_balance_label [lang::message::lookup "" intranet-hr.Vacation_Balance "Vacation Balance"]
 
-ad_form \
-    -name cost \
-    -cancel_url $return_url \
-    -action $action_url \
-    -mode $form_mode \
-    -export {next_url user_id return_url} \
-    -form {
-	employee_id:key
-	{department_id:text(select) {label $department_label} {options $department_options} }
-	{supervisor_id:text(select),optional {label $supervisor_label} {options $supervisor_options} }
-	{availability:text(text) {label $availability_label} {html {size 6}} }
-	{hourly_cost:text(text),optional {label $hourly_cost_label} {html {size 10}} }
-	{employee_status_id:text(select) {label $employee_status_label} {options $employee_status_options} }
-	{personnel_number:text(text),optional {label $personnel_number_label} {html {size 10}} }
-	{ss_number:text(text),optional {label $ss_number_label} {html {size 20}} }
-	{salary:text(text),optional {label $salary_label} {html {size 10}} }
-        {social_security:text(text),optional {label $social_security_label} {html {size 10}} }
-	{insurance:text(text),optional {label $insurance_label} {html {size 10}} }
-	{other_costs:text(text),optional {label $other_cost_label} {html {size 10}} }
-	{salary_payments_per_year:text(text),optional {label $salary_payments_per_year_label} {html {size 10}} }
 
-	{birthdate:text(text),optional {label $birthdate_label} {html {size 10}} }
-	{job_title:text(text),optional {label $job_title_label} {html {size 30}} }
-	{job_description:text(textarea),nospell,optional {label $job_description_label} {html {rows 5 cols 40}}}
+# -- ------------------------------------------------
+# -- New form 
+# -- ------------------------------------------------
 
-	{start_date:text(text),optional {label $start_date_label} {html {size 10}} }
-	{end_date:text(text),optional {label $end_date_label} {html {size 10}} }
+set form_id "employee_information"
 
-	{voluntary_termination_p:text(radio),optional {label $voluntary_termination_p_label} {options $voluntary_termination_options} }
-	{termination_reason:text(textarea),nospell,optional {label $termination_reason_label} {html {rows 5 cols 40}}}
-	{signed_nda_p:text(radio),optional {label $signed_nda_p_label} {options $voluntary_termination_options} }
-	{dependant_p:text(hidden),optional }
-	{only_job_p:text(hidden),optional }
-	{married_p:text(hidden),optional }
-	{dependants:text(hidden),optional }
-	{head_of_household_p:text(hidden),optional }
-	{skills:text(hidden),optional }
-	{first_experience:text(hidden),optional }
-	{years_experience:text(hidden),optional }
-	{referred_by:text(hidden),optional }
-	{experience_id:text(hidden),optional }
-	{source_id:text(hidden),optional }
-	{original_job_id:text(hidden),optional }
-	{current_job_id:text(hidden),optional }
-	{qualification_id:text(hidden),optional }
-	{vacation_days_per_year:text(text),optional {label $vacation_days_per_year_label} {html {size 5}} }
-	{vacation_balance:text(text),optional {label $vacation_balance_label} {html {size 5}} }
+template::form::create $form_id
+template::form::section $form_id ""
+template::element::create $form_id department_id -label $department_label -widget "select"  -options $department_options
+template::element::create $form_id supervisor_id -label $supervisor_label -widget "select"  -options $supervisor_options
+
+template::element::create $form_id availability -optional -label $availability_label -html {size 6}
+template::element::create $form_id hourly_cost -optional -label $hourly_cost_label -html {size 10} -datatype float
+template::element::create $form_id employee_status_id -label $employee_status_label -widget "select"  -options $employee_status_options
+template::element::create $form_id personnel_number -optional -label $personnel_number_label -html {size 10}
+template::element::create $form_id ss_number -optional -label $ss_number_label -html {size 20}
+template::element::create $form_id salary -optional -label $salary_label -html {size 10} -datatype float
+template::element::create $form_id social_security -optional -label $social_security_label -html {size 10} -datatype float
+template::element::create $form_id insurance -optional -label $insurance_label -html {size 10} -datatype float
+template::element::create $form_id other_costs -optional -label $other_cost_label -html {size 10} -datatype float
+template::element::create $form_id salary_payments_per_year -optional -label $salary_payments_per_year_label -html {size 10}
+template::element::create $form_id birthdate -optional -label $birthdate_label -html {size 10} -datatype date
+template::element::create $form_id job_title -optional -label $job_title_label -html {size 30} -datatype text
+template::element::create $form_id job_description -optional -datatype text -widget textarea -label $job_description_label -html {rows 5 cols 40}
+template::element::create $form_id start_date -optional -label $start_date_label -html {size 10} -datatype date
+template::element::create $form_id end_date -optional -label $end_date_label -html {size 10} -datatype date
+template::element::create $form_id voluntary_termination_p -label $voluntary_termination_p_label -widget "select"  -options $voluntary_termination_options -datatype text
+template::element::create $form_id termination_reason -optional -datatype text -widget textarea -label $termination_reason_label -html {rows 5 cols 40}
+template::element::create $form_id signed_nda_p -optional -datatype text -widget radio -label $signed_nda_p_label -options {{Yes t} {No f}}
+template::element::create $form_id dependant_p -optional -widget "hidden"
+template::element::create $form_id only_job_p -optional -widget "hidden"
+template::element::create $form_id married_p -optional -widget "hidden"
+template::element::create $form_id dependants -optional -widget "hidden"
+template::element::create $form_id head_of_household_p -optional -widget "hidden"
+template::element::create $form_id skills -optional -widget "hidden"
+template::element::create $form_id first_experience -optional -widget "hidden"
+template::element::create $form_id years_experience -optional -widget "hidden"
+template::element::create $form_id referred_by -optional -widget "hidden"
+template::element::create $form_id experience_id -optional -widget "hidden"
+template::element::create $form_id employee_id -optional -widget "hidden"
+template::element::create $form_id return_url -optional -widget "hidden"
+
+set field_cnt [im_dynfield::append_attributes_to_form \
+    -object_subtype_id "" \
+    -object_type "person" \
+    -form_id $form_id \
+    -object_id $employee_id \
+]
+
+set n_error 0
+
+if {[form is_submission $form_id]} {
+
+	# Form validation 
+# 	if { [catch { set birthdate_date_ansi [clock format [clock scan $birthdate] -format %Y-%m-%d] } ""] } {
+# 	    incr n_error
+# 		template::element::set_error $form_id birthdate "Bad date. Please use 'YYYY-MM-DD' to format the date."
+# 	}
+
+# 	if { [catch { set start_date_ansi [clock format [clock scan $start_date] -format %Y-%m-%d] } ""] } {
+# 	    incr n_error
+# 		template::element::set_error $form_id start_date "Bad date. Please use 'YYYY-MM-DD' to format the date."
+# 	}
+
+# 	if { [catch { set end_date_ansi [clock format [clock scan $end_date] -format %Y-%m-%d] } ""] } {
+# 	    incr n_error
+# 		template::element::set_error $form_id end_date "Bad date. Please use 'YYYY-MM-DD' to format the date."
+# 	}
+
+    if {$n_error > 0} {
+	    return
     }
-
-
-ad_form -extend -name cost -on_request {
-    # Populate elements from local variables
-
-} -select_query {
-
-	select	
-		e.*,
-                CASE	WHEN rc.start_date is null
-                        THEN to_date(:today,:date_format)
-                        ELSE to_date(to_char(rc.start_date,:date_format),:date_format)
-                END as start_date,
-                CASE	WHEN rc.end_date is null
-                        THEN to_date(:end_century,:date_format)
-                        ELSE to_date(to_char(rc.end_date,:date_format),:date_format)
-                END as end_date,
-		ci.*,
-		to_char(e.birthdate,:date_format) as birthdate
-	from	parties p,
-		im_employees e,
-		im_repeating_costs rc,
-		im_costs ci
-	where	
-		p.party_id = :employee_id
-		and p.party_id = e.employee_id
-		and p.party_id = ci.cause_object_id
-		and ci.cost_id = rc.rep_cost_id
-
-
-
-} -validate {
-
-    {birthdate
-	{ "" == $birthdate || [regexp {^....\-..\-..$} $birthdate] }
-	"Bad date. Please use 'YYYY-MM-DD' to format the date."
-    }
-
-    {end_date
-	{ "" == $end_date || [regexp {^....\-..\-..$} $end_date] }
-	"Bad date. Please use 'YYYY-MM-DD' to format the date."
-    }
-
-    {start_date
-	{ "" == $start_date || [regexp {^....\-..\-..$} $start_date] }
-	"Bad date. Please use 'YYYY-MM-DD' to format the date."
-    }
-
-} -after_submit {
 
     set cost_name $employee_name
     if {"" == $start_date} { set start_date [db_string now "select now()::date"]}
@@ -290,24 +286,24 @@ ad_form -extend -name cost -on_request {
     " -default 0]
 
     if {!$rep_cost_id} {
-	if [catch {
-	    set rep_cost_id [im_cost::new -cost_name $cost_name -cost_type_id [im_cost_type_repeating]]
-	    db_dml insert_repeating_costs "
-		insert into im_repeating_costs (
-			rep_cost_id,
-			start_date,
-			end_date
-		) values (
-			:rep_cost_id,
-			:start_date,
-			:end_date
-		)
-	    "
-	} err_msg] {
-	    ad_return_complaint 1 "<li>Error creating a new repeating cost 
-	    item for employee \#$employee_id:<br>
-	    <pre>$err_msg</pre>"
-	}
+		if [catch {
+		    set rep_cost_id [im_cost::new -cost_name $cost_name -cost_type_id [im_cost_type_repeating]]
+	    	db_dml insert_repeating_costs "
+			insert into im_repeating_costs (
+				rep_cost_id,
+				start_date,
+				end_date
+			) values (
+				:rep_cost_id,
+				:start_date,
+				:end_date
+			)
+		    "
+		} err_msg] {
+	    	ad_return_complaint 1 "<li>Error creating a new repeating cost 
+		    item for employee \#$employee_id:<br>
+		    <pre>$err_msg</pre>"
+		}
     }
 
     # Check if the im_employees entry already exists for the user
@@ -339,8 +335,8 @@ ad_form -extend -name cost -on_request {
     if {![exists_and_not_null vat]} { set vat 0 }
 
     if {[info exists supervisor_id] && $supervisor_id == $employee_id} {
-	ad_return_complaint 1 "[_ intranet-hr.Employee_Own_Supervisor]"
-	return
+		ad_return_complaint 1 "[_ intranet-hr.Employee_Own_Supervisor]"
+		return
     }
 
     db_dml employee_update "
@@ -385,42 +381,112 @@ ad_form -extend -name cost -on_request {
 		employee_id = :employee_id
 "
 
-    if [catch { 
-	db_dml update_costs "
-	update im_costs set
-		cost_name = :employee_name,
-		cost_center_id = :department_id,
-		customer_id = :internal_id,
-		provider_id = :internal_id,
-		cost_type_id = [im_cost_type_employee],
-		cost_status_id = [im_cost_status_created],
-		cause_object_id = :employee_id,
-		amount = (
-			cast(:salary as float) + 
-			cast(:social_security as float) + 
-			cast(:insurance as float) + 
-			cast(:other_costs as float)
-			) * :salary_payments_per_year / 12,
-		currency = :currency,
-		tax = :tax,
-		vat = :vat
-	where
-		cost_id	= :rep_cost_id
-	"
+   ns_log Notice "Dynfield: /intranet-hr/new: im_dynfield::attribute_store -object_type person -object_id $employee_id -form_id $form_id"
+		im_dynfield::attribute_store \
+			-object_type "person" \
+			-object_id $employee_id \
+			-form_id $form_id
 
-	db_dml insert_repeating_costs "
-	update im_repeating_costs set
-		start_date = :start_date,
-		end_date = :end_date
-	where
-		rep_cost_id = :rep_cost_id
-	"
+    if [catch { 
+		db_dml update_costs "
+			update im_costs set
+				cost_name = :employee_name,
+				cost_center_id = :department_id,
+				customer_id = :internal_id,
+				provider_id = :internal_id,
+				cost_type_id = [im_cost_type_employee],
+				cost_status_id = [im_cost_status_created],
+				cause_object_id = :employee_id,
+				amount = (
+					cast(:salary as float) + 
+					cast(:social_security as float) + 
+					cast(:insurance as float) + 
+					cast(:other_costs as float)
+					) * :salary_payments_per_year / 12,
+				currency = :currency,
+				tax = :tax,
+				vat = :vat
+			where
+				cost_id	= :rep_cost_id
+			"
+
+		db_dml insert_repeating_costs "
+			update im_repeating_costs set
+				start_date = :start_date,
+				end_date = :end_date
+			where
+				rep_cost_id = :rep_cost_id
+		"
+
 	 } err_msg] {
 	    ad_return_complaint 1 "<li>[_ intranet-hr.lt_Error_inserting_emplo]<BR>
             <pre>$err_msg</pre>"
-	}
+	 }
 
 	ad_returnredirect $return_url
 	ad_script_abort
-}
+} 
 
+if { [form is_request $form_id] } {
+
+	db_1row projects_info_query {
+    	select
+        	e.*,
+            CASE    WHEN rc.start_date is null
+                    THEN to_date(:today,:date_format)
+                    ELSE to_date(to_char(rc.start_date,:date_format),:date_format)
+            END as start_date,
+            CASE    WHEN rc.end_date is null
+                        THEN to_date(:end_century,:date_format)
+                        ELSE to_date(to_char(rc.end_date,:date_format),:date_format)
+            END as end_date,
+	        ci.*,
+    	    to_char(e.birthdate,:date_format) as birthdate
+    	from    
+			parties p,
+        	im_employees e,
+	        im_repeating_costs rc,
+    	    im_costs ci
+	    where
+	        p.party_id = :employee_id
+    	    and p.party_id = e.employee_id
+        	and p.party_id = ci.cause_object_id
+	        and ci.cost_id = rc.rep_cost_id
+	}
+
+	template::element::set_value $form_id department_id $department_id
+	template::element::set_value $form_id supervisor_id $supervisor_id
+	template::element::set_value $form_id availability $availability
+	template::element::set_value $form_id hourly_cost $hourly_cost
+	template::element::set_value $form_id employee_status_id $employee_status_id
+	template::element::set_value $form_id personnel_number $personnel_number
+	template::element::set_value $form_id ss_number $ss_number
+	template::element::set_value $form_id salary $salary
+	template::element::set_value $form_id social_security $social_security
+	template::element::set_value $form_id insurance $insurance
+	template::element::set_value $form_id other_costs $other_costs
+	template::element::set_value $form_id salary_payments_per_year $salary_payments_per_year
+	template::element::set_value $form_id birthdate $birthdate
+	template::element::set_value $form_id job_title $job_title
+	template::element::set_value $form_id job_description $job_description
+	template::element::set_value $form_id start_date $start_date
+	template::element::set_value $form_id end_date $end_date
+	template::element::set_value $form_id voluntary_termination_p $voluntary_termination_p
+	template::element::set_value $form_id termination_reason $termination_reason
+	template::element::set_value $form_id signed_nda_p $signed_nda_p
+	template::element::set_value $form_id dependant_p $dependant_p
+	template::element::set_value $form_id only_job_p $only_job_p
+	template::element::set_value $form_id married_p $married_p
+	template::element::set_value $form_id dependants $dependants
+	template::element::set_value $form_id head_of_household_p $head_of_household_p
+	template::element::set_value $form_id skills $skills
+	template::element::set_value $form_id first_experience $first_experience
+	template::element::set_value $form_id years_experience $years_experience
+	template::element::set_value $form_id referred_by $referred_by
+	template::element::set_value $form_id experience_id $experience_id
+	template::element::set_value $form_id employee_id $employee_id
+	template::element::set_value $form_id return_url "/intranet/users/view?user_id=$employee_id"
+}
+	
+if {[form is_valid $form_id]} {
+}
