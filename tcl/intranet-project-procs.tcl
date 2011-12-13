125171d8e287d6923975cc388621da6d26f506bc
Frank Bergmann <frank.bergmann@project-open.com>
2011-12-13 19:55:49 +0000

    - Fixed audit action

--- tcl/intranet-project-procs.tcl
+++ tcl/intranet-project-procs.tcl
@@ -337,7 +337,7 @@ namespace eval project {
         set project_id [db_exec_plsql create_new_project $sql]
 
 	# Write Audit Trail
-	# im_project_audit -action create -project_id $project_id
+	# im_project_audit -action after_create -project_id $project_id
 
         return $project_id
     }
