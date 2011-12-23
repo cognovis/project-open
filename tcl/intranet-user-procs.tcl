bfd20689c2910ec7474c3e4da32cfc48ef4fb084
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2011-12-23 17:00:27 +0000

    -- implemented name order

--- tcl/intranet-user-procs.tcl
+++ tcl/intranet-user-procs.tcl
@@ -279,18 +279,19 @@ ad_proc im_user_select {
 ad_proc im_employee_select_multiple { select_name { defaults "" } { size "6"} {multiple ""}} {
     set bind_vars [ns_set create]
     set employee_group_id [im_employee_group_id]
+    set name_order [parameter::get -package_id [apm_package_id_from_key intranet-core] -parameter "NameOrder" -default 1]
     set sql "
-select
-	u.user_id,
-	im_name_from_user_id(u.user_id) as employee_name
-from
-	registered_users u,
-	group_distinct_member_map gm
-where
-	u.user_id = gm.member_id
-	and gm.group_id = $employee_group_id
-order by lower(im_name_from_user_id(u.user_id))
-"
+	select
+		u.user_id,
+		im_name_from_user_id(u.user_id, $name_order) as employee_name
+	from
+		registered_users u,
+		group_distinct_member_map gm
+	where
+		u.user_id = gm.member_id
+		and gm.group_id = $employee_group_id
+	order by lower(im_name_from_user_id(u.user_id, $name_order))
+    "
     return [im_selection_to_list_box -translate_p "0" $bind_vars category_select $sql $select_name $defaults $size $multiple]
 }    
 
