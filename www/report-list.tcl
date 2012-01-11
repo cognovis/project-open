547b0a62caf58adecf2468608f93220aaa2b428c
Frank Bergmann <frank.bergmann@project-open.com>
2012-01-11 18:27:22 +0000

    - OpenOffice Reporting:   Added an index of the available reports

--- www/report-list.tcl
+++ www/report-list.tcl
@@ -17,12 +17,92 @@ ad_page_contract {
 # ---------------------------------------------------------------
 
 set current_user_id [ad_maybe_redirect_for_registration]
-
-# ---------------------------------------------------------------
-# Title
-# ---------------------------------------------------------------
-
 set page_title [lang::message::lookup "" intranet-reporting-openoffice.Report_List "Report List"]
 set context_bar [im_context_bar $page_title]
 
+set find_cmd [im_filestorage_find_cmd]
 
+# Determine the template
+set pageroot [ns_info pageroot]
+set serverroot [join [lrange [split $pageroot "/"] 0 end-1] "/"]
+
+# Package template path
+set template_path "$serverroot/filestorage/templates"
+set template_oo_path "$serverroot/packages/intranet-reporting-openoffice"
+
+set base_url "/intranet-reporting-openoffice"
+
+# ---------------------------------------------------------------
+# Check for a TCL file in the template directories
+# ---------------------------------------------------------------
+
+
+template::multirow create reports report_file_name url
+
+set pathes [list \
+		"$template_path/" \
+		"$template_oo_path/templates/" \
+		"$template_oo_path/www/" \
+	       ]
+
+foreach path $pathes {
+    set files [exec $find_cmd $path -noleaf -type f]
+    foreach file $files {
+
+	set file_name [lindex [split $file "/"] end]
+	set file_ext [lindex [split $file_name "."] end]
+	set file_body [lrange [split $file_name "."] 0 end-1]
+
+	switch $file_body {
+	    "report-list" { continue }
+	    "test-gantt" { continue }
+	    "test-list" { continue }
+	    "project-oneslide" { continue }
+	    default {
+		set hash($file_name) $file_name
+	    }
+	}
+    }
+}
+
+foreach file_name [array names hash] {
+    set file_ext [lindex [split $file_name "."] end]
+    set file_body [lrange [split $file_name "."] 0 end-1]
+    
+    switch $file_ext {
+	odp {
+	    # Check if there is a .tcl version of this file
+	    set tcl_file_name "$file_body.tcl"
+	    if {[info exists hash($tcl_file_name)]} { continue }
+	    template::multirow append reports $file_name "$base_url/$file_name"
+	}
+	tcl {
+	    template::multirow append reports $file_name "$base_url/$file_body"
+	}
+	default {
+	    # template::multirow append reports $file_name "$base_url/$file_body"
+	}
+    }
+    
+}
+
+template::multirow sort reports report_file_name
+
+list::create \
+    -name reports \
+    -multirow reports \
+    -key report_file_name \
+    -row_pretty_plural "[_ intranet-dynfield.Unmapped_Attributes]" \
+    -selected_format "normal" \
+    -class "list" \
+    -main_class "list" \
+    -sub_class "narrow" \
+    -pass_properties {
+    } -actions {
+    } -elements {
+        report_file_name {
+            label "[lang::message::lookup {} intranet-reporting-openoffice.Report_Filename {Report Name}]"
+            display_col report_file_name
+	    link_url_eval $url
+        }
+    }
