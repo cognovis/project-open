1414a512633a0e065e26afa86d59c64a67d67b37
Frank Bergmann <frank.bergmann@project-open.com>
2012-01-24 11:19:58 +0000

    - fixed upgrade script

--- sql/postgresql/upgrade/upgrade-4.0.3.0.1-4.0.3.0.2.sql
+++ sql/postgresql/upgrade/upgrade-4.0.3.0.1-4.0.3.0.2.sql
@@ -5,11 +5,11 @@ SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-4.
 -- Deal with PostgreSQL 8.4 tighter casting rules
 --
 CREATE OR REPLACE FUNCTION last_day(date)
-RETURNS date AS '
+RETURNS date AS $body$
 DECLARE
         p_date_in alias for $1;         -- date_id
        	v_date_out      date;
 begin		
        	select to_date(date_trunc('month',add_months(p_date_in,1))::text, 'YYYY-MM-DD'::text) - 1 into v_date_out;
        	return v_date_out;
-end;' LANGUAGE 'plpgsql';
+end;$body$ LANGUAGE 'plpgsql';
