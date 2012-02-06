e2a4d854c848a3ddf94c49f492ab56d21062f93c
Frank Bergmann <frank.bergmann@project-open.com>
2012-02-06 16:02:11 +0000

    - User List Page:   Not exporting the user_group_name to other pages of the submenu

--- www/users/index.tcl
+++ www/users/index.tcl
@@ -645,7 +645,7 @@ if {"" != $admin_html_links && [im_table_exists spam_messages]} {
 # 10. Join all parts together
 # ---------------------------------------------------------------
 
-set sub_navbar [im_user_navbar $letter "/intranet/users/index" $next_page_url $previous_page_url [list user_group_name] $menu_select_label]
+set sub_navbar [im_user_navbar $letter "/intranet/users/index" $next_page_url $previous_page_url [list] $menu_select_label]
 
 set left_navbar_html "
       <div class='filter-block'>
