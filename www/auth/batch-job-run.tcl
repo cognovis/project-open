4e6b5e8c0202d9aef7c3956ebcca3cbb016b31e1
Frank Bergmann <frank.bergmann@project-open.com>
2012-02-10 19:49:40 +0000

    - V4.0.3.0.0:   Updated to OpenACS 5.7

--- www/auth/batch-job-run.tcl
+++ www/auth/batch-job-run.tcl
@@ -91,4 +91,48 @@ db_dml insert_batch_results "
 
 # Fraber 110913:
 # set job_id [auth::authority::batch_sync -authority_id $authority_id]
-set job_url [export_vars -base batch-job { job_id }]
+# set job_url [export_vars -base batch-job { job_id }]
+ 
+set job_url ""
+
+
+# --------------------------------------------------------------
+# Get the paremeters for the autority
+# --------------------------------------------------------------
+
+auth::authority::get -authority_id $authority_id -array authority
+
+# Get the implementation id and implementation pretty name
+array set parameters [list]
+array set parameter_values [list]
+
+# Each element is a list of impl_ids which have this parameter
+array set param_impls [list]
+
+foreach element_name [auth::authority::get_sc_impl_columns] {
+    set name_column $element_name
+    regsub {^.*(_id)$} $element_name {_name} name_column
+    
+    set impl_params [auth::driver::get_parameters -impl_id $authority($element_name)]
+    
+    foreach { param_name dummy } $impl_params {
+        lappend param_impls($param_name) $authority($element_name)
+    }
+    
+    array set parameters $impl_params
+    
+    array set parameter_values [auth::driver::get_parameter_values \
+                                    -authority_id $authority_id \
+				    -impl_id $authority($element_name)]
+    
+}
+
+# --------------------------------------------------------------
+# Import users 
+# --------------------------------------------------------------
+
+set result [auth::ldap::batch_import::import_users [array get parameter_values] $authority_id]
+
+# set job_id [auth::authority::batch_sync -authority_id $authority_id]
+
+# set job_url [export_vars -base batch-job { job_id }]
