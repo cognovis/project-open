31189cac77293f70982157bdd8a0e0ba9c93f9a3
Frank Bergmann <frank.bergmann@project-open.com>
2012-02-24 12:05:32 +0000

    - DW-Light Malte:   Added dereference values to CSV output

--- tcl/intranet-dw-light-procs.tcl
+++ tcl/intranet-dw-light-procs.tcl
@@ -154,7 +154,7 @@ ad_proc im_companies_csv1 {
     set lol [im_dynfield::append_attributes_to_im_view -object_type "im_company"]
     set column_headers [concat $column_headers [lindex $lol 0]]
     set column_vars [concat $column_vars [lindex $lol 1]]
-
+    set derefs [lindex $lol 2]
 
     # ---------------------------------------------------------------
     # Let's generate the sql query
@@ -195,6 +195,7 @@ ad_proc im_companies_csv1 {
     set sql "
 	select
 		c.*,
+		[join $derefs "\n"]
 		c.note as company_note,
 		o.*,
 		c.primary_contact_id as company_contact_id,
