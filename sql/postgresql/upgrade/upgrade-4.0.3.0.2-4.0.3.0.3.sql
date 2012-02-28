fd389fb1a8ec5a3936a1b07218c9e6ea9e402e4e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2012-02-28 11:41:51 +0000

    -- added missing log update

--- sql/postgresql/upgrade/upgrade-4.0.3.0.2-4.0.3.0.3.sql
+++ sql/postgresql/upgrade/upgrade-4.0.3.0.2-4.0.3.0.3.sql
@@ -1,7 +1,6 @@
+-- upgrade-4.0.3.0.2-4.0.3.0.3.sql
 
-
-
-
+SELECT acs_log__debug('/packages/intranet-planning/sql/postgresql/upgrade/upgrade-4.0.3.0.2-4.0.3.0.3.sql','');
 
 -- Sequence to create fake object_ids for im_planning_items
 create or replace function inline_0 ()
