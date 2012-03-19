d132703b132d91c3b6894b13f7f7fcd7d824ffd1
Malte Sussdorff <malte.sussdorff@cognovis.de>
2012-03-19 14:18:04 +0100

    Removed validation

--- packages/intranet-core/www/projects/new.tcl
+++ packages/intranet-core/www/projects/new.tcl
@@ -231,41 +231,6 @@ ad_form -extend -name $form_id -new_request {
     set context_bar [im_context_bar [list /intranet/projects/ "[_ intranet-core.Projects]"] [list "/intranet/projects/view?[export_url_vars project_id]" "One project"] $page_title]
         
     set button_text "[_ intranet-core.Save_Changes]"
-} -validate { 
-    
-    {project_nr
-        {![var_contains_quotes $project_nr]}
-        {[_ intranet-core.lt_Quotes_in_Project_Nr_]}
-    }
-    {project_nr
-        {[regexp {^[a-z0-9_]+$} $project_nr match]}
-        {[lang::message::lookup "" intranet-core.Non_alphanum_chars_in_nr "The specified path contains invalid characters.<br> Allowed are only aphanumeric characters including a-z, 0-9 and '_'."]}
-    }
-    {project_nr
-        {![regexp {/} $project_nr]}
-        {[_ intranet-core.intranet-core.lt_Slashes__in_Project_P]}
-    }
-    {project_nr
-        {![regexp {\.} $project_nr]}
-        {[_ intranet-core.lt_Dots__in_Project_Path]}
-    }
-    {project_name
-        {![var_contains_quotes $project_name]}
-        {[_ intranet-core.lt_Quotes_in_Project_Nam]}
-    }
-    {parent_id
-        {![string equal $parent_id $project_id]}
-        {"Parent Project = Project"}
-    }
-    {percent_completed
-        {[expr {$percent_completed <= 100}]}
-        {"Number must be in range (0 .. 100)"}
-    }
-    {percent_completed
-        {[expr {$percent_completed >= 0}]}
-        {"Number must be in range (0 .. 100)"}
-    }
-    
 } -on_submit {
     
     # Permission check. Cases include a user with full add_projects rights,
