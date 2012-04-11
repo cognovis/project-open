13be334089994548fc61b5507255e09a1febe777
santander <>
2012-04-11 12:22:30 +0000

    - Cost Core:   Added field im_projects.project_cost_center_id together with a   trigger to keep subproject's CCs in sync with the parent's CC.

--- sql/postgresql/upgrade/upgrade-4.0.3.1.0-4.0.3.1.1.sql
+++ sql/postgresql/upgrade/upgrade-4.0.3.1.0-4.0.3.1.1.sql
@@ -3,13 +3,6 @@
 SELECT acs_log__debug('/packages/intranet-cost/sql/postgresql/upgrade/upgrade-4.0.3.1.0-4.0.3.1.1.sql','');
 
 
-select acs_privilege__create_privilege('view_projects_dept','View Department Projects','View Department Projects');
-select acs_privilege__add_child('admin', 'view_projects_dept');
-select acs_privilege__create_privilege('edit_projects_dept','Edit Department Projects','Edit Department Projects');
-select acs_privilege__add_child('admin', 'edit_projects_dept');
-
-
-
 
 create or replace function inline_0()
 returns integer as $body$
