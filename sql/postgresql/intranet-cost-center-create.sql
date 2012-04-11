bc03c11ef45b630a7fa47d9c7644512b1c6600ed
santander <>
2012-04-11 12:21:06 +0000

    - Cost-Center Perms:   Added department level permissions here

--- sql/postgresql/intranet-cost-center-create.sql
+++ sql/postgresql/intranet-cost-center-create.sql
@@ -8,6 +8,17 @@
 -- refer to http://www.project-open.com/modules/<module-key>
 
 
+-- Enable department level permissions
+SELECT acs_log__debug('/packages/intranet-cost-center/sql/postgresql/upgrade/upgrade-4.0.3.1.0-4.0.3.1.1.sql','');
+
+select acs_privilege__create_privilege('view_projects_dept','View Department Projects','View Department Projects');
+select acs_privilege__add_child('admin', 'view_projects_dept');
+select acs_privilege__create_privilege('edit_projects_dept','Edit Department Projects','Edit Department Projects');
+select acs_privilege__add_child('admin', 'edit_projects_dept');
+
+
+
+
 create or replace function inline_0 ()
 returns integer as'
 declare
@@ -57,3 +68,5 @@ begin
 end;' language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
+
+
