ff6fb90202a5f31c8aed4c3bd7155a377dab6ecd
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2012-05-03 08:40:10 +0000

    -- added AJAX mail dispatcher

---
+++ www/assign-mail-to-object.tcl
@@ -0,0 +1,77 @@
+# /packages/intranet-mail-import/www/get-mail-list.tcl
+#
+# Copyright (C) 2003 - 2012 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/ for licensing details.
+
+ad_page_contract {
+
+    return json of object atributes for users & projects 	
+		
+    @author klaus.hofeditz@project-open.com
+    @creation-date May 2012
+} {
+    { view_mode "json" }
+    { callback "" }
+    { query "" }
+    { email_id }	
+    { object_id }	
+    { remove_mails_p }
+}
+	set debug ""
+	set mail_dir "/web/projop/Maildir"
+	set defered_folder "$mail_dir/defered"
+	set processed_folder "$mail_dir/processed"
+
+	# Check if file exists 
+	if { ![file exists $email_id] } {
+                ns_log Notice "assign-mail-to-object: mail_id not found: $email_id"
+		ns_return 500 text/html 0
+    	}
+
+        # Get file name
+        set msg_paths [split $email_id "/"]
+        set email_file_name [lindex $msg_paths [expr [llength $msg_paths] - 1] ]
+
+	catch {
+		set cr_item_id [db_exec_plsql im_mail_import_new_message {}]
+                ns_log Notice "assign-mail-to-object: created spam_item $email_id"
+                append debug "created spam_item \\#$email_id\n"
+
+		set object_type [db_string get_object_type "select object_type from acs_objects where object_id = :object_id" -default 0]		
+		if { "user" == $object_type } {
+                    set rel_type "im_mail_from"
+                    set object_id_two $object_id
+                    set object_id_one $cr_item_id
+                    set creation_user $user_id
+                    set creation_ip $peeraddr
+		    set rel_id [db_exec_plsql im_mail_import_new_rel {}]
+                    ns_log Notice "assign-mail-to-object: created relationship \\#$rel_id"
+                    append debug "created relationship \\#$rel_id\n"
+		} else {
+                    set rel_type "im_mail_related_to"
+                    set object_id_two $object_id
+                    set object_id_one $cr_item_id
+                    set creation_user $user_id
+                    set creation_ip $peeraddr
+		    set rel_id [db_exec_plsql im_mail_import_new_rel {}]
+                    ns_log Notice "assign-mail-to-object: created relationship \\#$rel_id"
+                    append debug "created relationship \\#$rel_id\n"
+		}
+	}
+
+        # Move to "processed"
+	if { "true" != $remove_mails_p } {
+		if {[catch {
+                	ns_log Notice "assign-mail-to-object: Moving '$email_id' to processed: '$processed_folder/$email_id'"
+	                append debug "Moving '$email_id' to processed: '$processed_folder/$email_id'\n"
+        	        ns_rename $email_id "$processed_folder/$email_file_name"
+		} errmsg]} {
+        	        ns_log Notice "assign-mail-to-object: Error moving '$email_id' to processed: '$processed_folder/$email_file_name': '$errmsg'"
+                	append debug "Error moving '$email_id' to processes: '$processed_folder/$email_file_name': '$errmsg'\n"
+		}
+	}
+
+
+	ns_return 200 text/html $debug
---
+++ www/datasource-defered-mails.tcl
@@ -0,0 +1,172 @@
+# /packages/intranet-mail-import/www/get-mail-list.tcl
+#
+# Copyright (C) 2003 - 2012 ]project-open[
+#
+# All rights reserved. Please check
+# http://www.project-open.com/ for licensing details.
+
+ad_page_contract {
+
+    return json of object atributes for users & projects 	
+		
+    @author klaus.hofeditz@project-open.com
+    @creation-date May 2012
+} {
+    { view_mode "json" }
+    { callback "" }
+    { query ""}
+}
+
+    ad_proc -private extract_emails__ { line } {
+        Extract all emails (asdf@sdfg.dfg) from an email header line
+
+        @option header_line A mail header like such as "from" or "to".
+    } {
+        ns_log Notice "im_mail_import.extract_emails: line=$line"
+
+        set line [string tolower $line]
+        regsub -all {\<} $line " " line
+        regsub -all {\>} $line " " line
+        regsub -all {\"} $line " " line
+
+        set tokens [split $line " "]
+        set emails [list]
+
+        foreach token $tokens {
+            if {[regexp {^[a-z0-9_\.\-]+\@[a-z0-9_\.\-]+\.[a-z0-9_\.\-]+$} $token match ]} {
+                lappend emails $token
+            }
+        }
+
+        ns_log Notice "im_mail_import.extract_emails: email=$emails"
+        return $emails
+    }
+
+    set ctr 0
+    set record_list ""
+
+    set mail_dir "/web/projop/Maildir"
+    # Make sure the "Maildir/defered" folder exists"
+    set defered_folder "$mail_dir/defered"
+    if {![file exists $defered_folder]} {
+	if {[catch { ns_mkdir $defered_folder } errmsg]} {
+	    ns_log Notice "get_mails_from_defered_folder1: Error creating '$defered_folder' folder: '$errmsg'"
+	    append debug "Error creating '$defered_folder' folder: '$errmsg'\n"
+	    return $debug
+	}
+    }
+
+    if {[catch {
+	set messages [glob "$mail_dir/defered/*"]
+    } errmsg]} {
+            ns_log Notice "get_mails_from_defered_folder4: No messages: '$errmsg'"
+            append debug "No messages: '$errmsg'\n"
+            return $debug
+    }
+
+    set list_of_bounce_ids [list]
+    set new_messages_p 0
+
+
+    if {0 == [llength $messages]} { append debug "no messages in $mail_dir/defered/\n" }
+
+    set record_list_tmp [list]
+
+    # foreach mail in defered folder
+    foreach msg $messages {
+
+	# Get the last piece of the Msg
+	set msg_paths [split $msg "/"]
+	set msg_body [lindex $msg_paths [expr [llength $msg_paths] - 1] ]
+
+	# Read the entire mail into memory...
+	if [catch {
+	    set f [open $msg r]
+	    set file [read $f]
+	    close $f
+	}] {
+                continue
+	}
+	set file_lines [split $file "\n"]
+	        
+        set new_messages 1
+        set end_of_headers_p 0
+        set i 0
+	set line [lindex $file_lines $i]
+	set headers [list]
+	        
+            # walk through the headers and extract each one
+	while ![empty_string_p $line] {
+	    set next_line [lindex $file_lines [expr $i + 1]]
+	    if {[regexp {^[ ]*$} $next_line match] && $i > 0} {
+                    set end_of_headers_p 1
+	    }
+	    if {[regexp {^([^:]+):[ ]+(.+)$} $line match name value]} {
+                # concat header lines
+		if { ![regexp {^([^:]+):[ ]+(.+)$} $next_line match] && !$end_of_headers_p} {
+		    append line $next_line
+		    incr i
+		}
+		lappend headers [string tolower $name] $value
+		        
+		if {$end_of_headers_p} {
+		    incr i
+		    break
+		}
+	    } else {
+                # The headers and the body are delimited by a null line as specified by RFC822
+		if {[regexp {^[ ]*$} $line match]} {
+		    incr i
+		    break
+		}
+	    }
+                incr i
+	    set line [lindex $file_lines $i]
+	}
+	set body "\n[join [lrange $file_lines $i end] "\n"]"
+
+	ns_log Notice "get_mails_from_defered_folder6: mail_header='[join $headers "' '"]'"
+
+        # Extract headers values
+        array set email_headers $headers
+	set from_header ""
+	set to_header ""
+	set subject_header "No Subject"
+	catch {set from_header $email_headers(from)}
+	catch {set to_header $email_headers(to)}
+	catch {set subject_header $email_headers(subject)}
+
+	# Massage the header a bit
+	regsub {=\?iso-....-.\?.\?} $subject_header "" subject_header
+
+	# remove double quotes from 'from' and 'to'
+	regsub -all "\"" $from_header "" from_header
+
+	set rfc822_message_id ""
+	if {[info exists email_headers(message-id)]} {
+	    set rfc822_message_id $email_headers(message-id)
+	    # remove the <...> brackets
+	    if {[regexp {\<([^\>]*)\>} $rfc822_message_id match id]} {
+		        set rfc822_message_id $id
+	    }
+	    ns_log Notice "get_mails_from_defered_folder9: message-id=$rfc822_message_id"
+	} else {
+	    ns_log Notice "get_mails_from_defered_folder10: No message_id found"
+	}
+
+	# The the list of emails from the To and From fields
+	set to_emails [im_mail_import::extract_emails $to_header]
+	set from_emails [im_mail_import::extract_emails $from_header]
+
+        set json_record_list ""
+	append json_record_list "{\"msg_name\":\"$msg\",\n"
+        append json_record_list "\"from_header\":\"$from_header\",\n"
+        append json_record_list "\"to_header\":\"$to_header\",\n"
+        append json_record_list "\"subject_header\":\"$subject_header\",\n"
+	append json_record_list "}\n"
+        lappend record_list_tmp $json_record_list
+        incr ctr
+    }
+
+    set record_list [join $record_list_tmp ", "]
+
