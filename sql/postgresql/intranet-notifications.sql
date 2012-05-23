93a8ac6f456eb37032bafd567f4f5b1e20a9cc48
santander <>
2012-05-23 11:42:03 +0000

    - Core:   Updated the acs_lang_lookup_message PL/SQL   procedure in order to accept lookup for messages   with spaces in it. Spaces are replaced by underscores.

--- sql/postgresql/intranet-notifications.sql
+++ sql/postgresql/intranet-notifications.sql
@@ -62,7 +62,7 @@ end;' language 'plpgsql';
 
 
 -- Determine the message string for (locale, package_key, message_key):
-create or replace function acs_lang_lookup_message (text, text, text) returns text as '
+create or replace function acs_lang_lookup_message (text, text, text) returns text as $body$
 declare
 	p_locale		alias for $1;
 	p_package_key		alias for $2;
@@ -75,9 +75,10 @@ begin
 	-- Check full locale
 	select	message into v_message
 	from	lang_messages
-	where	message_key = p_message_key
+	where	(message_key = p_message_key OR message_key = replace(p_message_key, ' ', '_'))
 		and package_key = p_package_key
-		and locale = p_locale;
+		and locale = p_locale
+	LIMIT 1;
 	IF v_message is not null THEN return v_message; END IF;
 
 	-- --------------------------------------------
@@ -87,46 +88,49 @@ begin
 	select	locale into v_locale
 	from	ad_locales
 	where	language = v_locale
-		and enabled_p = ''t''
-		and (default_p = ''t'' or
+		and enabled_p = 't'
+		and (default_p = 't' or
 		(select count(*) from ad_locales where language = v_locale) = 1);
 
 	select	message into v_message
 	from	lang_messages
-	where	message_key = p_message_key
+	where	(message_key = p_message_key OR message_key = replace(p_message_key, ' ', '_'))
 		and package_key = p_package_key
-		and locale = v_locale;
+		and locale = v_locale
+	LIMIT 1;
 	IF v_message is not null THEN return v_message; END IF;
 
 	-- --------------------------------------------
 	-- Try System Locale
 	select	package_id into	v_acs_lang_package_id
 	from	apm_packages
-	where	package_key = ''acs-lang'';
-	v_locale := apm__get_value (v_acs_lang_package_id, ''SiteWideLocale'');
+	where	package_key = 'acs-lang';
+	v_locale := apm__get_value (v_acs_lang_package_id, 'SiteWideLocale');
 
 	select	message into v_message
 	from	lang_messages
-	where	message_key = p_message_key
+	where	(message_key = p_message_key OR message_key = replace(p_message_key, ' ', '_'))
 		and package_key = p_package_key
-		and locale = v_locale;
+		and locale = v_locale
+	LIMIT 1;
 	IF v_message is not null THEN return v_message; END IF;
 
 	-- --------------------------------------------
 	-- Try with English...
-	v_locale := ''en_US'';
+	v_locale := 'en_US';
 	select	message into v_message
 	from	lang_messages
-	where	message_key = p_message_key
+	where	(message_key = p_message_key OR message_key = replace(p_message_key, ' ', '_'))
 		and package_key = p_package_key
-		and locale = v_locale;
+		and locale = v_locale
+	LIMIT 1;
 	IF v_message is not null THEN return v_message; END IF;
 
 	-- --------------------------------------------
 	-- Nothing found...
-	v_message := ''MISSING '' || p_locale || '' TRANSLATION for '' || p_package_key || ''.'' || p_message_key;
+	v_message := 'MISSING ' || p_locale || ' TRANSLATION for ' || p_package_key || '.' || p_message_key;
 	return v_message;	
 
-end;' language 'plpgsql';
+end;$body$ language 'plpgsql';
 
 
