a634a1371cb266c89c62698f37b4e9e6722004aa
santander <>
2012-05-24 18:18:52 +0000

    - Audit:   Added an index on audit_date in order to speedup queries   looking into the history of an object

---
+++ sql/postgresql/upgrade/upgrade-4.0.3.0.4-4.0.3.0.5.sql
@@ -0,0 +1,22 @@
+-- upgrade-4.0.3.0.3-4.0.3.0.4.sql
+
+SELECT acs_log__debug('/packages/intranet-audit/sql/postgresql/upgrade/upgrade-4.0.3.0.3-4.0.3.0.4.sql','');
+
+create or replace function inline_0 ()
+returns integer as $body$
+DECLARE
+	v_count		integer;
+BEGIN
+	select	count(*) into v_count
+	from	pg_class
+	where	lower(relname) = 'im_audits_audit_date_idx';
+
+	IF v_count > 0 THEN return 1; END IF;
+
+	create index im_audits_audit_date_idx on im_audits(audit_date);
+
+        return 0;
+end; $body$ language 'plpgsql';
+select inline_0();
+drop function inline_0();
+
