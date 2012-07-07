b379082d6fc176b760a3d3dbd1c4a6c8630489d2
Malte Sussdorff <malte.sussdorff@cognovis.de>
2012-07-07 17:16:06 +0200

    fixed malte.sh

--- malte.sh
+++ malte.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/git/projop
-PKGS_LIST=/home/git/projop/pkgs-list.txt
+cd /home/git/project-open-orig
+PKGS_LIST=/home/git/project-open-orig/pkgs-list.txt
 echo "UPDATE STARTED:: " >/var/log/projop2git/projop.log
-echo `date` >>/home/git/projop/update.log 
+echo `date` >>/var/log/projop2git/projop.log
 for pkg in `cat $PKGS_LIST`; do
         echo "$pkg" >>/var/log/projop2git/projop.log
         ./github-update $pkg >>/var/log/projop2git/projop.log 2>&1
