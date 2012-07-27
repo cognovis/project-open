07ec28409c928c4b951c10b82c8b8d0687672c37
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2012-07-27 17:51:59 +0000

    -- fixed date selection

--- sql/postgresql/upgrade/upgrade-4.0.1.0.6-4.0.1.0.7.sql
+++ sql/postgresql/upgrade/upgrade-4.0.1.0.6-4.0.1.0.7.sql
@@ -22,7 +22,7 @@ create or replace function im_absences_get_absences_for_user(int4,date,date,int4
            FOR v_record IN
                     -- get user absences
                     select
-			    a.absence_id,
+                            a.absence_id,
                             a.start_date,
                             a.end_date,
                             a.absence_type_id
@@ -31,20 +31,27 @@ create or replace function im_absences_get_absences_for_user(int4,date,date,int4
                     where
                             a.group_id is null and
                             a.owner_id = v_user_id and (
-                                        a.start_date::date between v_start_date::date and v_end_date::date::date OR
-                                        a.end_date::date between v_start_date::date and v_end_date::date::date
+                                                (
+						a.start_date::date between v_start_date::date and v_end_date::date::date OR
+                                                a.end_date::date between v_start_date::date and v_end_date::date::date
+						)
+						OR
+						(
+						v_start_date::date between a.start_date::date and a.end_date::date OR 
+						v_end_date::date between a.start_date::date and a.end_date::date
+						)
                            )
                    UNION
                       -- get all group absences
                         select
-				i.absence_id,
+                                i.absence_id,
                                 i.start_date,
                                 i.end_date,
                                 i.absence_type_id
                         from (
                                 select
                                         a.absence_id,
-					a.start_date,
+                                        a.start_date,
                                         a.end_date,
                                         a.absence_type_id,
                                         a.group_id
@@ -52,18 +59,25 @@ create or replace function im_absences_get_absences_for_user(int4,date,date,int4
                                         im_user_absences a
                                 where
                                         group_id is not null and (
-	                                        a.start_date::date between v_start_date::date and v_end_date::date::date OR
-        	                                a.end_date::date between v_start_date::date and v_end_date::date::date
-                                        )
+                                                (
+						a.start_date::date between v_start_date::date and v_end_date::date::date OR
+                                                a.end_date::date between v_start_date::date and v_end_date::date::date
+						)
+						OR
+						(
+						v_start_date::date between a.start_date::date and a.end_date::date OR 
+						v_end_date::date between a.start_date::date and a.end_date::date 
+						)
+                                        ) 
                        ) i
                        where
-                        	acs_group__member_p(v_user_id, i.group_id, TRUE::boolean)
+                                acs_group__member_p(v_user_id, i.group_id, TRUE::boolean)
                 LOOP
                     -- Enumeration over start/end date of period
                     v_searchsql = 'select
                                         im_day_enumerator as d,
                                         1 as absence_type_id,
-					0 as absence_id
+                                        0 as absence_id
                                     from
                                         im_day_enumerator (''' || v_start_date || '''::date ,''' ||  v_end_date || '''::date +1)';
 
@@ -78,7 +92,7 @@ create or replace function im_absences_get_absences_for_user(int4,date,date,int4
                                 -- check for absence type
                                 IF v_absence_type_id = NULL OR v_record.absence_type_id = v_record.absence_type_id THEN
                                         select into v_sql_result.absence_type_id v_record.absence_type_id;
-					select into v_sql_result.absence_id v_record.absence_id;
+                                        select into v_sql_result.absence_id v_record.absence_id;
                                         RETURN next v_sql_result;
                                 END IF;
                             END IF;
