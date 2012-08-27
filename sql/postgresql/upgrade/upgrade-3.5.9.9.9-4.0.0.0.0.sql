d0aa46dff5e7b908b9956b0e40b5e49cb85b631e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2012-08-27 14:57:08 +0000

    -- fixed wrong acs_log_debug parameter

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -1,6 +1,6 @@
 -- upgrade-3.5.9.9.9-4.0.0.0.0.sql
 
-SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-3.5.0.0.0-3.5.0.0.1.sql','');
+SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql','');
 
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
 extra_select, extra_where, sort_order, visible_for) values (5636,56,NULL,'Currency',
