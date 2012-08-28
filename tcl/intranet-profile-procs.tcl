3611807a40966c07865d7743e4f739c497013748
Frank Bergmann <frank.bergmann@project-open.com>
2012-08-28 14:33:15 +0000

    - Profile Management:   Added "Skill Profile"

--- tcl/intranet-profile-procs.tcl
+++ tcl/intranet-profile-procs.tcl
@@ -70,6 +70,10 @@ ad_proc -public im_profile_helpdesk {} {
      return [im_profile::profile_id_from_name -profile "Helpdesk"] 
 }
 
+ad_proc -public im_profile_skill_profile {} { 
+     return [im_profile::profile_id_from_name -profile "Skill Profile"] 
+}
+
 ad_proc -public im_profile_registered_users {} { 
     return [util_memoize [list db_string registered_users "
 		select	object_id 
