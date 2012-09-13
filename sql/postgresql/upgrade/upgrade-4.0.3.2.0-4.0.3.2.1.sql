a9df1c804be32fd7de7f3d54e523fc8aaf2baf1f
Frank Bergmann <frank.bergmann@project-open.com>
2012-09-13 15:09:34 +0000

    - Timesheet Task List:   Fixed error deleting start_date and end_date

---
+++ sql/postgresql/upgrade/upgrade-4.0.3.2.0-4.0.3.2.1.sql
@@ -0,0 +1,24 @@
+-- upgrade-4.0.3.2.0-4.0.3.2.1.sql
+
+SELECT acs_log__debug('/packages/intranet-timesheet2-tasks/sql/postgresql/upgrade/upgrade-4.0.3.2.0-4.0.3.2.1.sql','');
+
+
+-- -------------------------------------------------------------------
+-- Timesheet TaskList
+-- -------------------------------------------------------------------
+
+--
+-- Wide View in "Tasks" page, including Description
+--
+delete from im_view_columns where column_id in (91007, 91008);
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (91007,910,NULL,'"Start"',
+'"<nobr>[string range $start_date 0 9]</nobr><input type=hidden name=start_date.$task_id value=''[string range $start_date 0 15]''>"','','',7,'');
+
+
+
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (91008,910,NULL,'"End"',
+'"[if {[string equal t $red_p]} { set t "<nobr><font color=red>[string range $end_date 0 9]</font></nobr>" } else { set t "<nobr>[string range $end_date 0 9]</nobr>" }]<input type=hidden name=end_date.$task_id value=''[string range $end_date 0 15]''>"','(child.end_date < now() and coalesce(child.percent_completed,0) < 100) as red_p','',8,'');
+
