139516949a6adce15535d0b574837b91024fb19d
Frank Bergmann <frank.bergmann@project-open.com>
2012-09-20 17:33:28 +0000

    - Trans Invoices:   Updated drop-script.

--- sql/postgresql/intranet-trans-invoices-drop.sql
+++ sql/postgresql/intranet-trans-invoices-drop.sql
@@ -8,6 +8,9 @@
 -- @author frank.bergmann@project-open.com
 -- @author juanjoruizx@yahoo.es
 
+
+delete from im_reports where report_menu_id in (select menu_id from im_menus where package_name = 'intranet-trans-invoices');
+
 select    im_component_plugin__del_module('intranet-trans-invoices');
 select    im_menu__del_module( 'intranet-trans-invoices');
 
@@ -29,12 +32,20 @@ BEGIN
 
     return 0;
 end;' language 'plpgsql';
-
 select inline_01 ();
-
 drop function inline_01 ();
 
 
+
+
+
+delete from im_view_columns where view_id = 195;
+delete from im_views where view_id = 195;
+delete from im_view_columns where column_id > 19500 and column_id < 19599;
+
+
+
+drop function im_file_type_from_trans_task (integer);
 drop function im_trans_prices_calc_relevancy (
      integer, integer, integer, integer, integer, integer, integer, integer, integer, integer
 );
@@ -43,8 +54,35 @@ drop function im_trans_prices_calc_relevancy (
      integer, integer, integer, integer, integer, integer, integer, integer, integer, integer, integer, integer
 );
 
-drop sequence im_trans_prices_seq;
-drop table im_trans_prices;
+drop function im_trans_invoice__new (
+        integer,        -- default null
+        varchar,        -- default im_trans_invoice
+        timestamptz,    -- default now()
+        integer,
+        varchar,        -- default null
+        integer,        -- default null
+        varchar,
+        integer,
+        integer,
+        integer,        -- default null
+        timestamptz,    -- default now()
+        char,           -- default EUR
+        integer,        -- default null
+        integer,        -- default 602
+        integer,        -- default 700
+        integer,        -- default null
+        integer,        -- default 30
+        numeric,
+        numeric,        -- default 0
+        numeric,        -- default 0
+        varchar         -- default null
+);
+
+
+drop function im_trans_invoice__delete (integer);
+drop function im_trans_invoice__name (integer);
+
+
 
 
 -- delete all im_trans_invoices without
@@ -62,10 +100,9 @@ begin
      end loop;
     return 0;
 end;' language 'plpgsql';
-
 select inline_02 ();
-
 drop function inline_02 ();
+
 
 
 -- delete links to edit im_trans_invoices objects...
@@ -73,6 +110,13 @@ delete from im_biz_object_urls where object_type = 'im_trans_invoice';
 
 -- drop main table and object_type
 drop table im_trans_invoices;
+
+drop sequence im_trans_prices_seq;
+drop table im_trans_prices;
+
+
+delete from im_categories where category_type = 'Intranet Translation File Type';
+
 
 
 
@@ -133,6 +177,8 @@ delete from acs_objects
 where object_type = 'im_trans_invoice';
 
 
+delete from acs_object_type_tables where object_type = 'im_trans_invoice';
+delete from im_biz_object_urls where object_type = 'im_trans_invoice';
 -- Now we can drop the object type
 select acs_object_type__drop_type('im_trans_invoice', 'f');
 
