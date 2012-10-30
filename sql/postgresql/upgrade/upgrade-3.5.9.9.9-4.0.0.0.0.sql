c192bcb71cc00c565e248987fa0dd10720b44291
Frank Bergmann <frank.bergmann@project-open.com>
2012-10-30 17:23:47 +0000

    - fixed upgrade script

--- sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql
@@ -2,6 +2,7 @@
 
 SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-3.5.9.9.9-4.0.0.0.0.sql','');
 
+delete from im_view_columns where column_id = 5636;
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
 extra_select, extra_where, sort_order, visible_for) values (5636,56,NULL,'Currency',
 '$currency','','',7,'');
