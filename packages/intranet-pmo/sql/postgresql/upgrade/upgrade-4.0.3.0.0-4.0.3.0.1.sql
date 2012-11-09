74497fc5ae479c061bdceb72f8e310acb2a19d08
Malte Sussdorff <malte.sussdorff@cognovis.de>
2012-11-09 09:47:40 +0100

    Fixed non recognized upgrade script

--- packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql
+++ packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql
@@ -1,3 +1,5 @@
+-- upgrade-4.0.3.0.0-4.0.3.0.1.sql
+SELECT acs_log__debug('/packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql','');
 create table im_biz_object_members_availability (
        rel_id integer constraint rel_id_fk references im_biz_object_members(rel_id) on delete cascade,
        start_date date not null,
