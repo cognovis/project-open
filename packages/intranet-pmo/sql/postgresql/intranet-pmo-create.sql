ef9a011c2bb20b63436315fa4326ce27282fdaeb
Malte Sussdorff <malte.sussdorff@cognovis.de>
2012-11-09 11:12:45 +0100

    Added im_project_assignments table to store the (planned) project assignments

--- packages/intranet-pmo/sql/postgresql/intranet-pmo-create.sql
+++ packages/intranet-pmo/sql/postgresql/intranet-pmo-create.sql
@@ -584,9 +584,11 @@ drop function inline_0 ();
 select acs_privilege__create_privilege('approve_budgets','Approve Budgets','Approve Budgets');
 select acs_privilege__add_child('admin', 'approve_budgets');
 
-create table im_biz_object_members_availability (
-       rel_id integer constraint rel_id_fk references im_biz_object_members(rel_id) on delete cascade,
+create table im_project_assignments (
+       user_id integer constraint user_id_fk references users(user_id) on delete cascade not null,
+       project_id integer constraint project_id_fk references im_projects(project_id) on delete cascade not null,
+       rel_id integer constraint rel_id_fk references im_biz_object_members(rel_id) on delete set null,
        start_date date not null,
        availability float default 100,
-       primary key(rel_id,start_date)
+       primary key(user_id,project_id,start_date)
 );
--- packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql
+++ packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql
@@ -1,8 +1,11 @@
 -- upgrade-4.0.3.0.0-4.0.3.0.1.sql
 SELECT acs_log__debug('/packages/intranet-pmo/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.1.sql','');
-create table im_biz_object_members_availability (
-       rel_id integer constraint rel_id_fk references im_biz_object_members(rel_id) on delete cascade,
+
+create table im_project_assignments (
+       user_id integer constraint user_id_fk references users(user_id) on delete cascade not null,
+       project_id integer constraint project_id_fk references im_projects(project_id) on delete cascade not null,
+       rel_id integer constraint rel_id_fk references im_biz_object_members(rel_id) on delete set null,
        start_date date not null,
        availability float default 100,
-       primary key(rel_id,start_date)
+       primary key(user_id,project_id,start_date)
 );
