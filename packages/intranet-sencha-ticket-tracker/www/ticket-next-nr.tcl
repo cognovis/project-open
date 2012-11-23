f9fc88a5f4dd90474fd208582367dcae6f93db3d
Malte Sussdorff <malte.sussdorff@cognovis.de>
2012-11-23 15:38:09 +0100

    removed

--- packages/intranet-sencha-ticket-tracker/www/ticket-next-nr.tcl
+++
@@ -1,3 +0,0 @@
-
-# Return the next free number for the ticket
-doc_return 200 "text/html" [im_ticket::next_ticket_nr]
--- packages/intranet-sencha-ticket-tracker/www/today-date-time.tcl
+++
@@ -1,3 +0,0 @@
-
-# Return the current date
-doc_return 200 "text/html" [db_string date "select to_char(now(), 'YYYY-MM-DD HH24:MI') from dual"]
