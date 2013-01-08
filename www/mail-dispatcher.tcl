7a9778fa095b5f0807ef060921ba298b6c2ec581
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-01-08 21:55:28 +0000

    -- privilege 'admin_mail_dispatcher' required

--- www/mail-dispatcher.tcl
+++ www/mail-dispatcher.tcl
@@ -22,7 +22,8 @@ ad_page_contract {
 
 set user_id [ad_maybe_redirect_for_registration]
 set user_is_admin_p [im_is_user_site_wide_or_intranet_admin $user_id]
-if {!$user_is_admin_p} {
+
+if {!$user_is_admin_p || ![im_permission $user_id admin_mail_dispatcher] } {
     ad_return_complaint 1 "You have insufficient privileges to use this page"
     return
 }
