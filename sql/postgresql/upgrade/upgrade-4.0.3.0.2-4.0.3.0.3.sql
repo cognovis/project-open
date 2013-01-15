b4c8ca84369d76acd6ac801904e59afcb6e1b3ad
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-01-15 16:19:33 +0000

    - Fixed upgrade script

--- sql/postgresql/upgrade/upgrade-4.0.3.0.2-4.0.3.0.3.sql
+++ sql/postgresql/upgrade/upgrade-4.0.3.0.2-4.0.3.0.3.sql
@@ -6,49 +6,6 @@ SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-4.
 -- with duplicate projects on the top-level
 
 
--- Identify projects with duplicate project_nr
--- and make them unique by adding the project_id
---
-update im_projects
-set project_nr = project_nr || '.' || project_id
-where project_id in (
-	select	project_id
-	from  	im_projects 
-	where	project_nr in (
-		select project_nr 
-		from (
-			select count(*) as cnt, project_nr 
-			from im_projects 
-			where parent_id is null 
-			group by project_nr 
-			order by cnt DESC
-			) t 
-		where cnt > 1
-	)
-);
-
-
--- Identify projects with duplicate project_path
--- and make them unique by adding the project_id
---
-update im_projects
-set project_path = project_path || '.' || project_id
-where project_id in (
-	select	project_id
-	from  	im_projects 
-	where	project_path in (
-		select project_path 
-		from (
-			select count(*) as cnt, project_path 
-			from im_projects 
-			where parent_id is null 
-			group by project_path 
-			order by cnt DESC
-			) t 
-		where cnt > 1
-	)
-);
-
 
 create or replace function inline_0 ()
 returns integer as $body$
@@ -113,7 +70,6 @@ begin
 	IF v_count > 0 THEN alter table im_projects drop constraint im_projects_nr_un; END IF;
 
 
-
 	-- Disambiguate project_nrs
 	FOR row IN
 		select	* 
@@ -159,12 +115,37 @@ create or replace function inline_0 ()
 returns integer as $body$
 declare
 	v_count  integer;
+	row	 RECORD;
 begin
 	-- Drop the old unique constraints
 	select count(*) into v_count from pg_constraint
 	where lower(conname) = 'im_projects_path_un';
 	IF v_count > 0 THEN alter table im_projects drop constraint im_projects_path_un; END IF;
 
+	-- Disambiguate project_paths
+	FOR row IN
+		select	* 
+		from	(
+			select	count(*) as cnt, 
+				project_path, 
+				company_id,
+				coalesce(parent_id,0) as parent_id
+			from	im_projects
+			group by 
+				project_path, 
+				company_id,
+				coalesce(parent_id,0)
+			) t 
+		where	cnt > 1
+	LOOP
+		RAISE NOTICE 'upgrade-4.0.3.0.2-4.0.3.0.3.sql: Found ambigous project_path "%", adding the project_id at the end', row.project_path;
+		update	im_projects
+		set	project_path = project_path || '_' || project_id
+		where	project_path = row.project_path and
+			company_id = row.company_id and
+			coalesce(parent_id,0) = coalesce(row.parent_id,0);
+
+	END LOOP;
 
 	-- Create the new unique indices
 	select count(*) into v_count from pg_class
