f3dc40b33cac845c876ecf0aca52845b1c6aad8d
Frank Bergmann <frank.bergmann@project-open.com>
2013-01-24 09:37:04 +0000

    - Configuration Database:   Fixed error searching and updating conf items   because of TSearch2 indexing

--- sql/postgresql/intranet-search-pg-create.sql
+++ sql/postgresql/intranet-search-pg-create.sql
@@ -794,6 +794,84 @@ update cr_items set name=name;
 
 
 
+
+
+
+-----------------------------------------------------------
+-- Conf_Items
+
+insert into im_search_object_types values (9, 'im_conf_item', 0.8);
+
+
+
+create or replace function im_conf_items_tsearch ()
+returns trigger as '
+declare
+	v_string	varchar;
+begin
+	select	coalesce(c.conf_item_code, '''') || '' '' ||
+		coalesce(c.conf_item_name, '''') || '' '' ||
+		coalesce(c.conf_item_nr, '''') || '' '' ||
+		coalesce(c.conf_item_version, '''') || '' '' ||
+		coalesce(c.description, '''') || '' '' ||
+		coalesce(c.ip_address, '''') || '' '' ||
+		coalesce(c.note, '''') || '' '' ||
+		coalesce(c.ocs_deviceid, '''') || '' '' ||
+		coalesce(c.ocs_id, '''') || '' '' ||
+		coalesce(c.ocs_username, '''') || '' '' ||
+		coalesce(c.os_comments, '''') || '' '' ||
+		coalesce(c.os_name, '''') || '' '' ||
+		coalesce(c.os_version, '''') || '' '' ||
+		coalesce(c.processor_text, '''') || '' '' ||
+		coalesce(c.win_company, '''') || '' '' ||
+		coalesce(c.win_owner, '''') || '' '' ||
+		coalesce(c.win_product_id, '''') || '' '' ||
+		coalesce(c.win_product_key, '''') || '' '' ||
+		coalesce(c.win_userdomain, '''') || '' '' ||
+		coalesce(c.win_workgroup, '''')
+	into    v_string
+	from    im_conf_items c
+	where   c.conf_item_id = new.conf_item_id;
+
+	perform im_search_update(new.conf_item_id, ''im_conf_item'', new.conf_item_id, v_string);
+
+	return new;
+end;' language 'plpgsql';
+
+
+
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+	v_count		integer;
+begin
+	-- Check if the table exists
+	select	count(*) into v_count from user_tab_columns
+	where	lower(table_name) = 'im_conf_items';
+	if v_count = 0 then return 1; end if;
+
+	-- Check if the trigger exists
+	select	count(*) into v_count from pg_trigger where tgname = 'im_conf_items_tsearch_tr';
+	IF v_count > 0 THEN
+		drop trigger im_conf_items_tsearch_tr on im_conf_items; 
+	END IF;
+
+	CREATE TRIGGER im_conf_items_tsearch_tr
+	AFTER INSERT or UPDATE
+	ON im_conf_items
+	FOR EACH ROW
+	EXECUTE PROCEDURE im_conf_items_tsearch();
+
+	update im_conf_items set conf_item_nr = conf_item_nr;
+
+	return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+
+
 -- fraber 080225:
 -- Set the default language to the current system languages
 -- fraber 110327: This does not work for PG 8.4 somehow...
