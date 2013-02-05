4b2c3c0ad991a089c279216b4d542128b03db8c4
Frank Bergmann <frank.bergmann@project-open.com>
2013-02-05 08:16:42 +0000

    - Timesheet Weekly Report:   Restricting view to current user unless special permissions   are available

--- www/hours/week.tcl
+++ www/hours/week.tcl
@@ -28,14 +28,25 @@ ad_page_contract {
     { user_id_from_search:integer "" }
 }
 
+set user_id [ad_maybe_redirect_for_registration]
+
 if { [empty_string_p $user_id_from_search] } {
     set user_id_from_search [ad_maybe_redirect_for_registration]
 }
+
+# Permissions - limit view to the user himself,
+# unless special permissions are available
+if {"" == $user_id_from_search || ![im_permission $user_id "add_hours_all"]} { 
+    set user_id_from_search $user_id 
+}
+
 
 
 if { [empty_string_p $julian_date] } {
     set julian_date [db_string sysdate_as_julian "select to_char(sysdate,'J') from dual"]
 }
+
+
 
 set user_name [db_string user_name "select im_name_from_user_id(:user_id_from_search) from dual"]
 
