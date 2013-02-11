8e8f711d2307a73ea075f4558b762207ddf9d870
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-02-11 11:47:46 +0100

    Added problem with not run upgrade script

--- packages/intranet-core/sql/postgresql/upgrade/upgrade-4.0.3.3.8-4.0.3.3.9.sql
+++ packages/intranet-core/sql/postgresql/upgrade/upgrade-4.0.3.3.8-4.0.3.3.9.sql
@@ -21,6 +21,24 @@
 
 SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-4.0.3.3.8-4.0.3.3.9.sql','');
 
+SELECT im_dynfield_widget__new (
+                null,                   -- widget_id
+                'im_dynfield_widget',   -- object_type
+                now(),                  -- creation_date
+                null,                   -- creation_user
+                null,                   -- creation_ip
+                null,                   -- context_id
+                'vat_type',              -- widget_name
+                '#intranet-core.VAT#',      -- pretty_name
+                '#intranet-core.VAT#',      -- pretty_plural
+                10007,                  -- storage_type_id
+                'integer',              -- acs_datatype
+                'im_category_tree',             -- widget
+                'integer',              -- sql_datatype
+                '{custom {category_type "Intranet VAT Type"}}', 
+                'im_name_from_id'
+);
+
 -- Widget for the payment terms
 SELECT im_dynfield_widget__new (
                 null,                   -- widget_id
@@ -145,6 +163,72 @@ begin
 end;$body$ language 'plpgsql';
 select inline_0 ();
 drop function inline_0 ();
+
+create or replace function inline_0 ()
+returns integer as $body$
+declare
+        v_count  integer;
+begin
+        select count(*) into v_count from user_tab_columns
+        where lower(table_name) = 'im_companies' and lower(column_name) = 'vat_type_id';
+
+        IF v_count = 0 THEN
+  	      alter table im_companies add column vat_type_id integer;
+        END IF;
+
+        return 0;
+end;$body$ language 'plpgsql';
+select inline_0 ();
+drop function inline_0 ();
+
+
+-- tax classification
+CREATE OR REPLACE FUNCTION inline_0 ()
+RETURNS integer AS '
+DECLARE
+	v_acs_attribute_id	integer;
+	v_attribute_id		integer;
+	v_count			integer;
+	row			record;
+BEGIN
+
+	  v_attribute_id := im_dynfield_attribute_new (
+	  	 ''im_company'',			-- object_type
+		 ''vat_type_id'',			-- column_name
+		 ''#intranet-core.Tax_classification#'',	-- pretty_name
+		 ''vat_type'',			-- widget_name
+		 ''integer'',				-- acs_datatype
+		 ''t'',					-- required_p   
+		 90,					-- pos y
+		 ''f'',					-- also_hard_coded
+		 ''im_companies''			-- table_name
+	  );
+
+	  
+
+	IF v_attribute_id != 1 THEN
+	FOR row IN 
+		SELECT category_id FROM im_categories WHERE category_type = ''Intranet Company Type''
+	LOOP
+			
+		SELECT count(*) INTO v_count FROM im_dynfield_type_attribute_map WHERE attribute_id = v_attribute_id AND object_type_id = row.category_id;
+		IF v_count = 0 THEN
+		   INSERT INTO im_dynfield_type_attribute_map
+		   	  (attribute_id, object_type_id, display_mode, help_text,section_heading,default_value,required_p)
+		   VALUES
+			  (v_attribute_id, row.category_id,''edit'',''Choose the appropriate Tax Classification'',null,null,''f'');
+		ELSE
+		   UPDATE im_dynfield_type_attribute_map SET display_mode = ''edit'', required_p = ''f'' WHERE attribute_id = v_attribute_id AND object_type_id = row.category_id;
+		END IF;
+
+	END LOOP;
+        END IF;
+
+	RETURN 0;
+END;' language 'plpgsql';
+
+SELECT inline_0 ();
+DROP FUNCTION inline_0 ();
 
 alter table im_companies add constraint im_companies_payment_term_id_fk foreign key (payment_term_id) references im_categories(category_id);
 alter table im_companies add constraint im_companies_vat_type_id_fk foreign key (vat_type_id) references im_categories(category_id);
