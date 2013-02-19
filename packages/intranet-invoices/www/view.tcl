5fa2602b52fa50ac1d5cf1bf13c125e56ae76490
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-02-19 13:32:36 +0100

    Fixed dummy_text error

--- packages/intranet-invoices/www/view.tcl
+++ packages/intranet-invoices/www/view.tcl
@@ -488,7 +488,7 @@ if {0 != $render_template_id} {
 # Check if the given locale throws an error
 # Reset the locale to the default locale then
 if {[catch {
-    lang::message::lookup $locale "dummy_text"
+    lang::message::lookup $locale "intranet-core.Reporting"
 } errmsg]} {
     set locale $user_locale
 }
