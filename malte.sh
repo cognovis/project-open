dbd80fde706277f142b00b7bd6584e137c9ae139
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-03-09 08:23:17 +0100

    Added support for local repository updates

--- malte.sh
+++ malte.sh
@@ -6,6 +6,6 @@ echo "UPDATE STARTED:: " >/var/log/projop2git/projop.log
 echo `date` >>/var/log/projop2git/projop.log
 for pkg in `cat $PKGS_LIST`; do
         echo "$pkg" >>/var/log/projop2git/projop.log
-        ./github-update $pkg >>/var/log/projop2git/projop.log 2>&1
+        ./repos-update $pkg >>/var/log/projop2git/projop.log 2>&1
 done
 /usr/bin/git push origin master
