718712e941ae57e0c1bab7da5ad601864effa370
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-03-09 08:41:42 +0100

    This file shouldnt even be here ....

--- packages/acs-mail-lite/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.2.sql
+++
@@ -1,7 +0,0 @@
-SELECT acs_log__debug('/packages/intranet-csv-import/sql/postgresql/upgrade/upgrade-4.0.3.0.0-4.0.3.0.2.sql','');
-
-
-update im_menus
-set url = '/intranet-csv-import/index?object_type=im_project'
-where label = 'projects_admin_csv_import';
-
