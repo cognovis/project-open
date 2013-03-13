5cef1d86ae16efea0864bf8b0eeb9a9164ed5b0d
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-03-13 16:23:53 +0000

    -- perform sanity check for duplicate emails als in case of an update

--- www/users/new.tcl
+++ www/users/new.tcl
@@ -288,25 +288,24 @@ ad_form -extend -name register -on_request {
     }
 
 
-#	20041124 fraber: disabled db_transaction because of problems with PostgreSQL?
+#    20041124 fraber: disabled db_transaction because of problems with PostgreSQL?
 #    db_transaction {
 	
-	# Do we create a new user or do we edit an existing one?
+    	# Do we create a new user or do we edit an existing one?
 	ns_log Notice "/users/new: editing_existing_user=$editing_existing_user"
 
+        # Check for duplicate email in case of updates AND newly to be created users 
+	set email [string trim $email]
+	set similar_user [db_string similar_user "select party_id from parties where lower(email) = lower(:email)" -default 0]
+	if {$similar_user > 0} {
+	    set view_similar_user_link "<A href=/intranet/users/view?user_id=$similar_user>[_ intranet-core.user]</A>"
+	    ad_return_complaint 1 "<li><b>[_ intranet-core.Duplicate_UserB]<br>
+       	        [_ intranet-core.lt_There_is_already_a_vi]<br>"
+	    return
+	}
+
 	if {!$editing_existing_user} {
-
 	    # New user: create from scratch
-	    set email [string trim $email]
-	    set similar_user [db_string similar_user "select party_id from parties where lower(email) = lower(:email)" -default 0]
-	    
-	    if {$similar_user > 0} {
-			set view_similar_user_link "<A href=/intranet/users/view?user_id=$similar_user>[_ intranet-core.user]</A>"
-			ad_return_complaint 1 "<li><b>[_ intranet-core.Duplicate_UserB]<br>
-        	        [_ intranet-core.lt_There_is_already_a_vi]<br>"
-			return
-	    }
-
 	    if {![info exists password] || [empty_string_p $password]} {
 		set password [ad_generate_random_string]
 		set password_confirm $password
@@ -386,6 +385,8 @@ ad_form -extend -name register -on_request {
 		-last_name $last_name
 	    
 	    ns_log Notice "/users/new: party::update -party_id=$user_id -url=$url -email=$email"
+
+
 	    party::update \
 		-party_id $user_id \
 		-url $url \
