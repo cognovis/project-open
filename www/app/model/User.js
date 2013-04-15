eb726c2c40895af0a45b558a74eef1f14982690b
Frank Bergmann <frank.bergmann@project-open.com>
2013-04-15 08:53:21 +0000

    - added user store

---
+++ www/app/model/User.js
@@ -0,0 +1,31 @@
+Ext.define('PO.model.User', {
+    extend: 'Ext.data.Model',
+    config: {
+	fields: [
+		'id',
+		'first_names',
+		'last_name'
+	],
+	proxy: {
+		type:		'rest',
+		url:		'/intranet-rest/user',
+		appendId:	true,			// Append the object_id: ../im_ticket/<object_id>
+		timeout:	300000,
+
+		extraParams: {
+			format:		'json',		// Tell the ]po[ REST to return JSON data.
+			deref_p:	'0',
+			columns:	'first_names,last_name'
+		},
+		reader: {
+			type:	'json',			// Tell the Proxy Reader to parse JSON
+			root:	'data',			// Where do the data start in the JSON file?
+			totalProperty:  'total'		// Total number of tickets for pagination
+		},
+			writer: {
+			type:	'json'			// Allow Sencha to write ticket changes
+		}
+	}
+    }
+});
+
