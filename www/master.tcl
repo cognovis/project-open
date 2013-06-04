eb76b92fe00dfd87588378b49478f0869105a837
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-06-04 16:18:24 +0000

    -- fixed feedback link

--- www/master.tcl
+++ www/master.tcl
@@ -117,10 +117,10 @@ set show_feedback_p [parameter::get -package_id [apm_package_id_from_key intrane
 
 if { $show_feedback_p } {
     template::head::add_css -href "/intranet/style/feedbackBadge.css" -media "screen" -order 1
-    template::head::add_javascript -src "/intranet/js/jquery.feedbackBadge.min.js" -order 1
+    template::head::add_javascript -src "/intranet/js/jquery.feedbackBadge.mifeedback-badge-rightn.js" -order 1
 }
 
-set feedback_url "<a href=\"[export_vars -base "/intranet/report-bug-on-page" {{[im_component_page_url] [im_url_with_query]}}]\" title=\"Give us feedback\" id=\"feedback-badge-right\">"
+set feedback_url "<a href=\"[export_vars -base "/intranet/report-bug-on-page" {{page_url [im_url_with_query]}}]\" title='Give us feedback' id='feedback-badge-right' target='new'>"
 append feedback_url "<span>[lang::message::lookup "" intranet-core.Feedback "Feedback"]</span></a>"
 
 # Fraber 121008: Write out permission debugging
