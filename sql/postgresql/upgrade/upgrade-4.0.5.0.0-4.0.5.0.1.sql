dce03b98429c1cc24153fa6ad8a351c8609c2662
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-08-21 09:20:15 +0000

    - SL:   Fixed upgrade script

--- sql/postgresql/upgrade/upgrade-4.0.5.0.0-4.0.5.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.0.5.0.0-4.0.5.0.1.sql
@@ -1,5 +1,5 @@
 -- upgrade4.0.5.0.0-4.0.5.0.1.sql
-SELECT acs_log__debug('/packages/intranet-invoices/sql/postgresql/upgrade/upgrade4.0.5.0.0-4.0.5.0.1.sql','');
+SELECT acs_log__debug('/packages/intranet-invoices/sql/postgresql/upgrade/upgrade-4.0.5.0.0-4.0.5.0.1.sql','');
 
 -- Defined InterCo Quote and Invoices as provider document
 -- 
