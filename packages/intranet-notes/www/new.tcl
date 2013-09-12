d5bc8cd3eb07dc09774ce41964eddb0657bd876e
Malte Sussdorff <malte.sussdorff@cognovis.de>
2013-09-12 09:22:33 +0200

    Fixed typo

--- packages/intranet-notes/www/new.tcl
+++ packages/intranet-notes/www/new.tcl
@@ -47,7 +47,7 @@ ad_form \
     -form {
 	note_id:key
 	{note_type_id:text(im_category_tree) {label "[lang::message::lookup {} intranet-notes.Notes_Type Type]"} {custom {category_type "Intranet Notes Type" translate_p 1 package_key intranet-notes include_empty_p 0}} }
-	{note:richttext(richttext) {label "[lang::message::lookup {} intranet-notes.Notes_Note Note]"} {html {cols 40} {rows 8} }}
+	{note:richtext(richtext) {label "[lang::message::lookup {} intranet-notes.Notes_Note Note]"} {html {cols 40} {rows 8} }}
     }
 
 # Add DynFields to the form
