9d8d09de820e19e04e72185bbf77c619d7d596c6
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-09-26 10:43:32 +0000

    -- added status of projects shown    show all projects, not ony active    show warning when number of items exceeds limit

--- lib/company-projects.tcl
+++ lib/company-projects.tcl
@@ -22,40 +22,38 @@ set projects_html ""
 set current_level 1
 set ctr 1
 set max_projects 15
-
+set project_status_name ""
+set warning ""
 
 # -- 
 #db_multirow starts here. it still is a draft and it is not finished.
-set where_clause1 "[im_project_status_canceled]"
-set where_clause2 "[im_project_status_deleted]"
-
 
 set where_clause3 "[im_project_type_task]"
 set where_clause4 "[im_project_type_ticket]"
 
-db_multirow -extend {llevel current_level} active_projects select_projects {} {
+db_multirow -extend {llevel current_level project_status_name} active_projects select_projects {} {
 
-#    ns_log Notice "db_multirow begin"
-#    ns_log Notice "level=$llevel | $current_level"
     if { $llevel > $current_level } {
 	incr current_level
     } elseif { $llevel < $current_level } {
 	set current_level [expr $current_level - 1]
     }
 
+    set project_status_name [im_category_from_id $project_status_id]
+    lappend tt [im_category_from_id $project_status_id] 
     set project_url [export_vars -base "../projects/view" {project_id}]
 
- #   ns_log Notice "name=$project_name | $project_nr | project_url"
-    
     incr ctr
-    if {$ctr > $max_projects} { break }
+    if {$ctr > $max_projects} { 
+	set warning  "<p class=\"warning\">[lang::message::lookup "" intranet-core.NumberOfItemsExceedsLimit "Please note: Only a subset of items is shown, total number exceeds limit"]</p>"
+	break 
+    }
 }
-
 
 set close_ul_p 0
 if { [exists_and_not_null level] && $llevel < $current_level } {
     set close_ul_p 1
 }
 
-
-set projects_html [im_table_with_title "[_ intranet-core.Projects]" "<ul>$projects_html</ul>"]
+# KH: ... this is obsolete
+# set projects_html [im_table_with_title "[_ intranet-core.Projects]" "$warning<ul>$projects_html</ul>"]
