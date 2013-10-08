3ef1fcaae6e61a234f055e0d68cf9fbdf27797d2
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2013-10-08 12:39:14 +0000

    - Core / DynFields:   Added new dereferencing function

--- sql/postgresql/intranet-defs.sql
+++ sql/postgresql/intranet-defs.sql
@@ -372,6 +372,50 @@ END;' language 'plpgsql';
 
 
 
+create or replace function im_boolean_from_id(varchar)
+returns varchar as $$
+DECLARE
+	p_boolean	alias for $1;
+	v_result	varchar;
+BEGIN
+	v_result := p_boolean;
+	IF 't' = lower(p_boolean) THEN v_result := 'true'; END IF;
+	IF 'f' = lower(p_boolean) THEN v_result := 'false'; END IF;
+	IF '1' = lower(p_boolean) THEN v_result := 'true'; END IF;
+	IF '0' = lower(p_boolean) THEN v_result := 'false'; END IF;
+	return v_result;
+END;$$ language 'plpgsql';
+
+
+create or replace function im_boolean_from_id(integer)
+returns varchar as $$
+DECLARE
+	p_boolean	alias for $1;
+	v_result	varchar;
+BEGIN
+	v_result := p_boolean;
+	IF 1 = p_boolean THEN v_result := 'true'; END IF;
+	IF 0 = p_boolean THEN v_result := 'false'; END IF;
+	return v_result;
+END;$$ language 'plpgsql';
+
+
+
+create or replace function im_boolean_from_id(boolean)
+returns varchar as $$
+DECLARE
+	p_boolean	alias for $1;
+	v_result	varchar;
+BEGIN
+	v_result := p_boolean;
+	IF true = p_boolean THEN v_result := 'true'; END IF;
+	IF false = p_boolean THEN v_result := 'false'; END IF;
+	return v_result;
+END;$$ language 'plpgsql';
+
+
+
+
 -- ------------------------------------------------------------------
 -- Special dereferencing function for green-yellow-red traffic light
 -- ------------------------------------------------------------------
